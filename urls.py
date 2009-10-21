89a677d054678dfaa6d8ee5a0c6b9236d88044ff
Jeff Balogh <me@jeffbalogh.org>
2009-10-21 14:56:43 -0700

    site skeleton in base.html

--- urls.py
+++ urls.py
@@ -2,15 +2,8 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^zamboni/', include('zamboni.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url('^$', 'jingo.views.direct_to_template',
+        {'template': 'base.html'}, name='home'),
 )
 
 if settings.DEBUG:
@@ -18,5 +11,5 @@ if settings.DEBUG:
     media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
     urlpatterns += patterns('',
         (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT})
+         {'document_root': settings.MEDIA_ROOT}),
     )
