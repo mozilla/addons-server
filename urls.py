3bedd697b936f6397f6959510aa6db2bdcc7a91d
Jeff Balogh <me@jeffbalogh.org>
2009-10-20 19:20:38 -0700

    serve static files for development

--- urls.py
+++ urls.py
@@ -1,8 +1,5 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -15,3 +12,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # Remove leading and trailing slashes so the regex matches.
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns('',
+        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT})
+    )
