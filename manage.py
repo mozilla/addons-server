a153f11d3988a5064a5d013175395a5eb7fadd55
Jeff Balogh <me@jeffbalogh.org>
2009-10-21 17:05:27 -0700

    try importing from local_settings first

--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 from django.core.management import execute_manager
+
 try:
-    import settings # Assumed to be in the same directory.
+    import local_settings as settings
 except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+    try:
+        import settings
+    except ImportError:
+        import sys
+        sys.stderr.write(
+            "Error: Tried importing 'local_settings.py' and 'settings.py' "
+            "but neither could be found (or they're throwing an ImportError)."
+            " Please come back and try again later.")
+
 
 if __name__ == "__main__":
     execute_manager(settings)
