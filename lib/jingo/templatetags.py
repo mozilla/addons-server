89ba6c37dd84bd3617272684198b641b2d177039
Jeff Balogh <me@jeffbalogh.org>
2009-10-22 15:34:56 -0700

    format, nl2br, and datetime filters

--- lib/jingo/templatetags.py
+++ lib/jingo/templatetags.py
@@ -1,4 +1,7 @@
 from django.core import urlresolvers
+from django.utils.translation import ugettext as _
+
+import jinja2
 
 from jingo import register
 
@@ -7,3 +10,19 @@ from jingo import register
 def url(viewname, *args, **kwargs):
     """Helper for Django's ``reverse`` in templates."""
     return urlresolvers.reverse(viewname, args=args, kwargs=kwargs)
+
+
+@register.filter
+def f(string, *args, **kwargs):
+    """Uses ``str.format`` for string interpolation."""
+    return string.format(*args, **kwargs)
+
+
+@register.filter
+def nl2br(string):
+    return jinja2.Markup('<br>'.join(jinja2.escape(string).splitlines()))
+
+
+@register.filter
+def datetime(t, format=_('%B %d, %Y')):
+    return t.strftime(format)
