20dd03f7815620d5658f79cb26eae859c1d23c4e
Jeff Balogh <me@jeffbalogh.org>
2009-10-23 11:05:00 -0700

    better docs for the f filter

--- lib/jingo/templatetags.py
+++ lib/jingo/templatetags.py
@@ -14,7 +14,12 @@ def url(viewname, *args, **kwargs):
 
 @register.filter
 def f(string, *args, **kwargs):
-    """Uses ``str.format`` for string interpolation."""
+    """
+    Uses ``str.format`` for string interpolation.
+
+    >>> {{ "{0} arguments and {x} arguments"|f('positional', x='keyword') }}
+    "positional arguments and keyword arguments"
+    """
     return string.format(*args, **kwargs)
 
 
