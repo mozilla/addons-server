554fd32ae4d6f52511375ce1809b6276168b082e
Dave Dash <dd+github@davedash.com>
2009-12-16 09:54:54 -0800

    [bug 535307] Some suggested settings for settings.py, including setting InnoDB as a default

--- settings.py
+++ settings.py
@@ -18,12 +18,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'amo.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'zamboni'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+DATABASE_OPTIONS = {
+   "init_command": "SET storage_engine=INNODB",
+}
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
