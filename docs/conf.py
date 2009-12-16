0be3537f7875e9c3266a7ecb8a29a34ff6a4d749
Jeff Balogh <me@jeffbalogh.org>
2009-12-15 21:30:19 -0800

    it takes a village

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'zamboni'
-copyright = u'2009, Jeff Balogh'
+copyright = u'2009, The Zamboni Collective'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
