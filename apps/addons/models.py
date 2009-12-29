5b89547702c7343a89b6b8c3b41a953aa99c19af
Jeff Balogh <me@jeffbalogh.org>
2009-12-29 17:39:30 -0500

    use Addon.defaultlocale to get fallback translations

--- apps/addons/models.py
+++ apps/addons/models.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.db import models
 
 import amo
-from translations.fields import TranslatedField
+from translations.fields import TranslatedField, translations_with_fallback
 
 
 class Addon(amo.ModelBase):
@@ -9,6 +10,8 @@ class Addon(amo.ModelBase):
     description = TranslatedField()
 
     adminreview = models.BooleanField(default=False)
+    defaultlocale = models.CharField(max_length=10,
+                                     default=settings.LANGUAGE_CODE)
 
     users = models.ManyToManyField('users.User')
 
@@ -18,3 +21,6 @@ class Addon(amo.ModelBase):
     def get_absolute_url(self):
         # XXX: use reverse
         return '/addon/%s' % self.id
+
+    def fetch_translations(self, ids, lang):
+        return translations_with_fallback(ids, lang, self.defaultlocale)
