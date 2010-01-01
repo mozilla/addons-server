0a4b7b36155c3b6083259561f4ad35f7941c2802
Jeff Balogh <me@jeffbalogh.org>
2010-01-01 16:32:21 -0500

    autodiscover admin models

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, url, include
+from django.contrib import admin
+
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
