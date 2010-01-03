8cfb246a732cf65419866de8fdbe7cada9a33ccc
Jeff Balogh <me@jeffbalogh.org>
2010-01-03 16:06:52 -0500

    admin tweaks for addons and features

--- apps/addons/admin.py
+++ apps/addons/admin.py
@@ -10,6 +10,8 @@ class AddonForm(forms.ModelForm):
         model = Addon
 
     def clean(self):
+        # Override clean so we ignore uniqueness checks on the django side,
+        # since they don't handle translations correctly.
         return self.cleaned_data
 
 
@@ -17,10 +19,45 @@ class AddonAdmin(admin.ModelAdmin):
     form = AddonForm
     raw_id_fields = ('users',)
 
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'guid', 'defaultlocale', 'addontype', 'status',
+                       'higheststatus', 'users', 'nominationdate'),
+        }),
+        ('Details', {
+            'fields': ('summary', 'description', 'homepage', 'eula',
+                       'privacypolicy', 'developercomments', 'icontype',
+                       'the_reason', 'the_future'),
+        }),
+        ('Support', {
+            'fields': ('supporturl', 'supportemail',
+                       'get_satisfaction_company', 'get_satisfaction_product'),
+        }),
+        ('Stats', {
+            'fields': ('averagerating', 'bayesianrating', 'totalreviews',
+                       'weeklydownloads', 'totaldownloads',
+                       'average_daily_downloads', 'average_daily_users',
+                       'sharecount'),
+        }),
+        ('Truthiness', {
+            'fields': ('inactive', 'trusted', 'viewsource', 'publicstats',
+                       'prerelease', 'adminreview', 'sitespecific',
+                       'externalsoftware', 'binary', 'dev_agreement',
+                       'show_beta'),
+        }),
+        ('Money', {
+            'fields': ('wants_contributions', 'paypal_id', 'suggested_amount',
+                       'annoying'),
+        }),
+        ('Dictionaries', {
+            'fields': ('target_locale', 'locale_disambiguation'),
+        }))
+
 
 class FeatureAdmin(admin.ModelAdmin):
     raw_id_fields = ('addon',)
     list_filter = ('application', 'locale')
+    list_display = ('addon', 'application', 'locale')
 
 
 admin.site.register(BlacklistedGuid)
