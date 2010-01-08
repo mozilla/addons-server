a92a186b6cbbb772667a73ce8a2fdb40ebf6d1ed
Dave Dash <dd+github@davedash.com>
2010-01-08 11:51:36 -0800

    Uncommenting virtualenv stuff

--- scripts/pylint.sh
+++ scripts/pylint.sh
@@ -1,5 +1,5 @@
-# VENV=$WORKSPACE/venv
-# source $VENV/bin/activate
+VENV=$WORKSPACE/venv
+source $VENV/bin/activate
 PYTHONPATH='$WORKSPACE/..'
 pylint --rcfile scripts/pylintrc -f parseable $WORKSPACE > pylint.txt
 echo "pylint complete"
