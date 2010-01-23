0ab2e71692cffb77e6d1b0151d8b068bacc270dc
Dave Dash <dd+github@davedash.com>
2010-01-22 16:54:08 -0800

    Added sphinxsearch's sphinxapi.py it is gross.

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric.api import local
 def pep8():
     local("pep8 --repeat --ignore E221"
         " --exclude *.sh,*.html,*.json,*.txt,*.pyc,.DS_Store,README,"
-        "migrations"
+        "migrations,sphinxapi.py"
         " apps", capture=False)
 
 
