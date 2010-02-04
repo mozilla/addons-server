fcb867d76549266e32e4e0f519ef0621e2b86962
Jeff Balogh <me@jeffbalogh.org>
2010-02-04 13:55:05 -0800

    use schematic_settings instead

--- migrations/schematic_settings.py
+++ migrations/schematic_settings.py
@@ -1,16 +1,8 @@
-"""
-This settings.py gets imported instead of zamboni's settings.py.  But then we
-need to access config in that settings.py.  What are we going to do?  Put the
-zamboni root at the beginning of sys.path, and delete this settings.py from
-sys.modules.  Python forgets that it ever imported us and starts finding
-zamboni's settings instead.
-"""
 import sys
 import os
 
 # This only works if you're running schematic from the zamboni root.
 sys.path.insert(0, os.path.realpath('.'))
-del sys.modules['settings']
 
 # Set up zamboni.
 import manage
