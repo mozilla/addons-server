280c086ee005d97a6e41e6d0a6b8f27e3a3c0bae
Jeff Balogh <me@jeffbalogh.org>
2010-02-12 12:06:08 -0800

    Revert "emaillink helper plus test".  wenzel is going to add this again when he commits the whole branch, and tests will then pass.
    This reverts commit cee8e252a22b0d2c5c4c7d8436c2c7b1a41242b3.

--- apps/users/tests/test_helpers.py
+++ apps/users/tests/test_helpers.py
@@ -1,39 +1,9 @@
-import re
-
-from django.core.urlresolvers import reverse
 from nose.tools import eq_
 
-from users.helpers import emaillink, user_link, users_list
+from users.helpers import user_link
 from users.models import UserProfile
-
-
-def test_emaillink():
-    email = 'me@example.com'
-    obfuscated = unicode(emaillink(email))
-
-    # remove junk
-    m = re.match(r'<span class="emaillink">(.*?)<span class="i">null</span>(.*)'
-                 '</span>', obfuscated)
-    obfuscated = (''.join((m.group(1), m.group(2)))
-                  .replace('&#x0040;', '@').replace('&#x002E;', '.'))[::-1]
-
-    eq_(email, obfuscated)
 
 
 def test_user_link():
     u = UserProfile(firstname='John', lastname='Connor', pk=1)
-    eq_(user_link(u), '<a href="%s">John Connor</a>' %
-        reverse('users.profile', args=[1]))
-
-
-def test_users_list():
-    u1 = UserProfile(firstname='John', lastname='Connor', pk=1)
-    u2 = UserProfile(firstname='Sarah', lastname='Connor', pk=2)
-    eq_(users_list([u1, u2]), ', '.join((user_link(u1), user_link(u2))))
-
-
-def test_user_link_unicode():
-    """make sure helper won't choke on unicode input"""
-    u = UserProfile(firstname=u'J\u00FCrgen', lastname=u'M\u00FCller', pk=1)
-    eq_(user_link(u), u'<a href="%s">J\u00FCrgen M\u00FCller</a>' %
-        reverse('users.profile', args=[1]))
+    eq_(user_link(u), """<a href="/users/1">John Connor</a>""")
