c5c7ea4df697834d7e1330a27e4660e9bec4abfa
Fred Wenzel <fwenzel@mozilla.com>
2010-02-12 19:07:48 +0100

    reviews_link helper and test

--- apps/reviews/helpers.py
+++ apps/reviews/helpers.py
@@ -1,6 +1,9 @@
 import math
+import urllib
+import urlparse
 
-from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _, ungettext
 
 import jinja2
 
@@ -17,3 +20,23 @@ def stars(num):
         s = ('<span class="stars stars-{num}" title="{msg}">{msg}</span>'
              .format(num=stars, msg=msg))
         return jinja2.Markup(s)
+
+
+@register.filter
+def reviews_link(addon, collection_uuid=None):
+    try:
+        rating = float(addon.average_rating)
+    except ValueError:
+        rating = None
+    stars_ = stars(rating)
+
+    url = list(urlparse.urlsplit(reverse('addons.detail', args=[addon.id])))
+    if collection_uuid:
+        url[3] = urllib.urlencode({'collection_uuid': collection_uuid})
+    url[4] = 'reviews'
+
+    msg = (ungettext('{num} review', '{num} reviews', addon.total_reviews)
+           .format(num=addon.total_reviews))
+    s = ('{stars} <a href="{url}"><strong>{msg}</strong></a>'
+         .format(stars=unicode(stars_), url=urlparse.urlunsplit(url), msg=msg))
+    return jinja2.Markup(s)
--- apps/reviews/tests/test_helpers.py
+++ apps/reviews/tests/test_helpers.py
@@ -2,6 +2,8 @@ from nose.tools import eq_
 
 import jingo
 
+from addons.models import Addon
+
 
 def render(s, context={}):
     t = jingo.env.from_string(s)
@@ -15,3 +17,21 @@ def test_stars():
     s = render('{{ num|stars }}', {'num': 1})
     msg = 'Rated 1 out of 5 stars'
     eq_(s, '<span class="stars stars-1" title="{0}">{0}</span>'.format(msg))
+
+
+def test_reviews_link():
+    a = Addon(average_rating=4, total_reviews=37, id=1)
+    s = render('{{ myaddon|reviews_link }}', {'myaddon': a})
+    expected = '{stars} <a href="{{url}}"><strong>{msg}</strong></a>'.format(
+        stars=render('{{ num|stars }}', {'num': a.average_rating}),
+        msg='37 reviews')
+
+    # without collection uuid
+    eq_(s, expected.format(url='/addon/1/#reviews'))
+
+    # with collection uuid
+    myuuid = 'f19a8822-1ee3-4145-9440-0a3640201fe6'
+    s = render('{{ myaddon|reviews_link(myuuid) }}', {'myaddon': a,
+                                                      'myuuid': myuuid})
+    uuid_url = '/addon/1/?collection_uuid=%s#reviews' % myuuid
+    eq_(s, expected.format(url=uuid_url))
