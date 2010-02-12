cee8e252a22b0d2c5c4c7d8436c2c7b1a41242b3
Fred Wenzel <fwenzel@mozilla.com>
2010-02-12 12:08:48 +0100

    emaillink helper plus test

--- apps/users/helpers.py
+++ apps/users/helpers.py
@@ -1,9 +1,46 @@
+import random
+
 import jinja2
 
-from jingo import register
+from jingo import register, env
+
+
+@register.filter
+def emaillink(email):
+    if not email:
+        return ""
+
+    fallback = email[::-1] # reverse
+    # inject junk somewhere
+    i = random.randint(0, len(email)-1)
+    fallback = u"%s%s%s" % (fallback[:i], u'<span class="i">null</span>',
+                            fallback[i:])
+    # replace @ and .
+    fallback = fallback.replace('@', '&#x0040;').replace('.', '&#x002E;')
+
+    node = u'<span class="emaillink">%s</span>' % fallback
+    return jinja2.Markup(node)
 
 
 @register.filter
 def user_link(user):
-    return jinja2.Markup('<a href="%s">%s</a>' %
-                         (user.get_absolute_url(), user.display_name))
+    return jinja2.Markup(_user_link(user))
+
+
+@register.filter
+def users_list(users):
+    return jinja2.Markup(', '.join(map(_user_link, users)))
+
+
+def _user_link(user):
+    return u'<a href="%s">%s</a>' % (
+        user.get_absolute_url(), unicode(jinja2.escape(user.display_name)))
+
+
+@register.filter
+def user_vcard(user, table_class='person-info',
+               about_addons=True):
+    c = {'profile': user, 'table_class': table_class,
+         'about_addons': about_addons}
+    t = env.get_template('users/vcard.html').render(**c)
+    return jinja2.Markup(t)
