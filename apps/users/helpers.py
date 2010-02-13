97e30b275f9db9f05a4e29436e04673e7cf760e7
Fred Wenzel <fwenzel@mozilla.com>
2010-02-13 12:44:24 +0100

    emaillink helper and test

--- apps/users/helpers.py
+++ apps/users/helpers.py
@@ -4,6 +4,24 @@ from jingo import register
 
 
 @register.filter
+def emaillink(email):
+    if not email:
+        return ""
+
+    fallback = email[::-1] # reverse
+    # inject junk somewhere
+    i = random.randint(0, len(email)-1)
+    fallback = u"%s%s%s" % (jinja2.escape(fallback[:i]),
+                            u'<span class="i">null</span>',
+                            jinja2.escape(fallback[i:]))
+    # replace @ and .
+    fallback = fallback.replace('@', '&#x0040;').replace('.', '&#x002E;')
+
+    node = u'<span class="emaillink">%s</span>' % fallback
+    return jinja2.Markup(node)
+
+
+@register.filter
 def user_link(user):
     return jinja2.Markup('<a href="%s">%s</a>' %
                          (user.get_absolute_url(), user.display_name))
--- apps/users/tests/test_helpers.py
+++ apps/users/tests/test_helpers.py
@@ -1,7 +1,20 @@
 from nose.tools import eq_
 
-from users.helpers import user_link
+from users.helpers import emaillink, user_link
 from users.models import UserProfile
+
+
+def test_emaillink():
+    email = 'me@example.com'
+    obfuscated = unicode(emaillink(email))
+
+    # remove junk
+    m = re.match(r'<span class="emaillink">(.*?)<span class="i">null</span>(.*)'
+                 '</span>', obfuscated)
+    obfuscated = (''.join((m.group(1), m.group(2)))
+                  .replace('&#x0040;', '@').replace('&#x002E;', '.'))[::-1]
+
+    eq_(email, obfuscated)
 
 
 def test_user_link():
