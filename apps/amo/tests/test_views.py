2fd2bbe505a72366c76ba3d46275cad5eea240ea
Jeff Balogh <me@jeffbalogh.org>
2010-02-19 14:43:19 -0800

    generating the header properly

--- apps/amo/tests/test_views.py
+++ apps/amo/tests/test_views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 from django import test
 
 from nose.tools import eq_
+import pyquery
 
 
 def test_404_no_app():
@@ -10,3 +11,15 @@ def test_404_no_app():
     url = reverse('amo.monitor')
     response = test.Client().get(url + 'nonsense')
     eq_(response.status_code, 404)
+
+
+def test_heading():
+    c = test.Client()
+    def title_eq(url, expected):
+        response = c.get(url, follow=True)
+        actual = pyquery.PyQuery(response.content)('#title').text()
+        eq_(expected, actual)
+
+    title_eq('/firefox', 'Add-ons for Firefox')
+    title_eq('/thunderbird', 'Add-ons for Thunderbird')
+    title_eq('/mobile', 'Mobile Add-ons for Firefox')
