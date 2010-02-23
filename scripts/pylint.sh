5a95d1666efe7de399f728868fd054e525979f59
Dave Dash <dd+github@davedash.com>
2010-02-23 10:34:00 -0800

    typo in shell script

--- scripts/pylint.sh
+++ scripts/pylint.sh
@@ -2,5 +2,5 @@
 VENV=$WORKSPACE/venv
 source $VENV/bin/activate
 export PYTHONPATH="$WORKSPACE/..:$WORKSPACE/apps:$WORKSPACE/lib"
-pylint --rcfile scripts/pylintrc > pylint.txt
+pylint --rcfile scripts/pylintrc $WORKSPACE > pylint.txt
 echo "pylint complete"
