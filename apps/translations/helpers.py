d28fc2d95747f07ba5d0211722a3acfd11afde35
Fred Wenzel <fwenzel@mozilla.com>
2010-02-25 16:41:26 +0100

    fixed locale_html helper

--- apps/translations/helpers.py
+++ apps/translations/helpers.py
@@ -9,10 +9,10 @@ from jingo import register
 @register.filter
 def locale_html(translatedfield):
     """HTML attributes for languages different than the site language"""
-    locale = translatedfield.locale
     sitelang = translation.get_language()
     sitelocale = translation.to_locale(sitelang)
-    if translatedfield.locale == sitelocale:
+    locale = translation.to_locale(translatedfield.locale)
+    if locale == sitelocale:
         return ''
     else:
         textdir = 'rtl' if locale in settings.RTL_LANGUAGES else 'ltr'
