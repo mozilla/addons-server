e82225201772794bf347c6e768d25f24a61b9b54
Ian Bicking <ianb@colorstudy.com>
2010-03-05 17:05:04 -0600

    Make the settings work when there's no port, and fix up the path manipulation

--- migrations/schematic_settings.py
+++ migrations/schematic_settings.py
@@ -1,8 +1,7 @@
 import sys
 import os
 
-# This only works if you're running schematic from the zamboni root.
-sys.path.insert(0, os.path.realpath('.'))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Set up zamboni.
 import manage
@@ -12,16 +11,20 @@ config = settings.DATABASES['default']
 config['HOST'] = config.get('HOST', 'localhost')
 config['PORT'] = config.get('PORT', '3306')
 
-if config['HOST'].endswith('.sock'):
+if not config['HOST'] or config['HOST'].endswith('.sock'):
     """ Oh you meant 'localhost'! """
     config['HOST'] = 'localhost'
 
-s = 'mysql --silent {NAME} -h{HOST} -P{PORT} -u{USER}'
+s = 'mysql --silent {NAME} -h{HOST} -u{USER}'
 
 if config['PASSWORD']:
     s += ' -p{PASSWORD}'
 else:
     del config['PASSWORD']
+if config['PORT']:
+    s += ' -P{PORT}'
+else:
+    del config['PORT']
 
 db = s.format(**config)
 table = 'schema_version'
