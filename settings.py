8ed63b2db8eda28d1d9404f3234dcddb88a5db31
Fred Wenzel <fwenzel@mozilla.com>
2010-03-05 17:29:19 +0100

    loop controls (= continue and break) are helpful.

--- settings.py
+++ settings.py
@@ -189,7 +189,7 @@ def JINJA_CONFIG():
     from django.conf import settings
     from caching.base import cache
     config = {'extensions': ['l10n.template.i18n', 'caching.ext.cache',
-                             'jinja2.ext.with_'],
+                             'jinja2.ext.with_', 'jinja2.ext.loopcontrols'],
               'finalize': lambda x: x if x is not None else ''}
     if 'memcached' in cache.scheme and not settings.DEBUG:
         # We're passing the _cache object directly to jinja because
