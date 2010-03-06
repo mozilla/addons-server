c9b816f50d69146cb8c69a5505dac751ad53ba8d
Jeff Balogh <me@jeffbalogh.org>
2010-03-05 19:21:32 -0800

    collection promobox

--- apps/addons/views.py
+++ apps/addons/views.py
@@ -1,13 +1,13 @@
-import random
-
 from django import http
+from django.db.models import Q
+from django.utils import translation
 
 import jingo
 from l10n import ugettext as _
 
 import amo
 from addons.models import Addon
-from bandwagon.models import Collection
+from bandwagon.models import Collection, CollectionFeature, CollectionPromo
 from stats.models import GlobalStat
 
 
@@ -64,6 +64,17 @@ class HomepageFilter(object):
 
 
 def home(request):
+    # Add-ons.
+    base = Addon.objects.listed(request.APP).exclude(type=amo.ADDON_PERSONA)
+    filter = HomepageFilter(request, base, key='browse', default='featured')
+    addon_sets = dict((key, qs[:4]) for key, qs in filter.all().items())
+
+    # Collections.
+    q = Collection.objects.filter(listed=True, application=request.APP.id)
+    collections = q.order_by('-weekly_subscribers')[:3]
+    promobox = CollectionPromoBox(request)
+
+    # Global stats.
     try:
         gs = GlobalStat.objects
         downloads = gs.filter(name='addon_total_downloads').latest()
@@ -71,15 +82,36 @@ def home(request):
     except GlobalStat.DoesNotExist:
         downloads = pings = None
 
-    q = Collection.objects.filter(listed=True, application=request.APP.id)
-    collections = q.order_by('-weekly_subscribers')[:3]
-
-    base = Addon.objects.listed(request.APP).exclude(type=amo.ADDON_PERSONA)
-    filter = HomepageFilter(request, base, key='browse', default='featured')
-    addon_sets = dict((key, qs[:4]) for key, qs in filter.all().items())
-
     return jingo.render(request, 'addons/home.html',
                         {'downloads': downloads, 'pings': pings,
                          'filter': filter, 'addon_sets': addon_sets,
-                         'collections': collections,
+                         'collections': collections, 'promobox': promobox,
                         })
+
+
+class CollectionPromoBox(object):
+
+    def __init__(self, request):
+        self.request = request
+
+    def features(self):
+        return CollectionFeature.objects.all()
+
+    def collections(self):
+        features = self.features()
+        lang = translation.get_language()
+        locale = Q(locale='') | Q(locale=lang)
+        promos = (CollectionPromo.objects.filter(locale)
+                  .filter(collection_feature__in=features)
+                  .select_related('collection'))
+        # Get a localized collection, if possible.
+        rv = {}
+        pdict = dict(((p.collection_feature_id, p.locale), p) for p in promos)
+        for feature in features:
+            _key = (feature.id, lang)
+            key = _key if _key in pdict else (feature.id, '')
+            rv[feature] = pdict[key].collection
+        return rv
+
+    def __nonzero__(self):
+        return self.request.APP == amo.FIREFOX
