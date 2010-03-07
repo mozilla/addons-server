02dad8ead1a0d72b4b0f1e85324d6704f04e2b85
Jeff Balogh <me@jeffbalogh.org>
2010-03-06 22:10:14 -0800

    drop the binary search, calculate the text length exactly

--- apps/translations/tests/test_util.py
+++ apps/translations/tests/test_util.py
@@ -7,7 +7,5 @@ def test_truncate():
     s = '   <p>one</p><ol><li>two</li><li> three</li> </ol><p> four five</p>'
 
     eq_(truncate(s, 100), s)
-
-    eq_(truncate(s, 2), '<p>on</p>...')
-    eq_(truncate(s, 6), '<p>one</p><ol><li>two</li></ol>...')
-    eq_(truncate(s, 11), '<p>one</p><ol><li>two</li><li> three</li></ol>...')
+    eq_(truncate(s, 6), '   <p>one</p><ol><li>two...</li></ol>')
+    eq_(truncate(s, 11), '   <p>one</p><ol><li>two</li><li>three...</li></ol>')
--- apps/translations/utils.py
+++ apps/translations/utils.py
@@ -1,39 +1,49 @@
 from django.utils.encoding import force_unicode
+
 from bleach import NODE_TEXT
 import html5lib
 import jinja2
 
 
-parser = html5lib.HTMLParser()
+def trim(tree, limit, killwords, end):
+    """Truncate the text of an html5lib tree."""
+    root = tree.cloneNode()
+    length = 0
+    for node in tree.childNodes:
+        # Stop if we have enough characters.
+        if length >= limit:
+            break
+
+        # We have a text node, so slurp up characters without going over.
+        if node.type == NODE_TEXT:
+            new = node.cloneNode()
+            root.appendChild(new)
+            text = new.value.strip()
+            if len(text) + length < limit:
+                length += len(text)
+            else:
+                # Don't let jinja add ``end`` because it doesn't know that
+                # we're truncating up here.
+                trunc = jinja2.filters.do_truncate(text, limit - length,
+                                                       killwords, end='')
+                new.value = trunc + end
+                length = limit
+        else:
+            # Recurse on other non-text nodes.
+            child, child_len = trim(node, limit - length, killwords, end)
+            root.appendChild(child)
+            length += child_len
+    return root, length
 
 
-def text_length(html):
+def text_length(tree):
     """Find the length of the text content, excluding markup."""
     def walk(tree):
         if tree.type == NODE_TEXT:
             return len(tree.value.strip())
         else:
             return sum(walk(node) for node in tree.childNodes)
-    return walk(parser.parseFragment(html, encoding='utf-8'))
-
-
-def _truncate(html, length):
-    # Binary search could drop us in the middle of a tag, but it's close
-    # enough.
-    if text_length(html) <= length:
-        return html
-
-    hi, lo = len(html), 0
-    while hi > lo:
-        mid = (hi + lo) / 2
-        _len = text_length(html[:mid])
-        if _len < length:
-            lo = mid + 1
-        elif _len > length:
-            hi = mid - 1
-        else:
-            return html[:mid]
-    return html[:mid]
+    return walk(tree)
 
 
 def truncate(html, length, killwords=False, end='...'):
@@ -42,9 +52,9 @@ def truncate(html, length, killwords=False, end='...'):
 
     killwords and end are currently ignored.
     """
-    if text_length(html) <= length:
+    tree = html5lib.parseFragment(html, encoding='utf-8')
+    if text_length(tree) <= length:
         return jinja2.Markup(html)
-
-    short = _truncate(html.strip(), length)
-    new = parser.parseFragment(short, encoding='utf-8').toxml()
-    return jinja2.Markup(force_unicode(new) + end)
+    else:
+        short, _ = trim(tree, length, killwords, end)
+        return jinja2.Markup(force_unicode(short.toxml()))
