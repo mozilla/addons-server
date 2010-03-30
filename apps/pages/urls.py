00f5959414c646c801313649675b5808d8a5f3cd
Fred Wenzel <fwenzel@mozilla.com>
2010-03-30 09:55:44 +0200

    rendering static pages directly to template, without the detour to views.py

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 
+from jingo.views import direct_to_template
+
 from . import views
 
 
 urlpatterns = patterns('',
-    url('^about$', views.about_amo, name='pages.about'),
-    url('^faq$', views.faq, name='pages.faq'),
+    url('^about$', direct_to_template, {'template': 'pages/about.lhtml'},
+        name='pages.about'),
+    url('^faq$', direct_to_template, {'template': 'pages/faq.lhtml'},
+        name='pages.faq'),
 )
