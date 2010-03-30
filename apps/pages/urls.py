d25303b33f1600e64fab7ce860dfc7d839ee39f6
Fred Wenzel <fwenzel@mozilla.com>
2010-03-30 10:35:34 +0200

    okay, why did this pass for me?

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 from jingo.views import direct_to_template
 
-from . import views
-
 
 urlpatterns = patterns('',
     url('^about$', direct_to_template, {'template': 'pages/about.lhtml'},
