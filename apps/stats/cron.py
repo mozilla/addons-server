0030c3ae79e12fa7115cbc0a7ab50f24ccd3e5e2
Wil Clouser <wclouser@mozilla.com>
2010-05-18 17:17:12 -0700

    Accept a date for calculating global stats; bug 562236

--- apps/stats/cron.py
+++ apps/stats/cron.py
@@ -67,13 +67,13 @@ def _update_collections_total(data, **kw):
 
 
 @cronjobs.register
-def update_global_totals():
+def update_global_totals(date=None):
     """Update global statistics totals."""
 
-    today = datetime.date.today()
+    today = date or datetime.date.today()
     today_jobs = [dict(job=job, date=today) for job in _get_daily_jobs()]
 
-    max_update = UpdateCount.objects.aggregate(max=Max('date'))['max']
+    max_update = date or UpdateCount.objects.aggregate(max=Max('date'))['max']
     metrics_jobs = [dict(job=job, date=max_update) for job in _get_metrics_jobs()]
 
     with establish_connection() as conn:
@@ -120,37 +120,37 @@ def _get_daily_jobs(date=None):
     # and not hammer the db with a ton of these all at once.
     stats = {
         # Add-on Downloads
-        'addon_total_downloads': lambda: DownloadCount.objects.aggregate(sum=Sum('count'))['sum'],
+        'addon_total_downloads': lambda: DownloadCount.objects.filter(date__lte=date).aggregate(sum=Sum('count'))['sum'],
         'addon_downloads_new': lambda: DownloadCount.objects.filter(date=date).aggregate(sum=Sum('count'))['sum'],
 
         # Add-on counts
-        'addon_count_public': Addon.objects.filter(status=amo.STATUS_PUBLIC, inactive=0).count,
-        'addon_count_pending': Version.objects.filter(files__status=amo.STATUS_PENDING).count,
-        'addon_count_experimental': Addon.objects.filter(status=amo.STATUS_UNREVIEWED, inactive=0).count,
-        'addon_count_nominated': Addon.objects.filter(status=amo.STATUS_NOMINATED, inactive=0).count,
+        'addon_count_public': Addon.objects.filter(created__lte=date, status=amo.STATUS_PUBLIC, inactive=0).count,
+        'addon_count_pending': Version.objects.filter(created__lte=date, files__status=amo.STATUS_PENDING).count,
+        'addon_count_experimental': Addon.objects.filter(created__lte=date, status=amo.STATUS_UNREVIEWED, inactive=0).count,
+        'addon_count_nominated': Addon.objects.filter(created__lte=date, status=amo.STATUS_NOMINATED, inactive=0).count,
         'addon_count_new': Addon.objects.extra(**extra).count,
 
         # Version counts
         'version_count_new': Version.objects.extra(**extra).count,
 
         # User counts
-        'user_count_total': UserProfile.objects.count,
+        'user_count_total': UserProfile.objects.filter(created__lte=date).count,
         'user_count_new': UserProfile.objects.extra(**extra).count,
 
         # Review counts
-        'review_count_total': Review.objects.filter(editorreview=0).count,
+        'review_count_total': Review.objects.filter(created__lte=date, editorreview=0).count,
         'review_count_new': Review.objects.filter(editorreview=0).extra(**extra).count,
 
         # Collection counts
-        'collection_count_total': Collection.objects.count,
+        'collection_count_total': Collection.objects.filter(created__lte=date).count,
         'collection_count_new': Collection.objects.extra(**extra).count,
         'collection_count_private': Collection.objects.filter(listed=0).count,
-        'collection_count_public': Collection.objects.filter(listed=1).count,
-        'collection_count_autopublishers': Collection.objects.filter(type=amo.COLLECTION_SYNCHRONIZED).count,
-        'collection_count_editorspicks': Collection.objects.filter(type=amo.COLLECTION_FEATURED).count,
-        'collection_count_normal': Collection.objects.filter(type=amo.COLLECTION_NORMAL).count,
+        'collection_count_public': Collection.objects.filter(created__lte=date, listed=1).count,
+        'collection_count_autopublishers': Collection.objects.filter(created__lte=date, type=amo.COLLECTION_SYNCHRONIZED).count,
+        'collection_count_editorspicks': Collection.objects.filter(created__lte=date, type=amo.COLLECTION_FEATURED).count,
+        'collection_count_normal': Collection.objects.filter(created__lte=date, type=amo.COLLECTION_NORMAL).count,
 
-        'collection_addon_downloads': lambda: AddonCollectionCount.objects.aggregate(sum=Sum('count'))['sum'],
+        'collection_addon_downloads': lambda: AddonCollectionCount.objects.filter(date__lte=date).aggregate(sum=Sum('count'))['sum'],
     }
 
     return stats
