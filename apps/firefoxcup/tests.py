5cb9669b3a7f95ba1d2d27ed9cf6bad7888f5630
Jeff Balogh <me@jeffbalogh.org>
2010-06-10 12:31:01 -0700

    fix fxcup tests

--- apps/firefoxcup/tests.py
+++ apps/firefoxcup/tests.py
@@ -5,7 +5,7 @@ from mock import patch
 from nose.tools import eq_
 from pyquery import PyQuery as pq
 import test_utils
-from twitter import _process_tweet, search, _search_query, _prepare_lang
+from firefoxcup import twitter
 
 
 class TestFirefoxCup(test_utils.TestCase):
@@ -16,15 +16,15 @@ class TestFirefoxCup(test_utils.TestCase):
 
     def test_prepare_lang(self):
         """Always use short lang code (e.g. en-US -> en)"""
-        eq_(_prepare_lang('en-US'), 'en')
+        eq_(twitter._prepare_lang('es-ES'), 'es')
 
         """Bad lang codes should fall back to 'all'"""
-        eq_(_prepare_lang('bad-lang-code'), 'all')
+        eq_(twitter._prepare_lang('bad-lang-code'), 'all')
 
     def test_process_tweet(self):
         """URLs and tags are linkified"""
 
-        a = map(_process_tweet,
+        a = map(twitter._process_tweet,
             ['http://www.mozilla.com', '#hash', '@person'])
 
         for v in a:
@@ -34,16 +34,17 @@ class TestFirefoxCup(test_utils.TestCase):
     def test_search_query_encoded(self):
         """Search query string is URL encoded"""
 
-        a = _search_query(['foo', '#bar'], 'en')
-        eq_(a, 'lang=en&ors=foo+%23bar')
+        a = twitter._search_query(['foo', '#bar'], 'en')
+        eq_(a, 'ors=foo+%23bar')
 
     @patch('firefoxcup.twitter.urllib2.urlopen')
     def test_search_data_decoded(self, urlopen):
         """Search results are JSON decoded,
         and only the tweet content is returned"""
         urlopen.return_value = self.twitter_results()
+        twitter.cache_tweets(lang='es-ES')
 
-        a = search([])
+        a = twitter.search(lang='es-ES')
         eq_(a, ['text'])
 
     @patch('firefoxcup.twitter.urllib2.urlopen')
@@ -53,4 +54,4 @@ class TestFirefoxCup(test_utils.TestCase):
         always return a list"""
 
         urlopen.side_effect = urllib2.URLError('Boom')
-        eq_(search([]), [])
+        eq_(twitter.search(lang='all'), [])
