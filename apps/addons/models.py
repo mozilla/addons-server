ef997ffee5b01c9b6d1ac48521369e58093f9c37
Jeff Balogh <me@jeffbalogh.org>
2010-06-14 16:36:58 -0700

    user_tags and dev_tags were switched

--- apps/addons/models.py
+++ apps/addons/models.py
@@ -353,8 +353,8 @@ class Addon(amo.models.ModelBase):
         # TODO(davedash): We can't cache these tags until /tags/ are moved
         # into Zamboni.
         tags = self.tags.not_blacklisted().no_cache()
-        dev_tags = list(tags.exclude(addon_tags__user__in=self.listed_authors))
-        user_tags = tags.exclude(id__in=[t.id for t in dev_tags])
+        user_tags = tags.exclude(addon_tags__user__in=self.listed_authors)
+        dev_tags = tags.exclude(id__in=[t.id for t in user_tags])
         return dev_tags, user_tags
 
     @amo.cached_property
