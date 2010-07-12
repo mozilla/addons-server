26bcd30b41ecf7aecbc1d5ade304bcd9490ad7b1
Jeff Balogh <jbalogh@mozilla.com>
2010-07-12 12:54:51 -0700

    use our login for admin pages (bug 558510)

--- apps/zadmin/urls.py
+++ apps/zadmin/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 from django.shortcuts import redirect
 
+from amo.urlresolvers import reverse
 from . import views
 
 
@@ -16,3 +17,12 @@ urlpatterns = patterns('',
     # The Django admin.
     url('^models/', include(admin.site.urls)),
 )
+
+
+# Hijack the admin's login to use our pages.
+def login(request):
+    url = '%s?to=%s' % (reverse('users.login'), request.path)
+    return redirect(url)
+
+
+admin.site.login = login
