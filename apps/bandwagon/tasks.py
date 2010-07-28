5f92bf55b7ca9de26dd7d49c888da991ecc861e4
Dave Dash <dd+github@davedash.com>
2010-07-28 15:55:33 -0700

    Comment out PIL dependencies.

--- apps/bandwagon/tasks.py
+++ apps/bandwagon/tasks.py
@@ -4,8 +4,9 @@ import os
 from django.db.models import Count
 
 from celery.decorators import task
-from easy_thumbnails import processors
-from PIL import Image
+# TODO(davedash): PIL needed
+# from easy_thumbnails import processors
+# from PIL import Image
 
 from . import cron  # Pull in tasks run through cron.
 from .models import Collection, CollectionVote
@@ -24,13 +25,13 @@ def collection_votes(*ids):
         qs.update(upvotes=votes.get(1, 0), downvotes=votes.get(-1, 0))
 
 
-@task
-def resize_icon(src, dest):
-    """Resizes collection icons to 32x32"""
-    try:
-        im = Image.open(src)
-        im = processors.scale_and_crop(im, (32, 32))
-        im.save(dest)
-        os.remove(src)
-    except Exception, e:
-        log.error("Error saving collection icon: %s" % e)
+#@task
+#def resize_icon(src, dest):
+#    """Resizes collection icons to 32x32"""
+#    try:
+#        im = Image.open(src)
+#        im = processors.scale_and_crop(im, (32, 32))
+#        im.save(dest)
+#        os.remove(src)
+#    except Exception, e:
+#        log.error("Error saving collection icon: %s" % e)
