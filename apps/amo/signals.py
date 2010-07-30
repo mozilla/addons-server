af3954eeba829d2d3c415b7decb04c66a2830dc9
Jeff Balogh <jbalogh@mozilla.com>
2010-07-30 01:38:47 -0700

    make sure we have urls to flush

--- apps/amo/signals.py
+++ apps/amo/signals.py
@@ -7,7 +7,8 @@ def flush_front_end_cache(sender, instance, **kwargs):
     furls = getattr(instance, 'flush_urls', None)
 
     urls = furls() if hasattr(furls, '__call__') else furls
-    tasks.flush_front_end_cache_urls.apply_async(args=[urls])
+    if urls:
+        tasks.flush_front_end_cache_urls.apply_async(args=[urls])
 
 models.signals.post_save.connect(flush_front_end_cache)
 models.signals.post_delete.connect(flush_front_end_cache)
