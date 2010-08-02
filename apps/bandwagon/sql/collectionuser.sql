e335ecf153d743f26a4027ea80e05435ebefff62
Dave Dash <dd+github@davedash.com>
2010-08-02 16:28:12 -0700

    Remove custom PK for collectionuser.

--- apps/bandwagon/sql/collectionuser.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `collections_users`
-    CHANGE COLUMN `id` `id` int(11) NOT NULL DEFAULT 0;
-
-ALTER TABLE `collections_users` DROP PRIMARY KEY,
-    ADD PRIMARY KEY(`collection_id`, `user_id`);
