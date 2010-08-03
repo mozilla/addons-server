3ac66d66ae197c07034470bf788a29545b234afc
Justin Scott <fligtar@gmail.com>
2010-08-03 15:02:40 -0700

    Rewriting FAQ (bug 570520)

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -6,6 +6,6 @@ from jingo.views import direct_to_template
 urlpatterns = patterns('',
     url('^about$', direct_to_template, {'template': 'pages/about.lhtml'},
         name='pages.about'),
-    url('^faq$', direct_to_template, {'template': 'pages/faq.lhtml'},
+    url('^faq$', direct_to_template, {'template': 'pages/faq.html'},
         name='pages.faq'),
 )
