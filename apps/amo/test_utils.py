0b0d6f82fe416da5aeec0c1ac5616592c98841fe
Jeff Balogh <jbalogh@mozilla.com>
2010-08-09 13:31:48 -0700

    clear the cache after extra setup

--- apps/amo/test_utils.py
+++ apps/amo/test_utils.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+
 from addons import cron
 from addons.models import Addon
 
@@ -7,3 +9,4 @@ class ExtraSetup(object):
         super(ExtraSetup, self)._pre_setup()
         cron._update_appsupport(Addon.objects.values_list('id', flat=True))
         cron._update_addons_current_version(Addon.objects.values_list('id'))
+        cache.clear()
