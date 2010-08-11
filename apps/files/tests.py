6d0b33ad0563264ed80176e432ad16c47ea1d4c6
Dave Dash <dd+github@davedash.com>
2010-08-11 08:10:15 -0700

    files tests <2s vs >8s

--- apps/files/tests.py
+++ apps/files/tests.py
@@ -11,27 +11,27 @@ class TestFile(test.TestCase):
     Tests the methods of the File model.
     """
 
-    fixtures = ['base/fixtures']
+    fixtures = ('base/addon_3615', 'base/addon_5579')
 
     def test_get_absolute_url(self):
-        f = File.objects.get(id=11993)
+        f = File.objects.get(id=67442)
         url = f.get_absolute_url(amo.FIREFOX, src='src')
-        expected = ('/firefox/downloads/file/11993/'
-                    'del.icio.us_bookmarks-1.0.43-fx.xpi?src=src&confirmed=1')
+        expected = ('/firefox/downloads/file/67442/'
+                    'delicious_bookmarks-2.1.072-fx.xpi?src=src&confirmed=1')
         assert url.endswith(expected), url
 
     def test_latest_url(self):
         # With platform.
-        f = File.objects.get(id=61321)
+        f = File.objects.get(id=74797)
         base = '/en-US/firefox/downloads/latest/'
-        expected = base + '{0}/platform:3/addon-{0}-latest.jar'
+        expected = base + '{0}/platform:3/addon-{0}-latest.xpi'
         eq_(expected.format(f.version.addon_id), f.latest_xpi_url())
 
         # No platform.
-        f = File.objects.get(id=11993)
+        f = File.objects.get(id=67442)
         expected = base + '{0}/addon-{0}-latest.xpi'
         eq_(expected.format(f.version.addon_id), f.latest_xpi_url())
 
     def test_eula_url(self):
-        f = File.objects.get(id=61321)
-        eq_(f.eula_url(), '/addon/6704/eula/61321')
+        f = File.objects.get(id=67442)
+        eq_(f.eula_url(), '/addon/3615/eula/67442')
