a43dcb137033b217cb41728cef7f6224f1311cf9
Jeff Balogh <jbalogh@mozilla.com>
2010-08-30 13:16:12 -0700

    safe csrf tokens (bug 590351)

--- settings.py
+++ settings.py
@@ -153,7 +153,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
-    'django.core.context_processors.csrf',
+    'csrf_context.csrf',
 
     'django.contrib.messages.context_processors.messages',
 
