366f3d249985a219a86c87955344578338fa9f3c
Jeff Balogh <jbalogh@mozilla.com>
2010-09-23 11:27:18 -0700

    turn off async signals, fix a syntax error

--- manage.py
+++ manage.py
@@ -56,9 +56,6 @@ import log_settings
 import djcelery
 djcelery.setup_loader()
 
-import async_signals
-async_signals.start_the_machine()
-
 
 if __name__ == "__main__":
     execute_manager(settings)
