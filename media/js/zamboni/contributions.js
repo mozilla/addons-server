edc0ac1075b1d74186d0cfbfcfc51697fcf78bf5
Matt Claypotch <thepotch@gmail.com>
2010-09-24 14:56:38 -0700

    bug 598067, fixed contribution popup

--- media/js/zamboni/contributions.js
+++ media/js/zamboni/contributions.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
+    $("#contribute-why").popup("#contribute-more-info", {
+        pointTo: "#contribute-more-info"
+    });
+
     $('canvas.pledge-o-meter').thermometer();
 });
