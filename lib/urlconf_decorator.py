e9776a1a45c986c799d62a454ae6015d7d702479
Jeff Balogh <jbalogh@mozilla.com>
2010-09-28 13:08:33 -0700

    a magical function to decorate urlconfs

---
+++ lib/urlconf_decorator.py
@@ -0,0 +1,35 @@
+"""
+Apply a decorator to a whole urlconf instead of a single view function.
+
+Usage::
+
+    >>> from urlconf_decorator import decorate
+    >>>
+    >>> def dec(f):
+    ...     def wrapper(*args, **kw):
+    ...         print 'inside the decorator'
+    ...         return f(*args, **kw)
+    ...     return wrapper
+    >>>
+    >>> urlpatterns = patterns(''
+    ...     url('^admin/', decorate(dec, include(admin.site.urls))),
+    ... )
+
+The decorator applied to the urlconf is a normal function decorator.  It gets
+wrapped around each callback in the urlconf as if you had @decorator above the
+function.
+
+"""
+from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
+
+
+def decorate(decorator, urlconf):
+    if isinstance(urlconf, (list, tuple)):
+        for item in urlconf:
+            decorate(decorator, item)
+    elif isinstance(urlconf, RegexURLResolver):
+        for item in urlconf.url_patterns:
+            decorate(decorator, item)
+    elif isinstance(urlconf, RegexURLPattern):
+        urlconf._callback = decorator(urlconf.callback)
+    return urlconf
