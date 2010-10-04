a5abfdb5a92394303d1a3710ccdf02ea76cd0a29
Jeff Balogh <jbalogh@mozilla.com>
2010-10-04 16:14:00 -0700

    move test signals into amo/signals.py

--- apps/amo/signals.py
+++ apps/amo/signals.py
@@ -1,8 +1,10 @@
 import contextlib
 
+from django import http
+from django.conf import settings
 from django.db import models
 
-from . import tasks
+from . import tasks, urlresolvers
 
 
 def flush_front_end_cache(sender, instance, **kwargs):
@@ -17,6 +19,7 @@ def _connect():
     models.signals.post_save.connect(flush_front_end_cache)
     models.signals.post_delete.connect(flush_front_end_cache)
 
+
 def _disconnect():
     models.signals.post_save.disconnect(flush_front_end_cache)
     models.signals.post_delete.disconnect(flush_front_end_cache)
@@ -29,3 +32,31 @@ def hera_disabled():
         yield
     finally:
         _connect()
+
+
+## Hook up test signals here, for lack of a better spot.
+
+def clean_url_prefixes(sender, **kwargs):
+    """Wipe the URL prefixer(s) after each test."""
+    urlresolvers.clean_url_prefixes()
+
+
+def default_prefixer(sender, **kwargs):
+    """Make sure each test starts with a default URL prefixer."""
+    request = http.HttpRequest()
+    request.META['SCRIPT_NAME'] = ''
+    prefixer = urlresolvers.Prefixer(request)
+    prefixer.app = settings.DEFAULT_APP
+    prefixer.locale = settings.LANGUAGE_CODE
+    urlresolvers.set_url_prefix(prefixer)
+
+
+# Register Django signals this app listens to.
+try:
+    import test_utils.signals
+except ImportError:
+    pass
+else:
+    # Clean up URL prefix cache when a new test is invoked.
+    test_utils.signals.pre_setup.connect(default_prefixer)
+    test_utils.signals.post_teardown.connect(clean_url_prefixes)
