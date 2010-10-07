ec18407ae9d62ca4d089b57ac67ae7fefb4f62c9
Jeff Balogh <jbalogh@mozilla.com>
2010-10-06 18:13:02 -0700

    adding some helpful redirects

--- apps/devhub/urls.py
+++ apps/devhub/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, url, include
+from django.shortcuts import redirect
 
 from urlconf_decorator import decorate
 
@@ -7,10 +8,10 @@ from . import views
 
 # These will all start with /addon/<addon_id>/
 detail_patterns = patterns('',
-    url('^edit$', views.addons_edit,
-        name='devhub.addons.edit'),
-    url('^ownership$', views.addons_owner,
-        name='devhub.addons.owner'),
+    # Redirect to the edit page from the base.
+    url('^$', lambda r: redirect('devhub.addons.edit', permanent=True)),
+    url('^edit$', views.addons_edit, name='devhub.addons.edit'),
+    url('^ownership$', views.addons_owner, name='devhub.addons.owner'),
 )
 
 urlpatterns = decorate(write, patterns('',
@@ -18,7 +19,11 @@ urlpatterns = decorate(write, patterns('',
 
     # URLs for a single add-on.
     ('^addon/(?P<addon_id>\d+)/', include(detail_patterns)),
+    # Redirect people who have /addons/ instead of /addon/.
+    ('^addons/\d+/.*', lambda r: redirect(r.path.replace('addons', 'addon', 1))),
 
+    # Redirect to /addons/ at the base.
+    url('^addon$', lambda r: redirect('devhub.addons', permanent=True)),
     url('^addons$', views.addons_dashboard, name='devhub.addons'),
     url('^addons/activity$', views.addons_activity,
         name='devhub.addons.activity'),
