59a2aa51e596be5d1089005c1912b54282073368
Wil Clouser <wclouser@mozilla.com>
2010-10-13 15:38:38 -0700

    Clean up users app; bug 597239

--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -1,7 +1,6 @@
 import os
 
 from django.conf import settings
-from django.db import connection, transaction
 
 import commonware.log
 from celeryutils import task
@@ -9,7 +8,6 @@ from celeryutils import task
 import amo.signals
 from amo.utils import resize_image
 from . import cron
-from users.models import UserProfile
 
 task_log = commonware.log.getLogger('z.task')
 
@@ -38,26 +36,3 @@ def resize_photo(src, dst):
         resize_image(src, dst, (200, 200))
     except Exception, e:
         task_log.error("Error saving userpic: %s" % e)
-
-
-@task(rate_limit='3/m')
-def fix_users_with_photos(ids):
-    """Temporary code, see bug 596477.  Delete me after 5.12."""
-    task_log.info('[%s@%s] Adding photo flag to users' %
-                  (len(ids), fix_users_with_photos.rate_limit))
-    cursor = connection.cursor()
-
-    query = "UPDATE users SET picture_type='image/png' WHERE id IN(%s)"
-    query = query % ','.join([str(i) for i in ids])
-    cursor.execute(query)
-    transaction.commit_unless_managed()
-
-
-# Temporary task to make auth.User rows for old users.
-@task(rate_limit='100/m')
-def make_django_user(*ids, **kw):
-    task_log.info('[%s@%s] Making Django babies.' %
-                  (len(ids), make_django_user.rate_limit))
-    with amo.signals.hera_disabled():
-        for user in UserProfile.uncached.filter(id__in=ids):
-            user.create_django_user()
