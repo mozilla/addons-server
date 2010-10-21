8202c49ff4d7f396de459f8cd91d54551b44032b
Wil Clouser <wclouser@mozilla.com>
2010-10-21 15:54:23 -0700

    robots.txt is off the root

--- settings.py
+++ settings.py
@@ -141,11 +141,12 @@ ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # paths that don't require an app prefix
 SUPPORTED_NONAPPS = ('admin', 'developers', 'editors', 'img',
-                     'jsi18n', 'localizers', 'media', 'statistics', 'services')
+                     'jsi18n', 'localizers', 'media', 'robots.txt',
+                     'statistics', 'services')
 DEFAULT_APP = 'firefox'
 
 # paths that don't require a locale prefix
-SUPPORTED_NONLOCALES = ('img', 'media', 'services',)
+SUPPORTED_NONLOCALES = ('img', 'media', 'robots.txt', 'services',)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'r#%9w^o_80)7f%!_ir5zx$tu3mupw9u%&s!)-_q%gy7i+fhx#)'
