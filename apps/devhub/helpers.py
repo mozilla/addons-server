9c631fcb0410ae8ee334057f7204986c3d404723
Andy McKay <amckay@mozilla.com>
2010-10-26 12:16:14 -0700

    remove URL on breadcrumbs (bug 607026)

--- apps/devhub/helpers.py
+++ apps/devhub/helpers.py
@@ -1,7 +1,5 @@
-from django.utils import encoding
-
 import jinja2
-from jingo import env, register
+from jingo import register
 from tower import ugettext as _
 
 from amo.urlresolvers import reverse
@@ -29,14 +27,20 @@ def dev_page_title(context, title=None, addon=None):
 
 @register.function
 @jinja2.contextfunction
-def dev_breadcrumbs(context, addon=None, items=None, add_default=False):
+def dev_breadcrumbs(context, addon=None, items=None, add_default=False,
+                    link_my_addons=True):
     """
     Wrapper function for ``breadcrumbs``. Prepends 'Developer Hub' breadcrumb
     to ``items`` argument, and ``add_default`` argument defaults to False.
     Accepts: [(url, label)]
+    addon: adds in a non-linked breadcrumb to the addon
+    link_my_addons: makes "My Add-ons" url linked, defaults to True
     """
-    crumbs = [(reverse('devhub.index'), _('Developer Hub')),
-              (reverse('devhub.addons'), _('My Add-ons'))]
+    crumbs = [(reverse('devhub.index'), _('Developer Hub')), ]
+    if link_my_addons:
+        crumbs.append((reverse('devhub.addons'), _('My Add-ons')))
+    else:
+        crumbs.append((None, _('My Add-ons')))
     if items:
         crumbs.extend(items)
     if addon:
