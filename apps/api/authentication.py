28a85144dc842a1aef51a1f51751b942c9e22fa5
Dave Dash <dd+github@davedash.com>
2010-10-26 17:11:31 -0700

    Authenticate by session.

--- apps/api/authentication.py
+++ apps/api/authentication.py
@@ -13,9 +13,14 @@ class AMOOAuthAuthentication(authentication.OAuthAuthentication):
     """^^^MOO!!!  Adds amo_user to the request object."""
 
     def is_authenticated(self, request):
+        if request.user.is_authenticated():
+            return True
+
         rv = super(AMOOAuthAuthentication, self).is_authenticated(request)
+
         if rv:
             ACLMiddleware().process_request(request)
+
         return rv
 
     def challenge(self, request):
