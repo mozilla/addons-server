5345c667ff201a3643dacbe747a414908ea692ed
Jeff Balogh <jbalogh@mozilla.com>
2010-11-03 20:18:10 -0700

    strip spaces from guids (bug 609506)

--- apps/search/client.py
+++ apps/search/client.py
@@ -116,7 +116,7 @@ def extract_filters(term, kwargs):
         for guid in guids.split(','):
             if not guid:
                 continue
-            guids_crc.append(crc32(guid.lower()))
+            guids_crc.append(crc32(guid.strip().lower()))
 
         filters['guid_ord'] = guids_crc
 
