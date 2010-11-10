d10391b3c486da3f004a763d52ab5de073a7c910
Andy McKay <amckay@mozilla.com>
2010-11-10 15:02:09 -0800

    remove Tags header (bug 611113)

--- apps/tags/tests/test_views.py
+++ apps/tags/tests/test_views.py
@@ -10,8 +10,19 @@ class TestManagement(test_utils.TestCase):
                 'tags/tags.json', ]
 
     def test_tags_details_view(self):
-        """Test that there are some tags being shown on the details page"""
-        url = reverse('addons.detail', args=[3615, ])
+        """Test that there are some tags being shown on the details page."""
+        url = reverse('addons.detail', args=[3615])
         r = self.client.get(url)
         doc = pq(r.content)
         eq_(len(doc('li.tag')), 4)
+        assert 'Tags' in [ d.text for d in doc('h3') ]
+
+class TestNoTags(test_utils.TestCase):
+    fixtures = ['base/addon_3615']
+
+    def test_tags_no_details_view(self):
+        """Test that there is no tag header tags being shown."""
+        url = reverse('addons.detail', args=[3615])
+        r = self.client.get(url)
+        doc = pq(r.content)
+        assert 'Tags' not in [ d.text for d in doc('h3') ]
