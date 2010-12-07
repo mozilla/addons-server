bb539e239bb2d8d8706b5c486d3e98f377a59336
Dave Dash <dd+github@davedash.com>
2010-12-07 13:22:04 -0800

    Remove advance search tests.

--- apps/search/tests/test_forms.py
+++ apps/search/tests/test_forms.py
@@ -1,30 +1,8 @@
-from django.test import client
-
 import test_utils
-from nose import SkipTest
 from nose.tools import eq_
-from pyquery import PyQuery as pq
 
 import amo
-from amo.urlresolvers import reverse
 from search import forms
-
-
-def test_form_version_label():
-    raise SkipTest()
-    for app in amo.APP_USAGE:
-        r = client.Client().get('/en-US/{0}/'.format(app.short))
-        doc = pq(r.content)
-        eq_(doc('#advanced-search label')[0].text,
-                '%s Version' % unicode(app.pretty))
-
-
-def test_korean():
-    raise SkipTest()
-    "All forms, regardless of nationality, should have an 'Any' version."
-    r = client.Client().get('/ko/firefox/')
-    doc = pq(r.content)
-    eq_(doc('#id_lver option')[0].values()[0], 'any')
 
 
 class TestSearchForm(test_utils.TestCase):
@@ -37,38 +15,3 @@ class TestSearchForm(test_utils.TestCase):
 
         # So you added a new appversion and this broke?  Sorry about that.
         eq_(actual, expected)
-
-    def test_personas_selected(self):
-        raise SkipTest()
-        r = self.client.get(reverse('browse.personas'), follow=True)
-        doc = pq(r.content)
-        eq_(doc('#cat option:selected').val(), 'personas')
-
-        # detail page
-        r = self.client.get(reverse('addons.detail', args=[15663]),
-                            follow=True)
-        doc = pq(r.content)
-        eq_(doc('#cat option:selected').val(), 'personas')
-
-    def test_no_personas(self):
-        """Sunbird, Mobile and Seamonkey don't have personas.  So don't
-        persuade people to search for them."""
-        apps = ('sunbird', 'mobile', 'seamonkey',)
-
-        for app in apps:
-            r = self.client.get('/en-US/%s/' % app, follow=True)
-            doc = pq(r.content)
-            eq_(len(doc('.cat-all [value=personas]')), 0,
-                '%s shows personas' % app)
-
-    def test_no_search_tools(self):
-        raise SkipTest()
-        """The Search Tools category should not be included for non-browser
-        applications."""
-
-        for app in ('thunderbird', 'sunbird'):
-            r = self.client.get('/en-US/%s/' % app, follow=True)
-            self.assertNotContains(r, 'Search Tools')
-
-        r = self.client.get('/en-US/firefox', follow=True)
-        self.assertContains(r, 'Search Tools')
