da65dd66e499670dc0711dfeb4c8a7e1196f5e81
Kumar McMillan <kumar.mcmillan@gmail.com>
2010-12-08 11:33:44 -0600

    Ported l10n JS tests to use the new sandbox.
    Also made the tests synchronous. Not sure why they were async,
    probably done while debugging JS lib loading.

--- media/js/zamboni/tests/l10n_tests.js
+++ media/js/zamboni/tests/l10n_tests.js
@@ -2,34 +2,28 @@ $(document).ready(function(){
 
 var transFixture = {
     setup: function() {
-        $("#qunit-fixture").append($("#l10n-translation").html());
+        this.sandbox = tests.createSandbox();
+        this.sandbox.append($("#l10n-translation").html());
+    },
+    teardown: function() {
+        this.sandbox.remove();
     }
 };
 
 module('z.refreshL10n', transFixture);
 
-asyncTest('English', function() {
-    tests.waitFor(function() {
-        return typeof(z.refreshL10n) !== 'undefined';
-    }).thenDo(function() {
-        z.refreshL10n('en-us');
-        equals($('#qunit-fixture textarea:visible').text().trim(),
-               'Firebug integrates with Firefox to put a wealth of ' +
-               'development tools...');
-        start();
-    });
+test('English', function() {
+    z.refreshL10n('en-us');
+    equals($('textarea:visible', this.sandbox).text().trim(),
+           'Firebug integrates with Firefox to put a wealth of ' +
+           'development tools...');
 });
 
-asyncTest('Japanese', function() {
-    tests.waitFor(function() {
-        return typeof(z.refreshL10n) !== 'undefined';
-    }).thenDo(function() {
-        z.refreshL10n('ja');
-        equals($('#qunit-fixture textarea:visible').text().trim(),
-               'Firebug は、Web ページを閲覧中にクリック一つで使える豊富な開発ツールを Firefox' +
-               ' に統合します。あなたはあらゆる');
-        start();
-    });
+test('Japanese', function() {
+    z.refreshL10n('ja');
+    equals($('textarea:visible', this.sandbox).text().trim(),
+           'Firebug は、Web ページを閲覧中にクリック一つで使える豊富な開発ツールを Firefox' +
+           ' に統合します。あなたはあらゆる');
 });
 
 });
