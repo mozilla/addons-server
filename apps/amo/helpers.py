9b15a54f0cd6c1eb112e9e5041bc2627e51d01a6
Jeff Balogh <jbalogh@mozilla.com>
2010-12-07 16:46:16 -0800

    bail if suggested_amount was not given (bug 617491)

--- apps/amo/helpers.py
+++ apps/amo/helpers.py
@@ -143,6 +143,8 @@ def numberfmt(num, format=None):
 
 @register.filter
 def currencyfmt(num, currency):
+    if num is None:
+        return ''
     return _get_format().currency(num, currency)
 
 
