c55b277442f05e303172d55119eb2ddf333ec04a
Dave Dash <dd+github@davedash.com>
2010-12-09 13:43:30 -0800

    Removing drop FKs from migration

--- migrations/103-cascade.sql
+++ migrations/103-cascade.sql
@@ -1,9 +1,7 @@
 ALTER TABLE `log_activity_user`
       MODIFY COLUMN `user_id` INT(11) UNSIGNED NOT NULL;
 
-ALTER TABLE `log_activity_user` DROP FOREIGN KEY `user_id_refs_id_e987c199`;
-
-ALTER TABLE `remora`.`log_activity_user`
+ALTER TABLE `log_activity_user`
       ADD CONSTRAINT `user_id_refs_id_e987c199`
           FOREIGN KEY `user_id_refs_id_e987c199` (`user_id`)
           REFERENCES `users` (`id`)
