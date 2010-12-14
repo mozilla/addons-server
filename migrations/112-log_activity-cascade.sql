a7d0261edc44f7ae57a5f778e93ae1a15c39fe64
Dave Dash <dd+github@davedash.com>
2010-12-14 10:45:23 -0800

    better migrations

--- migrations/112-log_activity-cascade.sql
+++ migrations/112-log_activity-cascade.sql
@@ -8,10 +8,10 @@ ALTER TABLE `log_activity_addon` ADD CONSTRAINT `activity_log_id_refs_id_9c20a92
     REFERENCES `log_activity` (`id`)
     ON DELETE CASCADE;
 
--- ALTER TABLE `log_activity_user`
--- DROP FOREIGN KEY `activity_log_id_refs_id_4f8d99d4`;
+ALTER TABLE `log_activity_user`
+    DROP FOREIGN KEY `activity_log_id_refs_id_4f8d99d4`;
 
-ALTER TABLE `log_activity_user` ADD CONSTRAINT `activity_log_id_refs_id_9c20a926` FOREIGN KEY `activity_log_id_refs_id_4f8d99d4` (`activity_log_id`)
+ALTER TABLE `log_activity_user` ADD CONSTRAINT `activity_log_id_refs_id_4f8d99d4` FOREIGN KEY `activity_log_id_refs_id_4f8d99d4` (`activity_log_id`)
     REFERENCES `log_activity` (`id`)
     ON DELETE CASCADE;
 
