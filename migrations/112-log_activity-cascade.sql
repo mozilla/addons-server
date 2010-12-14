faa6ceae7e6eff0f436eff1f0c84ce89ab6d6d95
Dave Dash <dd+github@davedash.com>
2010-12-14 10:02:01 -0800

    log_activity_user deletions should cascade from log_activity.

---
+++ migrations/112-log_activity-cascade.sql
@@ -0,0 +1,18 @@
+-- If these FKs don't exist, run them manually and use:
+-- schematic migrations/ -u 112
+
+ALTER TABLE `log_activity_addon`
+ DROP FOREIGN KEY `activity_log_id_refs_id_9c20a926`;
+
+ALTER TABLE `log_activity_addon` ADD CONSTRAINT `activity_log_id_refs_id_9c20a926` FOREIGN KEY `activity_log_id_refs_id_9c20a926` (`addon_id`)
+    REFERENCES `log_activity` (`id`)
+    ON DELETE CASCADE;
+
+ALTER TABLE `log_activity_user`
+ DROP FOREIGN KEY `activity_log_id_refs_id_4f8d99d4`;
+
+ALTER TABLE `log_activity_user` ADD CONSTRAINT `activity_log_id_refs_id_9c20a926` FOREIGN KEY `activity_log_id_refs_id_4f8d99d4` (`addon_id`)
+    REFERENCES `log_activity` (`id`)
+    ON DELETE CASCADE;
+
+
