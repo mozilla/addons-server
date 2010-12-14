a74948c589422591e26620f8e2fb980bc243c7e1
Dave Dash <dd+github@davedash.com>
2010-12-14 10:16:50 -0800

    better migrations

--- migrations/112-log_activity-cascade.sql
+++ migrations/112-log_activity-cascade.sql
@@ -1,8 +1,8 @@
 -- If these FKs don't exist, run them manually and use:
 -- schematic migrations/ -u 112
 
-ALTER TABLE `log_activity_addon`
- DROP FOREIGN KEY `activity_log_id_refs_id_9c20a926`;
+-- ALTER TABLE `log_activity_addon`
+--    DROP FOREIGN KEY `activity_log_id_refs_id_9c20a926`;
 
 ALTER TABLE `log_activity_addon` ADD CONSTRAINT `activity_log_id_refs_id_9c20a926` FOREIGN KEY `activity_log_id_refs_id_9c20a926` (`addon_id`)
     REFERENCES `log_activity` (`id`)
