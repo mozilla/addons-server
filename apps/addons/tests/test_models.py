85fc3ccc8f203b1a45ed3ccbc2765e479f3e9319
Andy McKay <amckay@mozilla.com>
2011-01-11 11:55:59 -0800

    its a bit longer, but wanted to give patch_object a try

--- apps/addons/tests/test_models.py
+++ apps/addons/tests/test_models.py
@@ -9,7 +9,7 @@ from django.core import mail
 from django.core.cache import cache
 from django.utils import translation
 
-from mock import patch
+from mock import patch, patch_object
 from nose.tools import eq_, assert_not_equal
 import test_utils
 
@@ -876,14 +876,19 @@ class TestAddonFromUpload(files.tests.UploadTest):
         translation.deactivate()
 
 
+REDIRECT_URL = 'http://outgoing.mozilla.org/v1/'
+
+
 class TestCharity(test_utils.TestCase):
     fixtures = ['base/charity.json']
 
+    @patch_object(settings._wrapped, 'REDIRECT_URL', REDIRECT_URL)
     def test_url(self):
         charity = Charity(name="a", paypal="b", url="http://foo.com")
         charity.save()
-        assert charity.outgoing_url.startswith('http://outgoing')
+        assert charity.outgoing_url.startswith(REDIRECT_URL)
 
+    @patch_object(settings._wrapped, 'REDIRECT_URL', REDIRECT_URL)
     def test_url_foundation(self):
         foundation = Charity.objects.get(pk=amo.FOUNDATION_ORG)
-        assert not foundation.outgoing_url.startswith('http://outgoing')
+        assert not foundation.outgoing_url.startswith(REDIRECT_URL)
