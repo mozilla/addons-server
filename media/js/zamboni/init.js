5ece6556f542b1d7b10bd3f9e9ac2b6a17cc3bf6
Matt Claypotch <thepotch@gmail.com>
2011-01-13 14:07:26 -0800

    slightly more performant js format function

--- media/js/zamboni/init.js
+++ media/js/zamboni/init.js
@@ -37,13 +37,15 @@ $(document).ready(function(){
  * >>> format('{x}', {x: 1})
  * "1"
  */
-function format(s, args) {
-    if (!args) return;
-    if (!(args instanceof Array || args instanceof Object))
-        args = Array.prototype.slice.call(arguments, 1);
+var format = (function() {
     var re = /\{([^}]+)\}/g;
-    return s.replace(re, function(_, match){ return args[match]; });
-}
+    return function(s, args) {
+        if (!args) return;
+        if (!(args instanceof Array || args instanceof Object))
+            args = Array.prototype.slice.call(arguments, 1);
+        return s.replace(re, function(_, match){ return args[match]; });
+    };
+})();
 function template(s) {
     return function(args) { return format(s, args); };
 }
