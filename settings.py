50233470905cf21a184352a6157590e41328f782
Andy McKay <amckay@mozilla.com>
2011-01-19 12:22:03 +0000

    add a .png on the end (bug 621827)

--- settings.py
+++ settings.py
@@ -484,7 +484,7 @@ PREVIEW_FULL_PATH = (PREVIEWS_PATH + '/full/%s/%d.png')
 STATIC_URL = SITE_URL
 ADDON_ICONS_DEFAULT_URL = MEDIA_URL + '/img/addon-icons'
 ADDON_ICON_BASE_URL = MEDIA_URL + 'img/amo2009/icons/'
-ADDON_ICON_URL = "%s/%s/%s/images/addon_icon/%%d/%%s" % (
+ADDON_ICON_URL = "%s/%s/%s/images/addon_icon/%%d.png?%%s" % (
         STATIC_URL, LANGUAGE_CODE, DEFAULT_APP)
 PREVIEW_THUMBNAIL_URL = (STATIC_URL +
         '/img/uploads/previews/thumbs/%s/%d.png?modified=%d')
