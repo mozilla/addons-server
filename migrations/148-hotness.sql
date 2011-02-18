fbad0852ee4f97ad5acddf5b3a611a43712d54da
Wil Clouser <wclouser@mozilla.com>
2011-02-18 09:39:23 -0800

    comment out long running sql.  will run separately

--- migrations/148-hotness.sql
+++ migrations/148-hotness.sql
@@ -9,4 +9,6 @@ ALTER TABLE addons
 CREATE INDEX hotness_idx on addons (hotness);
 
 -- See you in a couple hours.
-CREATE INDEX addon_date_idx ON update_counts (addon_id, date);
+-- I'm commenting this out and running it on production separately because I
+-- don't want it to block our roll out. --clouserw
+-- CREATE INDEX addon_date_idx ON update_counts (addon_id, date);
