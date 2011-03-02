d7b3f6da692bdbcf10b1d684b1304314d595d15e
Dave Dash <dd+github@davedash.com>
2011-03-02 11:06:07 -0800

    bug 637953, trim with precision

--- migrations/156-strip-whitespace.sql
+++ migrations/156-strip-whitespace.sql
@@ -1,4 +1,5 @@
 UPDATE translations
     SET localized_string=TRIM(localized_string)
-    WHERE localized_string LIKE ' %' or localized_string LIKE '% ';
--- Query OK, 187318 rows affected (45.45 sec)
+    WHERE
+        localized_string LIKE ' %' AND
+        id IN (SELECT name FROM addons);
