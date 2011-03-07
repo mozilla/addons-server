11518a990ff6381ed5bfe6b42820041481e8cae1
Wil Clouser <wclouser@mozilla.com>
2011-03-07 14:38:15 -0800

    Comment out this migration, will need to run manually in prod

--- migrations/155-drop-triggers.sql
+++ migrations/155-drop-triggers.sql
@@ -1,3 +1,4 @@
-DROP TRIGGER IF EXISTS trg_tag_stat_inc;
-DROP TRIGGER IF EXISTS trg_tag_stat_dec;
+-- You've got to have SUPER to run these, so commenting them out.  We'll run manually in production in the 6.0.0 push
+-- DROP TRIGGER IF EXISTS trg_tag_stat_inc;
+-- DROP TRIGGER IF EXISTS trg_tag_stat_dec;
 
