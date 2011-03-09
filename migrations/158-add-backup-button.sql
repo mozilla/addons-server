c22aaedb621b35fb0b998681333d394fedb627d9
Jeff Balogh <jbalogh@mozilla.com>
2011-03-08 18:28:12 -0800

    clear out addon_index to force new recommendations (bug 638629)

--- migrations/158-add-backup-button.sql
+++ migrations/158-add-backup-button.sql
@@ -1,2 +1,5 @@
 ALTER TABLE addons ADD COLUMN backup_version int(11) unsigned;
 ALTER TABLE addons ADD CONSTRAINT addons_ibfk_16 FOREIGN KEY (backup_version) REFERENCES versions (id) ON DELETE SET NULL;
+
+-- Lifted from #160.
+UPDATE collections SET addon_index=NULL;
