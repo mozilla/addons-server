0b6bf40309afd6d81ce569a18ebc836793c01eb0
Jeff Balogh <jbalogh@mozilla.com>
2011-03-10 12:16:34 -0800

    undo the migration merges

--- migrations/158-add-backup-button.sql
+++ migrations/158-add-backup-button.sql
@@ -1,12 +1,2 @@
 ALTER TABLE addons ADD COLUMN backup_version int(11) unsigned;
 ALTER TABLE addons ADD CONSTRAINT addons_ibfk_16 FOREIGN KEY (backup_version) REFERENCES versions (id) ON DELETE SET NULL;
-
--- Lifted from #160.
-UPDATE collections SET addon_index=NULL;
-
--- Lifted from #161.
-TRUNCATE addon_recommendations;
-
-UPDATE collections SET recommended_collection_id=NULL;
-
-DELETE FROM collections WHERE collection_type=3;
