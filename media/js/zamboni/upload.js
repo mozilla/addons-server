ad1a7820a1210fee02fd3332bc93089bc711aa44
Gregory Koberger <gkoberger@gmail.com>
2011-03-22 13:14:13 -0700

    Make validation open in new window

--- media/js/zamboni/upload.js
+++ media/js/zamboni/upload.js
@@ -357,7 +357,8 @@
                     if (results.full_report_url) {
                         // There might not be a link to the full report
                         // if we get an early error like unsupported type.
-                        upload_results.append($("<a>", {'href':results.full_report_url,
+                        upload_results.append($("<a>", {'href': results.full_report_url,
+                                                        'target': '_blank',
                                                         'text': gettext('See full validation report')}));
                     }
 
