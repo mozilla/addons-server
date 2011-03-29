fc8f0b6228a7ef777f64020778fd215315945809
Jeff Balogh <jbalogh@mozilla.com>
2011-03-29 15:17:43 -0700

    manually delete fks that aren't cascading (bug 645897)

--- migrations/170-remove-synced-collections.sql
+++ migrations/170-remove-synced-collections.sql
@@ -1,3 +1,18 @@
+-- Clear out all the foreign keys since there's no CASCADE.
+DELETE st FROM stats_addons_collections_counts st
+    INNER JOIN collections ON (
+        st.collection_id=collections.id AND
+        collection_type IN (1, 3) AND
+        author_id IS NULL);
+
+DELETE st FROM stats_collections_counts st
+    INNER JOIN collections ON (
+        st.collection_id=collections.id AND
+        collection_type IN (1, 3) AND
+        author_id IS NULL);
+
+UPDATE collections SET recommended_collection_id = NULL;
+
 -- Remove synced and recommended collections;
 DELETE FROM collections
     WHERE collection_type IN (1, 3) AND author_id IS NULL;
