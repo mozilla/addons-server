fe6836bc2ec68cc09fc5c2da63fca3596e13b047
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-03-31 14:13:07 -0500

    Adds scripts to start up a dev server and run QUnit tests from CI

---
+++ scripts/run_jstests.py
@@ -0,0 +1,69 @@
+"""
+A wrapper around nosetests to run JavaScript tests in a CI environment.
+
+Example::
+
+    python run_jstests.py --with-xunit \
+                           --with-zamboni --zamboni-host hudson.mozilla.org \
+                           --with-jstests \
+                           --jstests-server http://jstestnet.farmdev.com/ \
+                           --jstests-suite zamboni --jstests-browsers firefox
+
+"""
+import os
+import site
+import subprocess
+
+ROOT = os.path.join(os.path.dirname(__file__), '..')
+assert 'manage.py' in os.listdir(ROOT), (
+    'Expected this to be the root dir containing manage.py: %s' % ROOT)
+
+site.addsitedir(os.path.join(ROOT, 'vendor'))
+site.addsitedir(os.path.join(ROOT, 'vendor/lib/python'))
+
+from jstestnetlib import webapp
+from jstestnetlib.noseplugin import JSTests
+import nose
+from nose.plugins import Plugin
+
+
+class Zamboni(Plugin):
+    """Starts/stops Django runserver for tests."""
+    name = 'zamboni'
+
+    def options(self, parser, env=os.environ):
+        super(Zamboni, self).options(parser, env=env)
+        parser.add_option('--zamboni-host', default='0.0.0.0',
+                          help='Hostname or IP address to bind manage.py '
+                               'runserver to. This must match the host/ip '
+                               'configured in your --jstests-suite URL. '
+                               'Default: %default')
+        parser.add_option('--zamboni-port', default=9877,
+                          help='Port to bind manage.py runserver to. '
+                               'This must match the port '
+                               'configured in your --jstests-suite URL. '
+                               'Default: %default')
+        self.parser = parser
+
+    def configure(self, options, conf):
+        super(Zamboni, self).configure(options, conf)
+        self.options = options
+
+    def begin(self):
+        self.zamboni = webapp.WebappServerCmd(
+                                    ['python', 'manage.py', 'runserver',
+                                     '%s:%s' % (self.options.zamboni_host,
+                                                self.options.zamboni_port)],
+                                    cwd=ROOT)
+        self.zamboni.startup()
+
+    def finalize(self, result):
+        self.zamboni.shutdown()
+
+
+def main():
+    nose.main(plugins=[Zamboni(), JSTests()])
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/run_jstests.sh
@@ -0,0 +1,85 @@
+# This script should be called from within Hudson
+
+
+cd $WORKSPACE
+VENV=$WORKSPACE/venv
+VENDOR=$WORKSPACE/vendor
+LOCALE=$WORKSPACE/locale
+
+echo "Starting build on executor $EXECUTOR_NUMBER..." `date`
+
+if [ -z $1 ]; then
+    echo "Warning: You should provide a unique name for this job to prevent database collisions."
+    echo "Usage: $0 <name>"
+    echo "Continuing, but don't say you weren't warned."
+fi
+
+echo "Setup..." `date`
+
+# Make sure there's no old pyc files around.
+find . -name '*.pyc' | xargs rm
+
+if [ ! -d "$VENV/bin" ]; then
+  echo "No virtualenv found.  Making one..."
+  virtualenv $VENV
+fi
+
+source $VENV/bin/activate
+
+pip install -q -r requirements/compiled.txt
+
+# Create paths we want for addons
+if [ ! -d "/tmp/warez" ]; then
+    mkdir /tmp/warez
+fi
+
+if [ ! -d "$LOCALE" ]; then
+    echo "No locale dir?  Cloning..."
+    svn co http://svn.mozilla.org/addons/trunk/site/app/locale/ $LOCALE
+fi
+
+if [ ! -d "$VENDOR" ]; then
+    echo "No vendor lib?  Cloning..."
+    git clone --recursive git://github.com/jbalogh/zamboni-lib.git $VENDOR
+fi
+
+# Update the vendor lib.
+echo "Updating vendor..."
+pushd $VENDOR && git pull && git submodule update --init;
+popd
+
+cat > settings_local.py <<SETTINGS
+from settings import *
+ROOT_URLCONF = '%s.urls' % ROOT_PACKAGE
+LOG_LEVEL = logging.ERROR
+# Database name has to be set because of sphinx
+DATABASES['default']['NAME'] = 'zamboni_$1'
+DATABASES['default']['HOST'] = 'localhost'
+DATABASES['default']['USER'] = 'hudson'
+DATABASES['default']['TEST_NAME'] = 'test_zamboni_$1'
+DATABASES['default']['TEST_CHARSET'] = 'utf8'
+DATABASES['default']['TEST_COLLATION'] = 'utf8_general_ci'
+CACHE_BACKEND = 'caching.backends.locmem://'
+CELERY_ALWAYS_EAGER = True
+ADDONS_PATH = '/tmp/warez'
+
+TEST_SPHINX_CATALOG_PATH = TMP_PATH + '/$1/data/sphinx'
+TEST_SPHINX_LOG_PATH = TMP_PATH + '/$1/log/serachd'
+TEST_SPHINXQL_PORT = 340${EXECUTOR_NUMBER}
+TEST_SPHINX_PORT = 341${EXECUTOR_NUMBER}
+
+# Activate Qunit:
+INSTALLED_APPS += (
+    'django_qunit',
+)
+
+SETTINGS
+
+
+echo "Starting JS tests..." `date`
+
+# NOTE: the host value here needs to match the 'zambnoi' suite in jstestnet
+cd scripts
+python run_jstests.py --with-xunit --with-zamboni --zamboni-host hudson.mozilla.org --with-jstests --jstests-server http://jstestnet.farmdev.com/ --jstests-suite zamboni --jstests-browsers firefox
+
+echo 'shazam!'
