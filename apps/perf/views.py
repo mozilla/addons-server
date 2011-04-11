50a53de2d79c4d134fd60e512d02a46d2dee771d
Wil Clouser <wclouser@mozilla.com>
2011-04-11 12:42:30 -0700

    Adjust perf page; bug 647398

--- apps/perf/views.py
+++ apps/perf/views.py
@@ -1,4 +1,5 @@
 from django import http
+from django.conf import settings
 from django.db.models import Avg
 from django.shortcuts import get_list_or_404
 from django.views.decorators.cache import cache_control
@@ -12,8 +13,12 @@ from .models import Performance
 
 @cache_control(max_age=60 * 60 * 24)  # Cache for a day.
 def index(request):
+
+    # By default don't show less than 25; bug 647398
+    threshold = getattr(settings, 'PERF_THRESHOLD', 25)
+
     addons = (Addon.objects.listed(request.APP)
-              .filter(ts_slowness__isnull=False).order_by('-ts_slowness'))
+              .filter(ts_slowness__gte=threshold).order_by('-ts_slowness'))
     addons = get_list_or_404(addons[:50])
     return jingo.render(request, 'perf/index.html',
                         dict(addons=addons))
