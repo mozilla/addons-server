12352eb2a0ed32d3545e0ad1a9ae2a103cb2d7a0
Wil Clouser <wclouser@mozilla.com>
2011-04-11 12:42:30 -0700

    Adjust perf page; bug 647398

--- apps/perf/views.py
+++ apps/perf/views.py
@@ -1,4 +1,5 @@
 from django import http
+from django.conf import settings
 from django.db.models import Avg
 from django.shortcuts import get_list_or_404
 from django.views.decorators.cache import cache_control
@@ -13,9 +14,14 @@ from .models import Performance, PerformanceOSVersion
 
 @cache_control(max_age=60 * 60 * 24)  # Cache for a day.
 def index(request):
+
+    # By default don't show less than 25; bug 647398
+    threshold = getattr(settings, 'PERF_THRESHOLD', 25)
+
     addons = (Addon.objects.listed(request.APP)
-              .filter(ts_slowness__isnull=False).order_by('-ts_slowness'))
+              .filter(ts_slowness__gte=threshold).order_by('-ts_slowness'))
     addons = get_list_or_404(addons[:50])
+
     ids = [a.id for a in addons]
     redis = redisutils.connections['master']
     os_perf = dict(zip(ids, redis.hmget(Performance.ALL_PLATFORMS, ids)))
