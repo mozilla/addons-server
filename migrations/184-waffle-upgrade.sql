6d2dd532e03beb0cc31d797c55c923e35419b4df
Wil Clouser <wclouser@mozilla.com>
2011-04-22 10:16:27 -0700

    add new waffle fields

---
+++ migrations/184-waffle-upgrade.sql
@@ -0,0 +1,3 @@
+ALTER TABLE waffle_flag ADD COLUMN note text NOT NULL DEFAULT '';
+ALTER TABLE waffle_switch ADD COLUMN note text NOT NULL DEFAULT '';
+ALTER TABLE waffle_sample ADD COLUMN note text NOT NULL DEFAULT '';
