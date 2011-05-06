5f01603f778e8fe9391ce44fae3008ab1a62b205
Andy McKay <amckay@mozilla.com>
2011-05-06 14:24:42 -0700

    Revert "temp commit to get some more information (bug 653874)"
    This reverts commit 66188f8b0f000c745c5c5ec8ae1c7f5cce7d6b2f.

--- apps/files/helpers.py
+++ apps/files/helpers.py
@@ -3,8 +3,6 @@ import mimetypes
 import os
 import shutil
 import stat
-import sys
-import traceback
 
 from django.conf import settings
 from django.utils.datastructures import SortedDict
@@ -83,9 +81,7 @@ class FileViewer:
             try:
                 extract_xpi(self.src, self.dest, expand=True)
             except Exception, err:
-                exc_info = sys.exc_info()
-                task_log.error('Error (%s) extracting %s, %s' %
-                               (err, self.src, traceback.format_tb(exc_info[2])))
+                task_log.error('Error (%s) extracting %s' % (err, self.src))
                 raise
 
     def cleanup(self):
