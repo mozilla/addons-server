a725b852aead76ef6d36069cc4aad2d83a6d67c3
Matt Claypotch <thepotch@gmail.com>
2011-05-18 11:35:06 -0700

    promo carousel auto-advances

--- media/js/impala/homepage.js
+++ media/js/impala/homepage.js
@@ -2,11 +2,16 @@ $(function () {
     var promos_base = $('#promos').attr('data-promo-url'),
         promos_url = format('{0}?version={1}&platform={2}', promos_base, z.browserVersion, 'Darwin');
     $.get(promos_url, function(resp) {
-        $("#promos ul").append($(resp));
-        var $q = $("#promos").zCarousel({
+        $('#promos ul').append($(resp));
+        var $q = $('#promos').zCarousel({
             circular: true
         });
         $('.vtruncate').truncate({dir: 'v'});
-        $q.click($q.gofwd);
+        var interval = setInterval($q.gofwd, 7000);
+        $q.hover(function() {
+            clearInterval(interval);
+        }, function() {
+            interval = setInterval($q.gofwd, 7000);
+        });
     });
 });
