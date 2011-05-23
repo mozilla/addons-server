946e18de03a74d0ce4b53c3540c552ddfd5baa6f
Gregory Koberger <gkoberger@gmail.com>
2011-05-23 15:55:50 -0700

    Fix the log activity comments migration.

--- migrations/195-create-log-activity-comments.sql
+++ migrations/195-create-log-activity-comments.sql
@@ -4,10 +4,11 @@ CREATE TABLE `log_activity_comment` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `created` datetime NOT NULL,
     `modified` datetime NOT NULL,
-    `activity_log_id` integer NOT NULL,
+    `activity_log_id` int(11) NOT NULL,
     `comments` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 ;
 
-ALTER TABLE `log_activity_comment` ADD CONSTRAINT `activity_log_id_refs_id_4f8d99d4` FOREIGN KEY (`activity_log_id`) REFERENCES `log_activity` (`id`);
-CREATE INDEX `log_activity_comment_3bf68f54` ON `log_activity_comment` (`activity_log_id`);
+ALTER TABLE `log_activity_comment`
+      ADD CONSTRAINT FOREIGN KEY (`activity_log_id`) REFERENCES `log_activity` (`id`);
+
