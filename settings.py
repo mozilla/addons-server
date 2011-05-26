e53b6acc5e7991651ca7c7db20dc3fa2561ba3e5
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-05-26 13:34:33 -0500

    Configures the bulk validator to use its own queue (bug 658430)

--- settings.py
+++ settings.py
@@ -759,6 +759,7 @@ CELERY_ROUTES = {
     'users.tasks.delete_photo': {'queue': 'images'},
     'devhub.tasks.resize_icon': {'queue': 'images'},
     'devhub.tasks.resize_preview': {'queue': 'images'},
+    'zadmin.tasks.bulk_validate_file': {'queue': 'zamboni_bulk'},
 }
 
 
