c7801134a07945f096373fd5e5de569efb608f21
Matt Claypotch <thepotch@gmail.com>
2011-05-31 20:19:40 -0700

    listed does not like type properties

--- apps/addons/views.py
+++ apps/addons/views.py
@@ -396,7 +396,7 @@ def home(request):
 
 def impala_home(request):
     # Add-ons.
-    base = Addon.objects.listed(request.APP, type=amo.ADDON_EXTENSION)
+    base = Addon.objects.listed(request.APP).filter(type=amo.ADDON_EXTENSION)
     featured_ids = Addon.featured_random(request.APP, request.LANG)
 
     # Collections.
