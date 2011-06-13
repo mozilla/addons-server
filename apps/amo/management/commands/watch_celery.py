8cc15597fb9ba40bca61b72b42fbf288620f53ba
Jeff Balogh <jbalogh@mozilla.com>
2011-06-13 15:32:09 -0700

    use time.sleep, remove complexity

--- apps/amo/management/commands/watch_celery.py
+++ apps/amo/management/commands/watch_celery.py
@@ -1,4 +1,4 @@
-import logging
+import time
 
 from django.core.management.base import BaseCommand
 
@@ -7,17 +7,12 @@ from graphite import graphite
 
 from amo.tasks import task_stats
 
-log = logging.getLogger('z.redis')
-
 
 class Command(BaseCommand):
     help = "Subscribe to celery events and publish to graphite."
 
     def handle(self, *args, **kw):
         redis = redisutils.connections['master']
-        # We don't want this socket to timeout.
-        redis.connection.socket_timeout = None
-        redis.connection.disconnect()
         while 1:
             stats = []
             d = zip(['pending', 'failed', 'total'], task_stats.stats())
@@ -26,15 +21,4 @@ class Command(BaseCommand):
                     stats.append(('celery.tasks.%s.%s' % (key, name), value))
             graphite.sendall(*stats)
 
-            # We don't care about the message, just block until the next one.
-            redis.subscribe('celery.tasks.stats')
-            listener = redis.listen()
-            while 1:
-                if listener.next()['type'] == 'message':
-                    break
-
-            # Unsubscribe so we can process the stats.
-            redis.unsubscribe()
-            while 1:
-                if listener.next()['type'] == 'unsubscribe':
-                    break
+            time.sleep(2)
