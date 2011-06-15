e056595d19dc2915ab69f720aac942442935b966
Andy McKay <amckay@mozilla.com>
2011-06-15 09:42:33 -0700

    shebang is allowed now

--- apps/files/tests/test_views.py
+++ apps/files/tests/test_views.py
@@ -440,14 +440,14 @@ class TestDiffViewer(FilesBase, test_utils.TestCase):
     def test_view_left_binary(self):
         self.file_viewer.extract()
         filename = os.path.join(self.file_viewer.left.dest, 'install.js')
-        open(filename, 'w').write('#!')
+        open(filename, 'w').write('MZ')
         res = self.client.get(self.file_url(not_binary))
         assert 'This file is not viewable online' in res.content
 
     def test_view_right_binary(self):
         self.file_viewer.extract()
         filename = os.path.join(self.file_viewer.right.dest, 'install.js')
-        open(filename, 'w').write('#!')
+        open(filename, 'w').write('MZ')
         assert not self.file_viewer.is_diffable()
         res = self.client.get(self.file_url(not_binary))
         assert 'This file is not viewable online' in res.content
