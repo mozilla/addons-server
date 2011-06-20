0d59a2811d16e497da0e80af30ea15566de0465a
Jeff Balogh <jbalogh@mozilla.com>
2011-06-20 13:59:54 -0700

    use app instead of application_id

--- apps/bandwagon/search.py
+++ apps/bandwagon/search.py
@@ -4,7 +4,7 @@ import pyes.exceptions as pyes
 
 
 def extract(collection):
-    attrs = ('id', 'name', 'slug', 'author_username', 'type', 'application_id')
+    attrs = ('id', 'name', 'slug', 'author_username', 'type')
     d = dict(zip(attrs, attrgetter(*attrs)(collection)))
     d['app'] = d.pop('application_id')
     d['name'] = unicode(d['name'])  # Coerce to unicode.
