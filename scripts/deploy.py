3510b674b4e79c6a054d3d7e9b7a387749f10624
Andy McKay <amckay@mozilla.com>
2011-06-22 13:19:03 -0700

    add get_preview_sizes to deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -127,3 +127,4 @@ def update_amo(ctx):
     # Run management commands like this:
     # manage_cmd(ctx, 'cmd')
     manage_cmd(ctx, 'jetpackers')
+    manage_cmd(ctx, 'process_addons --task=get_preview_sizes')
