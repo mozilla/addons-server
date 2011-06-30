ed965a95b3d04c0bf73fcb9e50203b37db71cf33
Matt Claypotch <thepotch@gmail.com>
2011-06-30 13:06:55 -0700

    show firefox addons for chrome promobox (bug 668367)

--- media/js/impala/homepage.js
+++ media/js/impala/homepage.js
@@ -3,6 +3,9 @@
     $("#promos").show();
     var promos_base = $('#promos').attr('data-promo-url'),
         promos_url = format('{0}?version={1}&platform={2}', promos_base, z.browserVersion, z.platform);
+    if (z.badBrowser) {
+        promos_url = format('{0}?version={1}&platform={2}', promos_base, '5.0', 'mac');
+    }
     $.get(promos_url, function(resp) {
         $('#promos ul').append($(resp));
         $('#promos').append('<a href="#" class="control prev">&laquo;</a>\
