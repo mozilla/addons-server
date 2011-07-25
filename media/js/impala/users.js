b0dc61b667976b1c5deac1f63a5c475c760aaeff
Gregory Koberger <gkoberger@gmail.com>
2011-07-25 13:08:37 -0700

    Reorder items on user edit page (bug 674018)

--- media/js/impala/users.js
+++ media/js/impala/users.js
@@ -5,4 +5,12 @@ $(function() {
             $this.closest('li').find('input:not([disabled]').attr('checked', $this.hasClass('more-all'));
         }));
     }
+
+    // Hide change password box
+    $('#acct-password').hide();
+    $('#change-acct-password').click(_pd(function() {
+          $('#acct-password').show();
+          $('#id_oldpassword').focus();
+          $(this).closest('li').hide();
+    }));
 });
