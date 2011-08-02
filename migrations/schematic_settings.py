8bc240ec5ce2fdd162deef78cb5a67bc15a5151d
Jeff Balogh <jbalogh@mozilla.com>
2011-08-02 14:11:38 -0700

    use python2.6 for schematic

--- migrations/schematic_settings.py
+++ migrations/schematic_settings.py
@@ -28,4 +28,4 @@ else:
 
 db = s.format(**config)
 table = 'schema_version'
-handlers = {'.py': 'python -B manage.py runscript migrations.%s'}
+handlers = {'.py': 'python2.6 -B manage.py runscript migrations.%s'}
