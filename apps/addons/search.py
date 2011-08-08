5b9a5709c3489eb58a31d856ee2d747294a80e8b
Jeff Balogh <jbalogh@mozilla.com>
2011-08-08 16:11:44 -0700

    Revert "boost even harder" -- the boost was too hard!
    This reverts commit b205e92952ba5a0fe177316d3e3969cd70242f0a.

--- apps/addons/search.py
+++ apps/addons/search.py
@@ -41,8 +41,9 @@ def extract(addon):
                            for app, appver in addon.compatible_apps.items()
                            if appver)
     d['app'] = [app.id for app in addon.compatible_apps.keys()]
-    # Boost so that search rank is strongly correlated to add-on popularity.
-    d['_boost'] = addon.average_daily_users * .1
+    # Boost by the number of users on a logarithmic scale. The maximum boost
+    # (11,000,000 users for adblock) is about 5x.
+    d['_boost'] = addon.average_daily_users ** .2
     # Double the boost if the add-on is public.
     if addon.status == amo.STATUS_PUBLIC:
         d['_boost'] = max(d['_boost'], 1) * 4
