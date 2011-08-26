b41fe18d19a45eb9447f9d9519e1d2dfdfeedf19
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-08-26 17:04:40 -0500

    Sets min/max appversion so validator only checks compat w/ target (bug 682421)

--- apps/zadmin/tests/test_views.py
+++ apps/zadmin/tests/test_views.py
@@ -734,26 +734,13 @@ class TestBulkValidationTask(BulkValidationTest):
             data['notices'] + data['compatibility_summary']['notices'])
 
     @mock.patch('validator.validate.validate')
-    def test_max_version_override(self, validate):
+    def test_app_version_overrides(self, validate):
         validate.return_value = json.dumps(no_op_validation)
         self.start_validation(new_max='3.7a4')
         assert validate.called
         overrides = validate.call_args[1]['overrides']
+        eq_(overrides['targetapp_minVersion'], {amo.FIREFOX.guid: '3.7a4'})
         eq_(overrides['targetapp_maxVersion'], {amo.FIREFOX.guid: '3.7a4'})
-        minver = self.version.apps.filter(
-                                    application=amo.FIREFOX.id)[0].max.version
-        eq_(overrides['targetapp_minVersion'], {amo.FIREFOX.guid: minver})
-
-    @mock.patch('validator.validate.validate')
-    def test_no_min_appversion(self, validate):
-        validate.return_value = json.dumps(no_op_validation)
-        self.version.apps.all().delete()
-        job = self.create_job(target_version=self.appversion('3.7a4'))
-        res = self.create_result(job, self.create_file(), **{})
-        tasks.bulk_validate_file(res.id)
-        assert validate.called
-        eq_(validate.call_args[1]['overrides'],
-            {'targetapp_maxVersion': {amo.FIREFOX.guid: '3.7a4'}})
 
     def create_version(self, addon, statuses, version_str=None):
         max = self.max
