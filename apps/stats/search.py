27c73ab4ef6ce3ce25276282bee7d9cd059a669f
Jeff Balogh <jbalogh@mozilla.com>
2011-09-01 15:23:01 -0700

    watch out for bad data

--- apps/stats/search.py
+++ apps/stats/search.py
@@ -20,49 +20,58 @@ def es_dict(items):
  'os': [{'k': amo.PLATFORM.name, 'v': count}]
  'locales': [{'k': locale, 'v': count}  # (all locales lower case)
  'apps': {amo.APP.guid: [{'k': app version, 'v': count}}]
+ 'status': [{'k': status, 'v': count}
 """
 def extract_update_count(update, all_apps=None):
     doc = {'addon': update.addon_id,
-           'id': update.id,
            'date': update.date,
            'count': update.count,
-           'versions': es_dict(update.versions)}
+           'id': update.id,
+           'versions': es_dict(update.versions),
+           'os': [],
+           'locales': [],
+           'apps': [],
+           'status': []}
 
     # Only count platforms we know about.
-    os = collections.defaultdict(int)
-    for key, count in update.oses.items():
-        if key.lower() in amo.PLATFORM_DICT:
-            os[amo.PLATFORM_DICT[key.lower()].name] += count
-            doc['os'] = es_dict((unicode(k), v) for k, v in os.items())
+    if update.oses:
+        os = collections.defaultdict(int)
+        for key, count in update.oses.items():
+            if key.lower() in amo.PLATFORM_DICT:
+                os[amo.PLATFORM_DICT[key.lower()].name] += count
+                doc['os'] = es_dict((unicode(k), v) for k, v in os.items())
 
     # Case-normalize locales.
-    locales = collections.defaultdict(int)
-    for locale, count in update.locales.items():
-        try:
-            locales[locale.lower()] += int(count)
-        except ValueError:
-            pass
-    doc['locales'] = es_dict(locales)
+    if update.locales:
+        locales = collections.defaultdict(int)
+        for locale, count in update.locales.items():
+            try:
+                locales[locale.lower()] += int(count)
+            except ValueError:
+                pass
+        doc['locales'] = es_dict(locales)
 
     # Only count app/version combos we know about.
-    if all_apps is None:
-        all_apps = get_all_app_versions()
-    apps = collections.defaultdict(dict)
-    for guid, version_counts in update.applications.items():
-        if guid not in amo.APP_GUIDS:
-            continue
-        app = amo.APP_GUIDS[guid]
-        app_versions = all_apps[app.id]
-        for version, count in version_counts.items():
-            if version in app_versions:
-                try:
-                    apps[app.guid][version] = int(count)
-                except ValueError:
-                    pass
-    doc['apps'] = dict((app, es_dict(vals)) for app, vals in apps.items())
+    if update.applications:
+        if all_apps is None:
+            all_apps = get_all_app_versions()
+        apps = collections.defaultdict(dict)
+        for guid, version_counts in update.applications.items():
+            if guid not in amo.APP_GUIDS:
+                continue
+            app = amo.APP_GUIDS[guid]
+            app_versions = all_apps[app.id]
+            for version, count in version_counts.items():
+                if version in app_versions:
+                    try:
+                        apps[app.guid][version] = int(count)
+                    except ValueError:
+                        pass
+        doc['apps'] = dict((app, es_dict(vals)) for app, vals in apps.items())
 
-    doc['status'] = es_dict((k, v) for k, v in update.statuses.items()
-                            if k != 'null')
+    if update.statuses:
+        doc['status'] = es_dict((k, v) for k, v in update.statuses.items()
+                                if k != 'null')
     return doc
 
 
