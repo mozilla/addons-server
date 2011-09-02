17f28f374cec2bcfa5dabe031bd6b96d59e366d7
Chris Van <cwiemeersch@mozilla.com>
2011-09-02 11:12:27 -0700

    impala_extension_detail is no more

--- apps/webapps/views.py
+++ apps/webapps/views.py
@@ -8,4 +8,4 @@ from .models import Webapp
 def app_detail(request, app_slug):
     # TODO: check status.
     webapp = get_object_or_404(Webapp, app_slug=app_slug)
-    return addons.views.impala_extension_detail(request, webapp)
+    return addons.views.extension_detail(request, webapp)
