7a0201f783f64bc2749beba700d20d2224c8eb66
Andy McKay <amckay@mozilla.com>
2011-09-02 09:49:12 -0700

    simple cases for updating addonpurchase table (bug 682068)

--- apps/market/models.py
+++ apps/market/models.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.dispatch import receiver
 
 from translations.fields import TranslatedField
 
 from addons.models import Addon
 import amo
 import amo.models
+from stats.models import Contribution
 from users.models import UserProfile
 
 import commonware.log
@@ -22,7 +24,6 @@ class PriceManager(amo.models.ManagerBase):
 
 
 class Price(amo.models.ModelBase):
-
     active = models.BooleanField(default=True)
     name = TranslatedField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
@@ -60,3 +61,35 @@ class AddonPurchase(amo.models.ModelBase):
 
     def __unicode__(self):
         return u'%s: %s' % (self.addon, self.user)
+
+
+@receiver(models.signals.post_save, sender=Contribution,
+          dispatch_uid='create_addon_purchase')
+def create_addon_purchase(sender, instance, **kw):
+    """
+    When the contribution table is updated with the data from PayPal,
+    update the addon purchase table. Will figure out if we need to add to or
+    delete from the AddonPurchase table.
+    """
+    if (kw.get('raw') or
+        instance.type not in [amo.CONTRIB_PURCHASE, amo.CONTRIB_REFUND,
+                              amo.CONTRIB_CHARGEBACK]):
+        # Whitelist the types we care about. Forget about the rest.
+        return
+
+    log.debug('Processing addon purchase type: %s, addon %s, user %s'
+              % (amo.CONTRIB_TYPES[instance.type], instance.addon.pk,
+                 instance.user.pk))
+
+    if instance.type == amo.CONTRIB_PURCHASE:
+        log.debug('Creating addon purchase: addon %s, user %s'
+                  % (instance.addon.pk, instance.user.pk))
+        AddonPurchase.objects.create(addon=instance.addon, user=instance.user)
+
+    elif instance.type in [amo.CONTRIB_REFUND, amo.CONTRIB_CHARGEBACK]:
+        purchases = AddonPurchase.objects.filter(addon=instance.addon,
+                                                 user=instance.user)
+        for p in purchases:
+            log.debug('Deleting addon purchase: %s, addon %s, user %s'
+                      % (p.pk, instance.addon.pk, instance.user.pk))
+            p.delete()
--- apps/market/tests.py
+++ apps/market/tests.py
@@ -7,6 +7,7 @@ import amo.tests
 from amo.urlresolvers import reverse
 from addons.models import Addon
 from market.models import Price
+from stats.models import Contribution
 from users.models import UserProfile
 
 
@@ -61,3 +62,58 @@ class TestAddonPurchase(amo.tests.TestCase):
         res = self.client.get(self.url)
         eq_(res.status_code, 200)
         eq_(json.loads(res.content)['status'], 'invalid')
+
+
+class TestContribution(amo.tests.TestCase):
+    fixtures = ['base/addon_3615', 'base/users']
+
+    def setUp(self):
+        self.addon = Addon.objects.get(pk=3615)
+        self.user = UserProfile.objects.get(pk=999)
+
+    def create(self, type):
+        Contribution.objects.create(type=type, addon=self.addon,
+                                    user=self.user)
+
+    def purchased(self):
+        return self.addon.addonpurchase_set.filter(user=self.user).exists()
+
+    def test_purchase(self):
+        self.create(amo.CONTRIB_PURCHASE)
+        assert self.purchased()
+
+    def test_refund(self):
+        self.create(amo.CONTRIB_REFUND)
+        assert not self.purchased()
+
+    def test_purchase_and_refund(self):
+        self.create(amo.CONTRIB_PURCHASE)
+        self.create(amo.CONTRIB_REFUND)
+        assert not self.purchased()
+
+    def test_refund_and_purchase(self):
+        # This refund does nothing, there was nothing there to refund.
+        self.create(amo.CONTRIB_REFUND)
+        self.create(amo.CONTRIB_PURCHASE)
+        assert self.purchased()
+
+    def test_really_cant_decide(self):
+        self.create(amo.CONTRIB_PURCHASE)
+        self.create(amo.CONTRIB_REFUND)
+        self.create(amo.CONTRIB_PURCHASE)
+        self.create(amo.CONTRIB_REFUND)
+        self.create(amo.CONTRIB_PURCHASE)
+        assert self.purchased()
+
+    def test_purchase_and_chargeback(self):
+        self.create(amo.CONTRIB_PURCHASE)
+        self.create(amo.CONTRIB_CHARGEBACK)
+        assert not self.purchased()
+
+    def test_other_user(self):
+        other = UserProfile.objects.get(email='admin@mozilla.com')
+        Contribution.objects.create(type=amo.CONTRIB_PURCHASE,
+                                    addon=self.addon, user=other)
+        self.create(amo.CONTRIB_PURCHASE)
+        self.create(amo.CONTRIB_REFUND)
+        eq_(self.addon.addonpurchase_set.filter(user=other).count(), 1)
