f7918bc2030be197a950a3d0d9df69fcc6150fd5
Jeff Balogh <jbalogh@mozilla.com>
2011-09-08 18:36:05 -0700

    don't let watch-celery fail (bug 682878)

--- apps/amo/management/commands/watch_celery.py
+++ apps/amo/management/commands/watch_celery.py
@@ -1,3 +1,4 @@
+import logging
 import time
 
 from django.core.management.base import BaseCommand
@@ -8,17 +9,23 @@ from graphite import graphite
 from amo.tasks import task_stats
 
 
+log = logging.getLogger('z.watch-celery')
+
+
 class Command(BaseCommand):
     help = "Subscribe to celery events and publish to graphite."
 
     def handle(self, *args, **kw):
         redis = redisutils.connections['master']
         while 1:
-            stats = []
-            d = zip(['pending', 'failed', 'total'], task_stats.stats())
-            for key, dict_ in d:
-                for name, value in dict_.items():
-                    stats.append(('celery.tasks.%s.%s' % (key, name), value))
-            graphite.sendall(*stats)
+            try:
+                stats = []
+                d = zip(['pending', 'failed', 'total'], task_stats.stats())
+                for key, dict_ in d:
+                    for name, value in dict_.items():
+                        stats.append(('celery.tasks.%s.%s' % (key, name), value))
+                graphite.sendall(*stats)
 
+            except Exception, e:
+                log.error(e)
             time.sleep(2)
