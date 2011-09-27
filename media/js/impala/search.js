716a4dc6a38b20edbd54af72e66ee707d61ff25b
Chris Van <cwiemeersch@mozilla.com>
2011-09-26 18:59:21 -0700

    detect OS/browser version for search results (bug 688344)

--- media/js/impala/search.js
+++ media/js/impala/search.js
@@ -1,6 +1,5 @@
 $(function() {
-    var $facets = $('#search-facets');
-    $facets.delegate('li.facet', 'click', function() {
+    $('#search-facets').delegate('li.facet', 'click', function() {
         var $this = $(this);
         if ($this.hasClass('active')) {
             $this.removeClass('active');
@@ -8,8 +7,7 @@ $(function() {
             $this.closest('ul').find('.active').removeClass('active');
             $this.addClass('active');
         }
-    });
-    $facets.delegate('li.facet a', 'click', function(e) {
+    }).delegate('li.facet a', 'click', function(e) {
         e.stopPropagation();
     });
 });
