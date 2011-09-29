0d63fb7ec472b183492c0bb252b14231f894b3c7
Chris Van <cwiemeersch@mozilla.com>
2011-09-28 19:57:39 -0700

    kill IMPALA_EDIT flag

--- apps/amo/utils.py
+++ apps/amo/utils.py
@@ -166,7 +166,7 @@ def send_mail(subject, message, from_email=None, recipient_list=None,
 
     try:
         if white_list:
-            if settings.IMPALA_EDIT and perm_setting:
+            if perm_setting:
                 template = loader.get_template('amo/emails/unsubscribe.ltxt')
                 for recipient in white_list:
                     # Add unsubscribe link to footer
