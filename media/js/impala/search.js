8f382c0ecfe1044586d835d5b33746f9cc4ad731
Chris Van <cvan@mozilla.com>
2011-10-21 17:08:52 -0700

    use more events with ajax search results

--- media/js/impala/search.js
+++ media/js/impala/search.js
@@ -14,6 +14,12 @@ $(function() {
             $this.closest('ul').find('.active').removeClass('active');
             $this.addClass('active');
         }
+    }).delegate('.cnt', 'recount', function(e, newCount) {
+        // Update # of results on sidebar.
+        var $this = $(this);
+        if ($this.html() != newCount.html()) {
+            $this.replaceWith(newCount);
+        }
     });
 
     if ($('body').hasClass('pjax') && $.support.pjax) {
@@ -33,11 +39,6 @@ function initSearchPjax(container) {
                 url: url,
                 container: container,
                 timeout: 1500,
-                beforeSend: function(xhr) {
-                    this.trigger('begin.pjax', [xhr, url, container]);
-                    xhr.setRequestHeader('X-PJAX', 'true');
-                    loading();
-                },
                 error: function(xhr, textStatus, errorThrown) {
                     if (textStatus === 'timeout' && timeouts < 5) {
                         // Retry up to five times.
@@ -48,22 +49,13 @@ function initSearchPjax(container) {
                         // Upon `error` or `parsererror`.
                         window.location = url;
                     }
-                },
-                complete: function(xhr) {
-                    this.trigger('end.pjax', [xhr, url, container]);
-                    finished();
                 }
             });
         }
     }
 
     function hijackLink() {
-        var $this = $(this);
-        // Supress clicks on the currently selected sort filter.
-        if (!($this.parent('li.selected').length &&
-              $this.closest('#sorter').length)) {
-            pjaxOpen($(this).attr('href'));
-        }
+        pjaxOpen($(this).attr('href'));
     }
 
     function loading() {
@@ -95,21 +87,8 @@ function initSearchPjax(container) {
         // Remove the loading indicator.
         $wrapper.removeClass('loading').find('.updating').remove();
 
-        // Update # of results on sidebar.
-        var oldCount = $('#search-facets .cnt').attr('data-count'),
-            numItems = $wrapper.find('.item').length.toString(10),
-            newCount = ($wrapper.find('b.cnt').attr('data-count') || numItems),
-            newCountFmt = ($wrapper.find('b.cnt').text() || numItems);
-        if (oldCount != newCount) {
-            // Update old numeric count.
-            $('#search-facets .cnt').attr('data-count', newCount);
-
-            // Update old formatted count.
-            var msg = ngettext('<b>{0}</b> matching result',
-                               '<b>{0}</b> matching results',
-                               parseInt(newCount, 10));
-            $('#search-facets .cnt').html(format(msg, [newCountFmt]));
-        }
+        // Update the # of matching results on sidebar.
+        $('#search-facets .cnt').trigger('recount', [$wrapper.find('.cnt')]);
 
         // Scroll up.
         $('html, body').animate({scrollTop: 0}, 200);
@@ -132,5 +111,6 @@ function initSearchPjax(container) {
     }
 
     $('.pjax-trigger a').live('click', _pd(hijackLink));
+    $container.bind('start.pjax', loading).bind('end.pjax', finished);
     $(document).keyup(_.throttle(turnPages, 300));
 }
