4628e68d5603b0e34c72e01a484d60a4e13cd518
Gregory Koberger <gkoberger@gmail.com>
2011-10-26 15:54:23 -0700

    Undo slug thing andy did

--- apps/addons/decorators.py
+++ apps/addons/decorators.py
@@ -26,16 +26,7 @@ def addon_view(f, qs=Addon.objects.all):
         elif addon_id:
             addon = get(slug=addon_id)
         elif app_slug:
-            # XXXXX
-            # TODO(andym): this is temporary. Apps do not have a current
-            # version. Aaaaargh!
-            try:
-                addon = get(app_slug=app_slug)
-            except http.Http404:
-                addon = get_object_or_404(Addon, type=amo.ADDON_WEBAPP,
-                                          app_slug=app_slug,
-                                          disabled_by_user=False,
-                                          status__in=amo.VALID_STATUSES)
+            addon = get(app_slug=app_slug)
         return f(request, addon, *args, **kw)
     return wrapper
 
