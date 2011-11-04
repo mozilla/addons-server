7e9955e85c725fc0582e885dddb8e9043b4cdbfe
Matt Claypotch <thepotch@gmail.com>
2011-11-03 18:01:16 -0700

    don't replaceState when it is not available (bug 699640)

--- media/js/impala/stats/stats.js
+++ media/js/impala/stats/stats.js
@@ -3,22 +3,25 @@
     $(function() {
         "use strict";
 
-        $(window).bind("changeview", function(e, view) {
-            var queryParams = {},
-                range = view.range;
-            if (range) {
-                if (typeof range == "string") {
-                    queryParams.last = range.split(/\s+/)[0];
-                } else if (typeof range == "object") {
-                    // queryParams.start = z.date.date_string(new Date(range.start), '');
-                    // queryParams.end = z.date.date_string(new Date(range.end), '');
+        // Modify the URL when the page state changes, if the browser supports pushSate.
+        if (z.capabilities.replaceState) {
+            $(window).bind("changeview", function(e, view) {
+                var queryParams = {},
+                    range = view.range;
+                if (range) {
+                    if (typeof range == "string") {
+                        queryParams.last = range.split(/\s+/)[0];
+                    } else if (typeof range == "object") {
+                        // queryParams.start = z.date.date_string(new Date(range.start), '');
+                        // queryParams.end = z.date.date_string(new Date(range.end), '');
+                    }
                 }
-            }
-            queryParams = $.param(queryParams);
-            if (queryParams) {
-                history.replaceState(view, document.title, '?' + queryParams);
-            }
-        });
+                queryParams = $.param(queryParams);
+                if (queryParams) {
+                    history.replaceState(view, document.title, '?' + queryParams);
+                }
+            });
+        }
 
         // Set up initial default view.
         var initView = {
