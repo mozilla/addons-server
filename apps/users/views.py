116addc87bb75f6ee4ee2cf966835482227b5ff8
Chris Van <cvan@mozilla.com>
2011-11-21 01:12:11 -0500

    show apps template for mamo login

--- apps/users/views.py
+++ apps/users/views.py
@@ -343,6 +343,7 @@ def login(request, template=None):
 
 def _login(request, template=None, data=None, dont_redirect=False):
     data = data or {}
+    data.update(webapp=settings.APP_PREVIEW)
     # In case we need it later.  See below.
     get_copy = request.GET.copy()
 
