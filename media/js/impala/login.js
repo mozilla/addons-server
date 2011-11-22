8f2b58ba4122e076213d38950969bbf24094987b
Gregory Koberger <gkoberger@gmail.com>
2011-11-21 16:00:32 -0800

    First impressions matter, people.

--- media/js/impala/login.js
+++ media/js/impala/login.js
@@ -1,9 +1,11 @@
 // We don't want to wait for ready.
 if ($('.login .browserid').length) {
     $('#show-normal-login').click(_pd(function(e) {
+        $('#browserid-login').hide();
         $('.user-message').hide();
         $('#normal-login').show();
         $('#id_username').focus();
+        $(window).trigger('resize');
     }));
 }
 
