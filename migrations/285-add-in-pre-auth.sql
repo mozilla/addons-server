b5158482c03df2fdaa56ef7acaf5a1634db73661
Andy McKay <amckay@mozilla.com>
2011-12-09 17:01:42 -0800

    don't forget the unsigned

--- migrations/285-add-in-pre-auth.sql
+++ migrations/285-add-in-pre-auth.sql
@@ -3,10 +3,10 @@ INSERT INTO `waffle_flag`
     ('allow-pre-auth',0,NULL,0,0,0,0, 'Allow pre-auth of paypal payments');
 
 CREATE TABLE `users_preapproval` (
-    `id` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `id` int(11) unsigned AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `created` datetime NOT NULL,
     `modified` datetime NOT NULL,
-    `user_id` int(11) NOT NULL UNIQUE,
+    `user_id` int(11) unsigned NOT NULL UNIQUE,
     `paypal_key` varchar(255),
     `paypal_expiry` date
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
