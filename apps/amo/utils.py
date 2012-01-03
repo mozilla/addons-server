b7d273b034f0f4a985ac512ca065add1dbc4af72
Gregory Koberger <gkoberger@gmail.com>
2012-01-03 12:33:07 -0500

    Remove unsubscribe instructions

--- apps/amo/utils.py
+++ apps/amo/utils.py
@@ -197,7 +197,6 @@ def send_mail(subject, message, from_email=None, recipient_list=None,
                                               headers=headers or {},
                                               ).send(fail_silently=False)
             else:
-                log.info('Email w/o perm setting: %s' % subject)
                 result = EmailMessage(subject, message, from_email,
                                       white_list,
                                       connection=connection,
