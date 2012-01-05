63dba70b07bcafb31bd26bb1dcfa3e746d1d8740
Chris Van <cvan@mozilla.com>
2012-01-05 11:03:47 -0800

    Revert "allow sphinx to import settings_local"
    This reverts commit 816462652377cd65da43be8fdef92bc79e9a0338.

--- apps/search/tests/__init__.py
+++ apps/search/tests/__init__.py
@@ -6,7 +6,7 @@ from nose import SkipTest
 from test_utils import FastFixtureTestCase
 
 import amo.tests
-import settings_local as settings
+from manage import settings
 from search.utils import start_sphinx, stop_sphinx, reindex
 
 
