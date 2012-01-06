f21480dfcfd0b100078c9f0897b4f1c12606054a
Andy McKay <amckay@mozilla.com>
2012-01-06 09:31:27 -0800

    alter abuse destination and include type in subject (bug 715712)

--- settings.py
+++ settings.py
@@ -54,6 +54,7 @@ FLIGTAR = 'amo-admins@mozilla.org'
 EDITORS_EMAIL = 'amo-editors@mozilla.org'
 SENIOR_EDITORS_EMAIL = 'amo-admin-reviews@mozilla.org'
 MARKETPLACE_EMAIL = 'amo-marketplace@mozilla.org'
+ABUSE_EMAIL = 'marketplace-abuse@mozilla.org'
 NOBODY_EMAIL = 'nobody@mozilla.org'
 
 DATABASES = {
