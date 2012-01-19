1fa15c01ecc2bea9c0f4448a720f60727f5dac6a
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-01-19 15:08:48 -0600

    Fixes source of settings_local.py

--- vagrant/manifests/classes/zamboni.pp
+++ vagrant/manifests/classes/zamboni.pp
@@ -9,7 +9,7 @@ class zamboni {
 
     file { "$PROJ_DIR/settings_local.py":
         ensure => file,
-        source => "$PROJ_DIR/settings_local.py",
+        source => "$PROJ_DIR/docs/settings/settings_local.dev.py",
         replace => false;
     }
 
