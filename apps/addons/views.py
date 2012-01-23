7fcd00f3cd29f32d18003d413fa9376272568e45
Andy McKay <amckay@mozilla.com>
2012-01-23 10:59:51 -0800

    mark exempt (bug 720406)

--- apps/addons/views.py
+++ apps/addons/views.py
@@ -37,7 +37,7 @@ from bandwagon.models import Collection, CollectionFeature, CollectionPromo
 import paypal
 from reviews.forms import ReviewForm
 from reviews.models import Review, GroupedRating
-from session_csrf import anonymous_csrf
+from session_csrf import anonymous_csrf, anonymous_csrf_exempt
 from sharing.views import share as share_redirect
 from stats.models import Contribution
 from translations.query import order_by_translation
@@ -661,6 +661,7 @@ def purchase_error(request, addon):
 
 
 @addon_view
+@anonymous_csrf_exempt
 @post_required
 def contribute(request, addon):
     webapp = addon.is_webapp()
