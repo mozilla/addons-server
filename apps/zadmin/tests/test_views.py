ab5358d6b5648fa019d01c5d572356a5e51f518c
Allen Short <ashort@mozilla.com>
2012-01-30 21:25:13 -0600

    update test to account for new user in fixture

--- apps/zadmin/tests/test_views.py
+++ apps/zadmin/tests/test_views.py
@@ -1334,7 +1334,7 @@ class TestLookup(amo.tests.TestCase):
         eq_(self.client.get('%s?q=admin' % self.url).status_code, 403)
 
     def test_search(self):
-        for q, c in [('', 3), ('admin@mozilla.com', 1)]:
+        for q, c in [('', 4), ('admin@mozilla.com', 1)]:
             res = self.client.get('%s?q=%s' % (self.url, q))
             eq_(res.status_code, 200)
             content = json.loads(res.content)
