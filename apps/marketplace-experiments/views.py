f11c0e3051f99831a9a5dd12b1ed58b5bd22a1b2
Matt Claypotch <thepotch@gmail.com>
2012-02-03 16:42:44 -0800

    marketplace expeiments wip part 2, now with files!

---
+++ apps/marketplace-experiments/views.py
@@ -0,0 +1,24 @@
+import jingo
+import jinja2
+
+from addons.models import Addon
+from addons.decorators import (addon_view_factory, can_be_purchased,
+                               has_purchased, has_not_purchased)
+
+from reviews.forms import ReviewForm
+from reviews.models import Review, GroupedRating
+
+
+addon_view = addon_view_factory(qs=Addon.objects.valid)
+
+@addon_view
+def detail(request, addon):
+    """Product details page."""
+
+    ctx = {
+        'addon': addon,
+        'review_form': ReviewForm(),
+        'reviews': Review.objects.latest().filter(addon=addon),
+    }
+
+    return jingo.render(request, 'marketplace-experiments/detail.html', ctx)
