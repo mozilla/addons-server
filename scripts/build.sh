d27a9f39b325be5a0b46823bf7b5c698b9b71c33
Andy McKay <amckay@mozilla.com>
2012-02-13 16:01:43 -0800

    allow build runner to alter settings file

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ cd $WORKSPACE
 VENV=$WORKSPACE/venv
 VENDOR=$WORKSPACE/vendor
 LOCALE=$WORKSPACE/locale
+SETTINGS=default
 
 echo "Starting build on executor $EXECUTOR_NUMBER..." `date`
 
@@ -12,6 +13,13 @@ if [ -z $1 ]; then
     echo "Warning: You should provide a unique name for this job to prevent database collisions."
     echo "Usage: ./build.sh <name>"
     echo "Continuing, but don't say you weren't warned."
+fi
+
+if [ -z $2 ]; then
+    echo "Warning: no settings file specified, using: default"
+    echo "Usage: ./build.sh <name> <settings>"
+else
+    SETTINGS=$2
 fi
 
 echo "Setup..." `date`
@@ -50,6 +58,7 @@ git submodule --quiet sync && git submodule update --init --recursive
 
 cat > settings_local.py <<SETTINGS
 from lib.settings_base import *
+from ${SETTINGS}.settings import *
 ROOT_URLCONF = 'lib.urls_base'
 LOG_LEVEL = logging.ERROR
 # Database name has to be set because of sphinx
@@ -77,7 +86,7 @@ echo "Starting tests..." `date`
 export FORCE_DB='yes sir'
 
 # with-coverage excludes sphinx so it doesn't conflict with real builds.
-if [[ $2 = 'with-coverage' ]]; then
+if [[ $3 = 'with-coverage' ]]; then
     coverage run manage.py test -v 2 --noinput --logging-clear-handlers --with-xunit -a'!sphinx'
     coverage xml $(find apps lib -name '*.py')
 else
