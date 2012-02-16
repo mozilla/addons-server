6d9c79157e599a5c1ff1660be4fef216d74904c0
Andy McKay <amckay@mozilla.com>
2012-02-16 12:14:19 -0800

    basic event log (bug 674642)

--- apps/editors/admin.py
+++ apps/editors/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CannedResponse
+from .models import CannedResponse, EventLog
 
 
 class CannedResponseAdmin(admin.ModelAdmin):
@@ -8,3 +8,23 @@ class CannedResponseAdmin(admin.ModelAdmin):
 
 
 admin.site.register(CannedResponse, CannedResponseAdmin)
+
+
+class EventLogAdmin(admin.ModelAdmin):
+    list_display = ('created', 'type', 'action', 'field', 'user',
+                    'changed_id', 'added', 'removed', 'notes')
+    list_filter = ('type', 'action')
+    readonly_fields = list_display
+    date_hierarchy = 'created'
+    raw_id_fields = ('user',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_edit_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(EventLog, EventLogAdmin)
