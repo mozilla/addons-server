25ef3fd14d74ef15c6f64bc6269ccecb6e2f24a9
Chris Van <cvan@mozilla.com>
2012-02-16 18:22:44 -0800

    fork stylesheets for schmevhub

--- mkt/settings.py
+++ mkt/settings.py
@@ -80,6 +80,70 @@ NO_LOGIN_REQUIRED_MODULES = (
 
 # Extend the bundles.
 MINIFY_BUNDLES['css'].update({
+    'devreg': (
+    ),
+    'devreg-legacy': (
+        'css/devreg-legacy/main.css',
+        'css/devreg-legacy/main-mozilla.css',
+        'css/devreg-legacy/headerfooter.css',  # Potch, Remove.
+        'css/devreg-legacy/zamboni.css',
+        'css/devreg-legacy/formset.less',
+        'css/devreg-legacy/header.less',
+        'css/devreg-legacy/moz-tab.css',
+        'css/devreg-legacy/footer.less',
+        'css/devreg-legacy/faux-zamboni.less',
+
+        # Developer Hub-specific styles.
+        'css/devreg-legacy/tooltips.less',
+        'css/devreg-legacy/developers.css',
+        'css/devreg-legacy/docs.less',
+        'css/devreg-legacy/developers.less',
+        'css/devreg-legacy/devhub-listing.less',
+        'css/devreg-legacy/devhub-popups.less',
+        'css/devreg-legacy/formset.less',
+        'css/devreg-legacy/devhub-forms.less',
+        'css/devreg-legacy/submission.less',
+        'css/devreg-legacy/refunds.less',
+        'css/devreg-legacy/devhub-buttons.less',
+        'css/devreg-legacy/in-app-config.less',
+    ),
+    'devreg-impala': (
+        'css/devreg-impala/base.css',
+        'css/devreg-impala/site.less',
+        'css/devreg-impala/typography.less',
+        'css/devreg-impala/headerfooter.css',  # Potch, Remove.
+        'css/devreg-impala/forms.less',
+        'css/devreg-impala/header.less',
+        'css/devreg-impala/footer.less',
+        'css/devreg-impala/moz-tab.css',
+        'css/devreg-impala/reviews.less',
+        'css/devreg-impala/buttons.less',
+        'css/devreg-impala/addon_details.less',
+        'css/devreg-impala/policy.less',
+        'css/devreg-impala/expando.less',
+        'css/devreg-impala/popups.less',
+        'css/devreg-impala/l10n.less',
+        'css/devreg-impala/contributions.less',
+        'css/devreg-impala/prose.less',
+        'css/devreg-impala/paginator.less',
+        'css/devreg-impala/listing.less',
+        'css/devreg-impala/versions.less',
+        'css/devreg-impala/users.less',
+        'css/devreg-impala/tooltips.less',
+        'css/devreg-impala/login.less',
+        'css/devreg-impala/apps.less',
+        'css/devreg-impala/formset.less',
+        'css/devreg-impala/tables.less',
+
+        # Developer Hub-specific styles.
+        'css/devreg-impala/developers.less',
+        'css/devreg-impala/devhub-listing.less',
+        'css/devreg-impala/devhub-popups.less',
+        'css/devreg-impala/dashboard.less',
+        'css/devreg-impala/devhub-forms.less',
+        'css/devreg-impala/submission.less',
+        'css/devreg-impala/refunds.less',
+    ),
     'hub': (
         'css/impala/base.css',
         'css/hub/base.less',
@@ -98,6 +162,60 @@ MINIFY_BUNDLES['css'].update({
     ),
 })
 MINIFY_BUNDLES['js'].update({
+    'devreg-legacy': (
+        'js/lib/jquery-1.6.4.js',
+        'js/lib/underscore.js',
+        'js/zamboni/browser.js',
+        'js/amo2009/addons.js',
+        'js/zamboni/init.js',
+        'js/impala/capabilities.js',
+        'js/zamboni/format.js',
+        'js/lib/jquery.cookie.js',
+        'js/zamboni/storage.js',
+        'js/zamboni/apps.js',
+        'js/zamboni/buttons.js',
+        'js/zamboni/tabs.js',
+
+        # jQuery UI.
+        'js/lib/jquery-ui/jquery.ui.core.js',
+        'js/lib/jquery-ui/jquery.ui.position.js',
+        'js/lib/jquery-ui/jquery.ui.widget.js',
+        'js/lib/jquery-ui/jquery.ui.mouse.js',
+        'js/lib/jquery-ui/jquery.ui.autocomplete.js',
+        'js/lib/jquery-ui/jquery.ui.datepicker.js',
+        'js/lib/jquery-ui/jquery.ui.sortable.js',
+
+        'js/zamboni/truncation.js',
+        'js/zamboni/helpers.js',
+        'js/zamboni/global.js',
+        'js/impala/ratingwidget.js',
+        'js/lib/jquery-ui/jqModal.js',
+        'js/zamboni/l10n.js',
+        'js/zamboni/debouncer.js',
+
+        # Users.
+        'js/zamboni/users.js',
+
+        # Forms.
+        'js/impala/forms.js',
+
+        # Login.
+        'js/impala/login.js',
+
+        # Fix-up outgoing links.
+        'js/zamboni/outgoing_links.js',
+
+        # Stick.
+        'js/lib/stick.js',
+
+        # Developer Hub-specific scripts.
+        'js/zamboni/truncation.js',
+        'js/zamboni/upload.js',
+        'js/impala/formset.js',
+        'js/zamboni/devhub.js',
+        'js/zamboni/validator.js',
+        'js/zamboni/packager.js',
+    ),
     'hub': (
         'js/lib/underscore.js',
         'js/marketplace-experiments/jquery-1.7.1.min.js',
