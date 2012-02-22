4c6c4b24dfeb20f6a0157ce6cc57fdf1b34905e7
Rob Hudson <chudson@mozilla.com>
2012-02-22 14:53:32 -0800

    Added terms of use (bug 729059)

--- mkt/site/urls.py
+++ mkt/site/urls.py
@@ -6,4 +6,6 @@ from jingo.views import direct_to_template
 urlpatterns = patterns('',
     url('^privacy-policy$', direct_to_template,
         {'template': 'site/privacy-policy.html'}, name='site.privacy'),
+    url('^terms-of-use$', direct_to_template,
+        {'template': 'site/terms-of-use.html'}, name='site.terms'),
 )
