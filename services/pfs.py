9fa8a45090720f5b4d6b660aeccfb2ffd621056f
Wil Clouser <wclouser@mozilla.com>
2012-02-22 14:43:00 -0800

    classier if we don't have a newline at the start!

--- services/pfs.py
+++ services/pfs.py
@@ -16,7 +16,7 @@ log_configure()
 
 error_log = commonware.log.getLogger('z.pfs')
 
-xml_template = """
+xml_template = """\
 <?xml version="1.0"?>
 <RDF:RDF xmlns:RDF="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:pfs="http://www.mozilla.org/2004/pfs-rdf#">
 
