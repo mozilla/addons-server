e25808f1591df49039cbdacd91e0215927728ff6
Andy McKay <amckay@mozilla.com>
2012-02-23 09:51:06 -0800

    text cleaning and html tip

--- mkt/site/forms.py
+++ mkt/site/forms.py
@@ -5,7 +5,7 @@ from tower import ugettext as _
 
 APP_UPSELL_CHOICES = (
     (0, _("I don't have a free app to associate.")),
-    (1, _('This is a premium upgrade to:')),
+    (1, _('This is a premium upgrade.')),
 )
 
 
