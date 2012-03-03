16cd1908b1dd4dd2c10945c33a0c3f2777643e5c
Matt Claypotch <thepotch@gmail.com>
2012-03-03 00:38:17 -0800

    heh

--- apps/stats/urls.py
+++ apps/stats/urls.py
@@ -10,7 +10,7 @@ series_re = '%s-%s\.%s$' % (group_re, range_re, format_re)
 series = dict((type, '%s-%s' % (type, series_re)) for type in views.SERIES)
 
 urlpatterns = patterns('',
-    url('^$', views.dashboard, name='stats.dashboard'),
+    # url('^$', views.dashboard, name='stats.dashboard'),
     url('^site%s/%s$' % (format_re, group_date_re),
         views.site, name='stats.site'),
     url('^site-%s' % series_re, views.site, name='stats.site.new'),
