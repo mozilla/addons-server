5fa01ca2ca56b6f8e43a35348922e1f56833edcf
Matt Claypotch <thepotch@gmail.com>
2012-03-06 14:58:37 -0800

    global stats cleanup

--- apps/stats/helpers.py
+++ apps/stats/helpers.py
@@ -10,7 +10,7 @@ from amo.urlresolvers import reverse
 
 @register.function
 @jinja2.contextfunction
-def report_menu(context, request, report, obj=None, username='', slug=''):
+def report_menu(context, request, report, obj=None):
     """Reports Menu. navigation for the various statistic reports."""
     if obj:
         if isinstance(obj, Addon):
