7e36a2175796e4cbbb9cc39a5590b11aa7fed967
Andy McKay <amckay@mozilla.com>
2012-03-09 09:42:30 -0800

    the other robots.txt (bug 731434)

--- mkt/site/views.py
+++ mkt/site/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 import jingo
 
+from amo.decorators import no_login_required
 import api.views
 
 
@@ -25,6 +26,7 @@ def csrf_failure(request, reason=''):
                         status=403)
 
 
+@no_login_required
 def robots(request):
     """Generate a robots.txt"""
     template = jingo.render(request, 'site/robots.txt')
