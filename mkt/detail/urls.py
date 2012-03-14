2c9c1e7ca27130803a15cbe352bab33a9e223a32
Chris Van <cvan@mozilla.com>
2012-03-13 22:28:40 -0700

    fix app reviews tests

--- mkt/detail/urls.py
+++ mkt/detail/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = patterns('',
     url('^$', views.detail, name='detail'),
-    url('^abuse$', addons.views.report_abuse, name='detail.abuse'),
     url('^record$', views.record, name='detail.record'),
+
+    # TODO: Port abuse.
+    url('^abuse$', addons.views.report_abuse, name='detail.abuse'),
 )
--- mkt/webapps/urls.py
+++ mkt/webapps/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import include, patterns
+
+from reviews.urls import review_patterns
 
 
 APP_SLUG = r"""(?P<app_slug>[^/<>"']+)"""
 
 urlpatterns = patterns('',
+    # TODO: Port reviews and add to `mkt/details`.
+    ('^app/%s/reviews/' % APP_SLUG, include(review_patterns('apps'))),
 )
