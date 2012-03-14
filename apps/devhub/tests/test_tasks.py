5d0ad5511dcec65994fc523dfa9bf5bc3753a95c
Chris Van <cvan@mozilla.com>
2012-03-13 23:39:25 -0700

    remove unused apps task tests from AMO (bug 733126)

--- apps/devhub/tests/test_tasks.py
+++ apps/devhub/tests/test_tasks.py
@@ -1,10 +1,7 @@
-import json
 import os
 import path
 import shutil
-import socket
 import tempfile
-import urllib2
 
 from django.conf import settings
 
@@ -16,10 +13,7 @@ import amo
 import amo.tests
 from addons.models import Addon
 from amo.tests.test_helpers import get_image_path
-from amo.urlresolvers import reverse
-from amo.utils import ImageCheck
 from devhub import tasks
-from devhub.tests.test_views import BaseWebAppTest
 from files.models import FileUpload
 
 
@@ -152,162 +146,3 @@ class TestFlagBinary(amo.tests.TestCase):
         _mock.side_effect = RuntimeError()
         tasks.flag_binary([self.addon.pk])
         eq_(Addon.objects.get(pk=self.addon.pk).binary, False)
-
-
-class TestFetchManifest(amo.tests.TestCase):
-
-    def setUp(self):
-        self.upload = FileUpload.objects.create()
-        self.content_type = 'application/x-web-app-manifest+json'
-
-        patcher = mock.patch('devhub.tasks.urllib2.urlopen')
-        self.urlopen_mock = patcher.start()
-        self.addCleanup(patcher.stop)
-
-    @mock.patch('devhub.tasks.validator')
-    def test_success_add_file(self, validator_mock):
-        response_mock = mock.Mock()
-        response_mock.read.return_value = 'woo'
-        response_mock.headers = {'Content-Type': self.content_type}
-        self.urlopen_mock.return_value = response_mock
-
-        tasks.fetch_manifest('http://xx.com/manifest.json', self.upload.pk)
-        upload = FileUpload.objects.get(pk=self.upload.pk)
-        eq_(upload.name, 'http://xx.com/manifest.json')
-        eq_(open(upload.path).read(), 'woo')
-
-    @mock.patch('devhub.tasks.validator')
-    def test_success_call_validator(self, validator_mock):
-        response_mock = mock.Mock()
-        response_mock.read.return_value = 'woo'
-        ct = self.content_type + '; charset=utf-8'
-        response_mock.headers = {'Content-Type': ct}
-        self.urlopen_mock.return_value = response_mock
-
-        tasks.fetch_manifest('http://xx.com/manifest.json', self.upload.pk)
-        assert validator_mock.called
-
-    def check_validation(self, msg):
-        upload = FileUpload.objects.get(pk=self.upload.pk)
-        validation = json.loads(upload.validation)
-        eq_(validation['errors'], 1)
-        eq_(validation['success'], False)
-        eq_(len(validation['messages']), 1)
-        eq_(validation['messages'][0]['message'], msg)
-
-    def test_connection_error(self):
-        reason = socket.gaierror(8, 'nodename nor servname provided')
-        self.urlopen_mock.side_effect = urllib2.URLError(reason)
-        tasks.fetch_manifest('url', self.upload.pk)
-        self.check_validation('Could not contact host at "url".')
-
-    def test_url_timeout(self):
-        reason = socket.timeout('too slow')
-        self.urlopen_mock.side_effect = urllib2.URLError(reason)
-        tasks.fetch_manifest('url', self.upload.pk)
-        self.check_validation('Connection to "url" timed out.')
-
-    def test_other_url_error(self):
-        reason = Exception('Some other failure.')
-        self.urlopen_mock.side_effect = urllib2.URLError(reason)
-        tasks.fetch_manifest('url', self.upload.pk)
-        self.check_validation('Some other failure.')
-
-    def test_no_content_type(self):
-        response_mock = mock.Mock()
-        response_mock.read.return_value = 'woo'
-        response_mock.headers = {}
-        self.urlopen_mock.return_value = response_mock
-
-        tasks.fetch_manifest('url', self.upload.pk)
-        self.check_validation(
-            'Your manifest must be served with the HTTP header '
-            '"Content-Type: application/x-web-app-manifest+json".')
-
-    def test_bad_content_type(self):
-        response_mock = mock.Mock()
-        response_mock.read.return_value = 'woo'
-        response_mock.headers = {'Content-Type': 'x'}
-        self.urlopen_mock.return_value = response_mock
-
-        tasks.fetch_manifest('url', self.upload.pk)
-        self.check_validation(
-            'Your manifest must be served with the HTTP header '
-            '"Content-Type: application/x-web-app-manifest+json". We saw "x".')
-
-    def test_response_too_large(self):
-        response_mock = mock.Mock()
-        content = 'x' * (settings.MAX_WEBAPP_UPLOAD_SIZE + 1)
-        response_mock.read.return_value = content
-        response_mock.headers = {'Content-Type': self.content_type}
-        self.urlopen_mock.return_value = response_mock
-
-        tasks.fetch_manifest('url', self.upload.pk)
-        self.check_validation('Your manifest must be less than 2097152 bytes.')
-
-    def test_http_error(self):
-        self.urlopen_mock.side_effect = urllib2.HTTPError(
-            'url', 404, 'Not Found', [], None)
-        tasks.fetch_manifest('url', self.upload.pk)
-        self.check_validation('url responded with 404 (Not Found).')
-
-
-class TestFetchIcon(BaseWebAppTest):
-
-    def setUp(self):
-        super(TestFetchIcon, self).setUp()
-        self.content_type = 'image/png'
-        self.apps_path = (path.path(settings.ROOT) / 'apps'
-                          / 'devhub' / 'tests' / 'addons')
-        patcher = mock.patch('devhub.tasks.urllib2.urlopen')
-        self.urlopen_mock = patcher.start()
-        self.addCleanup(patcher.stop)
-
-    def webapp_from_path(self, path):
-        self.upload = self.get_upload(abspath=path)
-        self.url = reverse('devhub.submit.2')
-        assert self.client.login(username='regular@mozilla.com',
-                                 password='password')
-        self.client.post(reverse('devhub.submit.1'))
-        return self.post_addon()
-
-    def test_no_icons(self):
-        path = self.apps_path / 'noicon.webapp'
-        iconless_app = self.webapp_from_path(path)
-        tasks.fetch_icon(iconless_app)
-        assert not self.urlopen_mock.called
-
-    def check_icons(self, webapp):
-        manifest = webapp.get_manifest_json()
-        biggest = max([int(size) for size in manifest['icons']])
-
-        icon_dir = webapp.get_icon_dir()
-        for size in amo.ADDON_ICON_SIZES:
-            if not size <= biggest:
-                continue
-            icon_path = os.path.join(icon_dir, '%s-%s.png'
-                                     % (str(webapp.id), size))
-            with open(icon_path, 'r') as img:
-                checker = ImageCheck(img)
-                assert checker.is_image()
-                eq_(checker.img.size, (size, size))
-
-    def test_data_uri(self):
-        app_path = self.apps_path / 'dataicon.webapp'
-        webapp = self.webapp_from_path(app_path)
-
-        tasks.fetch_icon(webapp)
-        eq_(webapp.icon_type, self.content_type)
-
-        self.check_icons(webapp)
-
-    def test_hosted_icon(self):
-        app_path = self.apps_path / 'mozball.webapp'
-        webapp = self.webapp_from_path(app_path)
-
-        img_path = self.apps_path / 'mozball-128.png'
-        with open(img_path, 'r') as content:
-            tasks.save_icon(webapp, content.read())
-        eq_(webapp.icon_type, self.content_type)
-
-        self.check_icons(webapp)
