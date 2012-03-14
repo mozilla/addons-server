ccc999cd8e6a7fea8fab1468fb90c6dc29a4a6d5
Chris Van <cvan@mozilla.com>
2012-03-13 18:39:17 -0700

    kill old apps search tests (bug 735540)

--- apps/search/urls.py
+++ apps/search/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = patterns('',
     url('^(?:es)?$', views.search, name='search.search'),
+    url('^apps$', views.app_search, name='apps.search'),
     url('^ajax$', views.ajax_search, name='search.ajax'),
     url('^suggestions$', views.ajax_search_suggestions,
         name='search.suggestions'),
