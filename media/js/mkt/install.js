df6ffa5bf35d5662df7b3265663f0a8559e43479
Matt Claypotch <thepotch@gmail.com>
2012-03-14 18:21:36 -0700

    added the files needed for initial install/payment guts

---
+++ media/js/mkt/install.js
@@ -0,0 +1,7 @@
+// Hey there! I know how to install apps. Buttons are dumb now.
+
+$('#page').delegate('.button.install', 'click', startInstall);
+
+function startInstall(e) {
+    var $button = $(this);
+}
