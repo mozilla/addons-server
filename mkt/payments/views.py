c9ec35aff9f4f8be5ece8b5d75803d4c888dff46
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-03-20 17:37:02 -0500

    Converted in-app-payments prototype to popup (bug 737670)

--- mkt/payments/views.py
+++ mkt/payments/views.py
@@ -1,36 +1,13 @@
-import functools
 import json
 
 import jingo
 import jwt
 from session_csrf import anonymous_csrf
 from waffle.decorators import waffle_switch
-import lxml
-import lxml.html
 
 from django import http
-from django.conf import settings
 
 from amo.decorators import login_required
-
-
-def remote_jsonp_view(f):
-    @functools.wraps(f)
-    def wrapper(request, *args, **kw):
-        response = f(request, *args, **kw)
-        if response.status_code != 200:
-            return response
-        callback = request.GET.get('callback', 'callback')
-
-        # Make all URLs absolute.
-        # If we can get the whole thing working in an iframe then we won't
-        # need this. If we do then there is probably a more direct way.
-        html = lxml.html.make_links_absolute(response.content,
-                                             settings.SITE_URL)
-
-        return http.HttpResponse('%s(%s)' % (callback, json.dumps(html)),
-                                 content_type='application/javascript')
-    return wrapper
 
 
 def decode_request(signed_request):
@@ -46,7 +23,6 @@ def decode_request(signed_request):
 
 
 @anonymous_csrf
-@remote_jsonp_view
 @waffle_switch('in-app-payments-ui')
 def pay_start(request):
     signed_req = request.GET.get('req')
@@ -63,10 +39,9 @@ def pay_start(request):
 
 @anonymous_csrf
 @login_required
-@remote_jsonp_view
 @waffle_switch('in-app-payments-ui')
 def pay(request):
-    signed_req = request.GET.get('req')
+    signed_req = request.POST.get('req')
     if not signed_req:
         return http.HttpResponseBadRequest()
     # decoded_req = decode_request(signed_req)
@@ -75,9 +50,3 @@ def pay(request):
 
     data = {}
     return jingo.render(request, 'payments/thanks_for_payment.html', data)
-
-
-@remote_jsonp_view
-@waffle_switch('in-app-payments-ui')
-def inject_styles(request):
-    return jingo.render(request, 'payments/inject_styles.html', {})
