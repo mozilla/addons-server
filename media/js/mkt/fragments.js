c41534876244a4418a28c5e40958de6b18135d8f
Chris Van <cvan@mozilla.com>
2012-03-20 09:43:53 -0700

    add loading to fragments.js

--- media/js/mkt/fragments.js
+++ media/js/mkt/fragments.js
@@ -1,6 +1,12 @@
 (function() {
+    var threshold = 250,
+        timeout = false;
     z.page = $('#page');
     if (z.capabilities['replaceState']) {
+        var $loading = $('<div>', {'class': 'loading balloon',
+                                   'html': gettext('Loading&hellip;')})
+                        .prependTo($('#site-header'));
+
         z.page.on('click', 'a', function(e) {
             var href = this.getAttribute('href');
             if (!href || href.substr(0,4) == 'http' || href === '#') return;
@@ -10,8 +16,12 @@
         });
 
         function fetchFragment(href) {
+            timeout = setTimeout(function() { $loading.addClass('active'); },
+                                 threshold);
             $.get(href, function(d) {
+                clearTimeout(timeout);
                 z.page.html(d).trigger('fragmentloaded');
+                $loading.removeClass('active');
                 $('html, body').animate({scrollTop: 0}, 200);
             });
         }
