8208b22ec4db8599353807cb8669d9000252392f
Allen Short <ashort@mozilla.com>
2012-03-26 19:38:19 -0500

    Add fixture that more accurately expresses test intent

--- apps/users/tests/test_backends.py
+++ apps/users/tests/test_backends.py
@@ -9,21 +9,21 @@ class TestAmoUserBackend(amo.tests.TestCase):
 
     def test_success_without_user(self):
         """Make sure a contrib.auth.User gets created when we log in."""
-        u = UserProfile.objects.get(email='fligtar@gmail.com')
+        u = UserProfile.objects.get(email='newuser@example.com')
         assert u.user is None
-        assert authenticate(username='fligtar@gmail.com', password='foo')
-        u = UserProfile.objects.get(email='fligtar@gmail.com')
+        assert authenticate(username='newuser@example.com', password='foo')
+        u = UserProfile.objects.get(email='newuser@example.com')
         assert u.user is not None
-        assert u.user.email == 'fligtar@gmail.com'
+        assert u.user.email == 'newuser@example.com'
 
     def test_success_with_user(self):
         assert authenticate(username='jbalogh@mozilla.com', password='foo')
 
     def test_failure_without_user(self):
         """Make sure a user isn't created on a failed password."""
-        u = UserProfile.objects.get(email='fligtar@gmail.com')
+        u = UserProfile.objects.get(email='newuser@example.com')
         assert u.user is None
-        assert not authenticate(username='fligtar@gmail.com', password='bar')
+        assert not authenticate(username='newuser@example.com', password='bar')
         assert u.user is None
 
     def test_failure_with_user(self):
