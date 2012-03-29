65711ab312cae2fb7af4deb6346b26365e8b21c9
Matt Claypotch <thepotch@gmail.com>
2012-03-29 15:38:34 -0700

    log ajax, other remote debug improvements

--- media/js/debug/remote_debug.js
+++ media/js/debug/remote_debug.js
@@ -3,16 +3,25 @@
                                        .getAttribute('value') + '?';
     var oldConsole = window.console.log;
     window.console.log = function() {
+        oldConsole.apply(window, arguments);
+        for (var i=0; i<arguments.length; i++) {
+            arguments[i] = JSON.stringify(arguments[i]);
+        }
         (new Image()).src = base_url + JSON.stringify({
             msg: Array.prototype.join.call(arguments, ' , '),
             file: '', line: '', type: 'log'
         });
-        oldConsole.apply(window, arguments);
     };
     window.onerror = function(m,f,l) {
         (new Image()).src = base_url + JSON.stringify({
             msg: m, file: f, line: l, type: 'error'
         });
     };
+    window.addEventListener('load', function() {
+        console.log('woo online');
+        $.ajaxPrefilter(function(opts) {
+            console.log('starting ajax request', opts);
+        });
+    });
 })();
 
--- media/js/debug/remote_debug_server.node.js
+++ media/js/debug/remote_debug_server.node.js
@@ -1,6 +1,7 @@
 var html = require('fs').readFileSync(__dirname+'/remote_debug_viewer.html');
 var http = require('http');
 var url = require('url');
+var console = require('console');
 
 var server = http.createServer(function(req, res){
   res.end(html);
@@ -14,6 +15,12 @@ var everyone = nowjs.initialize(server);
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end();
-  var error = url.parse(req.url).query;
-  everyone.now.showError(error);
+  var msg = decodeURIComponent(url.parse(req.url).query);
+  console.log(msg);
+  var msgObj = JSON.parse(msg);
+  if (msgObj.type == 'error') {
+      everyone.now.showError(msg);
+  } else {
+      everyone.now.showLog(msg);
+  }
 }).listen(37767);
