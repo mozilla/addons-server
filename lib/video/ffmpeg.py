e3e0ea6aaba2007b1e0d96c86080b92f1c7d27e1
Andy McKay <amckay@mozilla.com>
2012-03-30 09:47:15 -0700

    output compat.

--- lib/video/ffmpeg.py
+++ lib/video/ffmpeg.py
@@ -29,7 +29,9 @@ def check_output(*popenargs, **kwargs):
         cmd = kwargs.get("args")
         if cmd is None:
             cmd = popenargs[0]
-        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+        error = subprocess.CalledProcessError(retcode, cmd)
+        error.output = output
+        raise error
     return output
 
 
