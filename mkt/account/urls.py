c743fa919705f9d3cee0aae453ae194247169f18
Chris Van <cvan@mozilla.com>
2012-03-30 11:07:59 -0700

    do not make /settings == /admin/settings

--- mkt/account/urls.py
+++ mkt/account/urls.py
@@ -18,13 +18,13 @@ settings_patterns = patterns('',
 
 
 urlpatterns = decorate(login_required, patterns('',
-    url('settings$', views.account_settings, name='account.settings'),
+    url('^settings$', views.account_settings, name='account.settings'),
     ('^settings/', include(settings_patterns)),
-    url('purchases/$', views.purchases, name='account.purchases'),
-    url(r'purchases/(?P<product_id>\d+)', views.purchases,
+    url('^purchases/$', views.purchases, name='account.purchases'),
+    url(r'^purchases/(?P<product_id>\d+)', views.purchases,
         name='account.purchases.receipt'),
 
     # Keeping the same URL pattern since admin pages already know about this.
-    url(r'user/(?:/(?P<user_id>\d+)/)?edit$', views.admin_edit,
+    url(r'^user/(?:/(?P<user_id>\d+)/)?edit$', views.admin_edit,
         name='users.admin_edit'),
 ))
