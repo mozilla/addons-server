f1b40af32aa410910b8f0a120f8b3876b63a3f5c
Chris Van <cvan@mozilla.com>
2012-04-02 18:32:14 -0700

    make review detail page specific to apps (bug 737629)

--- mkt/reviewers/forms.py
+++ mkt/reviewers/forms.py
@@ -1,8 +1,7 @@
-from editors.forms import ReviewAddonForm
+from tower import ugettext_lazy as _lazy
 
-# TODO: Remove
+from editors.forms import ReviewAddonForm, ReviewFileForm
 from editors.helpers import ReviewFiles
-from editors.forms import ReviewFileForm
 
 from mkt.reviewers.utils import ReviewAddon, ReviewHelper
 
@@ -13,6 +12,9 @@ class ReviewAppForm(ReviewAddonForm):
         super(ReviewAppForm, self).__init__(*args, **kw)
         # We don't want to disable any app files:
         self.addon_files_disabled = tuple([])
+        self.fields['notify'].label = _lazy(
+            u'Notify me the next time the manifest is updated. (Subsequent '
+             'updates will not generate an email.)')
 
 
 def get_review_form(data, request=None, addon=None, version=None):
