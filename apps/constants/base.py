a1c8b65a9840da55da5c148891c1de5a3b71b5e2
Chris Van <cvan@mozilla.com>
2012-04-05 11:59:23 -0700

    save 128px icons (bug 741584)

--- apps/constants/base.py
+++ apps/constants/base.py
@@ -199,7 +199,7 @@ MIN_TAG_LENGTH = 2
 MAX_CATEGORIES = 2
 
 # Icon upload sizes
-ADDON_ICON_SIZES = [32, 48, 64]
+ADDON_ICON_SIZES = [32, 48, 64, 128]
 
 # Preview upload sizes [thumb, full]
 ADDON_PREVIEW_SIZES = [(200, 150), (700, 525)]
