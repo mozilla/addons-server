935cc24d38beb21a3e92cc1a4fadbb2dbad2d5ba
Chris Van <cvan@mozilla.com>
2012-04-10 14:07:19 -0700

    add "Log In / Register" button to Marketplace footer

--- media/js/mkt/login.js
+++ media/js/mkt/login.js
@@ -1,21 +1,25 @@
 $(window).bind('login', function() {
     $('#login').addClass('show');
-});
-$(window).on('click', '.button.browserid', function() {
+}).on('click', '.button.browserid', _pd(function() {
+    var $this = $(this);
+    $this.addClass('loading-submit');
     $.when(z.login())
      .done(function() {
-        window.location.reload();
+         $this.removeClass('loading-submit');
+         window.location.reload();
      })
      .fail(function(err) {
-        if (err.privs) {
-            $('#login').addClass('show old');
-        } else {
-            alert(err);
-        }
+         $this.removeClass('loading-submit');
+         if (err.privs) {
+             $('#login').addClass('show old');
+         } else {
+             alert(err);
+         }
      });
-});
+}));
 // Hijack the login form to send us to the right place
 $("#login form").submit(function(e) {
-    var action = $(this).attr('action') + format("?to={0}", window.location.pathname);
-    $(this).attr('action', action);
+    var $this = $(this),
+        action = $this.attr('action') + format("?to={0}", window.location.pathname);
+    $this.attr('action', action);
 });
