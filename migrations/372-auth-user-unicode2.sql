5559c94d88d72bc11ff0a27d2445264e79d02d28
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-04-13 12:33:41 -0500

    Run dupe removal in the correct migration (bug 731639)

--- migrations/372-auth-user-unicode2.sql
+++ migrations/372-auth-user-unicode2.sql
@@ -1,7 +1,3 @@
 -- In addition to converting the table, also need to convert this column. See
 -- bug 731639
-
--- Set each username to a random value first. They were corrupted anyway and are not used
--- anywhere. The original value is available in users.
-update auth_user set username = UUID();
 ALTER TABLE auth_user MODIFY COLUMN username varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
