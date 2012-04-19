2dee3dea87a15f681585470b0eb78ff288add52a
Chris Van <cvan@mozilla.com>
2012-04-18 21:48:57 -0700

    dismiss overlay when leaving page (bug 746851)

--- media/js/mkt/overlay.js
+++ media/js/mkt/overlay.js
@@ -1,9 +1,12 @@
 (function() {
-    $(document).on('click', '.overlay.show', function(e) {
+    z.page.on('click', '.overlay.show', function(e) {
         // If we're clicking outside of the overlay, dismiss it.
         if ($(e.target).parent('body').length) {
             $(this).removeClass('show');
             $(window).trigger('overlay_dismissed');
         }
+    }).on('fragmentloaded', function(e) {
+        $('.overlay.show').removeClass('show');
+        $(window).trigger('overlay_dismissed');
     });
 })();
