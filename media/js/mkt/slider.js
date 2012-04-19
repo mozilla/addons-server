b6770c9883ecbb547f0b1ca4d818d0f9b0e7f2bd
Davor Spasovski <dspasovski@mozilla.com>
2012-04-19 15:23:23 -0700

    slider paging work

--- media/js/mkt/slider.js
+++ media/js/mkt/slider.js
@@ -66,31 +66,64 @@ function slider() {
 }
 
 z.page.on('fragmentloaded', function() {
-    // $('.slider').each(slider);
-});
+    function itemsPerPage($item) {
+        var contWidth = $('#page > section > div').width(),
+            margin = parseInt($item.css('margin-right'), 10);
 
-/*
-if ($('body.home').length) {
-    $('.slider').each(function() {
-        var $this = $(this),
-            $ul = $this.find('ul'),
-            $a = $ul.find('a');
-        $this.hover(function(e) {
-            $ul.addClass('moving-left');
-            // e.clientX > ($this.width() / 2)
+        return Math.floor(contWidth / ($item.width() + margin));
+    }
+
+    function numPages($list) {
+        var $item = $list.find('li'),
+            rawVal = $item.length / itemsPerPage($item),
+            floorVal = Math.floor($item.length / itemsPerPage($item));
+        return rawVal == floorVal ? floorVal - 1 : floorVal;
+    }
+
+    function initSliders() {
+        $('.slider').each(function() {
+            var currentPage,
+                $pager = $('<p class="slide-pager c"></p>'),
+                $nextLink = $('<a href="#" class="next-page"></a>'),
+                $prevLink = $('<a href="#" class="prev-page"></a>'),
+                $this = $(this),
+                maxPage = numPages($this.find('ul'));
+
+            if ($this.prev().find('.slide-pager').length === 0) {
+                $pager.append($prevLink, $nextLink);
+                $(this).prev().find('div').append($pager);
+                $prevLink.click(_pd(prevPage));
+                $nextLink.click(_pd(nextPage));
+            }
+
+            gotoPage(0);
+
+            function gotoPage(n) {
+                if (n < 0 || n > maxPage) return;
+                var $item = $this.find('ul li'),
+                    val = n * (itemsPerPage($item) *
+                         ($item.width() + parseInt($item.css('margin-right'), 10)));
+                    // Have no idea why the magic number is needed.
+                    val += n * (itemsPerPage($item) + 7);
+                currentPage = n;
+                $this.find('ul').css(z.prefixed('transform'),
+                                     'translateX(-'+val+'px)');
+            }
+
+            function nextPage() {
+                if (currentPage < maxPage) {
+                    gotoPage(currentPage+1);
+                }
+            }
+
+            function prevPage() {
+                if (currentPage) {
+                    gotoPage(currentPage-1);
+                }
+            }
         });
-        $this.mouseout(function() {
-            $a.removeClass('frozen');
-            $ul.addClass('done');
-        });
-        $a.hover(_.throttle(function(e) {
-            $(this).addClass('frozen');
-            $ul.addClass('done');
-        }, 250));
-        $a.mouseout(_.throttle(function(e) {
-            $(this).removeClass('frozen');
-            $ul.removeClass('done');
-        }, 250));
-    });
-}
-*/
+    }
+
+    initSliders();
+    $(window).bind('saferesize', initSliders);
+});
