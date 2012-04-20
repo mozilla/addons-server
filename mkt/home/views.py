0ae75161f769a9e13dc96dab133d19ea51ac8050
Chris Van <cvan@mozilla.com>
2012-04-20 11:25:15 -0700

    show featured apps on Marketplace homepage (bug 744641)

--- mkt/home/views.py
+++ mkt/home/views.py
@@ -3,6 +3,7 @@ import waffle
 
 import amo
 from addons.models import Category
+from bandwagon.models import Collection
 
 from mkt.developers.views import home as devhub_home
 from mkt.webapps.models import Webapp
@@ -12,8 +13,16 @@ def home(request):
     """The home page."""
     if not waffle.switch_is_active('unleash-consumer'):
         return devhub_home
-    featured = Webapp.objects.all()[:30]
-    popular = Webapp.objects.order_by('-weekly_downloads')[:30]
+    try:
+        featured = Collection.objects.get(author__username='mozilla',
+            slug='webapps_home', type=amo.COLLECTION_FEATURED)
+    except Collection.DoesNotExist:
+        featured = []
+    if featured:
+        featured = featured.addons.filter(status=amo.STATUS_PUBLIC,
+                                          disabled_by_user=False)[:30]
+    popular = (Webapp.objects.order_by('-weekly_downloads')
+               .filter(status=amo.STATUS_PUBLIC, disabled_by_user=False))[:6]
     categories = Category.objects.filter(type=amo.ADDON_WEBAPP)
     return jingo.render(request, 'home/home.html', {
         'featured': featured,
