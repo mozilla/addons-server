9604637155040e82cfb63a569a5d3498da4e2794
Chris Van <cvan@mozilla.com>
2012-04-21 20:45:27 -0700

    do not collapse upon clicking description on detail page

--- media/js/mkt/detail.js
+++ media/js/mkt/detail.js
@@ -5,9 +5,5 @@
     }
     z.page.on('click', 'a.collapse', _pd(function() {
         toggle($(this));
-    })).on('click', '.description', function(e) {
-        if (!$(e.target).is('a')) {
-            toggle($(this).find('a.collapse'));
-        }
-    });
+    }));
 })();
