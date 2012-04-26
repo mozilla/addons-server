5e54be61ac8da770cf56727ca97a7fea186a9599
Chris Van <cvan@mozilla.com>
2012-04-26 15:29:55 -0700

    order categories alphabetically

--- mkt/browse/helpers.py
+++ mkt/browse/helpers.py
@@ -4,6 +4,7 @@ import jinja2
 
 import amo
 from addons.models import AddonCategory, Category
+from translations.query import order_by_translation
 
 
 @register.function
@@ -17,6 +18,7 @@ def _categories():
                    .values_list('category', flat=True).distinct())
     categories = Category.objects.filter(type=amo.ADDON_WEBAPP, weight__gte=0,
                                          id__in=public_cats)
+    categories = order_by_translation(categories, 'name')
     t = env.get_template('browse/helpers/category_slider.html')
     return jinja2.Markup(t.render(categories=categories))
 
