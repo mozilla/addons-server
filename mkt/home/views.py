c7d21fa2810a47c16ab06a3c77e7df4322bea892
Chris Van <cvan@mozilla.com>
2012-04-26 12:04:57 -0700

    add a consumer homepage for non-whitelisted people who dare enter our walled garden

--- mkt/home/views.py
+++ mkt/home/views.py
@@ -1,16 +1,12 @@
 import jingo
 
-from django.shortcuts import redirect
-
-from amo.urlresolvers import reverse
-
 from mkt.webapps.models import Webapp
 
 
 def home(request):
     """The home page."""
     if not getattr(request, 'can_view_consumer', True):
-        return redirect(reverse('mkt.developers.index'))
+        return jingo.render(request, 'home/home_walled.html')
     featured = Webapp.featured('home')[:3]
     popular = Webapp.popular()[:6]
     return jingo.render(request, 'home/home.html', {
