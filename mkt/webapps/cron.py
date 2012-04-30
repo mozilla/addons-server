704c0972d49027411077fac0b8f4a2e7225ffca2
Chris Van <cvan@mozilla.com>
2012-04-29 21:17:42 -0700

    remove dead code from mkt/webapps

--- mkt/webapps/cron.py
+++ mkt/webapps/cron.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-import logging
 
 from django.db.models import Count
 
@@ -8,36 +7,9 @@ from celery.task.sets import TaskSet
 
 import amo
 from amo.utils import chunked
-from addons.tasks import index_addons
-from mkt.webapps.models import Installed
 
-from .models import Webapp
+from .models import Installed
 from .tasks import webapp_update_weekly_downloads
-
-task_log = logging.getLogger('z.task')
-
-
-@cronjobs.register
-def release_webapps():
-    """Turn apps from PENDING to PUBLIC so they show up on the site."""
-    flip_webapp_status(amo.WEBAPPS_UNREVIEWED_STATUS, amo.STATUS_PUBLIC)
-
-
-@cronjobs.register
-def restrict_webapps():
-    """Turn apps from PUBLIC to PENDING so they don't show up on the site."""
-    flip_webapp_status(amo.STATUS_PUBLIC, amo.WEBAPPS_UNREVIEWED_STATUS)
-
-
-def flip_webapp_status(from_, to):
-    qs = Webapp.objects.filter(status=from_)
-    # Grab the ids so we can get them reindexed.
-    ids = list(qs.values_list('id', flat=True))
-    qs.update(status=to)
-    ts = [index_addons.subtask(args=[chunk])
-          for chunk in chunked(ids, 150)]
-    # Delay these tasks to avoid slave lag.
-    TaskSet(ts).apply_async(countdown=30)
 
 
 @cronjobs.register
--- mkt/webapps/tests/test_views.py
+++ mkt/webapps/tests/test_views.py
@@ -1,54 +1,31 @@
-from nose.tools import eq_
-import waffle
-
 import amo
-import amo.tests
+from amo.tests import app_factory
 from market.models import AddonPremium, Price
-from users.models import UserProfile
-from versions.models import Version
 from mkt.webapps.models import Webapp
-
-
-class WebappTest(amo.tests.TestCase):
-
-    def setUp(self):
-        self.webapp = Webapp.objects.create(name='woo', app_slug='yeah',
-            weekly_downloads=9999, status=amo.STATUS_PUBLIC)
-        self.webapp._current_version = (Version.objects
-                                        .create(addon=self.webapp))
-        self.webapp.save()
-
-        self.webapp_url = self.url = self.webapp.get_url_path()
 
 
 class PaidAppMixin(object):
 
     def setup_paid(self, type_=None):
         type_ = amo.ADDON_PREMIUM if type_ is None else type_
-        self.free = [
-            Webapp.objects.get(id=337141),
-            amo.tests.app_factory(),
-        ]
-
+        self.free = [Webapp.objects.get(id=337141), app_factory()]
         self.paid = []
         for x in xrange(1, 3):
             price = Price.objects.create(price=x)
-            addon = amo.tests.app_factory(weekly_downloads=x * 100)
-            AddonPremium.objects.create(price=price, addon=addon)
-            addon.update(premium_type=type_)
-            self.paid.append(addon)
+            app = app_factory(weekly_downloads=x * 100)
+            AddonPremium.objects.create(price=price, addon=app)
+            app.update(premium_type=type_)
+            self.paid.append(app)
 
         # For measure add some disabled free apps ...
-        amo.tests.app_factory(disabled_by_user=True)
-        amo.tests.app_factory(status=amo.STATUS_NULL)
+        app_factory(disabled_by_user=True)
+        app_factory(status=amo.STATUS_NULL)
 
         # ... and some disabled paid apps.
-        addon = amo.tests.app_factory(disabled_by_user=True,
-                                      premium_type=amo.ADDON_PREMIUM)
-        AddonPremium.objects.create(price=price, addon=addon)
-        addon = amo.tests.app_factory(status=amo.STATUS_NULL,
-                                      premium_type=amo.ADDON_PREMIUM)
-        AddonPremium.objects.create(price=price, addon=addon)
+        a = app_factory(disabled_by_user=True, premium_type=amo.ADDON_PREMIUM)
+        AddonPremium.objects.create(price=price, addon=a)
+        a = app_factory(status=amo.STATUS_NULL, premium_type=amo.ADDON_PREMIUM)
+        AddonPremium.objects.create(price=price, addon=a)
 
         self.both = sorted(self.free + self.paid,
                            key=lambda x: x.weekly_downloads, reverse=True)
@@ -56,14 +33,3 @@ class PaidAppMixin(object):
                            reverse=True)
         self.paid = sorted(self.paid, key=lambda x: x.weekly_downloads,
                            reverse=True)
-
-
-class TestPremium(PaidAppMixin, amo.tests.TestCase):
-    fixtures = ['base/apps', 'base/users', 'webapps/337141-steamcube']
-
-    def setUp(self):
-        waffle.models.Switch.objects.create(name='marketplace', active=True)
-        self.user = UserProfile.objects.get(email='regular@mozilla.com')
-        self.setup_paid()
-        eq_(self.free, list(Webapp.objects.top_free()))
-        eq_(self.paid, list(Webapp.objects.top_paid()))
