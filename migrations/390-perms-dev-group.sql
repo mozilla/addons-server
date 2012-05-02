4861271144879b1decb0001e9abb05847d6690d3
Rob Hudson <chudson@mozilla.com>
2012-05-02 14:10:07 -0700

    Fixed duplicate primary key in migration.

--- migrations/390-perms-dev-group.sql
+++ migrations/390-perms-dev-group.sql
@@ -1,6 +1,6 @@
 INSERT INTO groups (id, name, rules, notes, created, modified) VALUES
-  (50055, 'Developers', 'AdminTools:View', '', NOW(), NOW());
+  (50056, 'Developers', 'AdminTools:View', '', NOW(), NOW());
 -- Copy current users in Developers Credits into this group.
 INSERT INTO groups_users (
-  SELECT NULL, 50055, groups_users.user_id FROM groups, groups_users
+  SELECT NULL, 50056, groups_users.user_id FROM groups, groups_users
   WHERE groups.id=groups_users.group_id AND groups.id=50046);
