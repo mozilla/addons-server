f74b1e15d50845787ce8f77e21a2811aa48fe530
Chris Van <cvan@mozilla.com>
2012-05-02 12:30:06 -0700

    remove user review feed tests for marketplace

--- apps/reviews/tests/test_feeds.py
+++ apps/reviews/tests/test_feeds.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
-
 import mock
 from nose.tools import eq_
-from pyquery import PyQuery as pq
 
-from amo.urlresolvers import reverse
 import amo.tests
 from reviews import feeds
 from translations.models import Translation
-from mkt.webapps.models import Webapp
 
 
 class FeedTest(amo.tests.TestCase):
@@ -47,40 +42,3 @@ class FeedTest(amo.tests.TestCase):
 
         self.user.username = self.u
         eq_(self.feed.item_author_name(self.review), self.u)
-
-
-class TestAppsFeed(amo.tests.TestCase):
-    fixtures = ['base/337141-steamcube']
-
-    def setUp(self):
-        self.app = Webapp.objects.get(id=337141)
-        self.url = reverse('apps.reviews.list.rss', args=[self.app.app_slug])
-
-    def test_get(self):
-        r = self.client.get(self.url)
-        eq_(r.status_code, 200)
-        return r
-
-    def get_pq(self):
-        # PyQuery doesn't know RDF, and files.utils.RDF is overkill.
-        return pq(self.test_get().content.replace('atom:', 'atom_'))
-
-    def test_title(self):
-        eq_(self.get_pq()('title').text(), 'Reviews for Something Something!')
-
-    @mock.patch.object(settings, 'SITE_URL', 'http://test.com')
-    def test_link(self):
-        url = self.get_pq()('atom_link').attr('href')
-        assert url.endswith(self.url), (
-            'Unexpected URL for <atom:link>: %r' % url)
-
-    def test_site_link(self):
-        url = self.get_pq()('link').text()
-        assert url.endswith(reverse('home')), (
-            'Unexpected URL for <link>: %r' % url)
-
-    @mock.patch.object(settings, 'MARKETPLACE', True)
-    def test_site_link_mkt(self):
-        url = self.get_pq()('link').text()
-        assert url.endswith('/en-US/developers/'), (
-            'Unexpected URL for <link>: %r' % url)
