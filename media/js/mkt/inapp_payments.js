356ecb16eea9ebbf266439d094d6b44e2297c6d5
Matt Claypotch <thepotch@gmail.com>
2012-05-04 13:53:20 -0700

    adding escape to inapp

--- media/js/mkt/inapp_payments.js
+++ media/js/mkt/inapp_payments.js
@@ -7,6 +7,14 @@ var z = {},
     }
 };
 
+var escape_ = function(s) {
+    if (s === undefined) {
+        return;
+    }
+    return s.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;')
+            .replace(/'/g, '&#39;').replace(/"/g, '&#34;');
+};
+
 (function() {
     var win_top = window.top;
     if (win_top.opener) {
@@ -21,24 +29,3 @@ var z = {},
     });
 
 })();
-// (function(exports) {
-//     "use strict";
-
-//     // Close the thank you window. TODO: Thank you windows are going away!
-//     $('#close-moz-pay-win').click(function(e) {
-//         e.preventDefault();
-//         _send('moz-pay-success');
-//     });
-
-//     $('#close-moz-error-win').click(function(e) {
-//         e.preventDefault();
-//         _send('moz-pay-failure');
-//     });
-
-//     function _send(msg) {
-//         // This is using '*' because we're not sure what the app domain is.
-//         // Maybe we can find that out.
-//         window.top.postMessage(msg, '*');
-//     }
-
-// })(typeof exports === 'undefined' ? (this.inapp_payments = {}) : exports);
