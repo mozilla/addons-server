e18808b45fe8454a8d2666148df7119d20e2bd26
Chris Van <cvan@mozilla.com>
2012-05-14 20:09:44 -0700

    use waffle_switch_amo for 'current_version_crons' switch migration

--- migrations/401-maybe-kill-crons.sql
+++ migrations/401-maybe-kill-crons.sql
@@ -1,3 +1,3 @@
-INSERT INTO waffle_switch (name, active, note)
+INSERT INTO waffle_switch_amo (name, active, note)
     VALUES ('current_version_crons', 1,
             'Flip this to disable some crons and see if stuff breaks. Bug 750510');
