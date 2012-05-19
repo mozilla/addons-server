2d47491b1aab073eee673024abb8d821261b4da3
Allen Short <ashort@mozilla.com>
2012-05-18 22:25:49 -0500

    Replace redis with db table for FakeEmailBackend

--- apps/amo/mail.py
+++ apps/amo/mail.py
@@ -1,26 +1,30 @@
 import logging
 
 from django.core.mail.backends.base import BaseEmailBackend
+from django.db import models
 
-import redisutils
+from amo.models import ModelBase
 
 log = logging.getLogger('z.amo.mail')
 
 
-class FakeEmailBackend(BaseEmailBackend):
-    key = 'amo:mail:fakemail'
+class FakeEmail(ModelBase):
+    message = models.TextField()
 
-    def __init__(self, *args, **kw):
-        super(FakeEmailBackend, self).__init__(*args, **kw)
-        self.redis = redisutils.connections['master']
+    class Meta:
+        db_table = 'fake_email'
+
+
+class FakeEmailBackend(BaseEmailBackend):
 
     def send_messages(self, messages):
         log.debug('Sending fake mail.')
         for msg in messages:
-            self.redis.rpush(self.key, msg.message().as_string())
+            FakeEmail.objects.create(message=msg.message().as_string())
 
     def view_all(self):
-        return self.redis.lrange(self.key, 0, -1)
+        return (FakeEmail.objects.values_list('message', flat=True)
+                .order_by('id'))
 
     def clear(self):
-        return self.redis.delete(self.key)
+        return FakeEmail.objects.all().delete()
