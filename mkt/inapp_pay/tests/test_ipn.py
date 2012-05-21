8b283539fbc5828b9c271797a8dcd9efbc2b0510
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-05-21 17:23:19 -0500

    Fix in-app mocks for requests in paypal

--- mkt/inapp_pay/tests/test_ipn.py
+++ mkt/inapp_pay/tests/test_ipn.py
@@ -1,5 +1,5 @@
 import fudge
-from mock import patch
+from mock import patch, Mock
 from nose.tools import eq_
 import waffle
 
@@ -14,7 +14,7 @@ from paypal.tests.test_views import (PaypalTest, sample_reversal,
 from users.models import UserProfile
 
 
-@patch('paypal.views.urllib2.urlopen')
+@patch('paypal.views.requests.post')
 @patch.object(settings, 'DEBUG', True)
 class TestInappIPN(InappPaymentUtil, PaypalTest):
     fixtures = ['webapps/337141-steamcube', 'base/users']
@@ -41,8 +41,8 @@ class TestInappIPN(InappPaymentUtil, PaypalTest):
         return Addon.objects.get(pk=337141)
 
     @fudge.patch('mkt.inapp_pay.tasks.chargeback_notify')
-    def test_reversal(self, urlopen, chargeback_notify):
-        urlopen.return_value = self.urlopener('VERIFIED')
+    def test_reversal(self, post, chargeback_notify):
+        post.return_value = Mock(text='VERIFIED')
         con = self.make_contrib(transaction_id=sample_reversal['tracking_id'])
         pay = self.make_payment(contrib=con)
 
@@ -54,8 +54,8 @@ class TestInappIPN(InappPaymentUtil, PaypalTest):
         eq_(response.status_code, 200)
 
     @fudge.patch('mkt.inapp_pay.tasks.chargeback_notify')
-    def test_refund(self, urlopen, chargeback_notify):
-        urlopen.return_value = self.urlopener('VERIFIED')
+    def test_refund(self, post, chargeback_notify):
+        post.return_value = Mock(text='VERIFIED')
         tx = sample_chained_refund['tracking_id']
         con = self.make_contrib(transaction_id=tx)
         pay = self.make_payment(contrib=con)
@@ -67,6 +67,6 @@ class TestInappIPN(InappPaymentUtil, PaypalTest):
         eq_(response.content.strip(), 'Success!')
         eq_(response.status_code, 200)
 
-    def test_completion(self, urlopen):
+    def test_completion(self, post):
         # TODO(Kumar) trigger notify hook from IPN
-        urlopen.return_value = self.urlopener('VERIFIED')
+        post.return_value = Mock(text='VERIFIED')
