d719c65d2b4ae8304b8acafb43a078360b1a9dd4
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-05-24 10:43:45 -0500

    Updates redis in vagrant (bug 736673)

--- vagrant/manifests/classes/redis.pp
+++ vagrant/manifests/classes/redis.pp
@@ -1,14 +1,23 @@
 class redis {
-    package { "redis-server":
-        ensure => installed;
+
+    exec { "sudo add-apt-repository ppa:cmsj/redis-stable":
+        alias => "add-redis-repo"
+    }
+
+    exec { "sudo apt-get update":
+        alias => "apt-update",
+        requires => Exec["add-redis-repo"]
+    }
+
+    exec { "sudo apt-get install redis-server":
+        alias => "install-redis",
+        requires => Exec["apt-update"]
     }
 
     service { "redis-server":
         ensure => running,
         enable => true,
         hasrestart => true,
-        require => [
-            Package['redis-server']
-        ]
+        require => Exec["install-redis"]
     }
 }
