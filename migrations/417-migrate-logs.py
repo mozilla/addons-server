7346001f90aced2913866feb1565e147c5f9964d
Andy McKay <amckay@mozilla.com>
2012-06-01 14:03:12 -0700

    right method name

--- migrations/417-migrate-logs.py
+++ migrations/417-migrate-logs.py
@@ -4,7 +4,7 @@ from django.db import connection, transaction
 def run():
     cursor = connection.cursor()
     cursor.execute('select activity_log_id from log_activity_app;')
-    ids = [r[0] for r in cursor.fetch_all()]
+    ids = [r[0] for r in cursor.fetchall()]
 
     cursor.execute('insert into log_activity_app_mkt '
                    'select * from log_activity_app;')
