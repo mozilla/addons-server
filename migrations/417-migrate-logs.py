c5aeaeb243da373a901807f7c2764709ef788900
Andy McKay <amckay@mozilla.com>
2012-06-01 14:20:21 -0700

    turn off foreign_key_checks

--- migrations/417-migrate-logs.py
+++ migrations/417-migrate-logs.py
@@ -6,9 +6,11 @@ def run():
     cursor.execute('select activity_log_id from log_activity_app;')
     ids = [r[0] for r in cursor.fetchall()]
 
+    cursor.execute('set foreign_key_checks = 0')
     cursor.execute('insert into log_activity_mkt '
                    'select * from log_activity where id IN %(ids)s;',
                    {'ids':ids})
     cursor.execute('insert into log_activity_app_mkt '
                    'select * from log_activity_app;')
+    cursor.execute('set foreign_key_checks = 1')
     transaction.commit()
