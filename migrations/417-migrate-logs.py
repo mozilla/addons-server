95586b8f586e6c9122758c5eb1909a0c47eb213b
Andy McKay <amckay@mozilla.com>
2012-06-01 11:59:32 -0700

    move over the relevant logs migrations/417-migrate-logs.py (bug 760464)

---
+++ migrations/417-migrate-logs.py
@@ -0,0 +1,14 @@
+from django.db import connection, transaction
+
+
+def run():
+    cursor = connection.cursor()
+    cursor.execute('select activity_log_id from log_activity_app;')
+    ids = [r[0] for r in cursor.fetch_all()]
+
+    cursor.execute('insert into log_activity_app_mkt '
+                   'select * from log_activity_app;')
+    cursor.execute('insert into log_activity_mkt '
+                   'select * from log_activity where id IN %(ids)s;',
+                   {'ids':ids})
+    transaction.commit()
