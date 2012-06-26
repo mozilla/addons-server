e1e64f48b4da2c40df72f3105c483fe6aee5b977
Andy McKay <amckay@mozilla.com>
2012-06-26 09:30:50 -0700

    flag cleanup and cope with missing domains

--- migrations/427-update-webapp-domain.py
+++ migrations/427-update-webapp-domain.py
@@ -3,4 +3,5 @@ from mkt.webapps.models import Webapp
 
 def run():
     for app in Webapp.objects.all():
-        app.update(app_domain=Webapp.domain_from_url(app.manifest_url))
+        if app.manifest_url:
+            app.update(app_domain=Webapp.domain_from_url(app.manifest_url))
---
+++ migrations/429-remove-advanced-payments-submission.sql
@@ -0,0 +1,1 @@
+DELETE FROM waffle_flag_mkt WHERE name = 'advanced-payments-submission';
