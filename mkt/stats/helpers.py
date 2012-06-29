7b27bc8051e42cf7e200f35d420b93f98bf8c2e0
Kevin Ngo <ktngo09@gmail.com>
2012-06-29 16:16:37 -0700

    redesign stats pages with side nav bar and refactor stats templates

--- mkt/stats/helpers.py
+++ mkt/stats/helpers.py
@@ -16,5 +16,22 @@ def check_contrib_stats_perms(context, addon):
 
 
 @register.function
+@jinja2.contextfunction
+def stats_url(context, action, metric=None):
+    """
+    Simplifies the templates a bit, no need to pass in addon/inapp into
+    parameters as it is inferred from the context and it makes the function
+    call shorter.
+    """
+    addon = context['addon']
+    if metric:
+        action = '%s_%s' % (metric, action)
+    if 'inapp' in context:
+        action += '_inapp'
+        inapp = context['inapp']
+    return addon.get_stats_url(action=action, inapp=inapp)
+
+
+@register.function
 def url_quote(url):
     return urlquote(url)
