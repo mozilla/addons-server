a54b434bccc2d9b837be864befbe155aad632413
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-07-05 14:09:00 -0700

    rnl.get() no longer uses redis

---
+++ migrations/433-no-redis-flag.sql
@@ -0,0 +1,6 @@
+INSERT INTO waffle_flag_mkt
+    (name, everyone, percent, superusers, staff, authenticated, rollout, note) VALUES
+    ('no-redis', 0, NULL, 0, 0, 0, 0, 'No redis');
+INSERT INTO waffle_flag_amo
+    (name, everyone, percent, superusers, staff, authenticated, rollout, note) VALUES
+    ('no-redis', 0, NULL, 0, 0, 0, 0, 'No redis');
