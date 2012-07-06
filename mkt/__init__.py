83d646652f836fe8ae08b32b24ede23e742b4a21
Chris Van <cvan@mozilla.com>
2012-07-06 15:52:34 -0700

    add list of country stores to Marketplace (bug 763679)

--- mkt/__init__.py
+++ mkt/__init__.py
@@ -1,3 +1,4 @@
+from mkt.constants import regions
 from mkt.constants.submit import *
 
 MKT_CUT = .70
---
+++ mkt/constants/regions.py
@@ -0,0 +1,48 @@
+# Don't know what an ITU MCC is? We'll be using the for carrier billing.
+# Read http://en.wikipedia.org/wiki/List_of_mobile_country_codes
+
+import inspect
+import sys
+
+from tower import ugettext_lazy as _lazy
+
+
+class REGION(object):
+    """A region is like a country but more confusing."""
+    id = None
+    name = None
+    default_currency = 'USD'
+    default_language = 'en-US'
+    adolescent = True
+    mcc = None  # See comment above.
+
+
+class WORLWIDE(REGION):
+    id = 1
+    name = _lazy(u'Worldwide')
+
+
+class USA(REGION):
+    id = 2
+    name = _lazy(u'United States')
+    mcc = 310
+
+
+class BRAZIL(REGION):
+    id = 3
+    name = _lazy(u'Brazil')
+    default_currency = 'BRL'
+    default_language = 'pt-BR'
+    mcc = 724
+
+
+# Create a list of tuples like so:
+#
+#     [('WORLWIDE', <class 'mkt.constants.regions.WORLWIDE'>),
+#      ('BRAZIL', <class 'mkt.constants.regions.BRAZIL'>),
+#      ...]
+#
+REGIONS_CHOICES = sorted(inspect.getmembers(sys.modules[__name__],
+                                            inspect.isclass),
+                         key=lambda x: x[1].id)
+REGIONS_DICT = dict(REGIONS_CHOICES)
