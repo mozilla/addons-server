d3c99336d6809c4bd2c92c6f6c6e3e53026fdd66
Matt Basta <me@mattbasta.com>
2012-07-12 13:44:00 -0700

    Unmark char counters as errors after form validation (bug 769244)
    After form validation, character counters are wrapped in a block with
    the class "error". Because of this, even after the field has been
    fixed to the correct length, the character counter is highlighted in
    red. This marks the field as not being an error after the length has
    been adjusted down.
    
    It's possible that the text field also has other errors, but it's
    assumed that if the user has modified the field and it's within the
    length requirements, it shouldn't continue to be flagged, anyway, so
    that case doesn't need to be dealt with.

--- media/js/mkt/utils.js
+++ media/js/mkt/utils.js
@@ -77,11 +77,15 @@ function initCharCount() {
         var $el = $(el),
             val = $el.val(),
             max = parseInt(cc.attr('data-maxlength'), 10),
-            left = max - val.length;
+            left = max - val.length,
+            cc_parent = cc.parent();
         // L10n: {0} is the number of characters left.
         cc.html(format(ngettext('<b>{0}</b> character left.',
                                 '<b>{0}</b> characters left.', left), [left]))
           .toggleClass('error', left < 0);
+        if(left >= 0 && cc_parent.hasClass('error')) {
+            cc_parent.removeClass('error');
+        }
     };
     $('.char-count').each(function() {
         var $cc = $(this),
