2c2e04533efbb79d201610929a2d0b55548f4f6e
Chris Van <cvan@mozilla.com>
2012-07-12 11:17:06 -0700

    locale_url can be dumb for Marketplace admin

--- apps/amo/helpers.py
+++ apps/amo/helpers.py
@@ -58,6 +58,8 @@ def babel_datetime(t, format='medium'):
 @register.function
 def locale_url(url):
     """Take a URL and give it the locale prefix."""
+    if settings.MARKETPLACE and settings.REGION_STORES:
+        return url
     prefixer = urlresolvers.get_url_prefix()
     script = prefixer.request.META['SCRIPT_NAME']
     parts = [script, prefixer.locale, url.lstrip('/')]
