f4586a23dfffbfdb73383d5f003c1350e54c35cb
Chris Van <cvan@mozilla.com>
2012-08-05 17:55:42 -0700

    add new stars to reviews on desktop

--- mkt/webapps/models.py
+++ mkt/webapps/models.py
@@ -406,7 +406,8 @@ class ContentRating(amo.models.ModelBase):
     """
     addon = models.ForeignKey('addons.Addon', related_name='content_ratings')
     ratings_body = models.PositiveIntegerField(
-        choices=[(k, rb.name) for k, rb in ratingsbodies.RATINGS_BODIES.items()],
+        choices=[(k, rb.name) for k, rb in
+                 ratingsbodies.RATINGS_BODIES.items()],
         null=False)
     rating = models.PositiveIntegerField(null=False)
 
