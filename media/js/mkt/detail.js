65588bd529a669db10c5b2b34ca06585bbd1e5dc
Chris Van <cvan@mozilla.com>
2012-08-06 02:20:13 -0700

    app detail page - polish tile header, improve summary+description+dev comments section, append ID to location.hash

--- media/js/mkt/detail.js
+++ media/js/mkt/detail.js
@@ -1,11 +1,18 @@
 (function() {
-    function toggle($e) {
+    z.page.on('click', '.show-toggle', _pd(function() {
+        var $this = $(this),
+            newTxt = $this.attr('data-toggle-text');
+        // Toggle "more..." or "less..." text.
+        $this.attr('data-toggle-text', $this.text());
+        $this.text(newTxt);
         // Toggle description + developer comments.
-        $e.toggleClass('expanded').closest('section').find('.collapse').toggleClass('show');
-    }
-    z.page.on('click', 'a.collapse', _pd(function() {
-        toggle($(this));
+        $this.closest('.blurbs').find('.collapsed').toggle();
     })).on('click', '.approval-pitch', _pd(function() {
         $('#preapproval-shortcut').submit();
+    }));
+
+    // When I click on the icon, append `#id=<id>` to the URL.
+    z.page.on('click', '.product-details .icon', _pd(function() {
+        window.location.hash = 'id=' + $('.product').data('product')['id'];
     }));
 })();
