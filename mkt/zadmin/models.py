e42e0711d28649605c5b51803b977db8f2fb2449
Matt Basta <me@mattbasta.com>
2012-08-09 15:57:32 -0700

    WIP featured app multiselect

--- mkt/zadmin/models.py
+++ mkt/zadmin/models.py
@@ -10,8 +10,15 @@ class FeaturedApp(models.Model):
     app = models.ForeignKey(Webapp, null=False)
     category = models.ForeignKey(Category, null=True)
     is_sponsor = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'zadmin_featuredapp'
+
+
+class FeaturedAppRegionExclusion(models.Model):
+    featured_app = models.ForeignKey(FeaturedApp, null=False)
     region = models.PositiveIntegerField(default=mkt.regions.WORLDWIDE.id,
                                          db_index=True)
 
     class Meta:
-        db_table = 'zadmin_featuredapp'
+        db_table = 'zadmin_featuredapp_regionexclusion'
