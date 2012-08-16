9467b5e16182ebc8ea3f5dd867d32628d43ceb47
Chris Van <cvan@mozilla.com>
2012-08-16 14:39:30 -0700

    exclude games in Brazil region (bug 783171)

---
+++ migrations/453-exclude-games-in-brazil.py
@@ -0,0 +1,11 @@
+import mkt
+from mkt.webapps.models import AddonExcludedRegion as AER, Webapp
+
+
+def run():
+    """Exclude Games in Brazil."""
+    games = Webapp.category('games')
+    if games:
+        apps = Webapp.objects.filter(categories=games.id)
+        for app in apps:
+            AER.objects.create(addon=app, region=mkt.regions.BR.id)
