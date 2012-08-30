2bcfef85ed8fa0d024edfde3c225574f62b07344
Rob Hudson <chudson@mozilla.com>
2012-08-30 15:29:22 -0700

    Fixed bad SQL comment

--- migrations/462-move-is-packaged.sql
+++ migrations/462-move-is-packaged.sql
@@ -3,4 +3,4 @@ CREATE INDEX `addons_is_packaged` ON `addons` (`is_packaged`);
 -- Move existing flags from files to addons.
 UPDATE files, versions, addons SET addons.is_packaged=1 WHERE files.version_id=versions.id AND versions.addon_id=addons.id AND files.is_packaged=1;
 -- Drop files column.
---ALTER TABLE `files` DROP COLUMN `is_packaged`;
+-- ALTER TABLE `files` DROP COLUMN `is_packaged`;
