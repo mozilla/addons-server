8a3c9b9d233fe9688edee4908dab19370d5fad4c
Andy McKay <amckay@mozilla.com>
2012-09-04 12:10:06 -0700

    run the pre-approval now (bug 782710)

--- migrations/452-nuke-pre-approval.sql
+++
@@ -1,5 +0,0 @@
-/*
- * This will need to be run once all the PayPal flow is removed.
- * UPDATE users_preapproval SET paypal_key = '';
- * UPDATE addons_premium SET paypal_permissions_token = '';
- */
---
+++ migrations/466-nuke-pre-approval.sql
@@ -0,0 +1,2 @@
+ UPDATE users_preapproval SET paypal_key = '';
+ UPDATE addons_premium SET paypal_permissions_token = '';
