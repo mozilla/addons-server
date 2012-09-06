8eea3878b05f4bdca5bb2ab2708481ed413ac3f2
Chris Van <cvan@mozilla.com>
2012-09-06 13:38:03 -0700

    delete dupe ImageAssets (bug 789152)

---
+++ migrations/468-delete-dupe-assets.py
@@ -0,0 +1,17 @@
+from amo.utils import chunked
+
+from mkt.constants import APP_IMAGE_SIZES
+from mkt.webapps.models import ImageAsset, Webapp
+
+
+SIZE_SLUGS = [size['slug'] for size in APP_IMAGE_SIZES]
+
+
+def run():
+    """Delete duplicate image assets."""
+    for chunk in chunked(Webapp.objects.all(), 50):
+        for app in chunk:
+            for slug in SIZE_SLUGS:
+                assets = ImageAsset.objects.filter(addon=app, slug=slug)
+                for asset in assets[1:]:
+                    asset.delete()
