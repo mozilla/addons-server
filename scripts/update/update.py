56d3d200dbdd19b8a15a12bf756a7cb18e8884f2
Jeremiah Orem <jeremy.orem@gmail.com>
2012-09-13 15:50:47 -0700

    We need our lib64 symlink to be relative.

--- scripts/update/update.py
+++ scripts/update/update.py
@@ -15,6 +15,7 @@ _src_dir = lambda *p: os.path.join(settings.SRC_DIR, *p)
 def create_virtualenv(ctx):
     venv = settings.VIRTUAL_ENV
     ctx.local("virtualenv --distribute --system-site-packages --never-download %s" % venv)
+    ctx.local("rm -f %s/lib64 && ln -s ./lib %s/lib64" % (venv, venv))
     ctx.local("%s/bin/pip install -I --download-cache=/tmp/pip-cache -i %s -r %s/requirements/prod.txt" %
                 (venv, settings.PYPI_MIRROR, settings.SRC_DIR))
     ctx.local("virtualenv --relocatable %s" % venv)
