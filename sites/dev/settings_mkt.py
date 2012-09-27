c020020b825ddd388d270563213c8c9d69bf7fc3
Jeremiah Orem <jeremy.orem@gmail.com>
2012-09-27 10:46:22 -0700

    Keep settings with project.

---
+++ sites/dev/settings_mkt.py
@@ -0,0 +1,135 @@
+from lib.settings_base import *
+from mkt.settings import *
+from settings_local_base import *
+
+import private_mkt
+
+SERVER_EMAIL = 'zmarketplacedev@addons.mozilla.org'
+
+DOMAIN = "marketplace-dev.allizom.org"
+SITE_URL = 'https://marketplace-dev.allizom.org'
+SERVICES_URL = SITE_URL
+STATIC_URL = 'https://marketplace-dev-cdn.allizom.org/'
+LOCAL_MIRROR_URL = '%s_files' % STATIC_URL
+MIRROR_URL = LOCAL_MIRROR_URL
+
+CSP_STATIC_URL = STATIC_URL[:-1]
+CSP_IMG_SRC = CSP_IMG_SRC + (CSP_STATIC_URL,)
+CSP_SCRIPT_SRC = CSP_SCRIPT_SRC + (CSP_STATIC_URL,)
+CSP_STYLE_SRC = CSP_STYLE_SRC + (CSP_STATIC_URL,)
+
+ADDON_ICON_URL = "%s/%s/%s/images/addon_icon/%%d-%%d.png?modified=%%s" % (STATIC_URL, LANGUAGE_CODE, DEFAULT_APP)
+ADDON_ICON_URL = STATIC_URL + 'img/uploads/addon_icons/%s/%s-%s.png?modified=%s'
+PREVIEW_THUMBNAIL_URL = (STATIC_URL +
+        'img/uploads/previews/thumbs/%s/%d.png?modified=%d')
+PREVIEW_FULL_URL = (STATIC_URL +
+        'img/uploads/previews/full/%s/%d.%s?modified=%d')
+# paths for uploaded extensions
+FILES_URL = STATIC_URL + "%s/%s/downloads/file/%d/%s?src=%s"
+
+SESSION_COOKIE_DOMAIN = ".%s" % DOMAIN
+
+# paths for uploaded extensions
+IMAGEASSET_FULL_URL = STATIC_URL + 'img/uploads/imageassets/%s/%d.png?modified=%d'
+USERPICS_URL = STATIC_URL + 'img/uploads/userpics/%s/%s/%s.png?modified=%d'
+COLLECTION_ICON_URL = STATIC_URL + '/img/uploads/collection_icons/%s/%s.png?m=%s'
+
+MEDIA_URL = STATIC_URL + 'media/'
+ADDON_ICONS_DEFAULT_URL = MEDIA_URL + 'img/hub'
+ADDON_ICON_BASE_URL = MEDIA_URL + 'img/icons/'
+
+INAPP_IMAGE_URL = STATIC_URL + 'inapp-image'
+
+
+CACHE_PREFIX = 'dev.mkt.%s' % CACHE_PREFIX
+CACHE_MIDDLEWARE_KEY_PREFIX = CACHE_PREFIX
+CACHES['default']['KEY_PREFIX'] = CACHE_PREFIX
+
+SYSLOG_TAG = "http_app_addons_marketplacedev"
+SYSLOG_TAG2 = "http_app_addons_marketplacedev_timer"
+SYSLOG_CSP = "http_app_addons_marketplacedev_csp"
+
+STATSD_PREFIX = 'addons-marketplacedev'
+
+## Celery
+BROKER_HOST = private_mkt.BROKER_HOST
+BROKER_PORT = private_mkt.BROKER_PORT
+BROKER_USER = private_mkt.BROKER_USER
+BROKER_PASSWORD = private_mkt.BROKER_PASSWORD
+BROKER_VHOST = private_mkt.BROKER_VHOST 
+CELERY_IGNORE_RESULT = True
+CELERY_DISABLE_RATE_LIMITS = True
+CELERYD_PREFETCH_MULTIPLIER = 1
+
+# sandbox
+PAYPAL_PAY_URL = 'https://svcs.sandbox.paypal.com/AdaptivePayments/'
+PAYPAL_FLOW_URL = 'https://www.sandbox.paypal.com/webapps/adaptivepayment/flow/pay'
+PAYPAL_API_URL = 'https://api-3t.sandbox.paypal.com/nvp'
+PAYPAL_EMAIL = private_mkt.PAYPAL_EMAIL
+PAYPAL_APP_ID = private_mkt.PAYPAL_APP_ID
+PAYPAL_PERMISSIONS_URL = 'https://svcs.sandbox.paypal.com/Permissions/'
+PAYPAL_CGI_URL = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
+PAYPAL_EMBEDDED_AUTH = {
+    'USER': private_mkt.PAYPAL_EMBEDDED_AUTH_USER,
+    'PASSWORD': private_mkt.PAYPAL_EMBEDDED_AUTH_PASSWORD,
+    'SIGNATURE': private_mkt.PAYPAL_EMBEDDED_AUTH_SIGNATURE,
+}
+
+PAYPAL_CGI_AUTH = { 'USER': private_mkt.PAYPAL_CGI_AUTH_USER,
+                    'PASSWORD': private_mkt.PAYPAL_CGI_AUTH_PASSWORD,
+                    'SIGNATURE': private_mkt.PAYPAL_CGI_AUTH_SIGNATURE,
+}
+
+PAYPAL_CHAINS = (
+    (30, private_mkt.PAYPAL_CHAINS_EMAIL),
+)
+
+WEBAPPS_RECEIPT_KEY = private_mkt.WEBAPPS_RECEIPT_KEY
+WEBAPPS_RECEIPT_URL = private_mkt.WEBAPPS_RECEIPT_URL
+
+APP_PREVIEW = True
+
+WEBAPPS_UNIQUE_BY_DOMAIN = False
+
+#Bug 744268
+INAPP_VERBOSE_ERRORS = True
+
+INAPP_REQUIRE_HTTPS = False
+
+SENTRY_DSN = private_mkt.SENTRY_DSN
+
+#Bug 747548
+METRICS_SERVER = 'https://data.mozilla.com/'
+
+WEBAPPS_PUBLIC_KEY_DIRECTORY = NETAPP_STORAGE + '/public_keys'
+INAPP_IMAGE_PATH = NETAPP_STORAGE + '/inapp-image'
+
+INAPP_KEY_PATHS = private_mkt.INAPP_KEY_PATHS
+
+WEBAPP_MANIFEST_NAME = 'Mozilla Marketplace (Dev)'
+
+SECLUSION_HOSTS = ('https://payments-dev.allizom.org',)
+
+PAYPAL_LIMIT_PREAPPROVAL = False
+
+MIDDLEWARE_CLASSES += (
+    'amo.middleware.NoConsumerMiddleware',
+    'amo.middleware.LoginRequiredMiddleware',
+)
+
+VALIDATOR_IAF_URLS = ['https://marketplace.mozilla.org', 
+                      'https://marketplace.allizom.org', 
+                      'https://marketplace-dev.allizom.org']
+
+AMO_LANGUAGES = AMO_LANGUAGES + ('dbg',)
+LANGUAGES = lazy(lazy_langs, dict)(AMO_LANGUAGES)
+LANGUAGE_URL_MAP = dict([(i.lower(), i) for i in AMO_LANGUAGES])
+
+BLUEVIA_SECRET = private_mkt.BLUEVIA_SECRET 
+
+#Bug 748403
+SIGNING_SERVER = private_mkt.SIGNING_SERVER
+SIGNING_SERVER_ACTIVE = True
+
+METLOG_CONF['logger'] = 'addons-marketplace-dev'
+METLOG = client_from_dict_config(METLOG_CONF)
