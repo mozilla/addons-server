1e03a6ba2a5277c6e665cde994e0ced8b15192c0
Matt Basta <me@mattbasta.com>
2012-10-03 11:14:43 -0700

    Fix migration to avoid unicode errors
    If the app has a name that's not ASCII, you'd get an encoding error.

--- migrations/482-generate-featured-image-assets.py
+++ migrations/482-generate-featured-image-assets.py
@@ -9,4 +9,4 @@ def run():
     for chunk in chunked(Webapp.objects.all(), 50):
         for app in chunk:
             generate_image_assets.delay(app, slug='featured_tile')
-            print u'Generated feature tile for %s' % app
+            print u'Generated feature tile for app %d' % app.id
