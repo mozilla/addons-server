28c16ecbe76d212efee840ffa8318145319accb2
Davor Spasovski <dspasovski@mozilla.com>
2012-10-10 14:03:17 -0700

    empty suggestions fix

--- media/js/mkt/mkt_suggestions.js
+++ media/js/mkt/mkt_suggestions.js
@@ -24,9 +24,9 @@
             url: settings['$results'].attr('data-src'),
             data: settings['$form'].serialize() + '&cat=' + settings.category,
             newItems: function(formdata, items) {
-                var eventName;
+                var eventName,
+                    litems = '';
                 if (items !== undefined) {
-                    var ul = '';
                     $.each(items, function(i, item) {
                         var d = {
                             url: escape_(item.url) || '#'
@@ -34,14 +34,16 @@
                         if (item.name) {
                             d.name = escape_(item.name);
                             // Append the item only if it has a name.
-                            ul += li_item(d);
+                            litems += li_item(d);
                         }
                     });
-                    settings['$results'].find('ul').html(ul);
+                    settings['$results'].find('ul').html(litems);
                 }
-                settings['$results'].addClass('visible')
-                                    .trigger('resultsUpdated', [items]);
-                $('#site-header').addClass('suggestions');
+                if (litems) {
+                    settings['$results'].addClass('visible')
+                                        .trigger('resultsUpdated', [items]);
+                    $('#site-header').addClass('suggestions');
+                }
             }
         });
 
