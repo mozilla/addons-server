1728d0c67d3c62dff5ad69989601e59198fdc5fc
Chris Van <cvan@mozilla.com>
2012-10-10 14:15:44 -0700

    tweak label for "desktop_apps" emailer

--- apps/zadmin/forms.py
+++ apps/zadmin/forms.py
@@ -36,7 +36,8 @@ class DevMailerForm(happyforms.Form):
                 ('apps', 'Developers of active apps (not add-ons)'),
                 ('payments',
                  'Developers of active apps (not add-ons) with payments'),
-                ('desktop_apps', 'Developers of active apps for desktop only')]
+                ('desktop_apps',
+                 'Developers of active apps supported on desktop')]
     recipients = forms.ChoiceField(choices=_choices, required=True)
     subject = forms.CharField(widget=forms.TextInput(attrs=dict(size='100')),
                               required=True)
