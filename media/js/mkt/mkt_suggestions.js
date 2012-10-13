4882fb5960c0c332caf445655abaccd38ddfac2d
Davor Spasovski <dspasovski@mozilla.com>
2012-10-12 17:46:10 -0700

    potential mobile suggestions fix (bug 800375)

--- media/js/mkt/mkt_suggestions.js
+++ media/js/mkt/mkt_suggestions.js
@@ -7,8 +7,6 @@
                                                  processResults, 'MKT');
     }
 
-    var previous_request;
-
     function processResults(settings) {
         if (!settings) {
             return;
@@ -18,14 +16,13 @@
             '<li><a href="{url}"><span>{name}</span></a></li>'
         );
 
-        // Note that if ajaxCache doesn't need to make a new request, it will
-        // return `undefined`.
-        var new_request = $.ajaxCache({
+        $.ajaxCache({
             url: settings['$results'].attr('data-src'),
             data: settings['$form'].serialize() + '&cat=' + settings.category,
             newItems: function(formdata, items) {
                 var eventName,
-                    litems = '';
+                    listitems = '';
+
                 if (items !== undefined) {
                     $.each(items, function(i, item) {
                         var d = {
@@ -34,22 +31,19 @@
                         if (item.name) {
                             d.name = escape_(item.name);
                             // Append the item only if it has a name.
-                            litems += li_item(d);
+                            listitems += li_item(d);
                         }
                     });
-                    settings['$results'].find('ul').html(litems);
                 }
-                if (litems) {
+
+                // Populate suggestions and make them visible.
+                if (listitems) {
+                    settings['$results'].find('ul').html(listitems);
                     settings['$results'].addClass('visible')
                                         .trigger('resultsUpdated', [items]);
                     $('#site-header').addClass('suggestions');
                 }
             }
         });
-
-        if (previous_request) {
-            previous_request.abort();
-        }
-        previous_request = new_request;
     }
 })();
