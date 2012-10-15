6a85d3ee011aa161c56fbcea3cc827273e091fd6
Chris Van <cvan@mozilla.com>
2012-10-15 14:07:01 -0700

    better migration for fixed rating sort (bug 787162)

--- migrations/488-reindex-ratings.py
+++ migrations/488-reindex-ratings.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python
+
 from amo.utils import chunked
 
-from reviews.models import Review
+from addons.models import Addon
 
 
 def run():
     """Fix app ratings in ES (bug 787162)."""
-    for chunk in chunked(Review.objects.all(), 50):
-        # Emit post-save signals so ES gets the correct bayesian ratings.
-        [review.save() for review in chunk]
+    for chunk in chunked(Addon.objects.filter(total_reviews__gt=0), 50):
+        for addon in chunk:
+            # Emit post-save signals so ES gets the correct bayesian ratings.
+            # One review is enough to fire off the tasks.
+            try:
+                addon.reviews[0].save()
+            except IndexError:
+                # It's possible that `total_reviews` is a liar.
+                print '- No reviews found for %s ...' % addon
