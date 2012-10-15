e1235edd3a206f8dc83fa27e360d15533d87b88a
jrconlin <jrconlin+git@mozilla.com>
2012-10-15 12:13:36 -0700

    Bug 789288 - Mkt specific changes for Persona login.
    This patch limits changes to the mkt branch.
    * Adds email to the page user info block.
    * Waits for persona initialization
    * updated test_helpers to include new value
    * added r fixes
    * added legacy account csrf handler

--- media/js/mkt/login.js
+++ media/js/mkt/login.js
@@ -1,28 +1,11 @@
 $(window).bind('login', function() {
     $('#login').addClass('show');
 }).on('click', '.browserid', function(e) {
+    $(this).addClass('loading-submit');
+    navigator.id.request({termsOfService: '/tos.html',
+                          privacyPolicy: '/privacy.html'});
     e.preventDefault();
-    var $this = $(this),
-        href = $this.attr('href'),
-        qs = z.getVars(href.substring(href.indexOf('?')));
-    $this.addClass('loading-submit');
-    $.when(z.login(qs.to))
-     .done(function() {
-         $this.removeClass('loading-submit');
-     })
-     .fail(function(err) {
-         $this.removeClass('loading-submit');
-         if (err.privs) {
-             $.post('/csrf', function(r) {
-                $('#login form').append($('<input>',
-                                        {type:'hidden', value:r.csrf,
-                                         name:'csrfmiddlewaretoken'}));
-             });
-             $('#login').addClass('show old');
-         } else {
-             alert(err.msg);
-         }
-     });
+
 });
 // Hijack the login form to send us to the right place
 $("#login form").submit(function(e) {
@@ -30,3 +13,83 @@ $("#login form").submit(function(e) {
         action = $this.attr('action') + format("?to={0}", window.location.pathname);
     $this.attr('action', action);
 });
+$(".logout").bind('click', function(e) {
+    // NOTE: Real logout operations happen on the action of the Logout
+    // link/button. This just tells Persona to clean up it's data.
+    if (navigator.id) {
+        navigator.id.logout();
+    }
+});
+function gotVerifiedEmail(assertion) {
+    if (assertion) {
+        $.ajax({
+            url: $('body').data('login-url'),
+            type: 'POST',
+            data: {'assertion': assertion},
+            success: finishLogin,
+            error: function(jqXHR, textStatus, error) {
+                // ask for additional credential info.
+                var err = {};
+                if (jqXHR.status == 400) {
+                    $.post('/csrf', function(r) {
+                        $('#login form').append($('<input>',
+                                        {type:'hidden', value:r.csrf,
+                                         name:'csrfmiddlewaretoken'}));
+                     });
+                     $('#login').addClass('show old');
+                } else {
+                    err.msg = jqXHR.responseText;
+                    if(!err.msg) {
+                        err.msg = gettext("BrowserID login failed. Maybe you don't have an account under that email address?") + " " + textStatus + " " + error;
+                    }
+                    alert(err.msg);
+                }
+                $.Deferred().reject(err);
+            }
+        })
+    } else {
+        $('.loading-submit').removeClass('loading-submit');
+    }
+}
+
+function finishLogin() {
+    var to = z.getVars(window.location.search).to;
+    $.Deferred().resolve();
+    if (to && to[0] == '/') {
+        window.location = to;
+    } else {
+        window.location.reload();
+    }
+}
+
+function init_persona() {
+    // Persona may not be completely initialized at page ready.
+    // This can cause the .watch function to not be set.
+    // If the attribute isn't present, indicate to the user that things aren't
+    // quite ready.
+    if(navigator.id) {
+        if ($('body').data('pers-timeout')) {
+            clearTimeout($('body').data('pers-timeout'));
+        }
+        $('.browserid').css('cursor', 'pointer');
+        var email = '';
+        if ($('body').data('user')) {
+            email = $('body').data('user').email;
+        }
+        navigator.id.watch({
+            loggedInUser: email,
+            onlogin: function(assert) {
+                gotVerifiedEmail(assert);
+            },
+            onlogout: function() {
+            }
+        })
+    } else {
+        $('.browserid').css('cursor', 'wait');
+        if (!$('body').data('pers-timeout')) {
+            $('body').data('pers-timeout', setInterval(init_persona, 500));
+        }
+    }
+}
+
+$(document).ready(init_persona);
