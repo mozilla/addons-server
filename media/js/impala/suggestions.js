998efb5843f53076d1ae004085058257513923a1
Davor Spasovski <dspasovski@mozilla.com>
2012-10-15 11:39:57 -0700

    race condition tweaking

--- media/js/impala/suggestions.js
+++ media/js/impala/suggestions.js
@@ -158,7 +158,7 @@ $.fn.searchSuggestions = function($results, processCallback, searchType) {
                 gestureHandler($self);
                 inputHandler($self);
                 return;
-            }, 500);
+            }, 150);
         });
     }
 
