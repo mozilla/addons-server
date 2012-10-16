6cbdd38f26bdd78a09e59f5769c021b2a3ce4b9b
Andy McKay <amckay@mozilla.com>
2012-10-16 13:37:55 -0700

    Revert "temporary to test -dev"
    This reverts commit 44336b89354a20f124e9f4b670c22dc1cd195ee8.

--- migrations/489-dummy.py
+++
@@ -1,9 +0,0 @@
-def run():
-    print '--- dummy migration started'
-    import sys
-    print sys.path
-    import django
-    print django.__file__
-    from django.conf import settings
-    print settings.ROOT
-    print '--- dummy migration done'
