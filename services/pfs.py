6bbc5758ed0784aa4373cd6e06a795765a7f8480
Wil Clouser <clouserw@gmail.com>
2012-10-17 07:54:58 -0700

    Update PFS for Java; bug 802297

--- services/pfs.py
+++ services/pfs.py
@@ -218,7 +218,7 @@ def get_output(data):
 
         plugin.update(
             name='Java Runtime Environment',
-            version='1.7 u7',
+            version='1.7 u9',
             manualInstallationURL='http://java.com/downloads',
             InstallerShowsUI='false',
             needsRestart='false')
@@ -231,12 +231,12 @@ def get_output(data):
             plugin.update(
                 guid='{fbe640ef-4375-4f45-8d79-767d60bf75b8}',
                 InstallerLocation='http://java.com/firefoxjre_exe',
-                InstallerHash='sha1:661a55518096c1609b5e297d5173c0f61c9cb79f')
+                InstallerHash='sha1:659fa7ccdcfa8fe5a363379bd660edc7af4dd8b4')
         elif g['clientOS'].startswith('Win'):
             plugin.update(
                 guid='{92a550f2-dfd2-4d2f-a35d-a98cfda73595}',
                 InstallerLocation='http://java.com/firefoxjre_exe',
-                InstallerHash='sha1:661a55518096c1609b5e297d5173c0f61c9cb79f',
+                InstallerHash='sha1:659fa7ccdcfa8fe5a363379bd660edc7af4dd8b4',
                 XPILocation='http://java.com/jre-install.xpi')
         else:
             plugin.update(
