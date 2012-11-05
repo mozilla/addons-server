91a7706ae47207993cabeb8f194bbb46de3fc2b2
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-11-05 08:53:35 -0800

    update vagrant to use virtualenv schematic
    fixes #491

--- vagrant/bin/start.sh
+++ vagrant/bin/start.sh
@@ -5,8 +5,8 @@ echo "Running ./project/vagrant/bin/start.sh"
 cd ~/project
 echo "Seeding product details JSON..."
 ./scripts/seed-prod-details.sh
-echo 'Checking for DB migrations...'
-python ./vendor/src/schematic/schematic migrations/
+echo "Checking for DB migrations..."
+python schematic migrations/
 if [ $? -eq 0 ]; then
     echo "Running python manage.py runserver 0.0.0.0:8000"
     python manage.py runserver 0.0.0.0:8000
