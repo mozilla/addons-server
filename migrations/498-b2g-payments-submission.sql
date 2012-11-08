f7433dfc8905c1cf28b16ddf40654af86c29e44e
Chris Van <cvan@mozilla.com>
2012-11-08 11:50:03 -0800

    Add timestamps to waffle migration

--- migrations/498-b2g-payments-submission.sql
+++ migrations/498-b2g-payments-submission.sql
@@ -1,3 +1,4 @@
-INSERT INTO `waffle_switch_mkt` (name, active, note)
+INSERT INTO `waffle_switch_mkt` (name, active, note, created, modified)
     VALUES ('allow-b2g-paid-submission', 0,
-            'Enable this to allow paid apps in the submission process.');
+            'Enable this to allow paid apps in the submission process.',
+            NOW(), NOW());
