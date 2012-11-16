ae22f6cba16c1b102fb86a11a296de1eb4593357
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-11-16 14:35:14 -0600

    Force gaia in middleware to workaround bug 812224

--- mkt/site/middleware.py
+++ mkt/site/middleware.py
@@ -276,6 +276,8 @@ class DeviceDetectionMiddleware(object):
 
 class AlwaysMobile(object):
     """Middleware for development that forces all responses into mobile mode."""
+    # TODO(Kumar) remove this when bug 812224 is fixed.
 
     def process_request(self, request):
         request.MOBILE = True
+        request.GAIA = True
