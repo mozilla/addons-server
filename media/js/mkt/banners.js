860d25af3e22ef7e1c321809f4e7eb336e9f61c5
Chris Van <cvan@mozilla.com>
2012-12-06 12:39:31 -0800

    Revert "fix filters position and cancel button (bug 818952, bug 818939)"
    This reverts commit 8a874d292a8267b02aff99ae5791d9a525c203bc.

--- media/js/mkt/banners.js
+++ media/js/mkt/banners.js
@@ -1,8 +1,6 @@
 (function() {
-    var $incompatible = $('.incompatible-browser');
-
-    if ($incompatible.length && (!z.capabilities.localStorage || !localStorage.seen_beta_pitch)) {
-        $incompatible.addClass('active');
+    if (!z.capabilities.localStorage || !localStorage.seen_beta_pitch) {
+        $('.incompatible-browser').addClass('active');
         z.body.addClass('incompatible');
     }
 
@@ -11,13 +9,13 @@
         if (z.capabilities.localStorage) {
             localStorage.seen_beta_pitch = '1';
         }
-        $incompatible.removeClass('active');
+        $('.incompatible-browser').removeClass('active');
         z.body.removeClass('incompatible');
     }).on('click', '.incompatible.button:not(.firefoxos)', _pd(function(e) {
         if (z.capabilities.localStorage) {
             delete localStorage.seen_beta_pitch;
         }
-        $incompatible.addClass('active');
+        $('.incompatible-browser').addClass('active');
         z.body.addClass('incompatible');
     }));
 
