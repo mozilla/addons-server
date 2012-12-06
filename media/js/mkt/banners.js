6d52227b5bdb9df59d2e69a6930f60d2e221e255
Chris Van <cvan@mozilla.com>
2012-12-06 00:22:13 -0800

    clean up filters a bit when Android banner shown

--- media/js/mkt/banners.js
+++ media/js/mkt/banners.js
@@ -1,6 +1,7 @@
 (function() {
     if (!z.capabilities.localStorage || !localStorage.seen_beta_pitch) {
         $('.incompatible-browser').addClass('active');
+        z.body.addClass('incompatible');
     }
 
     // Clicking cancel should dismiss notification boxes.
@@ -9,11 +10,13 @@
             localStorage.seen_beta_pitch = '1';
         }
         $('.incompatible-browser').removeClass('active');
+        z.body.removeClass('incompatible');
     }).on('click', '.incompatible.button:not(.firefoxos)', _pd(function(e) {
         if (z.capabilities.localStorage) {
             delete localStorage.seen_beta_pitch;
         }
         $('.incompatible-browser').addClass('active');
+        z.body.addClass('incompatible');
     }));
 
 })();
