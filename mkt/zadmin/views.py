c291cd17a7af28d4aa47385eb41709826bb6645b
Allen Short <ashort@mozilla.com>
2012-12-06 14:03:20 -0800

    Revert "Require explicit saving for featured apps changes. (bug 807818)"
    This reverts commit 9bd4eb603dc3fb3072bdbb43a774d461b18b9043.

--- mkt/zadmin/views.py
+++ mkt/zadmin/views.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 
 import jingo
 
@@ -26,6 +25,9 @@ from mkt.zadmin.models import (FeaturedApp, FeaturedAppCarrier,
                                FeaturedAppRegion)
 
 
+
+@transaction.commit_on_success
+@write
 @any_permission_required([('Admin', '%'),
                           ('FeaturedApps', '%')])
 def featured_apps_admin(request):
@@ -47,95 +49,84 @@ def ecosystem(request):
     return jingo.render(request, 'zadmin/ecosystem.html', ctx)
 
 
-@transaction.commit_on_success
-@write
 @any_permission_required([('Admin', '%'),
                           ('FeaturedApps', '%')])
 def featured_apps_ajax(request):
-    cat = request.REQUEST.get('category', None) or None
-    if cat:
-        cat = int(cat)
-    if request.method == 'POST':
+    if request.method == 'GET':
+        cat = request.GET.get('category', None) or None
+        if cat:
+            cat = int(cat)
+    elif request.method == 'POST':
         if not acl.action_allowed(request, 'FeaturedApps', 'Edit'):
             raise PermissionDenied
+        cat = request.POST.get('category', None) or None
+        if cat:
+            cat = int(cat)
         deleteid = request.POST.get('delete', None)
         if deleteid:
             FeaturedApp.uncached.filter(category__id=cat,
                                        app__id=int(deleteid)).delete()
-        bits = request.POST.get('save', None)
-        if bits:
-            appdata = json.loads(bits)
-            regions = set(appdata.get('regions', ()))
-            carriers = set(appdata.get('carriers', ()))
-            startdate = appdata.get('startdate', None)
-            enddate = appdata.get('enddate', None)
-            fa, created = FeaturedApp.uncached.get_or_create(
-                category_id=cat, app_id=appdata['id'])
+        appid = request.POST.get('add', None)
+        if appid:
+            app, created = FeaturedApp.uncached.get_or_create(category_id=cat,
+                                                              app_id=int(appid))
             if created:
                 FeaturedAppRegion.objects.create(
-                    featured_app=fa, region=mkt.regions.WORLDWIDE.id)
-            if regions or carriers:
-                fa.regions.exclude(region__in=regions).delete()
-                to_create = regions - set(fa.regions.filter(region__in=regions)
-                                          .values_list('region', flat=True))
-                excluded_regions = [e.region for e in
-                                    fa.app.addonexcludedregion.all()]
-                for i in to_create:
-                    if i in excluded_regions:
-                        continue
-                    FeaturedAppRegion.objects.create(featured_app=fa, region=i)
-
-                fa.carriers.exclude(carrier__in=carriers).delete()
-                to_create = carriers - set(
-                    fa.carriers.filter(carrier__in=carriers)
-                    .values_list('carrier', flat=True))
-                for c in to_create:
-                    FeaturedAppCarrier.objects.create(featured_app=fa,
-                                                      carrier=c)
-
-            if startdate:
-                fa.start_date = datetime.datetime.strptime(startdate,
-                                                           '%Y-%m-%d')
-            else:
-                fa.start_date = None
-            if enddate:
-                fa.end_date = datetime.datetime.strptime(enddate,
-                                                         '%Y-%m-%d')
-            else:
-                fa.end_date = None
-            fa.save()
-            return HttpResponse("saved")
-    extras = json.loads(request.GET.get('extras', '[]'))
-    apps_regions_carriers = [
-        [UnsavedFeaturedApp(x['id'],
-                            x['startdate'],
-                            x['enddate']),
-         x['regions'],
-         [],
-         x['carriers'],
-         True]
-        for x in extras]
+                    featured_app=app, region=mkt.regions.WORLDWIDE.id)
+    else:
+        cat = None
+    apps_regions_carriers = []
     for app in FeaturedApp.uncached.filter(category__id=cat):
         regions = app.regions.values_list('region', flat=True)
         excluded_regions = app.app.addonexcludedregion.values_list('region',
                                                                    flat=True)
         carriers = app.carriers.values_list('carrier', flat=True)
-        apps_regions_carriers.append((app, regions, excluded_regions, carriers,
-                                      False))
+        apps_regions_carriers.append((app, regions, excluded_regions, carriers))
     return jingo.render(request, 'zadmin/featured_apps_ajax.html',
                         {'apps_regions_carriers': apps_regions_carriers,
                          'regions': mkt.regions.REGIONS_CHOICES,
                          'carriers': settings.CARRIER_URLS})
 
 
-class UnsavedFeaturedApp(object):
-    def __init__(self, pk, start_date, end_date):
-        self.app = Webapp.objects.get(pk=pk)
-        self.is_sponsor = False
-        self.start_date = start_date and datetime.datetime.strptime(
-            start_date, '%Y-%m-%d')
-        self.end_date = end_date and datetime.datetime.strptime(
-            end_date, '%Y-%m-%d')
+@any_permission_required([('Admin', '%'),
+                          ('FeaturedApps', 'Edit')])
+def set_attrs_ajax(request):
+    regions = request.POST.getlist('region[]')
+    carriers = set(request.POST.getlist('carrier[]'))
+    startdate = request.POST.get('startdate', None)
+    enddate = request.POST.get('enddate', None)
+
+    app = request.POST.get('app', None)
+    if not app:
+        return HttpResponse()
+    fa = FeaturedApp.uncached.get(pk=app)
+    if regions or carriers:
+        regions = set(int(r) for r in regions)
+        fa.regions.exclude(region__in=regions).delete()
+        to_create = regions - set(fa.regions.filter(region__in=regions)
+                                  .values_list('region', flat=True))
+        excluded_regions = [e.region for e in fa.app.addonexcludedregion.all()]
+        for i in to_create:
+            if i in excluded_regions:
+                continue
+            FeaturedAppRegion.objects.create(featured_app=fa, region=i)
+
+        fa.carriers.exclude(carrier__in=carriers).delete()
+        to_create = carriers - set(fa.carriers.filter(carrier__in=carriers)
+                                   .values_list('carrier', flat=True))
+        for c in to_create:
+            FeaturedAppCarrier.objects.create(featured_app=fa, carrier=c)
+
+    if startdate:
+        fa.start_date = datetime.datetime.strptime(startdate, '%Y-%m-%d')
+    else:
+        fa.start_date = None
+    if enddate:
+        fa.end_date = datetime.datetime.strptime(enddate, '%Y-%m-%d')
+    else:
+        fa.end_date = None
+    fa.save()
+    return HttpResponse()
 
 
 @any_permission_required([('Admin', '%'),
