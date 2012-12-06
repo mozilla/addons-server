41cc8c911d97b7d3c9991a3fbe6e6b7f5e39951e
Alexis Métaireau <alexis@notmyidea.org>
2012-12-06 17:37:46 +0100

    Developers shouldn't be in a group.
    Update the import script and add a --clear option to remove the users before
    recreating them.

--- apps/amo/management/commands/add_test_users.py
+++ apps/amo/management/commands/add_test_users.py
@@ -1,8 +1,11 @@
 import hashlib
+from datetime import datetime
+from optparse import make_option
 
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.conf import settings
 
 import amo
 from access.models import GroupUser, Group
@@ -11,7 +14,7 @@ from mkt.api.models import Access
 
 
 @transaction.commit_on_success
-def create_user(email, group_name, salt):
+def create_user(email, salt, group_name=None, delete_user=False):
     """Create an user if he doesn't exist already, assign him to a group and
     create a token for him.
 
@@ -19,6 +22,10 @@ def create_user(email, group_name, salt):
     them are generated in a predictible way: md5(salt + email + 'key') or
     md5(salt + email + 'secret').
     """
+    if delete_user:
+        users = User.objects.filter(email=email)
+        if users.exists():
+            users[0].delete()
 
     # Create the user.
     profile, created = UserProfile.objects.get_or_create(
@@ -28,10 +35,15 @@ def create_user(email, group_name, salt):
     if created:
         profile.create_django_user()
 
+    if not profile.read_dev_agreement:
+        profile.read_dev_agreement = datetime.now()
+        profile.save()
+
     # Now, find the group we want.
-    if not profile.groups.filter(groupuser__group__name=group_name).exists():
-        group = Group.objects.get(name=group_name)
-        GroupUser.objects.create(group=group, user=profile)
+    if (group_name and not
+        profile.groups.filter(groupuser__group__name=group_name).exists()):
+            group = Group.objects.get(name=group_name)
+            GroupUser.objects.create(group=group, user=profile)
 
     # We also want to grant these users access, so let's create tokens for
     # them.
@@ -46,10 +58,20 @@ class Command(BaseCommand):
     help = """Create three users with different profiles (App Review, Admin,
               Developer)
            """
+    option_list = BaseCommand.option_list + (
+        make_option('--clear',
+                    action='store_true',
+                    dest='clear',
+                    default=False,
+                    help='Clear the users before recreating them'),)
 
     def handle(self, *args, **kw):
         options = {'salt': settings.API_SALT}
 
-        create_user('appreviewer@mozilla.com', 'App Reviewers', **options)
-        create_user('admin@mozilla.com', 'Admins', **options)
-        create_user('developer@mozilla.com', 'Developers', **options)
+        if kw['clear']:
+            options['delete_user'] = True
+
+        create_user('appreviewer@mozilla.com', group_name='App Reviewers',
+                    **options)
+        create_user('admin@mozilla.com', group_name='Admins', **options)
+        create_user('developer@mozilla.com', **options)
