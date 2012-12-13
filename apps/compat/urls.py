5130e88323f51c07dd36fa109b05c9ec6f20acbc
Chris Van <cvan@mozilla.com>
2012-12-12 21:28:24 -0800

    handle slashes in guids for compatibility reports (bug 817434)

--- apps/compat/urls.py
+++ apps/compat/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = (
     url('^incoming/?$', views.incoming, name='compat.incoming'),
     url('^reporter/?$', views.reporter, name='compat.reporter'),
-    url('^reporter/([^/]+)$',
-        views.reporter_detail, name='compat.reporter_detail'),
+    url('^reporter/(.+)$', views.reporter_detail,
+        name='compat.reporter_detail'),
 
     url('^(?P<version>[.\w]+)?$', views.index, name='compat.index'),
 )
