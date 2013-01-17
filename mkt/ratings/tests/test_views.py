df9a21edf5e00f6356c51dd7962460a9a06dfd79
Allen Short <ashort@mozilla.com>
2013-01-17 11:35:06 -0800

    revert some egregious test changes

--- mkt/ratings/tests/test_views.py
+++ mkt/ratings/tests/test_views.py
@@ -2,7 +2,7 @@ import mock
 from nose.tools import eq_
 from pyquery import PyQuery as pq
 
-from django.db import connection, models
+from django.db import models
 
 import amo
 from amo.models import ManagerBase
@@ -125,24 +125,16 @@ class TestCreate(ReviewTest):
                      'url https://example.com/foo/bar', 'host example.org',
                      'quote example%2eorg', 'IDNA www.xn--ie7ccp.xxx']:
             self.client.post(self.add, {'body': body, 'rating': 2})
-            cur = connection.cursor()
-            cur.execute(
-                """select rmf.flag_notes, r.flag, r.editorreview
-                   from reviews_moderation_flags as rmf, reviews as r
-                   where rmf.review_id = r.id and r.is_latest = 1
-                         and r.addon_id = %s and r.user_id = %s""",
-                (self.webapp.pk, self.user.pk))
-            eq_(cur.fetchone(), ('URLs', True, True))
-            cur.execute(
-                """delete rmf from reviews_moderation_flags as rmf, reviews as r
-                  where rmf.review_id = r.id and r.is_latest = 1
-                        and r.addon_id = %s and r.user_id = %s""",
-                (self.webapp.pk, self.user.pk))
-            cur.execute(
-                """update reviews as r set flag = 0, editorreview = 0
-                   where r.is_latest = 1 and r.addon_id = %s
-                         and r.user_id = %s""",
-                (self.webapp.pk, self.user.pk))
+            ff = Review.objects.filter(addon=self.webapp)
+            rf = ReviewFlag.objects.filter(review=ff[0])
+            eq_(ff[0].flag, True)
+            eq_(ff[0].editorreview, True)
+            eq_(rf[0].note, 'URLs')
+            rf.delete()
+            # Clear the flags so we can test review revision flagging
+            ff[0].flag = False
+            ff[0].editorreview = False
+            ff[0].save()
 
     def test_review_edit_review_initial(self):
         # Existing review? Then edit that review.
