b3666303d45b2128dc15d1c4d1a28f6eb277258d
Andy McKay <amckay@mozilla.com>
2013-01-24 14:55:47 -0800

    assume .json and warn on missing

--- mkt/site/fixtures/__init__.py
+++ mkt/site/fixtures/__init__.py
@@ -1,8 +1,18 @@
 import os
 
 from django.conf import settings
+from django.core.management.color import color_style
 
 
 def fixture(*names):
-    return [os.path.join(settings.ROOT, 'mkt/site/fixtures/data', n)
-            for n in names]
+    files = []
+    for name in names:
+        filename = os.path.join(settings.ROOT, 'mkt/site/fixtures/data', name)
+        if not os.path.splitext(filename)[1]:
+            filename += '.json'
+        if not os.path.exists(filename):
+            print color_style().ERROR('No fixture: %s, skipping.' % filename)
+            continue
+        files.append(filename)
+
+    return files
