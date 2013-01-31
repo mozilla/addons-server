a0620d530dcdde9a2c73d87d984ecedd6be6607f
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-01-30 18:02:07 -0800

    fix install counting for stats (bug 833880)

--- mkt/stats/search.py
+++ mkt/stats/search.py
@@ -174,6 +174,7 @@ def get_installed_daily(installed):
         'date': date,
         'addon': addon_id,
         'count': Installed.objects.filter(
+            addon__id=addon_id,
             created__year=date.year,
             created__month=date.month,
             created__day=date.day).count()
