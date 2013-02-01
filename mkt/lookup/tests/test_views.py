ngokevin/zamboni

hookup the transaction lookup to solitude (bug 822701)

mkt/lookup/tests/test_views.py
@@ -275,19 +275,72 @@ class TestTransactionSummary(TestCase):
 
     def setUp(self):
         self.tx_id = 45
+        self.buyer_uuid = 123
+        self.seller_uuid = 456
+        self.related_tx_uuid = 789
+
+        Contribution.objects.create(addon=addon_factory(),
+                                    transaction_id=self.tx_id)
+
         self.url = reverse('lookup.transaction_summary', args=[self.tx_id])
         self.client.login(username='support-staff@mozilla.com',
                           password='password')
 
-    def test_200(self):
+    @mock.patch('mkt.lookup.views.client')
+    def test_200(self, solitude):
+        solitude.lookup_transaction.side_effect = self.lookup_tx_side_effect
+        solitude.get.side_effect = self.get_side_effect
         r = self.client.get(self.url)
         eq_(r.status_code, 200)
 
-    def test_no_perm(self):
+    def test_no_perm_403(self):
         self.client.login(username='regular@mozilla.com',
                           password='password')
         r = self.client.get(self.url)
         eq_(r.status_code, 403)
+
+    @mock.patch('mkt.lookup.views.client')
+    def test_no_transaction_404(self, solitude):
+        solitude.lookup_transaction.side_effect = self.lookup_tx_side_effect
+        r = self.client.get(reverse('lookup.transaction_summary', args=[999]))
+        eq_(r.status_code, 404)
+
+    @mock.patch('mkt.lookup.views.client')
+    def test_transaction_summary(self, solitude):
+        """Mock Solitude API, check client follows the URIs around."""
+        solitude.lookup_transaction.side_effect = self.lookup_tx_side_effect
+        solitude.get.side_effect = self.get_side_effect
+
+        tx_data = _transaction_summary(self.tx_id)
+
+        eq_(tx_data['tx'], self.mock_transaction())
+        eq_(tx_data['seller']['uuid'], self.seller_uuid)
+        eq_(tx_data['buyer']['uuid'], self.buyer_uuid)
+        eq_(tx_data['related_tx']['uuid'], self.related_tx_uuid)
+
+    def lookup_tx_side_effect(self, *args, **kwargs):
+        if str(args[0]) == str(self.tx_id):
+            return self.mock_transaction()
+        raise SolitudeError
+
+    def get_side_effect(self, *args, **kwargs):
+        if args[0] == 'buyer_uri':
+            return {'uuid': self.buyer_uuid}
+        elif args[0] == 'seller_product_uri':
+            return {'seller': 'seller_uri'}
+        elif args[0] == 'seller_uri':
+            return {'uuid': self.seller_uuid}
+        elif args[0] == 'related_tx_uri':
+            return {'uuid': self.related_tx_uuid}
+
+    def mock_transaction(self):
+        return {
+            'uuid': self.tx_id,
+            'buyer': 'buyer_uri',
+            'seller_product': 'seller_product_uri',
+            'related': 'related_tx_uri',
+            'provider': 0,
+        }
 
 
 class TestTransactionRefund(TestCase):

mkt/lookup/views.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from django.conf import settings
 from django.db import connection
 from django.db.models import Sum, Count, Q
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 
 import jingo
@@ -19,7 +20,9 @@ from apps.access import acl
 from apps.bandwagon.models import Collection
 from devhub.models import ActivityLog
 from elasticutils.contrib.django import S
+from lib.pay_server import client, SolitudeError
 from market.models import AddonPaymentData, Refund
+from mkt.constants.payments import PROVIDERS
 from mkt.account.utils import purchase_list
 from mkt.lookup.forms import TransactionSearchForm
 from mkt.webapps.models import Installed
@@ -74,21 +77,60 @@ def user_summary(request, user_id):
 @permission_required('Transaction', 'View')
 def transaction_summary(request, tx_id):
     tx_form = TransactionSearchForm()
+    tx_data = _transaction_summary(tx_id)
+    if not tx_data:
+        raise Http404
 
     return jingo.render(request, 'lookup/transaction_summary.html',
-                        {'tx_id': tx_id, 'tx_form': tx_form,
-                         'tx': _transaction_summary(tx_id)})
+                        dict({'tx_id': tx_id, 'tx_form': tx_form}.items() +
+                             tx_data.items()))
 
 
 def _transaction_summary(tx_id):
     """Get transaction details from Solitude API."""
-    # TODO: Get transaction details from Solitude API.
+    buyer = None
+    seller = None
+    related_tx = None
+
+    contrib = get_object_or_404(Contribution, transaction_id=tx_id)
+
+    # Get tx.
+    try:
+        transaction = client.lookup_transaction(tx_id)
+    except (SolitudeError, ValueError):
+        return None
+
+    # Get buyer.
+    buyer_uri = transaction.get('buyer', None)
+    if buyer_uri:
+        buyer = client.get(buyer_uri)
+
+    # Get product.
+    seller_product_uri = transaction.get('seller_product', None)
+    if seller_product_uri:
+        seller_product = client.get(seller_product_uri)
+
+        # Get seller.
+        seller_uri = seller_product.get('seller', None)
+        if seller_uri:
+            seller = client.get(seller_uri)
+
+    # Get related transaction.
+    related_tx_uri = transaction.get('related', None)
+    if related_tx_uri:
+        related_tx = client.get(related_tx_uri)
+
     return {
-        'id': '',
-        'date': '',
-        'buyer': None,
-        'seller': None,
-        'amount': 0}
+        'tx': transaction,
+        'buyer': buyer,
+        'seller': seller,
+        'provider': PROVIDERS[transaction['provider']],
+        'related_tx': related_tx,
+
+        'contrib': contrib,
+        'type': amo.CONTRIB_TYPES[contrib.type],
+        'is_refund': contrib.type == amo.CONTRIB_REFUND,
+    }
 
 
 @post_required

mozilla/addons-server

mkt/lookup/tests/test_views.py
@@ -23,8 +23,9 @@ from amo.helpers import urlparams
 from amo.tests import addon_factory, app_factory, ESTestCase, TestCase
 from amo.urlresolvers import reverse
 from devhub.models import ActivityLog
+from lib.pay_server import SolitudeError
 from market.models import AddonPaymentData, AddonPremium, Price, Refund
-from mkt.lookup.views import transaction_refund
+from mkt.lookup.views import _transaction_summary, transaction_refund
 from mkt.site.fixtures import fixture
 from mkt.webapps.cron import update_weekly_downloads
 from mkt.webapps.models import Installed, Webapp
@@ -275,19 +276,61 @@ class TestTransactionSummary(TestCase):
 
     def setUp(self):
         self.uuid = 45
+        self.buyer_uuid = 123
+        self.seller_uuid = 456
+        self.related_tx_uuid = 789
+
+        Contribution.objects.create(addon=addon_factory(), uuid=self.uuid)
+
         self.url = reverse('lookup.transaction_summary', args=[self.uuid])
         self.client.login(username='support-staff@mozilla.com',
                           password='password')
 
-    def test_200(self):
+    @mock.patch('mkt.lookup.views.client')
+    def test_200(self, solitude):
+        solitude.lookup_transaction.side_effect = self.lookup_tx_side_effect
+        solitude.get.side_effect = self.get_side_effect
         r = self.client.get(self.url)
         eq_(r.status_code, 200)
 
-    def test_no_perm(self):
+    def test_no_perm_403(self):
         self.client.login(username='regular@mozilla.com',
                           password='password')
         r = self.client.get(self.url)
         eq_(r.status_code, 403)
+
+    @mock.patch('mkt.lookup.views.client')
+    def test_no_transaction_404(self, solitude):
+        solitude.lookup_transaction.side_effect = self.lookup_tx_side_effect
+        r = self.client.get(reverse('lookup.transaction_summary', args=[999]))
+        eq_(r.status_code, 404)
+
+    @mock.patch('mkt.lookup.views.client')
+    def test_transaction_summary(self, solitude):
+        """Mock Solitude API, check client follows the URIs around."""
+        solitude.lookup_transaction.side_effect = self.lookup_tx_side_effect
+        solitude.get.side_effect = self.get_side_effect
+
+        tx_data = _transaction_summary(self.uuid)
+
+        eq_(tx_data['tx'], self.mock_transaction())
+        eq_(tx_data['buyer']['uuid'], self.buyer_uuid)
+
+    def lookup_tx_side_effect(self, *args, **kwargs):
+        if str(args[0]) == str(self.uuid):
+            return self.mock_transaction()
+        raise SolitudeError
+
+    def get_side_effect(self, *args, **kwargs):
+        if args[0] == 'buyer_uri':
+            return {'uuid': self.buyer_uuid}
+
+    def mock_transaction(self):
+        return {
+            'uuid': self.uuid,
+            'buyer': 'buyer_uri',
+            'provider': 0,
+        }
 
 
 class TestTransactionRefund(TestCase):

mkt/lookup/views.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from django.core.exceptions import PermissionDenied
 from django.db import connection
 from django.db.models import Sum, Count, Q
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 
 import jingo
@@ -19,7 +20,9 @@ from apps.access import acl
 from apps.bandwagon.models import Collection
 from devhub.models import ActivityLog
 from elasticutils.contrib.django import S
+from lib.pay_server import client, SolitudeError
 from market.models import AddonPaymentData, Refund
+from mkt.constants.payments import PROVIDERS
 from mkt.account.utils import purchase_list
 from mkt.lookup.forms import TransactionRefundForm, TransactionSearchForm
 from mkt.lookup.tasks import email_buyer_refund
@@ -75,24 +78,43 @@ def user_summary(request, user_id):
 @login_required
 @permission_required('Transaction', 'View')
 def transaction_summary(request, uuid):
+    tx_data = _transaction_summary(uuid)
+    if not tx_data:
+        raise Http404
+
     tx_form = TransactionSearchForm()
     tx_refund_form = TransactionRefundForm()
 
     return jingo.render(request, 'lookup/transaction_summary.html',
-                        {'uuid': uuid, 'tx_form': tx_form,
-                         'tx_refund_form': tx_refund_form,
-                         'tx': _transaction_summary(uuid)})
+                        dict({'uuid': uuid, 'tx_form': tx_form,
+                              'tx_refund_form': tx_refund_form}.items() +
+                             tx_data.items()))
 
 
 def _transaction_summary(uuid):
     """Get transaction details from Solitude API."""
-    # TODO: Get transaction details from Solitude API.
+    contrib = get_object_or_404(Contribution, uuid=uuid)
+    # Get tx.
+    try:
+        transaction = client.lookup_transaction(uuid)
+    except (SolitudeError, ValueError):
+        return
+
+    # Get buyer.
+    buyer = None
+    buyer_uri = transaction.get('buyer')
+    if buyer_uri:
+        buyer = client.get(buyer_uri)
+
     return {
-        'id': '',
-        'date': '',
-        'buyer': None,
-        'seller': None,
-        'amount': 0
+        'tx': transaction,
+        'buyer': buyer,
+        'provider': PROVIDERS[transaction['provider']],
+
+        'contrib': contrib,
+        'type': amo.CONTRIB_TYPES[contrib.type],
+        'is_refund': contrib.has_refund(),
+        'related': contrib.related,
     }
 
 
@@ -101,11 +123,8 @@ def _transaction_summary(uuid):
 @permission_required('Transaction', 'Refund')
 def transaction_refund(request, uuid):
     contrib = get_object_or_404(Contribution, uuid=uuid)
-    try:
-        if contrib.has_refund():
-            raise PermissionDenied
-    except Refund.DoesNotExist:
-        pass
+    if contrib.has_refund():
+        raise PermissionDenied
 
     form = TransactionRefundForm(request.POST)
     if not form.is_valid():