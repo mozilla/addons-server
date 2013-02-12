53848c63cd9705d790d3a62d1a1dc5cbb35b37d7
Chris Van <cvan@mozilla.com>
2013-02-12 14:08:42 -0800

    AMD-ify account.js

--- media/js/mkt/account.js
+++ media/js/mkt/account.js
@@ -1,8 +1,8 @@
-(function() {
+define('account', [], function() {
     // For stylized <select>s.
     z.body.on('focus', '.styled.select select', function() {
         $(this).closest('.select').addClass('active');
     }).on('blur', '.styled.select select', function() {
         $(this).closest('.select').removeClass('active');
     });
-})();
+});
