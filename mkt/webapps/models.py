mozilla/addons-server

commit c0309315377f60244beba513d02eed5a7b0c5da1

Refactor featured app logic into manager methods

mkt/webapps/models.py
@@ -15,7 +15,6 @@ from django.dispatch import receiver
 from django.utils.http import urlquote
 
 import commonware.log
-import waffle
 from elasticutils.contrib.django import F, S
 from tower import ugettext as _
 
@@ -36,11 +35,11 @@ from files.models import File, nfd_str, Platform
 from files.utils import parse_addon, WebAppParser
 from lib.crypto import packaged
 from versions.models import Version
+from mkt.zadmin.models import FeaturedApp
 
 import mkt
 from mkt.constants import apps
 from mkt.constants import APP_IMAGE_SIZES
-from mkt.carriers import get_carrier
 from mkt.webapps.utils import get_locale_properties
 
 
@@ -526,61 +525,7 @@ class Webapp(Addon):
     @classmethod
     def featured(cls, cat=None, region=None, limit=6, mobile=False,
                  gaia=False):
-        FeaturedApp = models.get_model('zadmin', 'FeaturedApp')
-        qs = (FeaturedApp.objects
-              .filter(app__status=amo.STATUS_PUBLIC,
-                      app__disabled_by_user=False)
-              .order_by('-app__weekly_downloads'))
-        qs = (qs.filter(start_date__lte=cls.now())
-              | qs.filter(start_date__isnull=True))
-        qs = (qs.filter(end_date__gte=cls.now())
-              | qs.filter(end_date__isnull=True))
-
-        if waffle.switch_is_active('disabled-payments') or not gaia:
-            qs = qs.filter(app__premium_type__in=amo.ADDON_FREES)
-
-        if isinstance(cat, list):
-            qs = qs.filter(category__in=cat)
-        else:
-            qs = qs.filter(category=cat.id if cat else None)
-
-        locale_qs = FeaturedApp.objects.none()
-        worldwide_qs = FeaturedApp.objects.none()
-
-        carrier = get_carrier()
-        if carrier:
-            qs = qs.filter(carriers__carrier=carrier)
-
-        if gaia:
-            qs = qs.filter(
-                app__addondevicetype__device_type=amo.DEVICE_GAIA.id)
-        elif mobile:
-            qs = qs.filter(
-                app__addondevicetype__device_type=amo.DEVICE_MOBILE.id)
-
-        if region:
-            excluded = cls.get_excluded_in(region)
-            locale_qs = (qs.filter(regions__region=region.id)
-                         .exclude(app__id__in=excluded))
-
-            # Fill the empty spots with Worldwide-featured apps.
-            if limit:
-                empty_spots = limit - locale_qs.count()
-                if empty_spots > 0 and region != mkt.regions.WORLDWIDE:
-                    ww = mkt.regions.WORLDWIDE.id
-                    worldwide_qs = (qs.filter(regions__region=ww)
-                                    .exclude(id__in=[x.id for x in locale_qs])
-                                    .exclude(app__id__in=excluded))[:limit]
-
-        if limit:
-            locale_qs = locale_qs[:limit]
-
-        if worldwide_qs:
-            combined = ([fa.app for fa in locale_qs] +
-                        [fa.app for fa in worldwide_qs])
-            return list(set(combined))[:limit]
-
-        return [fa.app for fa in locale_qs]
+        return FeaturedApp.objects.featured(cat, region, limit, mobile, gaia)
 
     @classmethod
     def get_excluded_in(cls, region):

mkt/zadmin/tests/test_views.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 import json
 
@@ -8,12 +8,14 @@ from pyquery import PyQuery as pq
 
 import amo
 import amo.tests
-from addons.models import Addon, AddonCategory, Category
+import mkt
+from addons.models import Addon, AddonCategory, AddonDeviceType, Category
 from amo.utils import urlparams
 from amo.urlresolvers import reverse
 from editors.models import RereviewQueue
 from users.models import UserProfile
 
+from mkt.carriers import get_carrier
 from mkt.webapps.models import AddonExcludedRegion, Webapp
 from mkt.zadmin.models import (FeaturedApp, FeaturedAppCarrier,
                                FeaturedAppRegion)
@@ -287,6 +289,162 @@ class TestFeaturedApps(amo.tests.TestCase):
                                    'enddate': ''})
         eq_(r.status_code, 200)
         eq_(FeaturedApp.objects.get(pk=f.pk).end_date, None)
+
+
+class TestFeaturedAppQueryset(amo.tests.TestCase):
+
+    def setUp(self):
+        self.c1 = Category.objects.create(name='awesome',
+                                          type=amo.ADDON_WEBAPP)
+        self.c2 = Category.objects.create(name='groovy', type=amo.ADDON_WEBAPP)
+
+        self.a1 = Webapp.objects.create(status=amo.STATUS_PUBLIC,
+                                        name='awesome app 1',
+                                        type=amo.ADDON_WEBAPP)
+        self.a2 = Webapp.objects.create(status=amo.STATUS_PUBLIC,
+                                        name='awesome app 2',
+                                        type=amo.ADDON_WEBAPP)
+        self.g1 = Webapp.objects.create(status=amo.STATUS_PUBLIC,
+                                        name='groovy app 1',
+                                        type=amo.ADDON_WEBAPP)
+        self.s1 = Webapp.objects.create(status=amo.STATUS_PUBLIC,
+                                        name='splendid app 1',
+                                        type=amo.ADDON_WEBAPP)
+
+        AddonCategory.objects.create(category=self.c1, addon=self.a1)
+        AddonCategory.objects.create(category=self.c1, addon=self.a2)
+        AddonCategory.objects.create(category=self.c2, addon=self.g1)
+        AddonCategory.objects.create(category=self.c1, addon=self.s1)
+        AddonCategory.objects.create(category=self.c2, addon=self.s1)
+
+        yesterday = date.today() - timedelta(days=1)
+        tomorrow = date.today() + timedelta(days=1)
+
+        MOBILE = amo.DEVICE_MOBILE.id
+        GAIA = amo.DEVICE_GAIA.id
+        DESKTOP = amo.DEVICE_DESKTOP.id
+        TABLET = amo.DEVICE_TABLET.id
+
+        self.f1 = FeaturedApp.objects.create(app=self.a1, category=self.c1,
+                                             start_date=yesterday,
+                                             end_date=tomorrow)
+        self.f2 = FeaturedApp.objects.create(app=self.a1, category=self.c2)
+        self.far1 = FeaturedAppRegion.objects.create(featured_app=self.f1,
+                                                     region=mkt.regions.US.id)
+        self.far2 = FeaturedAppRegion.objects.create(featured_app=self.f2)
+        self.fac1 = FeaturedAppCarrier.objects.create(featured_app=self.f1,
+                                                      carrier='telerizon')
+        self.fac2 = FeaturedAppCarrier.objects.create(featured_app=self.f2,
+                                                      carrier='cingulizon')
+        self.aodt1 = AddonDeviceType.objects.create(addon=self.a1,
+                                                    device_type=MOBILE)
+        self.aodt2 = AddonDeviceType.objects.create(addon=self.a1,
+                                                    device_type=GAIA)
+        self.aodt3 = AddonDeviceType.objects.create(addon=self.a2,
+                                                    device_type=DESKTOP)
+        self.aodt3 = AddonDeviceType.objects.create(addon=self.a2,
+                                                    device_type=TABLET)
+
+    def _is_overlap(self, x, y):
+        """
+        Asserts whether there are any items in `y` that do not exist in `x`.
+        Useful for testing whether items in y were filtered from x by a
+        custom manager mesthod.
+        """
+        temp = set(y)  # Don't create the set more than once.
+        return all(item in temp for item in x)
+
+    def assertQuerySetEqual(self, qs1, qs2):
+        "Assertion to check the equality of two querysets"
+        return self.assertSetEqual(qs1.values_list('id', flat=True),
+                                   qs2.values_list('id', flat=True))
+
+    def test_queryset_for_category(self):
+        self.assertQuerySetEqual(FeaturedApp.objects.for_category(self.c1),
+                                 FeaturedApp.objects.filter(category=self.c1))
+
+    def test_queryset_worldwide(self):
+        worldwide = mkt.regions.WORLDWIDE.id
+        self.assertQuerySetEqual(
+            FeaturedApp.objects.worldwide(),
+            FeaturedApp.objects.filter(regions__region=worldwide)
+        )
+
+    def test_queryset_for_region(self):
+        self.assertQuerySetEqual(
+            FeaturedApp.objects.for_region(mkt.regions.US),
+            FeaturedApp.objects.filter(regions__region=mkt.regions.US.id)
+        )
+
+    def test_queryset_for_carrier(self):
+        carrier = 'telerizon'
+        self.assertQuerySetEqual(
+            FeaturedApp.objects.for_carrier(carrier),
+            FeaturedApp.objects.filter(carriers__carrier=carrier)
+        )
+
+    def test_queryset_mobile(self):
+        self.assertQuerySetEqual(
+            FeaturedApp.objects.mobile(),
+            FeaturedApp.objects.filter(
+                app__addondevicetype__device_type=amo.DEVICE_MOBILE.id)
+        )
+
+    def test_queryset_gaia(self):
+        self.assertQuerySetEqual(
+            FeaturedApp.objects.gaia(),
+            FeaturedApp.objects.filter(
+                app__addondevicetype__device_type=amo.DEVICE_GAIA.id)
+        )
+
+    def test_queryset_tablet(self):
+        self.assertQuerySetEqual(
+            FeaturedApp.objects.tablet(),
+            FeaturedApp.objects.filter(
+                app__addondevicetype__device_type=amo.DEVICE_TABLET.id)
+        )
+
+    def test_queryset_active_date(self):
+        now = date.today()
+        start_date = (FeaturedApp.objects.filter(start_date__lte=now) |
+                      FeaturedApp.objects.filter(start_date__isnull=True))
+        end_date = (FeaturedApp.objects.filter(end_date__gte=now) |
+                    FeaturedApp.objects.filter(end_date__isnull=True))
+        self.assertQuerySetEqual(FeaturedApp.objects.active_date(),
+                                 (start_date | end_date))
+
+    def test_queryset_active(self):
+        self.assertQuerySetEqual(FeaturedApp.objects.active(),
+                                 FeaturedApp.objects.active_date().public())
+
+    def test_queryset_featured(self):
+        carrier = get_carrier()
+        assert self._is_overlap(
+            FeaturedApp.objects.for_category(self.c1),
+            FeaturedApp.objects.featured(cat=self.c1)
+        ), 'Unexpected items in category %s' % self.c1
+        assert self._is_overlap(
+            FeaturedApp.objects.for_carrier(carrier),
+            FeaturedApp.objects.featured()
+        ), 'Unexpected items for carrier %s' % carrier
+        assert self._is_overlap(
+            FeaturedApp.objects.gaia(),
+            FeaturedApp.objects.featured(gaia=True)
+        ), 'Unexpected items in Gaia'
+        assert self._is_overlap(
+            FeaturedApp.objects.mobile(),
+            FeaturedApp.objects.featured(mobile=True)
+        ), 'Unexpected items in mobile'
+        assert self._is_overlap(
+            FeaturedApp.objects.tablet(),
+            FeaturedApp.objects.featured(tablet=True)
+        ), 'Unexpected items in tablet'
+        assert self._is_overlap(
+            FeaturedApp.objects.active(),
+            FeaturedApp.objects.featured()
+        ), 'Inactive items in featured queryset'
+        limited = FeaturedApp.objects.featured(limit=1).count()
+        assert eq_(limited, 1), '%s items returned, only 1 expected' % limited
 
 
 class TestAddonSearch(amo.tests.ESTestCase):

mkt/zadmin/models.py
@@ -1,19 +1,195 @@
+import datetime
+
 from django.db import models
+from django.db.models.loading import get_model
+
+import waffle
 
 from addons.models import Category
 
 import amo
-
 import mkt
-from mkt.webapps.models import Webapp
+from mkt.carriers import get_carrier
+
+
+class FeaturedAppQuerySet(models.query.QuerySet):
+    """
+    Custom QuerySet that encapsulates common filtering logic used when
+    querying QuerySets of FeaturedApp instances.
+
+    This QuerySet is implemented by FeaturedAppManager, which attempts to
+    check this class' attributes when its own __getattr__ method raises an
+    AttributeError.
+    """
+
+    def featured(self, cat=None, region=None, limit=6, mobile=False,
+                 gaia=False, tablet=False):
+        """
+        Filters the QuerySet, removing FeaturedApp instances that should
+        not be featured based on the passed criteria.
+
+        If a region is defined and there are fewer than `limit` items
+        remaining in the resultant queryset, the difference is populated by
+        additional apps that are featured worldwide.
+        """
+        qs = self.active()
+        Webapp = get_model('webapps', 'Webapp')
+
+        excluded = Webapp.objects.none()
+        if region:
+            excluded = Webapp.get_excluded_in(region)
+
+        if waffle.switch_is_active('disabled-payments') or not gaia:
+            qs = qs.filter(app__premium_type__in=amo.ADDON_FREES)
+
+        if cat:
+            qs = qs.for_category(cat)
+
+        carrier = get_carrier()
+        if carrier:
+            qs = qs.for_carrier(carrier)
+
+        if gaia:
+            qs = qs.gaia()
+        elif mobile:
+            qs = qs.mobile()
+
+        if tablet:
+            qs = qs.tablet()
+
+        if region:
+            qs = qs.for_region(region).exclude(app__id__in=excluded)
+
+            # Fill the empty spots with Worldwide-featured apps.
+            if limit:
+                empty_spots = limit - qs.count()
+                if empty_spots > 0 and region != mkt.regions.WORLDWIDE:
+                    qs |= (FeaturedApp.objects.active().worldwide()
+                           .exclude(id__in=[x.id for x in qs])
+                           .exclude(app__id__in=excluded))
+
+        if limit:
+            qs = qs[:limit]
+
+        return qs
+
+    def active(self):
+        """
+        Convenience method that bundles self.active_date and self.public
+        in a single call.
+        """
+        return self.active_date().public()
+
+    def active_date(self):
+        """
+        Removes features in the queryset whose start/end dates render the
+        feature inactive.
+        """
+        now = datetime.date.today()
+        qs = (self.filter(start_date__lte=now) |
+              self.filter(start_date__isnull=True))
+        qs = (qs.filter(end_date__gte=now) |
+              qs.filter(end_date__isnull=True))
+        return qs
+
+    def public(self):
+        """
+        Removes features in the queryset that are neither public nor
+        disabled by its owner.
+        """
+        return self.filter(app__status=amo.STATUS_PUBLIC,
+                           app__disabled_by_user=False)
+
+    def gaia(self):
+        """
+        Removes features in the queryset that are not available for Gaia.
+        """
+        return self.filter(
+            app__addondevicetype__device_type=amo.DEVICE_GAIA.id)
+
+    def mobile(self):
+        """
+        Removes features in the queryset that are not available for mobile
+        devices.
+        """
+        return self.filter(
+            app__addondevicetype__device_type=amo.DEVICE_MOBILE.id)
+
+    def tablet(self):
+        """
+        Removes features in the queryset that are not available for tablet
+        devices.
+        """
+        return self.filter(
+            app__addondevicetype__device_type=amo.DEVICE_TABLET.id)
+
+    def for_carrier(self, carrier):
+        """
+        Removes features in the queryset that are not available for the
+        passed carrier.
+        """
+        return self.filter(carriers__carrier=carrier)
+
+    def for_region(self, region):
+        """
+        Removes features in the queryset that are not available for the
+        passed region.
+        """
+        return self.filter(regions__region=region.id)
+
+    def worldwide(self):
+        """
+        Removes features in the queryset that are not available
+        worldwide.
+        """
+        return self.filter(regions__region=mkt.regions.WORLDWIDE.id)
+
+    def for_category(self, category):
+        """
+        Removes features in the queryset that are not in the passed
+        category. The passed category can be either a Category instance
+        or a list of Category instances.
+        """
+        if isinstance(category, list):
+            return self.filter(category__in=category)
+        return self.filter(category=category.id if category else None)
+
+
+class FeaturedAppManager(amo.models.ManagerBase):
+    """
+    Custom manager that implements FeaturedAppQuerySet on all querysets for
+    the model.
+
+    If a nonexistent attribute is accessed on this manager, FeaturedAppQuerySet
+    is also checked and is returned if it exists. This allows DRY definition of
+    filtration methods that can be accessed both on the QuerySet or on the
+    manager.
+    """
+    def __getattr__(self, attr, *args):
+        try:
+            return getattr(self.__class__, attr, *args)
+        except AttributeError, orig:
+            # The attribute attempting to be accessed doesn't exist on the
+            # manager. Let's see if it exists on the QuerySet.
+            try:
+                return getattr(self.get_query_set(), attr, *args)
+            except AttributeError:
+                # If it doesn't exist on the QuerySet either, reraise the
+                # original exception.
+                raise orig
+
+    def get_query_set(self):
+        return FeaturedAppQuerySet(self.model)
 
 
 class FeaturedApp(amo.models.ModelBase):
-    app = models.ForeignKey(Webapp, null=False)
+    app = models.ForeignKey('webapps.Webapp', null=False)
     category = models.ForeignKey(Category, null=True)
     is_sponsor = models.BooleanField(default=False)
     start_date = models.DateField(null=True)
     end_date = models.DateField(null=True)
+
+    objects = FeaturedAppManager()
 
     class Meta:
         db_table = 'zadmin_featuredapp'

commit db9a57319848f73a9c5dfb7085ad3d016f299a34

Fix build broken in c030931: * Update Webapp.featured to return a set of Webapp instances, rather than a   QuerySet of their FeaturedApp instances. * Fix incorrect use of the eq_ shortcut in   TestFeaturedAppQueryset.test_queryset_featured

mkt/webapps/models.py
@@ -525,7 +525,8 @@ class Webapp(Addon):
     @classmethod
     def featured(cls, cat=None, region=None, limit=6, mobile=False,
                  gaia=False):
-        return FeaturedApp.objects.featured(cat, region, limit, mobile, gaia)
+        qs = FeaturedApp.objects.featured(cat, region, limit, mobile, gaia)
+        return [w.app for w in qs]
 
     @classmethod
     def get_excluded_in(cls, region):

mkt/zadmin/tests/test_views.py
@@ -444,7 +444,7 @@ class TestFeaturedAppQueryset(amo.tests.TestCase):
             FeaturedApp.objects.featured()
         ), 'Inactive items in featured queryset'
         limited = FeaturedApp.objects.featured(limit=1).count()
-        assert eq_(limited, 1), '%s items returned, only 1 expected' % limited
+        eq_(limited, 1, '%s items returned, only 1 expected' % limited)
 
 
 class TestAddonSearch(amo.tests.ESTestCase):