272a1b3a4ba79f5f5f35c9863c49a017202a0ffc
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-02-19 17:22:37 -0600

    Re-enable forceIssuer for b2g persona (bug 830726)

--- lib/settings_base.py
+++ lib/settings_base.py
@@ -1507,8 +1507,7 @@ BROWSERID_JS_URL = 'https://login.persona.org/include.js'
 # If UNVERIFIED_ISSUER is set to None, forceIssuer will not
 # be sent to the client or the verifier.
 NATIVE_BROWSERID_DOMAIN = 'native-persona.org'
-UNVERIFIED_ISSUER = None
-#UNVERIFIED_ISSUER = 'b2g2pac.personatest.org'
+UNVERIFIED_ISSUER = 'b2g2pac.personatest.org'
 
 # This is a B2G (or other native) verifier. Adjust accordingly.
 NATIVE_BROWSERID_VERIFICATION_URL = ('https://%s/verify'
