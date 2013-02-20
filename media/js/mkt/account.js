ce4255dc31c1b3d612558adb3e56331ac722a21a
Chris Van <cvan@mozilla.com>
2013-02-20 15:45:24 -0800

    target language and region only for synchronous POST (bug 811939)

--- media/js/mkt/account.js
+++ media/js/mkt/account.js
@@ -3,7 +3,7 @@ define('account', [], function() {
     // then let's do a synchronous POST.
     z.page.on('submit', '#account-settings form', function(e) {
         var $this = $(this);
-        $this.find('select').each(function() {
+        $this.find('select[name=lang], select[name=region]').each(function() {
             var $this = $(this);
             if ($this.val() != $this.find('[data-default]').val()) {
                 $this.trigger('reloadonnext');
