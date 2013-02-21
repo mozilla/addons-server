fa194179f8748b55bcd5ca9396191d016feba8bd
Matt Claypotch <thepotch@gmail.com>
2013-02-21 11:10:13 -0800

    gray bgs for apps with no hue

--- media/js/mkt/home.js
+++ media/js/mkt/home.js
@@ -6,7 +6,7 @@
             var tile = el.querySelector('[data-hue]');
             if (!tile) return;
             var hue = +tile.getAttribute('data-hue');
-            if (!hue) return;
+            // if (!hue) return;
             var canvas = el.querySelector('canvas') || document.createElement('canvas');
             var cs = window.getComputedStyle(el, null);
             var width = parseInt(cs.getPropertyValue('width'), 10);
@@ -15,8 +15,13 @@
             canvas.height = height;
             var ctx = canvas.getContext('2d');
             var grad = ctx.createRadialGradient(width/2, height/2, 0, width/2, height/2, (width+height)/2.5);
-            grad.addColorStop(0, "hsl(" + hue + ",100%,85%)");
-            grad.addColorStop(1, "hsl(" + hue + ",75%,50%)");
+            if (hue) {
+                grad.addColorStop(0, "hsl(" + hue + ",100%,85%)");
+                grad.addColorStop(1, "hsl(" + hue + ",75%,50%)");
+            } else {
+                grad.addColorStop(0, "hsl(0,0%,94%)");
+                grad.addColorStop(1, "hsl(0,0%,80%)");
+            }
             ctx.fillStyle = grad;
             ctx.fillRect(0,0,width,height);
             el.insertBefore(canvas, el.firstChild);
