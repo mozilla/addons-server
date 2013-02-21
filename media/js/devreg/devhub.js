84b1e7cb685efe86f80000e8f6c95d6b7136198c
Matt Basta <me@mattbasta.com>
2013-02-21 15:09:08 -0800

    More syntax errors (fixes marketplace-assets)

--- media/js/devreg/devhub.js
+++ media/js/devreg/devhub.js
@@ -397,7 +397,7 @@ function addManifestRefresh() {
             var refreshed = gettext('Refreshed');
             $('#manifest-url th.label').append('<span class="hint">' + refreshed + '</span>');
         });
-    });
+    }));
 }
 
 function initEditAddon() {
