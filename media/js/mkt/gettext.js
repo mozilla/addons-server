f4224f42fe5f4582577d5fe021d8502e5359af7f
Matt Claypotch <thepotch@gmail.com>
2013-02-22 11:23:01 -0800

    typeof duh

--- media/js/mkt/gettext.js
+++ media/js/mkt/gettext.js
@@ -1,5 +1,5 @@
 // stub gettext for when servers are bad at things
-if (window.gettext !== 'function') {
+if (typeof window.gettext !== 'function') {
     window.gettext = function gettext(s) {
         return s;
     };
