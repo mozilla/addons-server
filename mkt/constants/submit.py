3cfafa0172ed2a77a43e62155e75b6254505b861
Chris Van <cvan@mozilla.com>
2013-02-27 10:38:59 -0800

    L10n the gallery icon labels (bug 825753)

--- mkt/constants/submit.py
+++ mkt/constants/submit.py
@@ -15,21 +15,21 @@ APP_IMAGE_SIZES = [
      'has_background': False,
      'required': False,
      'slug': 'featured_tile',
-     'name': 'Featured Tile',
+     'name': _('Featured Tile'),
      'description': _("The icon shown when your app is featured at the top of "
                       "category landing pages.")},
     {'size': (106, 106),
      'has_background': False,
      'required': False,
      'slug': 'mobile_tile',
-     'name': 'Mobile Tile',
+     'name': _('Mobile Tile'),
      'description': _("The image used for the app's tile in the mobile "
                       "Marketplace.")},
     {'size': (150, 130),
      'has_background': False,
      'required': False,
      'slug': 'desktop_tile',
-     'name': 'Desktop Tile',
+     'name': _('Desktop Tile'),
      'description': _("The image used for the app's tile in the desktop "
                       "Marketplace.")},
 ]
