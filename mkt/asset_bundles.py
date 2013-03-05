ab4abfd0853a98af9df88c9c68de0a63c770fc27
Kevin Ngo <me@ngokevin.com>
2013-03-04 19:09:55 -0800

    fix reviewer tab css regressions, remove menupicker (bug 847330)

--- mkt/asset_bundles.py
+++ mkt/asset_bundles.py
@@ -79,7 +79,6 @@ CSS = {
         'css/devreg/themes_review.less',
         'css/devreg/legacy-paginator.less',
         'css/devreg/files.less',
-        'css/devreg/menupicker.less',
     ),
     'mkt/splash': (
         'css/mkt/splash.less',
@@ -305,7 +304,6 @@ JS = {
         'js/zamboni/storage.js',
         'js/zamboni/editors.js',
         'js/mkt/reviewers.js',
-        'js/devreg/menupicker.js',
         'js/devreg/expandable.js',
         'js/devreg/mobile_review_actions.js',
         'js/devreg/reviewers_init.js',
