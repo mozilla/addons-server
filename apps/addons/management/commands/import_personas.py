152b06a2c3ffe95468bb87dc775101798f4280d8
Wil Clouser <wclouser@mozilla.com>
2013-03-08 09:42:07 -0800

    Fix crashes, add timing, add periodic commit/rollback

--- apps/addons/management/commands/import_personas.py
+++ apps/addons/management/commands/import_personas.py
@@ -36,6 +36,15 @@ class Command(BaseCommand):
     def log(self, msg):
         print msg
 
+    def commit_or_not(self, gogo):
+        if gogo == 'yes':
+            self.log('Committing changes.')
+            transaction.commit()
+        else:
+            self.log('Not committing changes, this is a dry run.')
+            transaction.rollback()
+
+
     def connect(self, **options):
         options = dict([(k, v) for k, v in options.items() if k in
                         ['host', 'db', 'user'] and v])
@@ -43,16 +52,10 @@ class Command(BaseCommand):
         self.connection = mysql.connect(**options)
         self.cursor = self.connection.cursor()
 
-    def do_import(self):
-        self.log('Importing users.')
-        count = self.count_users()
-        k, step = 0, 10
-        self.log('Found %s users.' % count)
-        for x in range(0, count, step):
-            k += step
-            self.log('Doing %s to %s' % (x, k))
-            for user in self.get_users(step, k):
-                self.handle_user(user)
+    def do_import(self, offset, limit):
+        self.log('Importing users %s to %s' % (offset, offset+limit))
+        for user in self.get_users(limit, offset):
+            self.handle_user(user)
 
     def count_users(self):
         self.cursor.execute('SELECT count(username) from users')
@@ -68,10 +71,6 @@ class Command(BaseCommand):
                             author)
         return self.cursor.fetchall()
 
-    def handle_images(self):
-        # TODO: bug 726190
-        pass
-
     def get_favourites(self, username):
         self.cursor.execute('SELECT id FROM favorites WHERE '
                             'username = %s', username)
@@ -82,20 +81,21 @@ class Command(BaseCommand):
                          'privs', 'change_code', 'news', 'description'], user))
 
         for k in ['username', 'description', 'display_username', 'email']:
-            user[k] = user[k].decode('latin1').encode('utf-8')
+            user[k] = (user.get(k) or '').decode('latin1').encode('utf-8')
 
         profile = UserProfile.objects.filter(email=user['email'])
         user['orig-username'] = user['username']
 
-        if profile:
-            self.log('Ignoring %s' % user['email'])
+        if profile.exists():
+            self.log(' Ignoring existing user: %s' % user['email'])
+            profile = profile[0]
         else:
             if UserProfile.objects.filter(username=user['username']).exists():
                 user['username'] = user['username'] + '-' + str(time())
-                self.log('Username already taken, so making username: %s'
+                self.log(' Username already taken, so making username: %s'
                          % user['username'])
 
-            self.log('Creating user for %s' % user['email'])
+            self.log(' Creating user for %s' % user['email'])
             note = 'Imported from personas, username: %s' % user['username']
             algo, salt, password = user['md5'].split('$')
             # The salt is a bytes string. In get personas it is base64
@@ -117,41 +117,55 @@ class Command(BaseCommand):
             try:
                 addon = Persona.objects.get(persona_id=fav[0]).addon
             except Persona.DoesNotExist:
-                self.log('Not found fav. %s for user %s' %
+                self.log('  Not found fav. %s for user %s' %
                          (fav[0], user['username']))
                 continue
             CollectionAddon.objects.create(addon=addon,
                     collection=profile.favorites_collection())
-            self.log('Adding fav. %s for user %s' % (addon, user['username']))
+            self.log('  Adding fav. %s for user %s' % (addon, user['username']))
 
         # Now link up the designers with the profile.
         for persona_id in self.get_designers(user['orig-username']):
             try:
                 persona = Persona.objects.get(persona_id=persona_id[0])
             except Persona.DoesNotExist:
-                self.log('Not found persona %s for user %s' %
+                self.log('  Not found persona %s for user %s' %
                          (persona_id[0], user['username']))
                 continue
             AddonUser.objects.create(addon=persona.addon, user=profile,
                                      listed=True)
-            self.log('Adding PersonAuthor for persona %s for user %s' %
+            self.log('  Adding PersonAuthor for persona %s for user %s' %
                      (user['username'], persona))
 
     @transaction.commit_manually
     def handle(self, *args, **options):
+        t_total_start = time()
+
         self.connect(**options)
 
+        self.log("You're running a script to import getpersonas.com to AMO!")
+        self.log("Grab some coffee and settle in.")
+
         try:
-            self.do_import()
+            count = self.count_users()
+            self.log('Going to import %s users.' % count)
+
+            step = 30
+            for offset in range(0, count, step):
+                t_start = time()
+                self.do_import(offset, step)
+                self.commit_or_not(options.get('commit'))
+                t_average = 1 / ((time()- t_total_start) / (offset + step))
+                print "> %.2fs for %s accounts. Averaging %.2f accounts/s" % (
+                        time() - t_start, step, t_average)
         except:
             self.log('Error, not committing changes.')
             transaction.rollback()
             raise
         finally:
+            self.commit_or_not(options.get('commit'))
             if options.get('commit') == 'yes':
-                self.log('Committing changes.')
-                transaction.commit()
+                self.log("Kicking off cron to reindex personas...")
                 cron.reindex_addons(addon_type=amo.ADDON_PERSONA)
-            else:
-                self.log('Not committing changes, this is a dry run.')
-                transaction.rollback()
+
+        self.log("Done. Total time: %s seconds" % (time() - t_total_start))
