383da56c384b3d39526af02581c589299f5e31e8
Chuck Harmston <charmston@mozilla.com>
2013-03-11 10:49:04 -0700

    Fix syntax error in migration 548

--- migrations/548-add-activitylogattachment-model.sql
+++ migrations/548-add-activitylogattachment-model.sql
@@ -5,7 +5,7 @@ CREATE TABLE `log_activity_attachment_mkt` (
     `activity_log_id` int(11) UNSIGNED NOT NULL,
     `filepath` varchar(255) NOT NULL,
     `description` varchar(255),
-    `mimetype` varchar(255),
+    `mimetype` varchar(255)
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE `log_activity_attachment_mkt` ADD CONSTRAINT `activity_log_id_log_activity_attachment_key_mkt`
 FOREIGN KEY (`activity_log_id`) REFERENCES `log_activity_mkt` (`id`);
