ee6266af810363acb41199b2848b4e9071d61e49
Matt Basta <me@mattbasta.com>
2013-03-14 14:32:45 -0700

    Add ambient APIs for ToS and privacy (bug 851274)

--- mkt/site/urls.py
+++ mkt/site/urls.py
@@ -2,6 +2,7 @@ from urlparse import urlparse
 
 from django.conf import settings
 from django.conf.urls import patterns, url
+from django.http import HttpResponse, HttpResponseServerError
 
 import jingo
 
@@ -10,20 +11,26 @@ from . import views
 
 
 def template_plus_xframe(request, template, **kwargs):
-    res = jingo.render(request, template, kwargs)
-    referrer = request.META.get('HTTP_REFERER')
-    if referrer:
-        referrer = urlparse(referrer).netloc
-        if referrer in settings.LEGAL_XFRAME_ALLOW_FROM:
-            res['x-frame-options'] = 'allow-from %s' % referrer
+    format = kwargs.get('format')
+    if format in (None, 'html'):
+        res = jingo.render(request, template, kwargs)
+        referrer = request.META.get('HTTP_REFERER')
+        if referrer:
+            referrer = urlparse(referrer).netloc
+            if referrer in settings.LEGAL_XFRAME_ALLOW_FROM:
+                res['x-frame-options'] = 'allow-from %s' % referrer
+    elif format == 'py':
+        res = HttpResponse('from marketplace import data\n')
+    else:
+        res = HttpResponseServerError()
     return res
 
 
 urlpatterns = patterns('',
     url('^mozmarket.js$', views.mozmarket_js, name='site.mozmarket_js'),
-    url('^privacy-policy$', template_plus_xframe,
+    url('^privacy-policy(.(?P<format>\w+))?$', template_plus_xframe,
         {'template': 'site/privacy-policy.html'}, name='site.privacy'),
-    url('^terms-of-use$', template_plus_xframe,
+    url('^terms-of-use(.(?P<format>\w+))?$', template_plus_xframe,
         {'template': 'site/terms-of-use.html'}, name='site.terms'),
     url('^robots.txt$', views.robots, name='robots.txt'),
     url('^manifest.webapp$', views.manifest, name='manifest.webapp'),
