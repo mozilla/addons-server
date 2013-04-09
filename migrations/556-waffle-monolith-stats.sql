b655226796b19d4b803c5cdbb362129524e6ad51
Chris Van <cvan@mozilla.com>
2013-04-09 10:43:32 -0700

    fix waffle-monolith-stats migration to add timestamps

--- migrations/556-waffle-monolith-stats.sql
+++ migrations/556-waffle-monolith-stats.sql
@@ -1,1 +1,1 @@
-INSERT INTO waffle_switch_mkt (name, active) VALUES ('monolith-stats', 0);
+INSERT INTO waffle_switch_mkt (name, active, created, modified) VALUES ('monolith-stats', 0, NOW(), NOW());
