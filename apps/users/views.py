d092842b60dcb5b67cb3d53a2bbc9da666cad9c7
Allen Short <ashort@mozilla.com>
2013-04-11 09:25:52 -0700

    order personas by ADU. (bug 860407)

--- apps/users/views.py
+++ apps/users/views.py
@@ -583,7 +583,7 @@ def profile(request, user):
             items = user.addons_listed.exclude(type=amo.ADDON_PERSONA)
             personas = user.addons_listed.filter(type=amo.ADDON_PERSONA)
             #XXX hack to protect performance until we have proper incremental loading
-            personas = personas[:60]
+            personas = personas.order_by('-average_daily_users')[:60]
         addons = amo.utils.paginate(request,
                                     items.order_by('-weekly_downloads'))
     else:
