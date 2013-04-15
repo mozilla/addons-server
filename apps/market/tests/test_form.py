4d94653b1b4aaff73dbc9961de6e782f6c293063
Andy McKay <amckay@mozilla.com>
2013-04-15 15:19:34 -0700

    remove stupid test

--- apps/market/tests/test_form.py
+++ apps/market/tests/test_form.py
@@ -16,14 +16,7 @@ class TestForm(amo.tests.TestCase):
         self.addon_premium = AddonPremium.objects.create(addon=self.addon,
                                                          price=self.tier_one)
 
-    def test_form_fails(self):
-        for currency, valid in (['EUR', False], ['CAD', False]):
-            form = PriceCurrencyForm(data={'currency': currency},
-                                     addon=self.addon)
-            eq_(bool(form.is_valid()), valid, 'Currency: %s' % currency)
-
     def test_form_passes(self):
-        self.addon_premium.update(currencies=['EUR', 'CAD'])
         for currency, valid in (['EUR', True], ['BRL', False], ['CAD', True]):
             form = PriceCurrencyForm(data={'currency': currency},
                                      addon=self.addon)
