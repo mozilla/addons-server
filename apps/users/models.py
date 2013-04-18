5dcdbe1789969cfd29822e2e3ac3e6ec874e6eb6
Chris Van <cvan@mozilla.com>
2013-04-18 16:54:26 -0700

    do not cache num_addons_listed

--- apps/users/models.py
+++ apps/users/models.py
@@ -178,7 +178,7 @@ class UserProfile(amo.models.OnChangeMixin, amo.models.ModelBase):
         return self.addons.reviewed().exclude(type=amo.ADDON_WEBAPP).filter(
             addonuser__user=self, addonuser__listed=True)
 
-    @amo.cached_property
+    @property
     def num_addons_listed(self):
         """Number of public add-ons this user is listed as author of."""
         return self.addons.reviewed().exclude(type=amo.ADDON_WEBAPP).filter(
