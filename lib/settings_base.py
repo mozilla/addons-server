mozilla/addons-server

commit 759256323f3bf427bb8d99b0055098f00d23422b

More consolidation of mailing lists.  No need to have seperate lists when we can filter on subject.

lib/settings_base.py
@@ -73,10 +73,10 @@ MANAGERS = ADMINS
 
 FLIGTAR = 'marketplace-staff@mozilla.org'
 EDITORS_EMAIL = 'amo-editors@mozilla.org'
-SENIOR_EDITORS_EMAIL = 'amo-admin-reviews@mozilla.org'
-THEMES_EMAIL = 'background-themes@mozilla.com'
-MARKETPLACE_EMAIL = 'amo-marketplace@mozilla.org'
-ABUSE_EMAIL = 'marketplace-abuse@mozilla.org'
+SENIOR_EDITORS_EMAIL = 'marketplace-staff@mozilla.org'
+THEMES_EMAIL = 'theme-reviews@mozilla.org'
+MARKETPLACE_EMAIL = 'marketplace-staff@mozilla.org'
+ABUSE_EMAIL = 'marketplace-staff@mozilla.org'
 NOBODY_EMAIL = 'nobody@mozilla.org'
 
 DATABASES = {

mkt/settings.py
@@ -31,10 +31,10 @@ ENGAGE_ROBOTS = True
 
 MKT_FEEDBACK_EMAIL = 'apps-feedback@mozilla.com'
 MKT_REVIEWERS_EMAIL = 'app-reviewers@mozilla.org'
-MKT_SENIOR_EDITORS_EMAIL = 'app-review-staff@mozilla.com'
+MKT_SENIOR_EDITORS_EMAIL = 'marketplace-staff@mozilla.com'
 MKT_SUPPORT_EMAIL = 'marketplace-developer-support@mozilla.org'
-MARKETPLACE_EMAIL = 'amo-marketplace@mozilla.org'
-ABUSE_EMAIL = 'Firefox Marketplace <marketplace-abuse@mozilla.org>'
+MARKETPLACE_EMAIL = 'marketplace-staff@mozilla.org'
+ABUSE_EMAIL = 'Firefox Marketplace <marketplace-staff@mozilla.org>'
 NOBODY_EMAIL = 'Firefox Marketplace <nobody@mozilla.org>'
 DEFAULT_FROM_EMAIL = 'Firefox Marketplace <nobody@mozilla.org>'
 

commit 35de8f38d44937c1a27a55a92e77472847318d2f

Ameliorate bulk email sorting nightmare caused by 759256323f3b and 15a272bcc705.

lib/settings_base.py
@@ -71,12 +71,12 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
-FLIGTAR = 'marketplace-staff@mozilla.org'
+FLIGTAR = 'marketplace-staff+random-goings-on@mozilla.org'
 EDITORS_EMAIL = 'amo-editors@mozilla.org'
-SENIOR_EDITORS_EMAIL = 'marketplace-staff@mozilla.org'
+SENIOR_EDITORS_EMAIL = 'amo-admin-reviews@mozilla.org'
 THEMES_EMAIL = 'theme-reviews@mozilla.org'
 MARKETPLACE_EMAIL = 'marketplace-staff@mozilla.org'
-ABUSE_EMAIL = 'marketplace-staff@mozilla.org'
+ABUSE_EMAIL = 'marketplace-staff+abuse@mozilla.org'
 NOBODY_EMAIL = 'nobody@mozilla.org'
 
 DATABASES = {

mkt/settings.py
@@ -31,10 +31,10 @@ ENGAGE_ROBOTS = True
 
 MKT_FEEDBACK_EMAIL = 'apps-feedback@mozilla.com'
 MKT_REVIEWERS_EMAIL = 'app-reviewers@mozilla.org'
-MKT_SENIOR_EDITORS_EMAIL = 'marketplace-staff@mozilla.com'
+MKT_SENIOR_EDITORS_EMAIL = 'marketplace-staff+senior-reviewers@mozilla.com'
 MKT_SUPPORT_EMAIL = 'marketplace-developer-support@mozilla.org'
 MARKETPLACE_EMAIL = 'marketplace-staff@mozilla.org'
-ABUSE_EMAIL = 'Firefox Marketplace <marketplace-staff@mozilla.org>'
+ABUSE_EMAIL = 'Firefox Marketplace Staff <marketplace-staff+abuse@mozilla.org>'
 NOBODY_EMAIL = 'Firefox Marketplace <nobody@mozilla.org>'
 DEFAULT_FROM_EMAIL = 'Firefox Marketplace <nobody@mozilla.org>'
 