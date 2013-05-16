1738f2ee633706cedc6faa9b6f023baee522186d
Chris Van <cvan@mozilla.com>
2013-05-16 15:30:56 -0700

    bump yulelog to 0.0.4

--- mkt/site/views.py
+++ mkt/site/views.py
@@ -173,7 +173,7 @@ def package_minifest(request):
         },
         #'release_notes': '',
         'type': 'privileged',
-        'version': '0.0.3',
+        'version': '0.0.4',
     })
 
     manifest_etag = hashlib.md5(manifest_content).hexdigest()
