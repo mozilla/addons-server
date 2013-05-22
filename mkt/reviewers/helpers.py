d58c6c88c04035b2b47706efc88088801fb0d897
Kevin Ngo <ngoke@mozilla.com>
2013-05-22 03:37:01 -0700

    fix theme preview images not shown on edit page

--- mkt/reviewers/helpers.py
+++ mkt/reviewers/helpers.py
@@ -121,8 +121,7 @@ def queue_tabnav_themes(context):
         tabs.append((
             'themes', 'pending', 'queue_themes', _('Pending'),
         ))
-    if acl.action_allowed(context['request'], 'SeniorPersonasTools',
-                          'View'):
+    if acl.action_allowed(context['request'], 'SeniorPersonasTools', 'View'):
         tabs.append((
             'themes', 'flagged', 'queue_flagged', _('Flagged'),
         ))
