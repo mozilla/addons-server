2d041322dd443fc33df080d41c4c6216865ea74f
Andy McKay <amckay@mozilla.com>
2013-05-28 09:46:54 -0700

    add in dumped-apps paths (bug 875486)

--- sites/prod/settings_mkt.py
+++ sites/prod/settings_mkt.py
@@ -162,6 +162,7 @@ APP_PURCHASE_AUD = DOMAIN
 APP_PURCHASE_SECRET = private_mkt.APP_PURCHASE_SECRET
 
 PRODUCT_ICON_PATH = NETAPP_STORAGE + '/product-icons'
+DUMPED_APPS_PATH = NETAPP_STORAGE + '/dumped-apps'
 
 if NEWRELIC_ENABLE:
     NEWRELIC_INI = '/etc/newrelic.d/marketplace.firefox.com.ini'
