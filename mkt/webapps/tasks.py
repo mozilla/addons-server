74fe7a37661116819429b1b6562ea30b44a9a0d9
Rob Hudson <chudson@mozilla.com>
2013-06-05 11:32:38 -0700

    Added @write to indexing tasks (bug 879047)

--- mkt/webapps/tasks.py
+++ mkt/webapps/tasks.py
@@ -252,6 +252,7 @@ def update_supported_locales(ids, **kw):
 
 
 @task(acks_late=True)
+@write
 def index_webapps(ids, **kw):
     task_log.info('Indexing apps %s-%s. [%s]' % (ids[0], ids[-1], len(ids)))
 
@@ -269,6 +270,7 @@ def index_webapps(ids, **kw):
 
 
 @task(acks_late=True)
+@write
 def unindex_webapps(ids, **kw):
     task_log.info('Un-indexing apps %s-%s. [%s]' % (ids[0], ids[-1], len(ids)))
 
