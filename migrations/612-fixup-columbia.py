62f152c76a626181bbe813d8df2cb41fe8966193
Andy McKay <amckay@mozilla.com>
2013-06-12 16:40:18 -0700

    add in 3 tiers missed in 607

--- migrations/612-fixup-columbia.py
+++ migrations/612-fixup-columbia.py
@@ -1,6 +1,6 @@
 from constants.payments import PAYMENT_METHOD_CARD
 
-from market.models import PriceCurrency
+from market.models import Price, PriceCurrency
 from mkt.constants import regions
 
 
@@ -16,6 +16,23 @@ def run():
         pc.delete()
         print 'Deleted PriceCurrency of {0} for CO'.format(tier)
 
+
+    for tier, amount in [('14.99', '31280.00'),
+                         ('19.99', '41720.00'),
+                         ('24.99', '52160.00')]:
+        try:
+            price = Price.objects.get(price=tier)
+        except Price.DoesNotExist:
+            print 'Skipping adding in {0} for CO'.format(tier)
+            continue
+
+        if not PriceCurrency.objects.filter(tier=price,
+                                            region=regions.CO.id).exists():
+            PriceCurrency.objects.create(region=regions.CO.id, currency='COP',
+                                         price=amount, carrier=None,
+                                         provider=1, tier=price)
+            print 'Created {0} for CO'.format(tier)
+
     for tier in ['6.99', '9.99', '12.49', '14.99', '19.99', '24.99', '29.99']:
         try:
             pc = PriceCurrency.objects.get(tier__price=tier,
@@ -26,3 +43,4 @@ def run():
 
         pc.method = PAYMENT_METHOD_CARD
         pc.save()
+        print 'Updated {0} for CO to card'.format(tier)
