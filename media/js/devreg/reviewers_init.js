d45495671428448a98d1a5e6429300f6e196799c
Davor Spasovski <dspasovski@mozilla.com>
2013-06-19 17:19:43 -0700

    kill profile param temporarily

--- media/js/devreg/reviewers_init.js
+++ media/js/devreg/reviewers_init.js
@@ -9,7 +9,8 @@
 
     define('reviewers', modules, function(buckets) {
         // Append the profile string to the URL if reviewer is on mobile.
-        if (z.capabilities.firefoxOS || z.capabilities.firefoxAndroid) {
+        //if (z.capabilities.firefoxOS || z.capabilities.firefoxAndroid) {
+        if (false) {
             location.search = '?pro=' + buckets.get_profile();
         }
     });
