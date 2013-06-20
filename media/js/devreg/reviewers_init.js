8a8c16d7b4bbd909fcb6726481934675d4cea430
Davor Spasovski <dspasovski@mozilla.com>
2013-06-20 15:22:49 -0700

    testing reviewers profile init

--- media/js/devreg/reviewers_init.js
+++ media/js/devreg/reviewers_init.js
@@ -9,9 +9,8 @@
 
     define('reviewers', modules, function(buckets) {
         // Append the profile string to the URL if reviewer is on mobile.
-        //if (z.capabilities.firefoxOS || z.capabilities.firefoxAndroid) {
-        if (false) {
-            location.search = '?pro=' + buckets.get_profile();
+        if (z.capabilities.firefoxOS || z.capabilities.firefoxAndroid) {
+            location.href = location.href + '?pro=' + buckets.get_profile();
         }
     });
 
