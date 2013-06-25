e73bb7650ee61639426871e872322af20bec4890
Rob Hudson <chudson@mozilla.com>
2013-06-25 15:28:48 -0700

    Removed the inner task

--- mkt/webapps/tasks.py
+++ mkt/webapps/tasks.py
@@ -470,9 +470,7 @@ def fix_missing_icons(ids, **kw):
 
 
 # TODO: Remove the collapse_summary calls when bug 862603 is completed.
-@task
-@write
-def _collapse_summary(app, **kw):
+def _collapse_summary(app):
 
     task_log.info('[Webapp:%s] Collapsing summary.' % app.id)
 
