70d6389c2ab68f0f935e10247e1ea8da53dbeba3
Andy McKay <amckay@mozilla.com>
2013-06-28 15:57:47 -0700

    make url match docs

--- mkt/installs/urls.py
+++ mkt/installs/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from mkt.installs.api import install
 
 urlpatterns = patterns('',
-    url(r'^installs/record', install, name='app-install-list')
+    url(r'^installs/record/', install, name='app-install-list')
 )
