8cbad4ea9bb36eea452616c1783c1ad9bd574da5
Chris Van <cvan@mozilla.com>
2013-07-08 11:20:31 -0700

    whitespace fix

--- mkt/fireplace/urls.py
+++ mkt/fireplace/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, patterns, url
 from tastypie.api import Api
 from mkt.fireplace.api import AppResource
 
+
 fireplace = Api(api_name='fireplace')
 fireplace.register(AppResource())
 
