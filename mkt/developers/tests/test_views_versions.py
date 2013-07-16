d39c7764fc975ee0a6fc7b2fbd9a1dd884987632
Mathieu Pillard <mpillard@mozilla.com>
2013-07-16 15:21:39 +0200

    Fix test broken by 250f6a78542e5b73e0bc2207c90058e7b53205af

--- mkt/developers/tests/test_views_versions.py
+++ mkt/developers/tests/test_views_versions.py
@@ -279,19 +279,19 @@ class TestVersionPackaged(amo.tests.WebappTestCase):
         self.app = self.get_app()
         doc = pq(self.client.get(self.url).content)
         eq_(doc('#version-status').length, 1)
-        eq_(doc('#version-list li').length, 2)
+        eq_(doc('#version-list tbody tr').length, 2)
         # 1 pending and 1 public.
         eq_(doc('#version-list span.status-pending').length, 1)
         eq_(doc('#version-list span.status-public').length, 1)
         # Check version strings and order of versions.
         eq_(map(lambda x: x.text, doc('#version-list h4 a')),
-            ['Version 2.0', 'Version 1.0'])
+            ['2.0', '1.0'])
         # There should be 2 delete buttons.
-        eq_(doc('#version-list a.delete-version').length, 2)
+        eq_(doc('#version-list a.delete-version.button').length, 2)
         # Check download url.
-        eq_(doc('#version-list a.button.download').eq(0).attr('href'),
+        eq_(doc('#version-list a.download').eq(0).attr('href'),
             self.app.versions.all()[0].all_files[0].get_url_path(''))
-        eq_(doc('#version-list a.button.download').eq(1).attr('href'),
+        eq_(doc('#version-list a.download').eq(1).attr('href'),
             self.app.versions.all()[1].all_files[0].get_url_path(''))
 
     def test_delete_version(self):
