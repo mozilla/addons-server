70f679f90270d34e00ba4c38baf374fd5cbd0b49
Kevin Ngo <me@ngokevin.com>
2013-07-16 14:14:40 -0700

    add operator group for lookup (bug 894109)

--- mkt/lookup/helpers.py
+++ mkt/lookup/helpers.py
@@ -28,9 +28,13 @@ def user_header(account, title, is_admin=False, page_type=''):
 @jinja2.contextfunction
 def app_header(context, app, page_type=''):
     t = env.get_template('lookup/helpers/app_header.html')
+
+    is_author = acl.check_ownership(context['request'], app)
+    is_operator = 'Operators' in [g.name for g in context['request'].groups]
     is_admin = acl.action_allowed(context['request'], 'Users', 'Edit')
     is_staff = acl.action_allowed(context['request'], 'Apps', 'Configure')
     is_reviewer = acl.check_reviewer(context['request'])
     return jinja2.Markup(t.render(app=app, page_type=page_type,
                                   is_admin=is_admin, is_staff=is_staff,
-                                  is_reviewer=is_reviewer))
+                                  is_reviewer=is_reviewer, is_author=is_author,
+                                  is_operator=is_operator))
