893aa2c942e5980aae291f62d73fbd5c46c28942
Kevin Ngo <me@ngokevin.com>
2013-07-17 14:13:30 -0700

    fix operator group migration

--- migrations/629-operator-group.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO groups (name, rules, created, modified)
-        VALUES ('Operators', 'Lookup:View,AppLookup:View', NOW(), NOW());
-
-UPDATE `groups` SET rules=CONCAT(rules, ',Lookup:View,AppLookup:View') WHERE name='Support Staff';
