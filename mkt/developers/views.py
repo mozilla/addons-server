mozilla/addons-server

commit 9dc1a94f933f203c33ebf0bac0900e920dd9986e

list qhd as requirement for new mobile-only uploads (bug 881834)

mkt/developers/views.py
@@ -212,6 +212,9 @@ def status(request, addon_id, addon, webapp=False):
             return redirect(addon.get_dev_url('versions'))
 
         elif 'upload-version' in request.POST and upload_form.is_valid():
+            mobile_only = (addon.latest_version and
+                           addon.latest_version.features.has_qhd)
+
             ver = Version.from_upload(upload_form.cleaned_data['upload'],
                                       addon, [amo.PLATFORM_ALL])
 
@@ -222,6 +225,13 @@ def status(request, addon_id, addon, webapp=False):
             keys = ['has_%s' % feature.lower()
                     for feature in validation_result['feature_profile']]
             data = defaultdict.fromkeys(keys, True)
+
+            # Set "Smartphone-Sized Displays" if it's a mobile-only app.
+            qhd_devices = (set(amo.DEVICE_GAIA),
+                           set(amo.DEVICE_MOBILE),
+                           set(amo.DEVICE_GAIA, amo.DEVICE_MOBILE))
+            if set(addon.device_types) in qhd_devices or mobile_only:
+                data['has_qhd'] = True
 
             # Update feature profile for this version.
             ver.features.update(**data)

commit e505de21e43f3750cf7f30fe070f1be64c2efed0

Fix breakage introduced in 9dc1a94f

mkt/developers/views.py
@@ -227,9 +227,9 @@ def status(request, addon_id, addon, webapp=False):
             data = defaultdict.fromkeys(keys, True)
 
             # Set "Smartphone-Sized Displays" if it's a mobile-only app.
-            qhd_devices = (set(amo.DEVICE_GAIA),
-                           set(amo.DEVICE_MOBILE),
-                           set(amo.DEVICE_GAIA, amo.DEVICE_MOBILE))
+            qhd_devices = (set((amo.DEVICE_GAIA,)),
+                           set((amo.DEVICE_MOBILE,)),
+                           set((amo.DEVICE_GAIA, amo.DEVICE_MOBILE,)))
             if set(addon.device_types) in qhd_devices or mobile_only:
                 data['has_qhd'] = True
 