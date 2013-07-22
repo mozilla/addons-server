3dc2aa582716c8f944a56fece7f6db239cef41e4
Chuck Harmston <charmston@mozilla.com>
2013-07-22 14:46:13 -0500

    Adds migration for rocketfuel waffle switch (bug 894431)

---
+++ migrations/631-waffle-rocketfuel.sql
@@ -0,0 +1,2 @@
+INSERT INTO waffle_switch_mkt (name, active, created, modified, note)
+       VALUES ('rocketfuel', 0, NOW(), NOW(), 'Functionality related to bug #889556: Create a Marketplace Publishing Tool');
