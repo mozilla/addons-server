9e026a407687a8e7a06598e88b93feb7bd21d2cf
Mathieu Pillard <mpillard@mozilla.com>
2013-08-23 16:49:16 +0200

    Fix TestTranslationSerializerField.test_field_to_native() test

--- mkt/api/tests/test_fields.py
+++ mkt/api/tests/test_fields.py
@@ -3,9 +3,14 @@ from nose.tools import eq_
 
 from amo.tests import TestCase
 from mkt.api.fields import TranslationSerializerField
+from mkt.site.fixtures import fixture
+from mkt.webapps.models import Webapp
+from translations.models import Translation
 
 
 class TestTranslationSerializerField(TestCase):
+    fixtures = fixture('user_2519', 'webapp_337141')
+
     def test_from_native(self):
         data = u'Translatiön'
         field = TranslationSerializerField()
@@ -26,9 +31,18 @@ class TestTranslationSerializerField(TestCase):
         eq_(result, unicode(data))
 
     def test_field_to_native(self):
-        class TestObject(object):
-            test_field = u'Yes We Can'
-
+        app = Webapp.objects.get(pk=337141)
         field = TranslationSerializerField()
-        result = field.field_to_native(TestObject(), 'test_field')
-        eq_(result, TestObject.test_field)
+        result = field.field_to_native(app, 'name')
+        expected = {
+            'en-US': Translation.objects.get(id=app.name.id, locale='en-US'),
+            'es': Translation.objects.get(id=app.name.id, locale='es')
+        }
+        eq_(result, expected)
+
+        result = field.field_to_native(app, 'description')
+        expected = {
+            'en-US': Translation.objects.get(id=app.description.id, 
+                                             locale='en-US'),
+        }
+        eq_(result, expected)
