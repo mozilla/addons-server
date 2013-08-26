2c6c4d036153d4d205e7ef2d12b754b89d50cb2a
Chuck Harmston <charmston@mozilla.com>
2013-08-26 11:11:59 -0500

    Accept application/x-www-form-urlencoded at all DRF API endpoints. (bug 908936)

--- mkt/settings.py
+++ mkt/settings.py
@@ -303,6 +303,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         # By default no-one gets anything. You will have to override this
