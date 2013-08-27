47893ad0c23ae6b84a670cee89d2aa54df538fe7
Chris Van <cvan@mozilla.com>
2013-08-26 17:05:23 -0700

    fix "allow-paid-app-search" waffle flag migration

--- migrations/613-waffle-paid-app-search.sql
+++ migrations/613-waffle-paid-app-search.sql
@@ -1,2 +1,2 @@
-INSERT INTO waffle_flag_mkt (name, everyone, percent, superusers, staff, authenticated, rollout, note, created, modified)
-    VALUES ('allow-paid-app-search', 0, NULL, 0, 0, 0, 0, 'ON: paid apps show up in search results on desktop and firefoxos but not android; OFF: paid apps show up nowhere', NOW(), NOW());
+INSERT INTO waffle_flag_mkt (name, everyone, percent, superusers, staff, authenticated, rollout, note, testing, languages, created, modified)
+    VALUES ('allow-paid-app-search', 0, NULL, 0, 0, 0, 0, 0, '', 'ON: paid apps show up in search results on desktop and firefoxos but not android; OFF: paid apps show up nowhere', NOW(), NOW());
