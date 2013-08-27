18acb9b2a4bc23d15faa8e4d1474a3cafefa3d95
Andy McKay <amckay@mozilla.com>
2013-08-27 10:24:16 -0700

    add on _mkt prefix

--- migrations/650-bango-portal-waffle.sql
+++ migrations/650-bango-portal-waffle.sql
@@ -1,2 +1,2 @@
-INSERT INTO waffle_switch (name, active, created, modified, note)
+INSERT INTO waffle_switch_mkt (name, active, created, modified, note)
 VALUES ('bango-portal', 0, NOW(), NOW(), 'Redirects to Bango Vendor Portal.');
