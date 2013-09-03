35978123a2a3a71795328445ddf2c7055781adb0
Andy McKay <amckay@mozilla.com>
2013-09-03 10:19:47 -0700

    payments in VE (bug 898605)

--- sites/prod/settings_mkt.py
+++ sites/prod/settings_mkt.py
@@ -165,4 +165,5 @@ if NEWRELIC_ENABLE:
 
 ES_USE_PLUGINS = True
 
-PURCHASE_ENABLED_REGIONS = [regions.PL.id, regions.SPAIN.id, regions.CO.id]
+PURCHASE_ENABLED_REGIONS = [regions.PL.id, regions.SPAIN.id, regions.CO.id,
+                            regions.VE.id]
