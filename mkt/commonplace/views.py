ce769f135bfcdb332b1bc26b11ce5fa187ec71a3
Chris Van <cvan@mozilla.com>
2013-09-09 17:39:07 -0700

    import /var/tmp/build_{repo}.py file to get BUILD_ID for commonplace projects

--- mkt/commonplace/views.py
+++ mkt/commonplace/views.py
@@ -1,4 +1,5 @@
 import importlib
+import site
 
 from django.conf import settings
 from django.http import HttpResponseNotFound
@@ -22,12 +23,18 @@ def commonplace(request, repo):
         'flags': waffles(request),
     }
 
+    # This is where the `build_{repo}.py` files get written to after
+    # compiling and minifying our assets.
+    site.addsitedir('/var/tmp/')
+
     try:
         # Get the `BUILD_ID` from `build_{repo}.py` and use that to
         # cache-bust the assets for this repo's CSS/JS minified bundles.
-        module = 'media.%s.build_%s' % (repo, repo)
+        module = 'build_%s' % repo
         ctx['BUILD_ID'] = importlib.import_module(module).BUILD_ID
     except ImportError:
+        # Fall back to `BUILD_ID_JS` which is written to `build.py` by
+        # jingo-minify.
         pass
 
     return jingo.render(request, 'commonplace/index.html', ctx)
