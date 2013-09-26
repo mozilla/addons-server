13ef3d91cf887c96412a3e479187cfeb085f5965
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-09-26 13:33:16 -0500

    really fix inapp notice constraint (bug 865134)

--- migrations/668-drop-inapp.sql
+++ migrations/668-drop-inapp.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS addon_inapp_log;
-DROP TABLE IF EXISTS addon_inapp_payment;
 DROP TABLE IF EXISTS addon_inapp_notice;
+DROP TABLE IF EXISTS addon_inapp_payment;
 DROP TABLE IF EXISTS addon_inapp_image;
 DROP TABLE IF EXISTS bluevia;
 DROP TABLE IF EXISTS addon_bluevia;
