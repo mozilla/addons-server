2239f005d14815c1fd74baef0ca995fc34c2d41b
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-09-26 13:26:10 -0500

    Maybe fix f. key error deleting inapp (bug 865134)

--- migrations/668-drop-inapp.sql
+++ migrations/668-drop-inapp.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS addon_inapp_log;
 DROP TABLE IF EXISTS addon_inapp_payment;
 DROP TABLE IF EXISTS addon_inapp_image;
+DROP TABLE IF EXISTS bluevia;
 DROP TABLE IF EXISTS addon_bluevia;
 DROP TABLE IF EXISTS addon_inapp;
 DROP TABLE IF EXISTS addon_inapp_notice;
