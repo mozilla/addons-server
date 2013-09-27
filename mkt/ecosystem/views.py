ea351795f7cc172c0797dba1a0def8e80d14d9f4
Chris Van <cvan@mozilla.com>
2013-09-27 14:25:38 -0700

    add badges page to docs (bug 758512)

--- mkt/ecosystem/views.py
+++ mkt/ecosystem/views.py
@@ -143,6 +143,12 @@ def publish_payments(request):
         {'page': 'publish_payments', 'category': 'publish'})
 
 
+def publish_badges(request):
+    """Publish - Marketplace badges."""
+    return jingo.render(request, 'ecosystem/publish_badges.html',
+        {'page': 'badges', 'category': 'publish'})
+
+
 def publish_packaged(request):
     """Publish - Packaged apps page."""
     html_sample = u'''
