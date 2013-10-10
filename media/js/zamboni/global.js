f2f15d62b80665797eb47474481bc69f6a1e5df4
Kevin Ngo <ngoke@mozilla.com>
2013-10-10 11:28:48 -0700

    count line breaks as two chars in char counter (bug 922744)

--- media/js/zamboni/global.js
+++ media/js/zamboni/global.js
@@ -482,7 +482,9 @@ function initCharCount() {
         var $el = $(el),
             val = $el.val(),
             max = parseInt(cc.attr('data-maxlength'), 10),
-            left = max - val.length;
+            // Count \r\n as one character, not two.
+            lineBreaks = val.split('\n').length - 1,
+            left = max - val.length - lineBreaks;
         // L10n: {0} is the number of characters left.
         cc.html(format(ngettext('<b>{0}</b> character left.',
                                 '<b>{0}</b> characters left.', left), [left]))
