34265e44a7d405488b3d55fbeed2e183cdd747d2
Chris Van <cvan@mozilla.com>
2013-10-16 13:07:00 -0700

    update buckets.js

--- media/js/common/buckets.js
+++ media/js/common/buckets.js
@@ -5,7 +5,7 @@ define('buckets', [], function() {
 
     // Compatibilty with PhantomJS, which doesn't implement canPlayType
     if (!('canPlayType' in aelem)) {
-        function noop() {return '';}
+        function noop() {return '';};
         velem = aelem = {canPlayType: noop};
     }
 
@@ -15,18 +15,37 @@ define('buckets', [], function() {
         if (!context) {
             context = window;
         }
-        if (property in context) {
-            return context[property];
+        try {
+            if (property in context) {
+                return context[property];
+            }
+        } catch(e) {
+            return false;
         }
         // Camel-case it.
         property = property[0].toUpperCase() + property.substr(1);
 
         for (var i = 0, e; e = prefixes[i++];) {
-            if (!!(context[e + property])) {
-                return context[e + property];
+            try {
+                if ((e + property) in context) {
+                    return context[e + property];
+                }
+            } catch(e) {
+                return false;
             }
         }
     }
+
+    var has_gum = prefixed('getUserMedia', navigator);
+    if (has_gum && navigator.mozGetUserMedia) {
+        // Gecko 18's gum is a noop. FFFFFFFFFUUUUUUUUUUUUUU
+        try {
+            navigator.mozGetUserMedia(); // Should throw a TypeError.
+            has_gum = false;
+        } catch(e) {}
+    }
+
+    var has_audiocontext = !!(window.webkitAudioContext || window.AudioContext);
 
     var capabilities = [
         'mozApps' in navigator,
@@ -56,19 +75,35 @@ define('buckets', [], function() {
         window.screen.width <= 540 && window.screen.height <= 960,  // qHD support
         !!aelem.canPlayType('audio/mpeg').replace(/^no$/, ''),  // mp3 support
         !!(window.Audio),  // Audio Data API
-        !!(window.webkitAudioContext || window.AudioContext),  // Web Audio API
+        has_audiocontext,  // Web Audio API
         !!velem.canPlayType('video/mp4; codecs="avc1.42E01E"').replace(/^no$/,''),  // H.264
         !!velem.canPlayType('video/webm; codecs="vp8"').replace(/^no$/,''),  // WebM
         !!prefixed('cancelFullScreen', document),  // Full Screen API
         !!prefixed('getGamepads', navigator),  // Gamepad API
-        !!(prefixed('persistentStorage') || window.StorageInfo)  // Quota Management API
+        !!(prefixed('persistentStorage') || window.StorageInfo),  // Quota Management API
+        // WebRTC:
+        has_gum && !prefixed('cameras', navigator),  // Can take photos
+        has_gum && has_audiocontext &&
+            !!((new (window.AudioContext || window.webkitAudioContext)()).createMediaStreamSource),  // Can record audio
+        has_gum && false,  // XXX: Google WebRTC issue 2088
+        'MediaStream' in window,
+        'DataChannel' in window,
+        prefixed('RTCPeerConnection'),
+        prefixed('SpeechSynthesisEvent'),  // WebSpeech Synthesis
+        prefixed('SpeechInputEvent'),  // WebSpeech Input
+        prefixed('requestPointerLock', document.documentElement),  // Pointer lock
+        prefixed('notification', navigator),  // TODO: window.webkitNotifications?
+        prefixed('alarms', navigator),  // Alarms
+        'mozSystem' in (new XMLHttpRequest()),  // mozSystemXHR
+        prefixed('TCPSocket', navigator),  // mozTCPSocket/mozTCPSocketServer
+        prefixed('mozInputMethod', navigator)
     ];
 
     var profile = parseInt(capabilities.map(function(x) {return !!x ? '1' : '0';}).join(''), 2).toString(16);
     // Add a count.
     profile += '.' + capabilities.length;
     // Add a version number.
-    profile += '.1';
+    profile += '.3';
 
     return {
         get_profile: function() {return profile;},
