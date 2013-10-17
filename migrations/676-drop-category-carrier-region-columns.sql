1c19e57a170216fbae1bd18477103cd6df844a45
Chris Van <cvan@mozilla.com>
2013-10-17 14:18:50 -0700

    fix migration
    This was run manually on -dev. Run manually locally if needed.

--- migrations/676-drop-category-carrier-region-columns.sql
+++ migrations/676-drop-category-carrier-region-columns.sql
@@ -1,6 +1,10 @@
 -- Delete supervisors of legacy operator shelves/special categories.
 drop table categories_supervisors;
 
+-- Cascade deletes for addons_categories.
+alter table addons_categories drop foreign key addons_categories_ibfk_4;
+alter table addons_categories add constraint addons_categories_ibfk_4 foreign key (category_id) REFERENCES categories (id) ON DELETE CASCADE;
+
 -- Delete legacy operator shelves/special categories.
 delete from categories where carrier is not null or region is not null;
 
