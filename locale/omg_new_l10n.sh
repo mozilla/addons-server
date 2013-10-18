a2d49d2202df9fbe6baa291d054d5bf15c695260
Wil Clouser <clouserw@gmail.com>
2013-10-18 13:35:21 -0700

    update l10n script

--- locale/omg_new_l10n.sh
+++ locale/omg_new_l10n.sh
@@ -1,22 +1,37 @@
 #! /bin/bash
 
 # This script will do the following:
-#   - Update your /locales/
-#   - Extract all new strings
+#   - Update your code
+#   - Extract new strings and push to the .po files
 #   - Compile all .po files
 #   - Commit all your changes
-#   - Email the localizers that you've extracted
+#   - Email the l10n list
 #
 # This script makes a lot of assumptions and has no error checking, so read it
 # over before you run it.
 #
 # Questions?  Talk to clouserw.
 
-LOCALIZERS="dev-l10n-web@lists.mozilla.org"
-MERGE_FLAGS="--update --no-fuzzy-matching --width=200"
+
+EMAIL_FROM="Marketplace Developers <dev-l10n-web@lists.mozilla.org>"
+EMAIL_TO="Awesome Localizers <dev-l10n-web@lists.mozilla.org>"
+EMAIL_SUBJECT="[Marketplace] .po files updated"
+
+# A link to the .po files
+EMAIL_SOURCE="https://github.com/mozilla/zamboni/tree/master/locale"
+
+# gettext flags
 CLEAN_FLAGS="--no-obsolete --width=200"
+MERGE_FLAGS="--update --no-fuzzy-matching --width=200 --backup=none"
+unset DOALLTHETHINGS
+
+# -------------------------------------------------------------------
 
 function confirm {
+    if [ ! -z $DOALLTHETHINGS ]; then
+        return 0
+    fi
+
     PROMPT=$1
     read -p "$PROMPT [y/n]: " YESNO
     if [[ $YESNO == 'y' ]]
@@ -27,27 +42,37 @@ function confirm {
     fi
 }
 
+if [[ "$1" == "--do-all-the-things" ]]; then
+    DOALLTHETHINGS=1
+fi
+
 if [ ! -d "locale" ]; then
     echo "Sorry, please run from the root of the project, eg.  ./locale/omg_new_l10n.sh"
+    exit 1
+fi
+
+if [ ! -z "$(git status --porcelain)" ]; then
+    echo "Looks like you have some local changes.  Please clean up your root before we start committing random things."
+    git status
     exit 1
 fi
 
 echo "Alright, here we go..."
 
 if confirm "Update locales?"; then
-    pushd locale > /dev/null
-    git co master
+    git co master --quiet
     git pull
-    popd > /dev/null
 fi
 
 if confirm "Extract new strings?"; then
     ./manage.py extract
+    commonplace extract_strings
 fi
 
 if confirm "Merge new strings to .po files?"; then
     pushd locale > /dev/null
-    echo "Merging any new zamboni keys..."
+
+    echo "Merging any new keys..."
     for i in `find . -name "messages.po" | grep -v "en_US"`; do
         msgmerge $MERGE_FLAGS "$i" "templates/LC_MESSAGES/messages.pot"
     done
@@ -74,6 +99,11 @@ if confirm "Process your debug language?"; then
     podebug --rewrite=unicode locale/templates/LC_MESSAGES/javascript.pot locale/dbg/LC_MESSAGES/javascript.po
 fi
 
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Looks like there are no new strings to commit."
+    exit 0
+fi
+
 if confirm "Compile all the .po files?"; then
     pushd locale > /dev/null
     ./compile-mo.sh .
@@ -81,37 +111,36 @@ if confirm "Compile all the .po files?"; then
 fi
 
 if confirm "Commit your changes?"; then
-    pushd locale > /dev/null
-    git commit . -m "Extract/compile script.  Today's lucky number is $RANDOM."
+    git commit locale -m "Extract/compile script.  Today's lucky number is $RANDOM."
     git push mozilla master
-    popd > /dev/null
 fi
 
 echo "Calculating changes...."
 pushd locale > /dev/null
-SUBJECT="[Marketplace] .po files updated"
 CHANGES=$(cat <<MAIL
-From: "Marketplace Developers" <marketplace-devs@mozilla.org>
-To: "Awesome Localizers" <$LOCALIZERS>
-Subject: $SUBJECT
+CHANGES=$(cat <<MAIL
+From: $EMAIL_FROM
+To: $EMAIL_TO
+Subject: $EMAIL_SUBJECT
 
 Hi,
 
-I am an automated script letting you know the main Marketplace .po files have
-just been updated.  Unless something unusual is happening, we do weekly pushes
-on Tuesdays so any strings committed by then will go live.  To give you an idea
-of the number of new strings I will calculate untranslated strings.
+I am an automated script letting you know some .po files have just been
+updated.  Unless something unusual is happening, we do weekly pushes on
+Tuesdays so any strings committed by then will go live.  To give you an idea of
+the number of new strings I will calculate untranslated strings below.
 
 `./stats-po.sh`
 
-Source files: https://github.com/mozilla/zamboni/tree/master/locale
+Source files: $EMAIL_SOURCE
 
 If you have any questions please reply to the list.
 
 Thanks so much for all your help!
 
 
-MAIL)
+MAIL
+)
 popd > /dev/null
 
 echo "-----------------------------------------------"
@@ -119,12 +148,9 @@ echo "$CHANGES"
 echo "-----------------------------------------------"
 
 # Uses sendmail so we can set a real From address
-if confirm "Do you want to send that to $LOCALIZERS?"; then
+if confirm "Do you want to send that to $EMAIL_TO?"; then
     echo "$CHANGES" | /usr/lib/sendmail -t
 fi
 
-if confirm "Do you want to email Milos? :D"; then
-    echo "Please update AMO in Verbatim. Thanks." | mail -s "Verbatim update" milos@mozilla.com
-fi
-
+unset DOALLTHETHINGS
 echo "done."
