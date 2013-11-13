bf50a755f32407bc787258e8a5ccf8a219a86106
Jason Thomas <jason@lithiumfox.com>
2013-11-13 13:54:05 -0800

    Add GOOGLE_ANALYTICS_CREDENTIALS to settings_base.py on stage.

--- sites/stage/settings_base.py
+++ sites/stage/settings_base.py
@@ -121,6 +121,7 @@ CRONJOB_LOCK_PREFIX = 'marketplace-stage'
 BUILDER_SECRET_KEY = private.BUILDER_SECRET_KEY
 BUILDER_VERSIONS_URL = "https://builder-addons.allizom.org/repackage/sdk-versions/"
 
+GOOGLE_ANALYTICS_CREDENTIALS = private.GOOGLE_ANALYTICS_CREDENTIALS
 GOOGLE_API_CREDENTIALS = private.GOOGLE_API_CREDENTIALS
 
 ES_HOSTS = splitstrip(private.ES_HOSTS)
