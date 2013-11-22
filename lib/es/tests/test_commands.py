96454db31539ea803519adc4478a8ecb1f260742
Mathieu Agopian <mathieu.agopian@gmail.com>
2013-11-22 20:46:17 +0100

    fix reindex management command test

--- lib/es/tests/test_commands.py
+++ lib/es/tests/test_commands.py
@@ -1,18 +1,16 @@
-import os
-import subprocess
-import sys
-import time
+import StringIO
+import threading
 
-import mock
+from nose.exc import SkipTest
 from nose.tools import eq_
 from pyelasticsearch.exceptions import ElasticHttpNotFoundError
 
 from django.conf import settings
+from django.core import management
 from django.db import connection
 
 import amo.search
 import amo.tests
-from addons.models import AddonCategory, Category
 from amo.urlresolvers import reverse
 from amo.utils import urlparams
 from es.management.commands.reindex import (call_es, database_flagged,
@@ -24,7 +22,12 @@ from mkt.webapps.models import Webapp, WebappIndexer
 
 
 class TestIndexCommand(amo.tests.ESTestCase):
-    fixtures = fixture('webapp_337141')
+
+    @classmethod
+    def setUpClass(cls):
+        if settings.MARKETPLACE:
+            raise SkipTest('Only an amo management command')
+        super(TestIndexCommand, cls).setUpClass()
 
     def setUp(self):
         super(TestIndexCommand, self).setUp()
@@ -32,185 +35,116 @@ class TestIndexCommand(amo.tests.ESTestCase):
             unflag_database()
 
         self.url = reverse('search.search')
-        self.webapp = Webapp.objects.get(id=337141)
-        self.apps = [self.webapp]
-        self.cat = Category.objects.create(name='Games', type=amo.ADDON_WEBAPP)
-        AddonCategory.objects.create(addon=self.webapp, category=self.cat)
-        # Emit post-save signal so the app gets reindexed.
-        self.webapp.save()
-        self.refresh()
 
-        # XXX I have not find a better way for now
-        source = os.path.join(os.path.dirname(__file__), 'settings.tmpl')
-        self.settings = 'settings_%s' % os.urandom(5).encode('hex')
-        self.target = os.path.join(settings.ROOT, self.settings + '.py')
-        self.target_pyc = self.target + 'c'
-        with open(source) as f:
-            data = {'DB': settings.DATABASES['default']['NAME']}
-            with open(self.target, 'w') as target:
-                target.write(f.read() % data)
-
-        # any index created during the test will be deleted
+        # Any index created during the test will be deleted.
         self.indices = call_es('_status').json()['indices'].keys()
 
     def tearDown(self):
-        for file_ in (self.target, self.target_pyc):
-            if os.path.exists(file_):
-                os.remove(file_)
-
         current_indices = call_es('_status').json()['indices'].keys()
         for index in current_indices:
             if index not in self.indices:
                 call_es(index, method='DELETE')
 
-    def check_results(self, params, expected, sorted=True):
-        r = self.client.get(urlparams(self.url, **params), follow=True)
-        eq_(r.status_code, 200, str(r.content))
-        got = self.get_results(r)
-        if sorted:
-            got.sort()
-            expected.sort()
+    def check_results(self, expected):
+        """Make sure the expected addons are listed in a standard search."""
+        response = self.client.get(urlparams(self.url, sort='downloads'))
+        eq_(response.status_code, 200, str(response.content))
+        got = self.get_results(response)
 
-        eq_(got, expected,
-            'Got: %s. Expected: %s. Parameters: %s' % (got, expected, params))
-        return r
+        for addon in expected:
+            assert addon.pk in got, '%s is not in %s' % (addon.pk, got)
+        return response
 
-    def get_results(self, r, sort=False):
+    def get_results(self, response):
         """Return pks of add-ons shown on search results page."""
-        pager = r.context['pager']
+        pager = response.context['pager']
         results = []
         for page_num in range(pager.paginator.num_pages):
             results.extend([item.pk for item
                             in pager.paginator.page(page_num + 1)])
-        if sort:
-            results = sorted(results)
         return results
 
-    def _create_app(self, name='app', signal=True):
-        webapp = Webapp.objects.create(status=amo.STATUS_PUBLIC,
-                                       name=name,
-                                       type=amo.ADDON_WEBAPP)
-        AddonCategory.objects.create(addon=webapp, category=self.cat)
-        webapp.save(_signal=signal)
-        return webapp
+    def get_indices_aliases(self):
+        """Return the test indices with an alias."""
+        indices = call_es('_aliases').json()
+        items = [(index, aliases['aliases'].keys()[0])
+                 for index, aliases in indices.items()
+                 if len(aliases['aliases']) > 0 and index.startswith('test')]
+        items.sort()
+        return items
 
     def test_reindexation(self):
-        # adding a web app
-        webapp2 = self._create_app('neat app 2')
+        # Adding an addon.
+        addon = amo.tests.addon_factory()
         self.refresh()
 
-        # this search should return both apps
-        r = self.check_results({'sort': 'popularity'},
-                               [webapp2.pk, self.webapp.pk])
+        # The search should return the addon.
+        wanted = [addon]
+        self.check_results(wanted)
 
-        # adding 5 more apps
-        webapps = [self._create_app('moarneatapp %d' % i)
-                   for i in range(5)]
-        self.refresh()
+        # Current indices with aliases.
+        old_indices = self.get_indices_aliases()
 
-        # XXX is there a cleaner way ?
-        # all I want is to have those webapp in the DB
-        # so the reindex command sees them
+        # This is to start a reindexation in the background.
+        class ReindexThread(threading.Thread):
+            def __init__(self):
+                self.stdout = StringIO.StringIO()
+                super(ReindexThread, self).__init__()
+
+            def run(self):
+                management.call_command('reindex', stdout=self.stdout)
+        t = ReindexThread()
+        t.start()
+
+        # Wait for the reindex in the thread to flag the database.
+        # The database transaction isn't shared with the thread, so force the
+        # commit.
+        while t.is_alive() and not database_flagged():
+            connection._commit()
+            connection.clean_savepoints()
+
+        # We should still be able to search in the foreground while the reindex
+        # is being done in the background.
+        #
+        # We should also be able to index new documents, and
+        # they should not be lost.
+        old_addons_count = len(wanted)
+        while t.is_alive() and len(wanted) < old_addons_count + 3:
+            wanted.append(amo.tests.addon_factory())
+            connection._commit()
+            connection.clean_savepoints()
+            amo.search.get_es().refresh()
+            self.check_results(wanted)
+
+        if len(wanted) == old_addons_count:
+            raise AssertionError("Could not index objects in foreground while "
+                                 "reindexing in the background.")
+
+        t.join()  # Wait for the thread to finish.
+        t.stdout.seek(0)
+        stdout = t.stdout.read()
+        assert 'Reindexation done' in stdout, stdout
+
+        # The reindexation is done, let's double check we have all our docs.
         connection._commit()
         connection.clean_savepoints()
-
-        # right now, the DB should be composed of
-        # two indexes, and two aliases, let's check
-        # we have two aliases
-        aliases = call_es('_aliases').json()
-        old_aliases = [(index, aliases['aliases'].keys()[0])
-                       for index, aliases in aliases.items()
-                       if len(aliases['aliases']) > 0 and
-                       index.startswith('test')]
-        old_aliases.sort()
-
-        # now doing a reindexation in a background process
-        args = [sys.executable, 'manage.py', 'reindex', '--prefix=test_',
-                '--settings=%s' % self.settings]
-
-        indexer = subprocess.Popen(args,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   cwd=settings.ROOT)
-
-        try:
-            # we should be able to continue some searching in the foreground
-            # and always get our documents
-            #
-            # we should also be able to index new documents, and
-            # they should not be lost
-            count = 1
-            wanted = [app.pk for app in webapps] + [webapp2.pk, self.webapp.pk]
-
-            # let's add more apps, and also do some searches
-            while indexer.poll() is None and count < 8:
-                r = self.client.get(urlparams(self.url, sort='popularity'),
-                                    follow=True)
-                eq_(r.status_code, 200, str(r.content))
-                got = self.get_results(r)
-                got.sort()
-                self.assertEqual(len(got), len(wanted), (got, wanted))
-                wanted.append(self._create_app('moar %d' % count).pk)
-                self.refresh()
-                connection._commit()
-                connection.clean_savepoints()
-                count += 1
-                time.sleep(.1)
-
-            if count < 3:
-                raise AssertionError("Could not index enough objects for the "
-                                     "test to be meaningful.")
-        except Exception:
-            indexer.terminate()
-            raise
-
-        stdout, stderr = indexer.communicate()
-        self.assertTrue('Reindexation done' in stdout, stdout + '\n' + stderr)
-
         amo.search.get_es().refresh()
-        # the reindexation is done, let's double check we have all our docs
-        self.check_results({'sort': 'popularity'}, wanted)
+        self.check_results(wanted)
 
-        # let's check the aliases as well, we should have 2
-        aliases = call_es('_aliases').json()
-        new_aliases = [(index, aliases['aliases'].keys()[0])
-                       for index, aliases in aliases.items()
-                       if len(aliases['aliases']) > 0 and
-                       index.startswith('test')]
-        new_aliases.sort()
-        self.assertTrue(len(new_aliases), 2)
-
-        # and they should be new aliases
-        self.assertNotEqual(new_aliases, old_aliases)
-
-    def test_remove_index(self):
-        # Putting a test_amo index in the way.
-        es = amo.search.get_es()
-
-        for index in es.get_indices().keys():
-            for prefix in ('test_amo', 'test_amo_stats'):
-                if index.startswith(prefix + '-'):
-                    es.delete_alias(prefix, [index])
-                    es.delete_index(index)
-                    es.create_index(prefix)
-
-        # reindexing the first app
-        self.webapp.save()
-        self.refresh()
-
-        # now doing a reindexation in a background process
-        args = [sys.executable, 'manage.py', 'reindex', '--prefix=test_',
-                '--settings=%s' % self.settings]
-
-        indexer = subprocess.Popen(args,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   cwd=settings.ROOT)
-        stdout, stderr = indexer.communicate()
+        # New indices have been created, and aliases now point to them.
+        new_indices = self.get_indices_aliases()
+        eq_(len(old_indices), len(new_indices), (old_indices, new_indices))
+        assert new_indices != old_indices
 
 
 class TestFixupCommand(amo.tests.ESTestCase):
     fixtures = fixture('webapp_337141')
+
+    @classmethod
+    def setUpClass(cls):
+        if not settings.MARKETPLACE:
+            raise SkipTest('Only a marketplace management command')
+        super(TestFixupCommand, cls).setUpClass()
 
     def setUp(self):
         super(TestFixupCommand, self).setUp()
