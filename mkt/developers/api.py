61fa6242e1638ecbbb40351aff23b6eaaf0b1f09
Rob Hudson <chudson@mozilla.com>
2013-12-11 12:10:30 -0800

    Added more logging to iarc pingback

--- mkt/developers/api.py
+++ mkt/developers/api.py
@@ -109,11 +109,15 @@ class ContentRatingsPingback(CORSMixin, SlugOrIdMixin, CreateAPIView):
             app.set_interactives(data.get('interactives', []))
 
             # Update status if incomplete status.
-            log.debug('Reasons for app:%s submission incompleteness: %s' %
-                      (app.id, app.is_fully_complete()[1]))
-            if app.is_incomplete() and app.is_fully_complete()[0]:
+            log.debug('Checking app:%s completeness after IARC pingback.'
+                      % app.id)
+            is_fully_complete, reasons = app.is_fully_complete()[0]
+            if app.is_incomplete() and is_fully_complete:
                 log.debug('Updating app status from IARC pingback for app:%s' %
                           app.id)
                 app.update(status=amo.STATUS_PENDING)
+            else:
+                log.debug('Reasons for app:%s incompleteness after IARC '
+                          'pingback: %s' % (app.id, reasons))
 
         return Response('ok')
