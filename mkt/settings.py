5082f6e5872341edc0fe1da8633637c3881d320e
Mathieu Pillard <mpillard@mozilla.com>
2014-01-22 19:04:38 +0100

    Don't bother respecting Accept if we only have one renderer (bug 956860)

--- mkt/settings.py
+++ mkt/settings.py
@@ -280,6 +280,9 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'mkt.api.renderers.SuccinctJSONRenderer',
     ),
+    'DEFAULT_CONTENT_NEGOTIATION_CLASS': (
+        'mkt.api.renderers.FirstAvailableRenderer'
+    ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
