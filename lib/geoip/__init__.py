f46c39120b0f381abb25145d17269850cf95e7f4
Mathieu Pillard <m@virgule.net>
2014-02-12 22:39:11 +0100

    Log geoip requests and responses (bug 941939)

--- lib/geoip/__init__.py
+++ lib/geoip/__init__.py
@@ -57,6 +57,9 @@ class GeoIP:
                     log.error('Geodude connection error: {0}'.format(str(e)))
                 if res and res.status_code == 200:
                     statsd.incr('z.geoip.success')
-                    return res.json().get('country_code',
-                                          self.default_val).lower()
+                    country_code = res.json().get('country_code',
+                        self.default_val).lower()
+                    log.info(('Geodude lookup for {0} returned {1}'
+                              .format(address, country_code)))
+                    return country_code
         return self.default_val
