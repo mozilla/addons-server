87c357d135100ab358e44ff54e393d7138065986
Mathieu Pillard <m@virgule.net>
2014-02-13 13:31:14 +0100

    Remove Accept-Language fallback in RegionMiddleware (bug 941939)
    It does us more harm than good, I shouldn't be localized in the US
    just because I have en-US as my prefered language.

--- mkt/regions/middleware.py
+++ mkt/regions/middleware.py
@@ -10,18 +10,9 @@ import mkt
 log = commonware.log.getLogger('mkt.regions')
 
 
-def _save_if_changed(request, user_region):
-    """
-    Update the region on the user object if it changed.
-    """
-    amo_user = getattr(request, 'amo_user', None)
-    if amo_user and amo_user.region != user_region.slug:
-        amo_user.region = user_region.slug
-        amo_user.save()
-
-
 class RegionMiddleware(object):
-    """Figure out the user's region and store it in a cookie."""
+    """Figure out the user's region and set request.REGION accordingly, storing
+    it on the request.amo_user if there is one."""
 
     def __init__(self):
         self.geoip = GeoIP(settings)
@@ -44,7 +35,7 @@ class RegionMiddleware(object):
             mkt.regions.set_region(restofworld)
             return
 
-        # ?region= -> geoip -> lang
+        # Try 'region' in POST/GET data first, if it's not there try geoip.
         url_region = request.REQUEST.get('region')
         if url_region in regions:
             statsd.incr('z.regions.middleware.source.url')
@@ -52,44 +43,19 @@ class RegionMiddleware(object):
             log.info('Region {0} specified in URL; region set as {1}'
                      .format(url_region, user_region.slug))
         else:
+            statsd.incr('z.regions.middleware.source.geoip')
             user_region = self.region_from_request(request)
             log.info('Region not specified in URL; region set as {0}'
                      .format(user_region.slug))
-            # If the above fails, let's try `Accept-Language`.
-            if user_region == restofworld:
-                log.info('Region from GeoIP was RESTOFWORLD')
-                statsd.incr('z.regions.middleware.source.accept-lang')
-                if request.LANG == settings.LANGUAGE_CODE:
-                    choices = mkt.regions.REGIONS_CHOICES[1:]
-                else:
-                    choices = mkt.regions.REGIONS_CHOICES
-                if request.LANG:
-                    for name, region in choices:
-                        if name.lower() in request.LANG.lower():
-                            user_region = region
-                            _save_if_changed(request, user_region)
-                            break
-                # All else failed, try to match against our forced Language.
-                if user_region == mkt.regions.RESTOFWORLD:
-                    # Try to find a suitable region.
-                    for name, region in choices:
-                        if region.default_language == request.LANG:
-                            user_region = region
-                            _save_if_changed(request, user_region)
-                            break
-                log.info('Region set as {0}'.format(user_region.slug))
 
-                accept_language = request.META.get('HTTP_ACCEPT_LANGUAGE')
-                if (user_region == mkt.regions.US
-                        and accept_language is not None
-                        and not accept_language.startswith('en')):
-                    # Let us default to restofworld if it's not English.
-                    user_region = mkt.regions.RESTOFWORLD
-            else:
-                statsd.incr('z.regions.middleware.source.geoip')
+        # Update the region on the user object if it changed.
+        amo_user = getattr(request, 'amo_user', None)
+        if amo_user and amo_user.region != user_region.slug:
+            amo_user.region = user_region.slug
+            amo_user.save()
 
-        _save_if_changed(request, user_region)
         log.info('Region finally set as {0}'.format(user_region.slug))
 
+        # Persist the region on the request / local thread.
         request.REGION = user_region
         mkt.regions.set_region(user_region)
