7aa48b9c1e6d8fc798f3dcb893868d12100245a1
Mathieu Pillard <mpillard@mozilla.com>
2014-02-14 17:28:47 +0100

    Only consider Webapps in ratings API (bug 956405)

--- mkt/ratings/views.py
+++ mkt/ratings/views.py
@@ -42,7 +42,7 @@ class RatingPaginator(Paginator):
 
 class RatingViewSet(CORSMixin, MarketplaceView, ModelViewSet):
     # Unfortunately, the model class name for ratings is "Review".
-    queryset = Review.objects.valid()
+    queryset = Review.objects.valid().filter(addon__type=amo.ADDON_WEBAPP)
     cors_allowed_methods = ('get', 'post', 'put', 'delete')
     permission_classes = [ByHttpMethod({
         'options': AllowAny,  # Needed for CORS.
