089c3473362e934888fd8e4afbc460f711b48a45
Kevin Ngo <ngoke@mozilla.com>
2014-02-20 10:09:47 -0800

    point mkt stage towards iarc demo

--- sites/stage/settings_mkt.py
+++ sites/stage/settings_mkt.py
@@ -180,10 +180,10 @@ BANGO_BASE_PORTAL_URL = 'https://mozilla.bango.com/login/al.aspx?'
 MONOLITH_INDEX = 'mktstage-time_*'
 
 # IARC content ratings.
-IARC_ENV = 'prod'
+IARC_ENV = 'test'
 IARC_MOCK = False
 IARC_PASSWORD = private_mkt.IARC_PASSWORD
 IARC_PLATFORM = 'Firefox'
-IARC_SERVICE_ENDPOINT = 'https://www.globalratings.com/IARCProdService/IARCServices.svc'
+IARC_SERVICE_ENDPOINT = 'https://www.globalratings.com/IARCDEMOService/IARCServices.svc'
 IARC_STOREFRONT_ID = 4
-IARC_SUBMISSION_ENDPOINT = 'https://www.globalratings.com/IARCProdRating/Submission.aspx'
+IARC_SUBMISSION_ENDPOINT = 'https://www.globalratings.com/IARCDEMORating/Submission.aspx'
