639aaaf60cf57404b921c30ef8e5b97c25a2876f
Allen Short <ashort@mozilla.com>
2014-04-02 15:20:47 -0500

    switch back to mysql pool

--- lib/settings_base.py
+++ lib/settings_base.py
@@ -79,8 +79,6 @@ MARKETPLACE_EMAIL = 'marketplace-staff@mozilla.org'
 ABUSE_EMAIL = 'marketplace-staff+abuse@mozilla.org'
 NOBODY_EMAIL = 'nobody@mozilla.org'
 
-CONN_MAX_AGE = 60
-
 DATABASES = {
     'default': {
         'NAME': 'zamboni',
