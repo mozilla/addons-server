c9865e8228f5a6de75c19e199338412f174cf19c
Allen Short <ashort@mozilla.com>
2014-04-24 10:59:39 -0500

    try again with paid-addons schema changes

---
+++ migrations/759-remove-market.sql
@@ -0,0 +1,17 @@
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS refunds;
+DROP TABLE IF EXISTS price_currency;
+DROP TABLE IF EXISTS addons_premium;
+DROP TABLE IF EXISTS inapp_products;
+DROP TABLE IF EXISTS addon_purchase;
+DROP TABLE IF EXISTS addon_payment_data;
+DROP TABLE IF EXISTS paypal_checkstatus;
+ALTER TABLE stats_contributions
+    DROP FOREIGN KEY price_tier_id_refs,
+    DROP FOREIGN KEY user_id_refs,
+    DROP COLUMN price_tier_id,
+    DROP COLUMN related_id,
+    DROP COLUMN user_id,
+    DROP COLUMN type;
+DROP TABLE IF EXISTS prices;
+SET foreign_key_checks = 1;
