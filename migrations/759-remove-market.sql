41c7797217b8f9bf99da7b610f5cbb9f7ca9231e
Mathieu Agopian <mathieu.agopian@gmail.com>
2014-04-24 19:16:18 +0200

    fix database migration (bug 997695)

--- migrations/759-remove-market.sql
+++ migrations/759-remove-market.sql
@@ -1,4 +1,3 @@
-SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS refunds;
 DROP TABLE IF EXISTS price_currency;
 DROP TABLE IF EXISTS addons_premium;
@@ -6,13 +5,41 @@ DROP TABLE IF EXISTS inapp_products;
 DROP TABLE IF EXISTS addon_purchase;
 DROP TABLE IF EXISTS addon_payment_data;
 DROP TABLE IF EXISTS paypal_checkstatus;
-ALTER TABLE stats_contributions
-    DROP FOREIGN KEY related_id_refs,
-    DROP FOREIGN KEY price_tier_id_refs,
-    DROP FOREIGN KEY user_id_refs,
-    DROP COLUMN price_tier_id,
-    DROP COLUMN related_id,
-    DROP COLUMN user_id,
-    DROP COLUMN type;
-DROP TABLE IF EXISTS prices;
-SET foreign_key_checks = 1;
+
+
+drop procedure if exists drop_fk_col_if_exists;
+
+delimiter ';;'
+
+create procedure drop_fk_col_if_exists() begin
+    /* drop the foreign keys if they exist */
+    if exists (select * from information_schema.table_constraints where table_schema = (SELECT DATABASE()) and table_name = 'stats_contributions' and constraint_name = 'related_id_refs') then
+        alter table stats_contributions drop foreign key related_id_refs;
+    end if;
+    if exists (select * from information_schema.table_constraints where table_schema = (SELECT DATABASE()) and table_name = 'stats_contributions' and constraint_name = 'price_tier_id_refs') then
+        alter table stats_contributions drop foreign key price_tier_id_refs;
+    end if;
+    if exists (select * from information_schema.table_constraints where table_schema = (SELECT DATABASE()) and table_name = 'stats_contributions' and constraint_name = 'user_id_refs') then
+        alter table stats_contributions drop foreign key user_id_refs;
+    end if;
+
+    /* drop the columns if they exist */
+    if exists (select * from information_schema.columns where table_schema = (SELECT DATABASE()) and table_name = 'stats_contributions' and column_name = 'price_tier_id') then
+        alter table stats_contributions drop column price_tier_id;
+    end if;
+    if exists (select * from information_schema.columns where table_schema = (SELECT DATABASE()) and table_name = 'stats_contributions' and column_name = 'related_id') then
+        alter table stats_contributions drop column related_id;
+    end if;
+    if exists (select * from information_schema.columns where table_schema = (SELECT DATABASE()) and table_name = 'stats_contributions' and column_name = 'user_id') then
+        alter table stats_contributions drop column user_id;
+    end if;
+    if exists (select * from information_schema.columns where table_schema = (SELECT DATABASE()) and table_name = 'stats_contributions' and column_name = 'type') then
+        alter table stats_contributions drop column type;
+    end if;
+end;;
+
+delimiter ';'
+
+call drop_fk_col_if_exists();
+
+drop procedure if exists drop_fk_col_if_exists;
