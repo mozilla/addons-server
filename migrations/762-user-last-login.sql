f2b827e17d5b2547696da109a7be0174fee79d75
Allen Short <ashort@mozilla.com>
2014-05-09 13:04:34 -0500

    use real date

--- migrations/762-user-last-login.sql
+++ migrations/762-user-last-login.sql
@@ -1,2 +1,2 @@
 ALTER TABLE users
-    CHANGE COLUMN last_login last_login datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
+    CHANGE COLUMN last_login last_login datetime NOT NULL DEFAULT '1970-01-01 00:00:00';
