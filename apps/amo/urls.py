df96eb55c90aae3f42516ee4a34bf32cb0b6e846
Wil Clouser <clouserw@gmail.com>
2014-05-12 15:26:59 -0700

    upgrade django-csp; bug 1009196

--- apps/amo/urls.py
+++ apps/amo/urls.py
@@ -1,4 +1,3 @@
-import csp.views
 from waffle.views import wafflejs
 
 from django.conf.urls import include, patterns, url
@@ -10,7 +9,6 @@ services_patterns = patterns('',
     url('^monitor(.json)?$', never_cache(views.monitor),
         name='amo.monitor'),
     url('^loaded$', never_cache(views.loaded), name='amo.loaded'),
-    url('^csp/policy$', csp.views.policy, name='amo.csp.policy'),
     url('^csp/report$', views.cspreport, name='amo.csp.report'),
     url('^builder-pingback', views.builder_pingback,
         name='amo.builder-pingback'),
