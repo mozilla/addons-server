8a8c61e87ebb33cae4c458eccd467a814a0b07ba
jpfeif66 <jpfeiffe@adobe.com>
2014-08-05 11:39:32 -0700

    Update pfs.py

--- services/pfs.py
+++ services/pfs.py
@@ -109,8 +109,8 @@ def get_output(data):
                 iconUrl='http://fpdownload2.macromedia.com/pub/flashplayer/current/fp_win_installer.ico',
                 needsRestart='false',
                 InstallerShowsUI='true',
-                version='14.0.0.145',
-                InstallerHash='sha256:678b3946f1e0b1bfdd14c0d6050a71a3267646e901c4aa7a730340e067330f03',
+                version='14.0.0.179',
+                InstallerHash='sha256:17c1e0b09abd4dc6919abd6405ffd9c6f29460f3c1c6f44409585351b2ab1c68',
                 InstallerLocation='http://fpdownload2.macromedia.com/pub/flashplayer/pdc/fp_pl_pfs_installer.exe')
 
     elif (g['mimetype'] == 'application/x-director' and
