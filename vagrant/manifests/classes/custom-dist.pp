fa8f17ec023c4b01ab7924e0dbe3738d9de2ea89
Mathieu Agopian <mathieu@agopian.info>
2014-09-02 15:48:38 +0200

    remove vagrant (bug 1061190)

--- vagrant/manifests/classes/custom-dist.pp
+++
@@ -1,8 +0,0 @@
-# Copy this file to custom.pp and declare any commands you want to run on your
-# local machine only. Anything you add to custom.pp will *not* be committed to
-# version control.
-class custom {
-    # exec { "install-nosenicedots":
-    #     command => "sudo pip install nosenicedots"
-    # }
-}
