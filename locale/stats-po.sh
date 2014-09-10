70747c172be9d3c63d2538cf5ffcf4586fd2048e
Wil Clouser <clouserw@gmail.com>
2014-09-10 10:09:46 -0700

    make stats-po more accurate

--- locale/stats-po.sh
+++ locale/stats-po.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 # syntax:
 # stats-po.sh
 
 echo "Printing number of untranslated strings found in locales:"
 
-for lang in `find $1 -type f -name "messages.po" | sort`; do
-    dir=`dirname $lang`
-    stem=`basename $lang .po`
+for lang in $(find $1 -type f -name "messages.po" | sort); do
+    dir=$(dirname $lang)
+    stem=$(basename $lang .po)
     js="$dir/javascript.po"
-    count=$(msgattrib --untranslated $lang | grep -c "msgid")
-    count2=$(msgattrib --untranslated $js | grep -c "msgid")
+    count=$(msgattrib $lang --untranslated --no-obsolete --no-fuzzy | grep -c 'msgid ')
+    if [ $count -gt 0 ]; then
+        count=$(($count-1))
+    fi
+    count2=$(msgattrib $js --untranslated --no-obsolete --no-fuzzy | grep -c 'msgid ')
+    if [ $count2 -gt 0 ]; then
+        count2=$(($count2-1))
+    fi
     echo -e "$(dirname $dir)\t\tmain=$count\tjs=$count2"
 done
