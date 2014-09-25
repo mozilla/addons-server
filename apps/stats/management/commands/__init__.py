43d1f4910e1d32f8b33b9d825eb39c408a798930
Mathieu Agopian <mathieu@agopian.info>
2014-09-25 14:34:05 +0200

    update hive queries with IP filtering (bug 1061734)

--- apps/stats/management/commands/__init__.py
+++ apps/stats/management/commands/__init__.py
@@ -7,6 +7,43 @@ from django.core.management.base import BaseCommand
 
 from .hive_connection import query_to_file
 
+IP_BLACKLIST = """
+    -- Mozilla Network
+    ip_address NOT LIKE '63.245.208.%' AND
+    ip_address NOT LIKE '63.245.209.%' AND
+    ip_address NOT LIKE '63.245.21%' AND
+    ip_address NOT LIKE '63.245.220.%' AND
+    ip_address NOT LIKE '63.245.221.%' AND
+    ip_address NOT LIKE '63.245.222.%' AND
+    ip_address NOT LIKE '63.245.223.%' AND
+
+    -- Not sure, but grepped an hour of logs, nothing.
+    ip_address NOT LIKE '180.92.184.%' AND
+
+    -- CN adm
+    ip_address NOT LIKE '59.151.50%' AND
+    ip_address NOT IN (
+        -- Not sure
+        '72.26.221.66',
+        '72.26.221.67',
+
+        -- white hat
+        '209.10.217.226',
+
+        -- CN lbs
+        '223.202.6.11',
+        '223.202.6.12',
+        '223.202.6.13',
+        '223.202.6.14',
+        '223.202.6.15',
+        '223.202.6.16',
+        '223.202.6.17',
+        '223.202.6.18',
+        '223.202.6.19',
+        '223.202.6.20'
+    )
+"""
+
 
 class HiveQueryToFileCommand(BaseCommand):
     """Base command for the "query counts" requests from HIVE, save to disk.
@@ -41,7 +78,10 @@ class HiveQueryToFileCommand(BaseCommand):
         if not os.path.isdir(os.path.join(folder, day)):
             os.makedirs(os.path.join(folder, day), 0755)
         filepath = os.path.join(folder, day, self.filename)
-        return query_to_file(self.query % (day, limit_str), filepath, sep)
+        return query_to_file(self.query.format(day=day,
+                                               ip_filtering=IP_BLACKLIST,
+                                               limit=limit_str),
+                             filepath, sep)
 
 
 def get_date_from_file(filepath, sep):
