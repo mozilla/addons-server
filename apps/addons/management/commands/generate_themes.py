fc9f28a8c22b4f177dc370da909559719f2fa114
David Larlet <david@larlet.fr>
2014-10-13 15:42:34 +0200

    Improving the add-ons generator to deal with apps (bug 1080145)

---
+++ apps/addons/management/commands/generate_themes.py
@@ -0,0 +1,40 @@
+from optparse import make_option
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from addons.themegenerator import generate_themes
+
+
+class Command(BaseCommand):
+    """
+    Generate example themes for development/testing purpose.
+
+    Themes will have 1 preview image + header & footer, 2 translations
+    (French and Spanish) and 5 ratings. If you don't provide any --owner
+    email address, all created add-ons will have 'nobody@mozilla.org'
+    as owner.
+
+    Categories from production (Abstract, Causes, Fashion, etc)
+    will be created and randomly populated with generated themes.
+
+    Usage:
+
+        python manage.py generate_themes <num_themes> [--owner <email>]
+
+    """
+
+    help = __doc__
+    option_list = BaseCommand.option_list + (
+        make_option('--owner', action='store', dest='email',
+                    default='nobody@mozilla.org',
+                    help="Specific owner's email to be created."),
+    )
+
+    def handle(self, *args, **kwargs):
+        if not settings.DEBUG:
+            raise CommandError('You can only run this command with your '
+                               'DEBUG setting set to True.')
+        num = int(args[0])
+        email = kwargs.get('email')
+        generate_themes(num, email)
--- apps/addons/tests/test_themegenerator.py
+++ apps/addons/tests/test_themegenerator.py
@@ -4,15 +4,14 @@ from nose.tools import eq_, ok_
 
 import amo
 import amo.tests
-from addons.addongenerator import categories_choices, generate_addon_data
-from constants.applications import APPS
+from addons.themegenerator import categories_choices, generate_theme_data
 
 
-class _BaseAddonGeneratorTests(amo.tests.TestCase):
+class ThemeGeneratorTests(amo.tests.TestCase):
 
     def test_tinyset(self):
         size = 4
-        data = list(generate_addon_data(size, self.app))
+        data = list(generate_theme_data(size))
         eq_(len(data), size)
         # Names are unique.
         eq_(len(set(addonname for addonname, cat in data)), size)
@@ -21,41 +20,25 @@ class _BaseAddonGeneratorTests(amo.tests.TestCase):
 
     def test_smallset(self):
         size = 60
-        data = list(generate_addon_data(size, self.app))
+        data = list(generate_theme_data(size))
         eq_(len(data), size)
-        # Addons are split up equally within each categories.
+        # Themes are split up equally within each categories.
         categories = collections.defaultdict(int)
         for addonname, category in data:
             categories[category.slug] += 1
         eq_(set(categories.values()),
-            set([size / len(categories_choices[self.app.short])]))
+            set([size / len(categories_choices)]))
         eq_(len(set(addonname for addonname, cat in data)), size)
         ok_(not any(addonname[-1].isdigit() for addonname, cat in data))
 
     def test_bigset(self):
         size = 300
-        data = list(generate_addon_data(size, self.app))
+        data = list(generate_theme_data(size))
         eq_(len(data), size)
         categories = collections.defaultdict(int)
         for addonname, cat in data:
             categories[cat] += 1
-        # Addons are spread between categories evenly - the difference
+        # Themes are spread between categories evenly - the difference
         # between the largest and smallest category is less than 2.
         ok_(max(categories.values()) - min(categories.values()) < 2)
         eq_(len(set(addonname for addonname, cat in data)), size)
-
-
-class FirefoxAddonGeneratorTests(_BaseAddonGeneratorTests):
-    app = APPS['firefox']
-
-
-class ThunderbirdAddonGeneratorTests(_BaseAddonGeneratorTests):
-    app = APPS['thunderbird']
-
-
-class AndroidAddonGeneratorTests(_BaseAddonGeneratorTests):
-    app = APPS['android']
-
-
-class SeamonkeyAddonGeneratorTests(_BaseAddonGeneratorTests):
-    app = APPS['seamonkey']
