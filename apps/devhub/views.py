mozilla/addons-server

commit ff8dfd43d86b61d6c263a70b98be42d0e495f3a5

Remove files.Platform model (bug 1062958)

apps/devhub/views.py
@@ -542,10 +542,10 @@ def file_perf_tests_start(request, addon_id, addon, file_id):
         raise PermissionDenied
     file_ = get_object_or_404(File, pk=file_id)
 
-    plats = perf.PLATFORM_MAP.get(file_.platform.id, None)
+    plats = perf.PLATFORM_MAP.get(file_.platform, None)
     if plats is None:
         log.info('Unsupported performance platform %s for file %s'
-                 % (file_.platform.id, file_))
+                 % (file_.platform, file_))
         # TODO(Kumar) provide a message about this
         return {'success': False}
 

apps/devhub/forms.py
@@ -28,7 +28,7 @@ from amo.urlresolvers import reverse
 from amo.utils import raise_required, slugify
 
 from applications.models import Application, AppVersion
-from files.models import File, FileUpload, Platform
+from files.models import File, FileUpload
 from files.utils import parse_addon, VERSION_RE
 from translations.widgets import TranslationTextarea, TranslationTextInput
 from translations.fields import TransTextarea, TransField
@@ -500,21 +500,19 @@ class AddonUploadForm(WithSourceMixin, happyforms.Form):
 
 
 class NewAddonForm(AddonUploadForm):
-    desktop_platforms = forms.ModelMultipleChoiceField(
-        queryset=Platform.objects,
+    desktop_platforms = forms.TypedMultipleChoiceField(
+        choices=amo.DESKTOP_PLATFORMS_CHOICES,
         widget=forms.CheckboxSelectMultiple(attrs={'class': 'platform'}),
         initial=[amo.PLATFORM_ALL.id],
-        required=False
+        required=False,
+        coerce=int
     )
-    desktop_platforms.choices = ((p.id, p.name)
-                                 for p in amo.DESKTOP_PLATFORMS.values())
-    mobile_platforms = forms.ModelMultipleChoiceField(
-        queryset=Platform.objects,
+    mobile_platforms = forms.TypedMultipleChoiceField(
+        choices=amo.MOBILE_PLATFORMS_CHOICES,
         widget=forms.CheckboxSelectMultiple(attrs={'class': 'platform'}),
-        required=False
+        required=False,
+        coerce=int
     )
-    mobile_platforms.choices = ((p.id, p.name)
-                                for p in amo.MOBILE_PLATFORMS.values())
 
     def clean(self):
         if not self.errors:
@@ -560,10 +558,17 @@ class NewVersionForm(NewAddonForm):
 
 
 class NewFileForm(AddonUploadForm):
-    platform = File._meta.get_field('platform').formfield(empty_label=None,
-                    widget=forms.RadioSelect(attrs={'class': 'platform'}))
-    platform.choices = sorted((p.id, p.name)
-                              for p in amo.SUPPORTED_PLATFORMS.values())
+    platform = forms.TypedChoiceField(
+        choices=amo.SUPPORTED_PLATFORMS_CHOICES,
+        widget=forms.RadioSelect(attrs={'class': 'platform'}),
+        coerce=int,
+        # We don't want the id value of the field to be output to the user
+        # when choice is invalid. Make a generic error message instead.
+        error_messages={
+            'invalid_choice': _lazy(u'Select a valid choice. That choice is '
+                                    u'not one of the available choices.')
+        }
+    )
 
     def __init__(self, *args, **kw):
         self.addon = kw.pop('addon')
@@ -571,8 +576,8 @@ class NewFileForm(AddonUploadForm):
         super(NewFileForm, self).__init__(*args, **kw)
         # Reset platform choices to just those compatible with target app.
         field = self.fields['platform']
-        field.choices = sorted((k, v.name) for k, v in
-                               self.version.compatible_platforms().items())
+        field.choices = sorted((p.id, p.name) for p in
+                               self.version.compatible_platforms().values())
         # Don't allow platforms we already have.
         to_exclude = set(File.objects.filter(version=self.version)
                                      .values_list('platform', flat=True))
@@ -587,7 +592,6 @@ class NewFileForm(AddonUploadForm):
         to_exclude.add(amo.PLATFORM_ALL_MOBILE.id)
 
         field.choices = [p for p in field.choices if p[0] not in to_exclude]
-        field.queryset = Platform.objects.filter(id__in=dict(field.choices))
 
     def clean(self):
         if not self.version.is_allowed_upload():
@@ -603,7 +607,7 @@ class NewFileForm(AddonUploadForm):
 
 
 class FileForm(happyforms.ModelForm):
-    platform = File._meta.get_field('platform').formfield(empty_label=None)
+    platform = File._meta.get_field('platform').formfield()
 
     class Meta:
         model = File
@@ -619,7 +623,7 @@ class FileForm(happyforms.ModelForm):
             # See bug 646268.
             if amo.PLATFORM_ALL_MOBILE.id in compat:
                 del compat[amo.PLATFORM_ALL_MOBILE.id]
-            pid = int(kw['instance'].platform_id)
+            pid = int(kw['instance'].platform)
             plats = [(p.id, p.name) for p in compat.values()]
             if pid not in compat:
                 plats.append([pid, amo.PLATFORMS[pid].name])
@@ -647,7 +651,7 @@ class BaseFileFormSet(BaseModelFormSet):
                  if not f.cleaned_data.get('DELETE', False)]
 
         if self.forms and 'platform' in self.forms[0].fields:
-            platforms = [f['platform'].id for f in files]
+            platforms = [f['platform'] for f in files]
 
             if amo.PLATFORM_ALL.id in platforms and len(files) > 1:
                 raise forms.ValidationError(

apps/files/forms.py
@@ -18,14 +18,13 @@ log = commonware.log.getLogger('z.files')
 class FileSelectWidget(widgets.Select):
     def render_options(self, choices, selected_choices):
         def option(files, label=None):
-            addon = files[0].version.addon
             # Make sure that if there's a non-disabled version,
             # that's the one we use for the ID.
             files.sort(lambda a, b: ((a.status == amo.STATUS_DISABLED) -
                                      (b.status == amo.STATUS_DISABLED)))
 
             if label is None:
-                label = u', '.join(unicode(os.platform) for os in f)
+                label = u', '.join(f.get_platform_display() for f in files)
 
             output = [u'<option value="', jinja2.escape(files[0].id), u'" ']
             if selected in files:

apps/files/views.py
@@ -1,7 +1,4 @@
-from urlparse import urljoin
-
 from django import http, shortcuts
-from django.conf import settings
 from django.shortcuts import render
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt

apps/versions/forms.py
@@ -72,7 +72,7 @@ class XPIForm(happyforms.Form):
         hash = hashlib.sha256()
 
         f = File(version=version,
-                 platform_id=amo.PLATFORM_DICT[data['platform']].id,
+                 platform=amo.PLATFORM_DICT[data['platform']].id,
                  size=xpi.size)
 
         filename = f.generate_filename()

apps/versions/views.py
@@ -111,7 +111,7 @@ def download_latest(request, addon, type='xpi', platform=None):
                                 version=addon._current_version_id)
     try:
         # If there's a file matching our platform, it'll float to the end.
-        file = sorted(files, key=lambda f: f.platform_id == platforms[-1])[-1]
+        file = sorted(files, key=lambda f: f.platform == platforms[-1])[-1]
     except IndexError:
         raise http.Http404()
     args = [file.id, type] if type else [file.id]

commit 324582e38278abf0c8d649fb1203e84f36c2ab63

Fix platform display in places where it was broken by ff8dfd43d86b.

apps/devhub/views.py
@@ -545,7 +545,7 @@ def file_perf_tests_start(request, addon_id, addon, file_id):
     plats = perf.PLATFORM_MAP.get(file_.platform, None)
     if plats is None:
         log.info('Unsupported performance platform %s for file %s'
-                 % (file_.platform, file_))
+                 % (file_.get_platform_display(), file_))
         # TODO(Kumar) provide a message about this
         return {'success': False}
 

apps/editors/forms.py
@@ -228,7 +228,8 @@ class AddonFilesMultipleChoiceField(forms.ModelMultipleChoiceField):
         addon = addon_file.version.addon
         # L10n: 0 = platform, 1 = filename, 2 = status message
         return jinja2.Markup(_(u"<strong>%s</strong> &middot; %s &middot; %s")
-                             % (addon_file.platform, addon_file.filename,
+                             % (addon_file.get_platform_display(),
+                                addon_file.filename,
                                 file_review_status(addon, addon_file)))
 
 