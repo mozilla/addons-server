bc44dc27935f357cfcadfe65b9c2557559fbada5
David Larlet <david@larlet.fr>
2014-10-28 19:30:26 +0100

    Switch the new statistics on for real (bug 1070238)

---
+++ migrations/797-switch-stats-for-real.sql
@@ -0,0 +1,5 @@
+-- The Following ALTER, which will take a long time, is to be run manually.
+-- ALTER TABLE `update_counts` MODIFY COLUMN `application` MEDIUMTEXT;
+
+DROP TABLE download_counts_tmp;
+DROP TABLE update_counts_tmp;
