e9111a09a3de8ca1665d166b4fe164a2f5e5093f
David Larlet <david@larlet.fr>
2014-11-10 17:40:04 +0100

    Fix constraints and indexes failures on db creation (bug 1095956)

--- apps/addons/sql/addonuser.sql
+++ apps/addons/sql/addonuser.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `addons_users` DROP FOREIGN KEY `addon_id_refs_id_2d0960a2`;
-ALTER TABLE `addons_users` ADD CONSTRAINT `addon_id_refs_id_2d0960a2` FOREIGN KEY (`addon_id`) REFERENCES `addons` (`id`) ON DELETE CASCADE;
+ALTER TABLE `addons_users` DROP FOREIGN KEY `addon_id_refs_id_0ace0094`;
+ALTER TABLE `addons_users` ADD CONSTRAINT `addon_id_refs_id_0ace0094` FOREIGN KEY (`addon_id`) REFERENCES `addons` (`id`) ON DELETE CASCADE;
