4b2b03a365b771e2fc99176b46b8d89e953170a5
Mathieu Agopian <mathieu@agopian.info>
2015-01-20 11:15:24 +0100

    disable travis cache (too slow)

--- scripts/travis_es.sh
+++ scripts/travis_es.sh
@@ -4,7 +4,8 @@ TARGET="/tmp/elasticsearch"
 
 if [ ! -f "$TARGET/elasticsearch-1.2.4/bin/elasticsearch" ]; then
     echo "$TARGET not found. Building..."
-    pushd $TARGET
+    mkdir -p $TARGET
+    cd $TARGET
     wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.2.tar.gz
     tar xvf elasticsearch-1.3.2.tar.gz
 else
