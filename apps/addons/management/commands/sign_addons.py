4d8f2479cfec9673ed4397cddfcf2217efd0d5f8
Mathieu Agopian <mathieu@agopian.info>
2015-03-25 18:14:07 +0100

    Override the signing server for the sign_addons management command

--- apps/addons/management/commands/sign_addons.py
+++ apps/addons/management/commands/sign_addons.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from optparse import make_option
+
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from django.test.utils import override_settings
 
 from lib.crypto.packaged import sign, SigningError
 from versions.models import Version
@@ -8,6 +12,15 @@ from versions.models import Version
 class Command(BaseCommand):
     args = '<addon_id addon_id ...>'
     help = 'Sign a list of addons.'
+    option_list = BaseCommand.option_list + (
+        make_option('--signing-server', action='store', type='string',
+                    dest='signing_server',
+                    help='The signing server to use for full reviews'),
+        make_option('--preliminary-signing-server', action='store',
+                    type='string',
+                    dest='preliminary_signing_server',
+                    help='The signing server to use for preliminary reviews'),
+    )
 
     def handle(self, *args, **options):
         if len(args) == 0:  # Sign all the addons?
@@ -15,6 +28,9 @@ class Command(BaseCommand):
                 'Please provide at least one addon id to sign. If you want to '
                 'sign them all, use the "process_addons --task sign_addons" '
                 'management command.')
+        signing_server = options.get('signing_server', settings.SIGNING_SERVER)
+        preliminary_signing_server = options.get(
+            'preliminary_signing_server', settings.PRELIMINARY_SIGNING_SERVER)
 
         addon_ids = [int(addon_id) for addon_id in args]
         to_sign = Version.objects.filter(addon_id__in=addon_ids)
@@ -24,7 +40,10 @@ class Command(BaseCommand):
         for version in to_sign:
             try:
                 self.stdout.write('Signing version %s' % version.pk)
-                sign(version)
+                with override_settings(
+                        SIGNING_SERVER=signing_server,
+                        PRELIMINARY_SIGNING_SERVER=preliminary_signing_server):
+                    sign(version)
             except SigningError as e:
                 self.stderr.write(
                     'Error while signing version %s: %s' % (version.pk, e))
