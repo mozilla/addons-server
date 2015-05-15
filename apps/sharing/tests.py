9d8c92232660cdf1038c10586e8b16d3a3c662c4
Radhakrishna Pemmasani <pemmasanikrish@gmail.com>
2015-05-16 02:05:10 +0530

    Bug 780356

--- apps/sharing/tests.py
+++ apps/sharing/tests.py
@@ -50,7 +50,9 @@ class SharingModelsTestCase(BaseTestCase):
 
     def test_share_count(self):
         addon = Addon.objects.get(id=3615)
+
         eq_(addon.share_counts[FACEBOOK.shortname], 29)
+
         # total count with no shares
         eq_(addon.share_counts[FACEBOOK.shortname], 0,
             'Total count with no shares must be 0')
