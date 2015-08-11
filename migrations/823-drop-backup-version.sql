7189af087fd59a3307fc59495ba8f8ae5f551a39
Mathieu Agopian <mathieu@agopian.info>
2015-08-11 15:20:31 +0200

    Fix (again) the backup_version drop migration

--- migrations/823-drop-backup-version.sql
+++ migrations/823-drop-backup-version.sql
@@ -1,11 +1,14 @@
-ALTER TABLE
-    `addons`
-DROP FOREIGN KEY
-    `addons_ibfk_16`
-;
+# It seems the constraint name for the `backup_version` column differs between the local and dev/stage/prod servers.
+# We thus need some hackery to find its name, and remove it dynamically.
+#
+# First get the constraint name.
+SET @CONSTRAINT_NAME := (select CONSTRAINT_NAME from information_schema.key_column_usage where TABLE_NAME="addons" and COLUMN_NAME="backup_version");
 
-ALTER TABLE
-    `addons`
-DROP COLUMN
-    `backup_version`
-;
+# Then remove it.
+SET @QUERY = CONCAT('ALTER TABLE addons DROP FOREIGN KEY ', @constraint_name, ';');
+PREPARE stmt FROM @QUERY;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+# Finally drop the column.
+ALTER TABLE `addons` DROP COLUMN `backup_version`;
