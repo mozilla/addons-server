f7f59d9649d43074e746ff5b03c8844ff1634bdc
Kris Maglione <maglione.k@gmail.com>
2015-08-29 00:24:23 -0700

    Explicitly import `olympia` from manage.py. Fixes discovery of local management commands.

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-import os
 import sys
+# Required for sys.path setup prior to other imports.
+import olympia  # noqa
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
