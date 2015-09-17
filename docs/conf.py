3732bec93168cbd8987a29f2c1aa0707b0c0e1ab
Stuart Colville <scolville@mozilla.com>
2015-09-17 11:24:29 +0100

    Use rtd theme for local docs builds

--- docs/conf.py
+++ docs/conf.py
@@ -98,6 +98,17 @@ pygments_style = 'sphinx'
 #html_theme_path = ['_themes']
 #html_theme = 'mozilla'
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed
+# from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
