68c51ef61640e0fb40eb52cc981321fc545d1422
Mike Nolan <me@nolski.rocks>
2015-10-08 14:43:50 -0400

    Fix styling regression for login form (bug 1212989)

--- lib/settings_base.py
+++ lib/settings_base.py
@@ -555,6 +555,7 @@ MINIFY_BUNDLES = {
             'css/devhub/submission.less',
             'css/devhub/search.less',
             'css/devhub/refunds.less',
+            'css/impala/devhub-api.less',
         ),
         'zamboni/editors': (
             'css/zamboni/editors.styl',
