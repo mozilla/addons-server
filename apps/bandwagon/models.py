75d321a47f95f9c5ac079914a8f06d80cd050d95
Christopher Grebs <cg@webshox.org>
2015-10-20 15:01:58 -0700

    Improve collections ajax list performance. (bug 1211692)
    This uses a simple extra query to check if the addon
    exists in a collection or not.
    
    This avoids the for-loop and thus n queries on a (possibly) huge
    list of addon ids.

--- apps/bandwagon/models.py
+++ apps/bandwagon/models.py
@@ -45,10 +45,29 @@ class TopTags(object):
         cache.set(self.key(obj), value, two_days)
 
 
+class CollectionQuerySet(caching.CachingQuerySet):
+
+    def with_has_addon(self, addon_id):
+        """Add a `has_addon` property to each collection.
+
+        `has_addon` will be `True` if `addon_id` exists in that
+        particular collection.
+        """
+        has_addon = """
+            select 1 from addons_collections as ac
+                where ac.addon_id = %s and ac.collection_id = collections.id
+                limit 1"""
+
+        return self.extra(
+            select={'has_addon': has_addon},
+            select_params=(addon_id,))
+
+
 class CollectionManager(amo.models.ManagerBase):
 
     def get_query_set(self):
         qs = super(CollectionManager, self).get_query_set()
+        qs = qs._clone(klass=CollectionQuerySet)
         return qs.transform(Collection.transformer)
 
     def manual(self):
