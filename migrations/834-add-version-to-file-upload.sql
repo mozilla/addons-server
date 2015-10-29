6b86df86d21ba87605f9e7a4ccd75ea1e14e4b0a
Kumar McMillan <kumar.mcmillan@gmail.com>
2015-10-29 10:09:02 -0500

    Fix foreign key constraint (was bug 1208572)

--- migrations/834-add-version-to-file-upload.sql
+++ migrations/834-add-version-to-file-upload.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `file_uploads`
     ADD COLUMN `version` varchar(255),
-    ADD COLUMN `addon_id` int(11);
+    ADD COLUMN `addon_id` int(11) unsigned;
 ALTER TABLE `file_uploads`
     ADD CONSTRAINT `file_uploads_refs_addon_id`
     FOREIGN KEY `file_uploads_refs_addon_id` (`addon_id`)
