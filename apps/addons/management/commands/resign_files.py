9092ffaa12269b5e55590022cd4dab7f47ad1b72
Mathieu Agopian <mathieu@agopian.info>
2015-11-18 15:59:16 +0100

    Resign wrongly signed files (bug 1225036)

---
+++ apps/addons/management/commands/resign_files.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from optparse import make_option
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.test.utils import override_settings
+
+from lib.crypto.tasks import resign_files
+
+
+class Command(BaseCommand):
+    args = '<file_id file_id ...>'
+    help = 'Resign a list of already signed files.'
+    option_list = BaseCommand.option_list + (
+        make_option('--signing-server', action='store', type='string',
+                    dest='signing_server',
+                    help='The signing server to use for full reviews'),
+        make_option('--preliminary-signing-server', action='store',
+                    type='string',
+                    dest='preliminary_signing_server',
+                    help='The signing server to use for preliminary reviews'),
+    )
+
+    def handle(self, *args, **options):
+        if len(args) == 0:
+            raise CommandError('Please provide at least one file id to resign')
+        full_server = options.get('signing_server') or settings.SIGNING_SERVER
+        prelim_server = (options.get('preliminary_signing_server') or
+                         settings.PRELIMINARY_SIGNING_SERVER)
+
+        file_ids = [int(file_id) for file_id in args]
+        with override_settings(
+                SIGNING_SERVER=full_server,
+                PRELIMINARY_SIGNING_SERVER=prelim_server):
+            resign_files(file_ids)
