ad5d53dc540facb9329abcf4b52df07064e25fcd
Stuart Colville <scolville@mozilla.com>
2016-01-12 18:36:42 +0000

    remove media url added to SCRIPT_SRC

--- sites/dev/settings.py
+++ sites/dev/settings.py
@@ -13,6 +13,7 @@ DEV_CDN_HOST = 'https://addons-dev-cdn.allizom.org'
 CSP_SCRIPT_SRC += (DEV_CDN_HOST,)
 CSP_IMG_SRC += (DEV_CDN_HOST,)
 CSP_STYLE_SRC += (DEV_CDN_HOST,)
+CSP_FRAME_SRC = CSP_FRAME_SRC + ("https://sandbox.paypal.com",)
 
 ENGAGE_ROBOTS = False
 
@@ -44,8 +45,6 @@ SERVICES_URL = SITE_URL
 STATIC_URL = 'https://addons-dev-cdn.allizom.org/static/'
 MEDIA_URL = 'https://addons-dev-cdn.allizom.org/user-media/'
 
-CSP_FRAME_SRC = CSP_FRAME_SRC + ("https://sandbox.paypal.com",)
-CSP_SCRIPT_SRC = CSP_SCRIPT_SRC + (MEDIA_URL[:-1],)
 
 SESSION_COOKIE_DOMAIN = ".%s" % DOMAIN
 
