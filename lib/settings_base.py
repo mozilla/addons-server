6817087691f36cc5c473101d4f773c809e9613b8
Mathieu Agopian <mathieu@agopian.info>
2016-01-15 17:00:00 +0100

    Patch highcharts to not use unsafe inlines

--- lib/settings_base.py
+++ lib/settings_base.py
@@ -798,7 +798,7 @@ MINIFY_BUNDLES = {
             'js/zamboni/validator.js',
         ),
         'zamboni/editors': (
-            'js/lib/highcharts.src.js',
+            'js/lib/highcharts-PATCHED.src.js',
             'js/zamboni/editors.js',
             'js/lib/jquery.hoverIntent.js',  # Used by jquery.zoomBox.
             'js/lib/jquery.zoomBox.js',  # Used by themes_review.
@@ -855,7 +855,7 @@ MINIFY_BUNDLES = {
             'js/common/ratingwidget.js',
         ),
         'zamboni/stats': (
-            'js/lib/highcharts.src.js',
+            'js/lib/highcharts-PATCHED.src.js',
             'js/impala/stats/csv_keys.js',
             'js/impala/stats/helpers.js',
             'js/impala/stats/dateutils.js',
