96ff4fd2361c932bb5c76eafc9d5ccb0c8f96ad3
Mathieu Pillard <mpillard@mozilla.com>
2016-01-19 15:17:28 +0100

    Fix migration 854 (some collections had subscribers...)

--- migrations/854-remove-recommendations.sql
+++ migrations/854-remove-recommendations.sql
@@ -1,3 +1,7 @@
 DROP TABLE IF EXISTS `addon_recommendations`;
+DELETE collection_subscriptions FROM collection_subscriptions
+    INNER JOIN collections
+    ON collection_subscriptions.collection_id=collections.id
+    WHERE collections.collection_type IN (1, 3);
 DELETE FROM collections WHERE collection_type IN (1, 3); -- COLLECTION_SYNCHRONIZED or COLLECTION_RECOMMENDED
 DELETE FROM waffle_flag WHERE name = 'disco-pane-show-recs';
