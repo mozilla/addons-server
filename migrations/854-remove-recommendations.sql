c16bbfbf268b60128b04a8d641a89198a7d77fb7
Mathieu Pillard <mpillard@mozilla.com>
2016-01-19 17:16:46 +0100

    Cancel this migration, data cleanup would be nice but it takes too much time

--- migrations/854-remove-recommendations.sql
+++ migrations/854-remove-recommendations.sql
@@ -1,57 +1,60 @@
 DROP TABLE IF EXISTS `addon_recommendations`;
 
- -- collection_type 1: COLLECTION_SYNCHRONIZED
- -- collection_type 3: COLLECTION_RECOMMENDED
+--  -- collection_type 1: COLLECTION_SYNCHRONIZED
+--  -- collection_type 3: COLLECTION_RECOMMENDED
 
-DELETE addons_collections FROM addons_collections
-    INNER JOIN collections
-    ON addons_collections.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- Deletions removed because they're too slow for a migration.
 
-DELETE collection_promos FROM collection_promos
-    INNER JOIN collections
-    ON collection_promos.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE addons_collections FROM addons_collections
+--     INNER JOIN collections
+--     ON addons_collections.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE collection_subscriptions FROM collection_subscriptions
-    INNER JOIN collections
-    ON collection_subscriptions.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE collection_promos FROM collection_promos
+--     INNER JOIN collections
+--     ON collection_promos.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE collections_users FROM collections_users
-    INNER JOIN collections
-    ON collections_users.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE collection_subscriptions FROM collection_subscriptions
+--     INNER JOIN collections
+--     ON collection_subscriptions.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE collections_votes FROM collections_votes
-    INNER JOIN collections
-    ON collections_votes.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE collections_users FROM collections_users
+--     INNER JOIN collections
+--     ON collections_users.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE featured_collections FROM featured_collections
-    INNER JOIN collections
-    ON featured_collections.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE collections_votes FROM collections_votes
+--     INNER JOIN collections
+--     ON collections_votes.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE stats_addons_collections_counts FROM stats_addons_collections_counts
-    INNER JOIN collections
-    ON stats_addons_collections_counts.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE featured_collections FROM featured_collections
+--     INNER JOIN collections
+--     ON featured_collections.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE stats_collections FROM stats_collections
-    INNER JOIN collections
-    ON stats_collections.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE stats_addons_collections_counts FROM stats_addons_collections_counts
+--     INNER JOIN collections
+--     ON stats_addons_collections_counts.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE stats_collections_counts FROM stats_collections_counts
-    INNER JOIN collections
-    ON stats_collections_counts.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE stats_collections FROM stats_collections
+--     INNER JOIN collections
+--     ON stats_collections.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE stats_collections_share_counts_totals FROM stats_collections_share_counts_totals
-    INNER JOIN collections
-    ON stats_collections_share_counts_totals.collection_id=collections.id
-    WHERE collections.collection_type IN (1, 3);
+-- DELETE stats_collections_counts FROM stats_collections_counts
+--     INNER JOIN collections
+--     ON stats_collections_counts.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
 
-DELETE FROM collections WHERE collection_type IN (1, 3);
+-- DELETE stats_collections_share_counts_totals FROM stats_collections_share_counts_totals
+--     INNER JOIN collections
+--     ON stats_collections_share_counts_totals.collection_id=collections.id
+--     WHERE collections.collection_type IN (1, 3);
+
+-- DELETE FROM collections WHERE collection_type IN (1, 3);
+
 DELETE FROM waffle_flag WHERE name = 'disco-pane-show-recs';
