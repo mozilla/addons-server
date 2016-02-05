4dc508bb291b5361af2c8e464c2c7b9dde0a2b23
Mathieu Pillard <m@virgule.net>
2016-02-05 15:10:53 +0100

    Remove obsolete AddonDeviceType model / table / constants
    This is a Marketplace model.

--- src/olympia/constants/applications.py
+++ src/olympia/constants/applications.py
@@ -149,40 +149,6 @@ class UNKNOWN_APP(App):
     pretty = _(u'Unknown')
 
 
-class DEVICE_DESKTOP(object):
-    id = 1
-    name = _(u'Desktop')
-    class_name = 'desktop'
-    api_name = 'desktop'
-
-
-class DEVICE_MOBILE(object):
-    id = 2
-    name = _(u'Firefox Mobile')
-    class_name = 'android-mobile'
-    api_name = 'android-mobile'
-
-
-class DEVICE_TABLET(object):
-    id = 3
-    name = _(u'Firefox Tablet')
-    class_name = 'android-tablet'
-    api_name = 'android-tablet'
-
-
-class DEVICE_GAIA(object):
-    id = 4
-    name = _(u'Firefox OS')
-    class_name = 'firefoxos'
-    api_name = 'firefoxos'
-
-
-DEVICE_TYPE_LIST = [DEVICE_DESKTOP, DEVICE_MOBILE, DEVICE_TABLET, DEVICE_GAIA]
-DEVICE_TYPES = dict((d.id, d) for d in DEVICE_TYPE_LIST)
-REVERSE_DEVICE_LOOKUP = dict((d.id, d.api_name) for d in DEVICE_TYPE_LIST)
-DEVICE_LOOKUP = dict((d.api_name, d) for d in DEVICE_TYPE_LIST)
-
-
 # UAs will attempt to match in this order.
 APP_DETECT = (ANDROID, MOBILE, THUNDERBIRD, SEAMONKEY, FIREFOX)
 APP_USAGE = _apps = (FIREFOX, THUNDERBIRD, ANDROID, MOBILE, SEAMONKEY)
