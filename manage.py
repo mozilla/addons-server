e78b31215cb9532ed9c7e3555d0e2f0d1d457bf8
Christopher Grebs <cg@webshox.org>
2016-02-09 09:17:10 +0100

    Remove olympia.startup import from manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 import sys
 
-
-# This needs to be imported so it can perform path adjustments necessary for
-# management command discovery, since that's one of the very few Django
-# functions that doesn't import all apps.
-from olympia import startup
-
-
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
