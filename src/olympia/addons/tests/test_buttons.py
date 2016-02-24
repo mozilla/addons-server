diox/olympia

Port more code to new jingo version and use 'get_env' rather than 'env' directly.

apps/addons/tests/test_buttons.py
@@ -74,7 +74,7 @@ class ButtonTest(amo.tests.TestCase):
             'request': self.request,
         }
 
-    @patch('addons.buttons.jingo.env.get_template')
+    @patch('addons.buttons.jingo.Environment.get_template')
     def get_button(self, t_mock, **kwargs):
         """Proxy for calling install_button."""
         template_mock = Mock()

mozilla/addons-server

src/olympia/addons/tests/test_buttons.py
@@ -73,7 +73,7 @@ class ButtonTest(TestCase):
             'request': self.request,
         }
 
-    @patch('olympia.addons.buttons.jingo.env.get_template')
+    @patch('olympia.buttons.jingo.Environment.get_template')
     def get_button(self, t_mock, **kwargs):
         """Proxy for calling install_button."""
         template_mock = Mock()