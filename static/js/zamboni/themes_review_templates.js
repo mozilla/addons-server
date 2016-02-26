471617d011671c5d5f1f498d36e02e7a65ae016f
Stuart Colville <scolville@mozilla.com>
2016-02-26 17:54:31 +0000

    Fix up theme search templating

--- static/js/zamboni/themes_review_templates.js
+++ static/js/zamboni/themes_review_templates.js
@@ -12,9 +12,9 @@ _.template(`
   <table id="search-queue" class="data-grid items">
     <thead>
       <tr class="listing-header">
-        <th>{{ _('Theme') }}</th>
-        <th>{{ _('Reviewer') }}</th>
-        <th>{{ _('Status') }}</th>
+        <th>{{ gettext('Theme') }}</th>
+        <th>{{ gettext('Reviewer') }}</th>
+        <th>{{ gettext('Status') }}</th>
       </tr>
     </thead>
     <tbody><%= rows %></tbody>
@@ -28,11 +28,11 @@ function search_results_template(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
 __p+='\n  <table id="search-queue" class="data-grid items">\n    <thead>\n      <tr class="listing-header">\n        <th>'+
-((__t=( _('Theme') ))==null?'':_.escape(__t))+
+((__t=( gettext('Theme') ))==null?'':_.escape(__t))+
 '</th>\n        <th>'+
-((__t=( _('Reviewer') ))==null?'':_.escape(__t))+
+((__t=( gettext('Reviewer') ))==null?'':_.escape(__t))+
 '</th>\n        <th>'+
-((__t=( _('Status') ))==null?'':_.escape(__t))+
+((__t=( gettext('Status') ))==null?'':_.escape(__t))+
 '</th>\n      </tr>\n    </thead>\n    <tbody>'+
 ((__t=( rows ))==null?'':__t)+
 '</tbody>\n  </table>\n';
@@ -43,16 +43,18 @@ return __p;
 
 /*
 _.template(`
-<script type="text/template" id="queue-search-row-template">
   <tr class="addon-row">
     <td class="app-name">
       <span class="addon-id"><%= id %></span>
       <a href="<%= review_url %>"><%- name %></a>
     </td>
-    <td><a href="<%= reviewer %>"><%= reviewer %></a></td>
+    <td>
+      <% if (typeof reviewer !== "undefined") { %>
+        <%- reviewer %>
+      <% } %>
+    </td>
     <td><%= status %></td>
   </tr>
-</script>
 `).source;
 */
 
@@ -60,17 +62,21 @@ _.template(`
 function search_result_row_template(obj){
 var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
 with(obj||{}){
-__p+='\n<script type="text/template" id="queue-search-row-template">\n  <tr class="addon-row">\n    <td class="app-name">\n      <span class="addon-id">'+
+__p+='\n <tr class="addon-row">\n   <td class="app-name">\n     <span class="addon-id">'+
 ((__t=( id ))==null?'':__t)+
-'</span>\n      <a href="'+
+'</span>\n     <a href="'+
 ((__t=( review_url ))==null?'':__t)+
-'"><%- name %></a>\n    </td>\n    <td><a href="'+
-((__t=( reviewer ))==null?'':__t)+
 '">'+
-((__t=( reviewer ))==null?'':__t)+
-'</a></td>\n    <td>'+
+((__t=( name ))==null?'':_.escape(__t))+
+'</a>\n   </td>\n   <td>\n     ';
+if (typeof reviewer !== "undefined") {
+__p+='\n       '+
+((__t=( reviewer ))==null?'':_.escape(__t))+
+'\n     ';
+}
+__p+='\n   </td>\n   <td>'+
 ((__t=( status ))==null?'':__t)+
-'</td>\n  </tr>\n</script>\n';
+'</td>\n </tr>';
 }
 return __p;
 }
