a5261fe4e2c3bcedf9319409f337da7d1ca765d5
Christopher Grebs <cg@webshox.org>
2016-05-09 19:40:32 +0200

    Small cleanup, move webext default versions to constants

--- src/olympia/files/tests/test_utils_.py
+++ src/olympia/files/tests/test_utils_.py
@@ -181,6 +181,10 @@ class TestManifestJSONExtractor(TestCase):
         return AppVersion.objects.create(application=amo.APPS[name].id,
                                          version=version)
 
+    def create_webext_default_versions(self):
+        self.create_appversion('firefox', amo.DEFAULT_WEBEXT_MIN_VERSION)
+        self.create_appversion('firefox', amo.DEFAULT_WEBEXT_MAX_VERSION)
+
     def test_instanciate_without_data(self):
         """Without data, we load the data from the file path."""
         data = {'id': 'some-id'}
@@ -232,8 +236,7 @@ class TestManifestJSONExtractor(TestCase):
         """Use the min and max versions if provided."""
         firefox_min_version = self.create_appversion('firefox', '30.0')
         firefox_max_version = self.create_appversion('firefox', '30.*')
-        self.create_appversion('firefox', '42.0')  # Default AppVersions.
-        self.create_appversion('firefox', '*')
+        self.create_webext_default_versions()
         data = {
             'applications': {
                 'gecko': {
@@ -248,16 +251,15 @@ class TestManifestJSONExtractor(TestCase):
 
     def test_apps_use_default_versions_if_none_provided(self):
         """Use the default min and max versions if none provided."""
-        # Default AppVersions.
-        firefox_min_version = self.create_appversion('firefox', '42.0')
-        firefox_max_version = self.create_appversion('firefox', '*')
+        min_version, max_version = self.create_webext_default_versions()
+
         data = {'applications': {'gecko': {'id': 'some-id'}}}
         apps = self.parse(data)['apps']
         assert len(apps) == 1  # Only Firefox for now.
         app = apps[0]
         assert app.appdata == amo.FIREFOX
-        assert app.min == firefox_min_version
-        assert app.max == firefox_max_version
+        assert app.min == min_version
+        assert app.max == max_version
 
     def test_invalid_app_versions_are_ignored(self):
         """Invalid versions are ignored."""
@@ -281,16 +283,15 @@ class TestManifestJSONExtractor(TestCase):
         See https://github.com/mozilla/addons-server/issues/2586 and
         probably many others.
         """
-        # Default AppVersions, see `ManifestJSONExtractor.app`
-        firefox_min_version = self.create_appversion('firefox', '42.0')
-        firefox_max_version = self.create_appversion('firefox', '*')
+        min_version, max_version = self.create_webext_default_versions()
+
         data = {}
         apps = self.parse(data)['apps']
         assert len(apps) == 1  # Only Firefox for now.
         app = apps[0]
         assert app.appdata == amo.FIREFOX
-        assert app.min == firefox_min_version
-        assert app.max == firefox_max_version
+        assert app.min == min_version
+        assert app.max == max_version
 
 
 def test_zip_folder_content():
