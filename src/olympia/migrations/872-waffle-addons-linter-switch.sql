aa620a89eeba2061f25bf59c9a62cf46368c6db2
Christopher Grebs <cg@webshox.org>
2016-05-10 07:14:53 +0200

    Add missing migration to add the addons-linter waffle-switch. (#2599)

---
+++ src/olympia/migrations/872-waffle-addons-linter-switch.sql
@@ -0,0 +1,5 @@
+INSERT INTO waffle_switch (name, active, note, created, modified)
+    VALUES (
+        'addons-linter', 0,
+        'Waffle switch to enable addons-linter integration for WebExtensions.',
+        NOW(), NOW());
