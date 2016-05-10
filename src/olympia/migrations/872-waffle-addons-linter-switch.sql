0d166b4b899b4fabf9802dd07c85dee3879a1646
Christopher Grebs <cg@webshox.org>
2016-05-10 16:28:00 +0200

    Skip addons-linter waffle-flag creation if it already exists.

--- src/olympia/migrations/872-waffle-addons-linter-switch.sql
+++ src/olympia/migrations/872-waffle-addons-linter-switch.sql
@@ -1,5 +1,3 @@
-INSERT INTO waffle_switch (name, active, note, created, modified)
-    VALUES (
-        'addons-linter', 0,
-        'Waffle switch to enable addons-linter integration for WebExtensions.',
-        NOW(), NOW());
+INSERT INTO waffle_switch (name, active, note)
+SELECT * FROM (SELECT 'addons-linter', 0, 'Waffle switch to enable addons-linter integration for WebExtensions.') as tmp
+WHERE NOT EXISTS (SELECT name FROM waffle_switch WHERE name = 'addons-linter') LIMIT 1;
