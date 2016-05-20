13e8ab72f74c8cce79efb969b49424ebfb4c8c81
Mathieu Pillard <m@virgule.net>
2016-05-20 14:36:39 +0200

    Make discovery API url routing more flexible / less hacky

--- src/olympia/discovery/api_urls.py
+++ src/olympia/discovery/api_urls.py
@@ -4,10 +4,8 @@ from rest_framework.routers import SimpleRouter
 
 from . import views
 
-# Because we're registering things at "/discovery/" we don't want the trailing
-# slash that comes with the Router by default.
-discovery = SimpleRouter(trailing_slash=False)
-discovery.register(r'', views.DiscoveryViewSet, base_name='discovery')
+discovery = SimpleRouter()
+discovery.register('discovery', views.DiscoveryViewSet, base_name='discovery')
 
 urlpatterns = [
     url(r'', include(discovery.urls)),
