3f8c4aa1e32d7b06efdc2bd4e98244d01bd50e54
Mathieu Pillard <mpillard@mozilla.com>
2016-06-10 13:33:25 +0200

    Drop deprecated index_name property from our ES mapping
    This is necessary to switch to ES 2.x. Fortunately, while the old
    mapping is active both variants are accepted, so the new code
    should be backwards-compatible with the old mapping while we reindex.

--- src/olympia/search/views.py
+++ src/olympia/search/views.py
@@ -354,7 +354,7 @@ def _filter_search(request, qs, query, filters, sorting,
         ps = (amo.PLATFORM_DICT[query['platform']].id, amo.PLATFORM_ALL.id)
         # If we've selected "All Systems" don't filter by platform.
         if ps[0] != ps[1]:
-            qs = qs.filter(platform__in=ps)
+            qs = qs.filter(platforms__in=ps)
     if 'appver' in show:
         # Get a min version less than X.0.
         low = version_int(query['appver'])
@@ -378,7 +378,7 @@ def _filter_search(request, qs, query, filters, sorting,
         if 'cat' in show:
             qs = qs.filter(category=query['cat'])
     if 'tag' in show:
-        qs = qs.filter(tag=query['tag'])
+        qs = qs.filter(tags=query['tag'])
     if 'sort' in show:
         qs = qs.order_by(sorting[query['sort']])
     elif not query.get('q'):
@@ -434,9 +434,11 @@ def search(request, tag_name=None, template=None):
         sort[1] = extra_sort[1]
         del extra_sort[1]
 
+    # Perform search, using facets so that we can build the filter UI. Note
+    # that we don't facet on platforms, that filter it built from our constants
+    # directly, using the current application for this request (request.APP).
     qs = (Addon.search_public().filter(app=APP.id)
-          .facet(tags={'terms': {'field': 'tag'}},
-                 platforms={'terms': {'field': 'platform'}},
+          .facet(tags={'terms': {'field': 'tags'}},
                  appversions={'terms':
                               {'field': 'appversion.%s.max' % APP.id}},
                  categories={'terms': {'field': 'category', 'size': 200}}))
