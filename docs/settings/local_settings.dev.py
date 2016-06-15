f026664ae14da089961832fba935a53b8d67e71d
Christopher Grebs <cg@webshox.org>
2016-06-15 16:15:55 +0200

    I feel like we're not using it and I don't see why we should. Let's get rid of it.

--- docs/settings/local_settings.dev.py
+++ docs/settings/local_settings.dev.py
@@ -3,7 +3,6 @@ from settings import *  # noqa
 # To activate the Django debug toolbar.
 INSTALLED_APPS += (
     'debug_toolbar',
-    'fixture_magic',
 )
 DEBUG_TOOLBAR_PATCH_SETTINGS = False  # Prevent DDT from patching the settings.
 
