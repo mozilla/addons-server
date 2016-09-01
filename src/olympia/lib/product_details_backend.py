2acfe625d2cb7c44743c98eb9205707c31e8f484
Mathieu Pillard <mpillard@mozilla.com>
2016-09-01 15:51:28 +0200

    Upgrade django-product-details to the latest version

--- src/olympia/lib/product_details_backend.py
+++ src/olympia/lib/product_details_backend.py
@@ -9,7 +9,14 @@ class NoCachePDFileStorage(PDFileStorage):
     def data(self, name):
         """Return the parsed JSON data of the requested file name.
 
-        Doesn't use the django-cache.
+        Doesn't use the django-cache, doesn't validate the name, just gets the
+        data from the filesystem using self.content() and store it in a dict
+        on the product details backend instance.
+
+        It makes it more efficient than just using PDFileStorage with a dummy
+        cache backend, because we do store the result, just not in a cache
+        backend that needs network round-trips, and we avoid walking the
+        filesystem entirely.
         """
         cache_key = self._get_cache_key(name)
 
--- src/olympia/lib/settings_base.py
+++ src/olympia/lib/settings_base.py
@@ -169,7 +169,6 @@ def lazy_langs(languages):
 
 # Where product details are stored see django-mozilla-product-details
 PROD_DETAILS_DIR = path('src', 'olympia', 'lib', 'product_json')
-PROD_DETAILS_URL = 'https://svn.mozilla.org/libs/product-details/json/'
 PROD_DETAILS_STORAGE = 'olympia.lib.product_details_backend.NoCachePDFileStorage'  # noqa
 
 # Override Django's built-in with our native names
