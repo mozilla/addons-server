e1ad5229bbd44d58d4f082b37b21c0cfad597095
Andrew Williamson <awilliamson@mozilla.com>
2016-09-14 15:55:26 +0100

    Add to test to check for missing MessageId too

--- src/olympia/activity/tests/test_tasks.py
+++ src/olympia/activity/tests/test_tasks.py
@@ -5,6 +5,9 @@ from olympia.activity.tasks import process_email
 
 @mock.patch('olympia.activity.tasks.add_email_to_activity_log_wrapper')
 def test_process_email(_mock):
+    # MessageId not in the message we pass to process_email should fail too.
+    process_email({})
+    assert _mock.call_count == 0
     message = {'MessageId': 'Dave'}
     process_email(message)
     assert _mock.call_count == 1
