mozilla/addons-server

commit 24d85149d3c6b201e67a75c411f4ba32b381f22c

Remove javascript email address validation (bug 1214844).

static/js/impala/persona_creation.js
@@ -3,9 +3,6 @@
         return;
     }
 
-    $(document).delegate('.email-autocomplete', 'keyup paste', validateUser);
-    validateUser();
-
     initOwnership();
 
     var $ownerForm = $('.owner-email form');
@@ -52,45 +49,6 @@
         }, false);
         toggleOwnership();
     }
-
-
-    var lastVal;
-    function validateUser(e) {
-        var $this = e ? $(this) :  $('.email-autocomplete');
-        if ($this.val() && $this.val().length > 2) {
-            var timeout, request;
-            if (timeout) {
-                clearTimeout(timeout);
-            }
-            timeout = setTimeout(function() {
-                $this.addClass('ui-autocomplete-loading')
-                     .removeClass('invalid valid');
-                lastVal = $this.val();
-                request = $.ajax({
-                    url: $this.attr('data-src'),
-                    data: {q: lastVal},
-                    success: function(data) {
-                        $this.removeClass('ui-autocomplete-loading tooltip formerror')
-                             .removeAttr('title')
-                             .removeAttr('data-oldtitle');
-                        $('#tooltip').hide();
-                        if (data.status == 1) {
-                            $this.addClass('valid');
-                        } else {
-                            $this.addClass('invalid tooltip formerror')
-                                 .attr('title', data.message);
-                        }
-                        checkOwnerValid();
-                    },
-                    error: function() {
-                        $this.removeClass('ui-autocomplete-loading')
-                             .addClass('invalid');
-                    }
-                });
-            }, 500);
-        }
-    }
-
 
     function initLicense() {
         var $licenseField = $('#id_license');

static/js/zamboni/devhub.js
@@ -949,9 +949,7 @@ function initAuthorFields() {
             .attr("title", $(this).text());
     });
 
-    $("#author_list").delegate(".email-autocomplete", "keypress", validateUser)
-    .delegate(".email-autocomplete", "keyup", validateUser)
-    .delegate(".remove", "click", function (e) {
+    $("#author_list").delegate(".remove", "click", function (e) {
         e.preventDefault();
         var tgt = $(this),
             row = tgt.parents("li");
@@ -988,46 +986,6 @@ function initAuthorFields() {
                    .placeholder();
         manager.val(author_list.children(".author").length);
         renumberAuthors();
-    }
-    function validateUser(e) {
-        var tgt = $(this),
-            row = tgt.parents("li");
-        if (row.hasClass("blank")) {
-            tgt.removeClass("placeholder")
-               .attr("placeholder", undefined);
-            row.removeClass("blank")
-               .addClass("author");
-            addAuthorRow();
-        }
-        if (tgt.val().length > 2) {
-            if (timeout) clearTimeout(timeout);
-            timeout = setTimeout(function() {
-                tgt.addClass("ui-autocomplete-loading")
-                   .removeClass("invalid")
-                   .removeClass("valid");
-                request = $.ajax({
-                    url: tgt.attr("data-src"),
-                    data: {q: tgt.val()},
-                    success: function(data) {
-                        tgt.removeClass('ui-autocomplete-loading tooltip')
-                           .removeClass('formerror')
-                           .removeAttr('title')
-                           .removeAttr('data-oldtitle');
-                        $('#tooltip').hide();
-                        if (data.status == 1) {
-                            tgt.addClass("valid");
-                        } else {
-                            tgt.addClass("invalid tooltip formerror")
-                               .attr('title', data.message);
-                        }
-                    },
-                    error: function() {
-                        tgt.removeClass("ui-autocomplete-loading")
-                           .addClass("invalid");
-                    }
-                });
-            }, 500);
-        }
     }
 }
 

commit 7d6070451efefa2756693fa1cf5d9c4aed90fccf

Fix needlessly disabled save button for theme ownership transfer.
Also make sure the toggle stays toggled after clicking "Save"
so that the user can see the error message.

This got probably broken in 24d85149d3c6b201e67a75c411f4ba32b381f22c

Fixes #898

src/olympia/devhub/templates/devhub/personas/edit.html
@@ -28,11 +28,11 @@
         <a href="#ownership" class="button transfer-ownership">{{ _('Transfer ownership') }}</a>
       </h3>
       <div class="owner-email hidden">
-        <form action="{{ request.path }}" method="post">
+        <form action="{{ request.path }}#ownership" method="post">
           {{ csrf() }}
           {{ owner_form.owner.errors }}
           {{ owner_form.owner }}
-          <button name="owner_submit" disabled type="submit">{{ _('Save Changes') }}</button>
+          <button name="owner_submit" type="submit">{{ _('Save Changes') }}</button>
         </form>
       </div>
     </div>

static/js/impala/persona_creation.js
@@ -5,12 +5,6 @@
 
     initOwnership();
 
-    var $ownerForm = $('.owner-email form');
-    $ownerForm.delegate('input, select, textarea', 'change keyup paste', function(e) {
-        checkOwnerValid();
-    });
-    checkOwnerValid();
-
     // Validate the form.
     var $form = $('#submit-persona form');
     $form.delegate('input, select, textarea', 'change keyup paste', function(e) {
@@ -22,11 +16,6 @@
     initCharCount();
     initPreview();
     initReuploadTheme();
-
-
-    function checkOwnerValid() {
-        $ownerForm.find('button').prop('disabled', !$ownerForm.find('input').hasClass('valid'));
-    }
 
 
     function toggleOwnership() {