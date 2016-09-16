mozilla/addons-server

commit 673d597c03def1797d6d9d6fc34d799d8ed73d9f

Make sure soft-deleted instances are inaccessible everywhere
get_object_or_404(model, ...) will use the default manager, which
does not filter out deleted instances. Using model.objects prevents
this issue.

src/olympia/devhub/views.py
@@ -158,7 +158,7 @@ def ajax_compat_error(request, addon_id, addon):
 def ajax_compat_update(request, addon_id, addon, version_id):
     if not addon.accepts_compatible_apps():
         raise http.Http404()
-    version = get_object_or_404(Version, pk=version_id, addon=addon)
+    version = get_object_or_404(Version.objects, pk=version_id, addon=addon)
     compat_form = forms.CompatFormSet(request.POST or None,
                                       queryset=version.apps.all())
     if request.method == 'POST' and compat_form.is_valid():
@@ -809,7 +809,7 @@ def json_bulk_compat_result(request, addon_id, addon, result_id):
 def json_upload_detail(request, upload, addon_slug=None):
     addon = None
     if addon_slug:
-        addon = get_object_or_404(Addon, slug=addon_slug)
+        addon = get_object_or_404(Addon.objects, slug=addon_slug)
     result = upload_validation_context(request, upload, addon=addon)
     plat_exclude = []
     if result['validation']:
@@ -856,11 +856,7 @@ def json_upload_detail(request, upload, addon_slug=None):
     return result
 
 
-def upload_validation_context(request, upload, addon_slug=None, addon=None,
-                              url=None):
-    if addon_slug and not addon:
-        addon = get_object_or_404(Addon, slug=addon_slug)
-
+def upload_validation_context(request, upload, addon=None, url=None):
     if not url:
         if addon:
             url = reverse('devhub.upload_detail_for_addon',
@@ -1121,7 +1117,7 @@ def upload_image(request, addon_id, addon, upload_type):
 
 @dev_required
 def version_edit(request, addon_id, addon, version_id):
-    version = get_object_or_404(Version, pk=version_id, addon=addon)
+    version = get_object_or_404(Version.objects, pk=version_id, addon=addon)
     version_form = forms.VersionForm(
         request.POST or None,
         request.FILES or None,
@@ -1195,7 +1191,7 @@ def _log_max_version_change(addon, version, appversion):
 @transaction.atomic
 def version_delete(request, addon_id, addon):
     version_id = request.POST.get('version_id')
-    version = get_object_or_404(Version, pk=version_id, addon=addon)
+    version = get_object_or_404(Version.objects, pk=version_id, addon=addon)
     if 'disable_version' in request.POST:
         messages.success(request, _('Version %s disabled.') % version.version)
         version.is_user_disabled = True
@@ -1211,7 +1207,7 @@ def version_delete(request, addon_id, addon):
 @transaction.atomic
 def version_reenable(request, addon_id, addon):
     version_id = request.POST.get('version_id')
-    version = get_object_or_404(Version, pk=version_id, addon=addon)
+    version = get_object_or_404(Version.objects, pk=version_id, addon=addon)
     messages.success(request, _('Version %s re-enabled.') % version.version)
     version.is_user_disabled = False
     version.addon.update_status()
@@ -1329,7 +1325,7 @@ def version_add(request, addon_id, addon):
 @dev_required
 @post_required
 def version_add_file(request, addon_id, addon, version_id):
-    version = get_object_or_404(Version, pk=version_id, addon=addon)
+    version = get_object_or_404(Version.objects, pk=version_id, addon=addon)
     new_file_form = forms.NewFileForm(request.POST, request.FILES, addon=addon,
                                       version=version, request=request)
     if not new_file_form.is_valid():

src/olympia/devhub/tests/test_views.py
@@ -2330,6 +2330,29 @@ class TestUploadDetail(BaseUploadTest):
         msg = data['validation']['messages'][0]
         assert msg['tier'] == 1
 
+    def test_upload_detail_for_addon(self):
+        user = UserProfile.objects.get(email='regular@mozilla.com')
+        addon = addon_factory()
+        addon.addonuser_set.create(user=user)
+        self.post()
+
+        upload = FileUpload.objects.get()
+        response = self.client.get(reverse('devhub.upload_detail_for_addon',
+                                           args=[addon.slug, upload.uuid.hex]))
+        assert response.status_code == 200
+
+    def test_upload_detail_for_addon_deleted(self):
+        user = UserProfile.objects.get(email='regular@mozilla.com')
+        addon = addon_factory()
+        addon.addonuser_set.create(user=user)
+        addon.delete()
+        self.post()
+
+        upload = FileUpload.objects.get()
+        response = self.client.get(reverse('devhub.upload_detail_for_addon',
+                                           args=[addon.slug, upload.uuid.hex]))
+        assert response.status_code == 404
+
     def test_detail_json_addons_linter(self):
         self.upload_file('valid_webextension.xpi')
 
@@ -2691,6 +2714,11 @@ class TestVersionAddFile(UploadTest):
         self.post()
         assert len(qs.all()) == 2
         assert qs.get(platform=amo.PLATFORM_MAC.id)
+
+    def test_version_deleted(self):
+        self.version.delete()
+        response = self.post()
+        assert response.status_code == 404
 
     def test_upload_not_found(self):
         r = self.client.post(self.url, dict(upload='xxx',

src/olympia/addons/tests/test_views.py
@@ -467,9 +467,16 @@ class TestLicensePage(TestCase):
         self.version = self.addon.current_version
 
     def test_legacy_redirect(self):
-        r = self.client.get('/versions/license/%s' % self.version.id,
-                            follow=True)
-        self.assert3xx(r, self.version.license_url(), 301)
+        response = self.client.get(
+            '/en-US/firefox/versions/license/%s' % self.version.id,
+            follow=True)
+        self.assert3xx(response, self.version.license_url(), 301)
+
+    def test_legacy_redirect_deleted(self):
+        self.version.delete()
+        response = self.client.get(
+            '/en-US/firefox/versions/license/%s' % self.version.id)
+        assert response.status_code == 404
 
     def test_explicit_version(self):
         url = reverse('addons.license', args=['a3615', self.version.version])
@@ -1316,6 +1323,13 @@ class TestEula(TestCase):
         assert old != self.addon.current_version
         r = self.client.get(self.get_url([old.all_files[0].id]))
         assert r.context['version'] == old
+
+    def test_deleted_version(self):
+        old = self.addon.versions.order_by('created')[0]
+        assert old != self.addon.current_version
+        old.delete()
+        response = self.client.get(self.get_url([old.all_files[0].id]))
+        assert response.status_code == 404
 
     def test_redirect_no_eula(self):
         self.addon.update(eula=None)

src/olympia/addons/views.py
@@ -559,7 +559,7 @@ def license(request, addon, version=None):
 
 @non_atomic_requests
 def license_redirect(request, version):
-    version = get_object_or_404(Version, pk=version)
+    version = get_object_or_404(Version.objects, pk=version)
     return redirect(version.license_url(), permanent=True)
 
 
@@ -584,7 +584,7 @@ def persona_redirect(request, persona_id):
         # Newer themes have persona_id == 0, doesn't mean anything.
         return http.HttpResponseNotFound()
 
-    persona = get_object_or_404(Persona, persona_id=persona_id)
+    persona = get_object_or_404(Persona.objects, persona_id=persona_id)
     try:
         to = reverse('addons.detail', args=[persona.addon.slug])
     except Addon.DoesNotExist:

src/olympia/editors/views.py
@@ -785,7 +785,7 @@ def queue_viewing(request):
 @json_view
 @addons_reviewer_required
 def queue_version_notes(request, addon_id):
-    addon = get_object_or_404(Addon, pk=addon_id)
+    addon = get_object_or_404(Addon.objects, pk=addon_id)
     version = addon.latest_version
     return {'releasenotes': unicode(version.releasenotes),
             'approvalnotes': version.approvalnotes}

src/olympia/versions/views.py
@@ -81,7 +81,7 @@ def update_info(request, addon, version_num):
 
 @non_atomic_requests
 def update_info_redirect(request, version_id):
-    version = get_object_or_404(Version, pk=version_id)
+    version = get_object_or_404(Version.objects, pk=version_id)
     return redirect(reverse('addons.versions.update_info',
                             args=(version.addon.id, version.version)),
                     permanent=True)
@@ -151,7 +151,7 @@ def download_latest(request, addon, beta=False, type='xpi', platform=None):
 
 @non_atomic_requests
 def download_source(request, version_id):
-    version = get_object_or_404(Version, pk=version_id)
+    version = get_object_or_404(Version.objects, pk=version_id)
 
     # General case: addon is listed.
     if version.addon.is_listed:

src/olympia/devhub/tests/test_views_versions.py
@@ -127,6 +127,11 @@ class TestVersion(TestCase):
         res = self.client.get(reverse('addons.detail', args=[self.addon.slug]))
         assert res.status_code == 200
 
+    def test_version_delete_version_deleted(self):
+        self.version.delete()
+        response = self.client.post(self.delete_url, self.delete_data)
+        assert response.status_code == 404
+
     def test_cant_delete_version(self):
         self.client.logout()
         res = self.client.post(self.delete_url, self.delete_data)
@@ -147,9 +152,17 @@ class TestVersion(TestCase):
     def test_reenable_version(self):
         Version.objects.get(pk=81551).all_files[0].update(
             status=amo.STATUS_DISABLED, original_status=amo.STATUS_LITE)
-        self.delete_url = reverse('devhub.versions.reenable', args=['a3615'])
-        self.client.post(self.delete_url, self.delete_data)
+        self.reenable_url = reverse('devhub.versions.reenable', args=['a3615'])
+        response = self.client.post(
+            self.reenable_url, self.delete_data, follow=True)
+        assert response.status_code == 200
         assert not Version.objects.get(pk=81551).is_user_disabled
+
+    def test_reenable_deleted_version(self):
+        Version.objects.get(pk=81551).delete()
+        self.delete_url = reverse('devhub.versions.reenable', args=['a3615'])
+        response = self.client.post(self.delete_url, self.delete_data)
+        assert response.status_code == 404
 
     def _extra_version_and_file(self, status):
         version = Version.objects.get(id=81551)
@@ -566,6 +579,15 @@ class TestVersionEditDetails(TestVersionEditBase):
         platforms = set(self.version.compatible_platforms()) - set(taken)
         assert len(choices) == len(platforms)
 
+    def test_version_deleted(self):
+        self.version.delete()
+        response = self.client.get(self.url)
+        assert response.status_code == 404
+
+        data = self.formset(releasenotes='xx', approvalnotes='yy')
+        response = self.client.post(self.url, data)
+        assert response.status_code == 404
+
     def test_can_upload(self):
         self.version.files.all().delete()
         r = self.client.get(self.url)
@@ -936,6 +958,15 @@ class TestVersionEditCompat(TestVersionEditBase):
         assert av.max.version == '5.0'
         assert list(ActivityLog.objects.all().values_list('action')) == (
             [(amo.LOG.MAX_APPVERSION_UPDATED.id,)])
+
+    def test_ajax_update_on_deleted_version(self):
+        url = reverse('devhub.ajax.compat.update',
+                      args=['a3615', self.version.id])
+        d = self.get_form(url)
+        d.update(min=self.v1.id, max=self.v5.id)
+        self.version.delete()
+        r = self.client.post(url, self.formset(d, initial_count=1))
+        assert r.status_code == 404
 
     def test_delete_appversion(self):
         # Add thunderbird compat so we can delete firefox.

src/olympia/versions/tests.py
@@ -397,6 +397,12 @@ class TestVersion(TestCase):
                       args=(self.version.addon.slug, self.version.version))
         self.assert3xx(r, url, 301)
 
+    def test_version_update_info_legacy_redirect_deleted(self):
+        self.version.delete()
+        response = self.client.get(
+            '/en-US/firefox/versions/updateInfo/%s' % self.version.id)
+        assert response.status_code == 404
+
     def _reset_version(self, version):
         version.all_files[0].status = amo.STATUS_PUBLIC
         version.deleted = False
@@ -1107,6 +1113,13 @@ class TestDownloadSource(TestCase):
         assert response[settings.XSENDFILE_HEADER].decode('utf8') == path
 
     def test_anonymous_should_not_be_allowed(self):
+        response = self.client.get(self.url)
+        assert response.status_code == 404
+
+    def test_deleted_version(self):
+        self.version.delete()
+        GroupUser.objects.create(user=self.user, group=self.group)
+        self.client.login(username=self.user.email, password="password")
         response = self.client.get(self.url)
         assert response.status_code == 404
 

commit d31e3b314a984fdfa602a9c6b10b3caf74aa9696

Fix json upload detail view for unlisted add-ons
Was broken in 673d597c03def1797d6d9d6fc34d799d8ed73d9f

src/olympia/devhub/views.py
@@ -801,7 +801,7 @@ def json_bulk_compat_result(request, addon_id, addon, result_id):
 def json_upload_detail(request, upload, addon_slug=None):
     addon = None
     if addon_slug:
-        addon = get_object_or_404(Addon.objects, slug=addon_slug)
+        addon = get_object_or_404(Addon.with_unlisted, slug=addon_slug)
     result = upload_validation_context(request, upload, addon=addon)
     plat_exclude = []
     if result['validation']:

src/olympia/devhub/tests/test_views.py
@@ -2202,6 +2202,17 @@ class TestUploadDetail(BaseUploadTest):
                                            args=[addon.slug, upload.uuid.hex]))
         assert response.status_code == 200
 
+    def test_upload_detail_for_addon_unlisted(self):
+        user = UserProfile.objects.get(email='regular@mozilla.com')
+        addon = addon_factory(is_listed=False)
+        addon.addonuser_set.create(user=user)
+        self.post()
+
+        upload = FileUpload.objects.get()
+        response = self.client.get(reverse('devhub.upload_detail_for_addon',
+                                           args=[addon.slug, upload.uuid.hex]))
+        assert response.status_code == 200
+
     def test_upload_detail_for_addon_deleted(self):
         user = UserProfile.objects.get(email='regular@mozilla.com')
         addon = addon_factory()