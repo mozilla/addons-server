c6029e54d2aeddb6bff5d790807edafcecce22e1
Mark Striemer <mstriemer@gmail.com>
2016-09-30 08:50:56 -0500

    Custom createsuperuser command without password (supports #3049) (#3626)

---
+++ src/olympia/users/tests/test_commands.py
@@ -0,0 +1,39 @@
+from mock import patch
+
+from olympia.amo.tests import TestCase
+from olympia.users.management.commands.createsuperuser import (
+    Command as CreateSuperUser)
+from olympia.users.models import UserProfile
+
+
+@patch('olympia.users.management.commands.createsuperuser.input')
+def test_createsuperuser_username_validation(input):
+    responses = ['', 'myusername']
+    input.side_effect = lambda *args: responses.pop(0)
+    command = CreateSuperUser()
+    assert command.get_value('username') == 'myusername'
+
+
+@patch('olympia.users.management.commands.createsuperuser.input')
+def test_createsuperuser_email_validation(input):
+    responses = ['', 'myemail', 'me@mozilla.org']
+    input.side_effect = lambda *args: responses.pop(0)
+    command = CreateSuperUser()
+    assert command.get_value('email') == 'me@mozilla.org'
+
+
+class TestCreateSuperUser(TestCase):
+    fixtures = ['users/test_backends']
+
+    @patch('olympia.users.management.commands.createsuperuser.input')
+    def test_creates_user(self, input):
+        responses = {
+            'Username: ': 'myusername',
+            'Email: ': 'me@mozilla.org',
+        }
+        input.side_effect = lambda label: responses[label]
+        count = UserProfile.objects.count()
+        CreateSuperUser().handle()
+        assert UserProfile.objects.count() == count + 1
+        user = UserProfile.objects.get(username='myusername')
+        assert user.email == 'me@mozilla.org'
