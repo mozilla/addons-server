3655ec2baa983f0d8bb53900b6f1da7005f8ce0e
Mark Striemer <mstriemer@mozilla.com>
2016-09-30 13:42:21 -0500

    Test the test backend

--- src/olympia/users/tests/test_backends.py
+++ src/olympia/users/tests/test_backends.py
@@ -1,14 +1,31 @@
-from django.contrib.auth import authenticate
-
 from olympia.amo.tests import TestCase
+from olympia.users.backends import AmoUserBackend, TestUserBackend
+from olympia.users.models import UserProfile
 
 
 class TestAmoUserBackend(TestCase):
     fixtures = ['users/test_backends']
 
     def test_success(self):
-        assert authenticate(username='jbalogh@mozilla.com',
-                            password='password')
+        assert AmoUserBackend().authenticate(
+            username='jbalogh@mozilla.com', password='password')
 
     def test_failure(self):
-        assert not authenticate(username='jbalogh@mozilla.com', password='x')
+        assert not AmoUserBackend().authenticate(
+            username='jbalogh@mozilla.com', password='x')
+
+
+class TestTestUserBackend(TestCase):
+
+    def test_login_with_email(self):
+        user = UserProfile.objects.create(
+            email='me@mozilla.com', username='whatever', password='pass')
+        assert TestUserBackend().authenticate(email='me@mozilla.com') == user
+
+    def test_login_with_username(self):
+        user = UserProfile.objects.create(
+            email='me@mozilla.com', username='whatever', password='pass')
+        assert TestUserBackend().authenticate(username='whatever') == user
+
+    def test_no_user(self):
+        assert TestUserBackend().authenticate(username='hmm') is None
