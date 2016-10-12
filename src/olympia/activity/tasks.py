ad66b09f8ffb8aa07cc2620f2013c37cb73b5445
Andrew Williamson <awilliamson@mozilla.com>
2016-10-12 11:49:13 +0100

    Add @write to process_email task

--- src/olympia/activity/tasks.py
+++ src/olympia/activity/tasks.py
@@ -1,5 +1,6 @@
 import commonware.log
 from olympia.amo.celery import task
+from olympia.amo.decorators import write
 from olympia.activity.utils import add_email_to_activity_log_wrapper
 
 
@@ -7,6 +8,7 @@ log = commonware.log.getLogger('z.amo.activity')
 
 
 @task
+@write
 def process_email(message, **kwargs):
     """Parse emails and save activity log entry."""
     res = add_email_to_activity_log_wrapper(message)
