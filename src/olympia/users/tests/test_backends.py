b70639d0ce1151d2188f7d19d0d3515317d13e1f
Mark Striemer <mstriemer@mozilla.com>
2016-11-01 00:41:39 -0500

    Remove more password code from tests

--- src/olympia/users/tests/test_backends.py
+++ src/olympia/users/tests/test_backends.py
@@ -7,26 +7,13 @@ class TestTestUserBackend(TestCase):
 
     def test_login_with_email(self):
         user = UserProfile.objects.create(
-            email='me@mozilla.com', username='whatever', password='pass')
+            email='me@mozilla.com', username='whatever')
         assert TestUserBackend().authenticate(email='me@mozilla.com') == user
 
     def test_login_with_username(self):
         user = UserProfile.objects.create(
-            email='me@mozilla.com', username='whatever', password='pass')
+            email='me@mozilla.com', username='whatever')
         assert TestUserBackend().authenticate(username='whatever') == user
 
     def test_no_user(self):
         assert TestUserBackend().authenticate(username='hmm') is None
-
-    def test_login_with_password(self):
-        UserProfile.objects.create(
-            email='me@mozilla.com', username='whatever', password='pass')
-        with self.assertRaises(TypeError):
-            TestUserBackend().authenticate(
-                email='me@mozilla.com', password='pass')
-
-    def test_client_login_does_not_work_with_password(self):
-        UserProfile.objects.create(
-            email='me@mozilla.com', username='whatever', password='pass')
-        with self.assertRaises(TypeError):
-            self.client.login(email='me@mozilla.com', password='pass')
