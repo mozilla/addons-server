839aabdd0c9d9f078202534677786a2a9f416238
Andrew Williamson <awilliamson@mozilla.com>
2016-12-01 18:40:26 +0000

    Remove is_listed from ES index now its no longer used (#4105)

--- src/olympia/addons/tests/test_indexers.py
+++ src/olympia/addons/tests/test_indexers.py
@@ -23,7 +23,7 @@ class TestAddonIndexer(TestCase):
     simple_fields = [
         'average_daily_users', 'bayesian_rating', 'created', 'default_locale',
         'guid', 'hotness', 'icon_type', 'id', 'is_disabled', 'is_experimental',
-        'is_listed', 'last_updated', 'modified', 'public_stats', 'slug',
+        'last_updated', 'modified', 'public_stats', 'slug',
         'status', 'type', 'view_source', 'weekly_downloads',
     ]
 
