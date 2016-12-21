7e0e1207121282a018863c256376b5f3b3e146a1
Andrew Williamson <awilliamson@mozilla.com>
2016-12-21 14:04:43 +0000

    make decision branches clearer

--- src/olympia/activity/tests/test_serializers.py
+++ src/olympia/activity/tests/test_serializers.py
@@ -74,10 +74,10 @@ class TestReviewNotesSerializerOutput(TestCase, LogMixin):
 
     def test_log_entry_without_details(self):
         # Create a log but without a details property.
-        entry = amo.log(
-            amo.APPROVAL_NOTES_CHANGED, self.addon,
+        self.entry = amo.log(
+            amo.LOG.APPROVAL_NOTES_CHANGED, self.addon,
             self.addon.find_latest_version(channel=amo.RELEASE_CHANNEL_LISTED),
             user=self.user)
         result = self.serialize()
         # Should default to the string representation of the log event.
-        assert result['comments'] == entry.to_string()
+        assert result['comments'] == self.entry.to_string()
