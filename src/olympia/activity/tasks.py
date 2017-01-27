f2dafe975078d7d811e28749867a3ac5570b460d
Andrew Williamson <awilliamson@mozilla.com>
2017-01-27 13:28:27 +0000

    parse alternative Message-ID email header too

--- src/olympia/activity/tasks.py
+++ src/olympia/activity/tasks.py
@@ -13,7 +13,13 @@ log = commonware.log.getLogger('z.amo.activity')
 @write
 def process_email(message, **kwargs):
     """Parse emails and save activity log entry."""
-    msg_id = message.get('MessageId', None)
+    # Some emails (gmail, at least) come with Message-ID instead of MessageId.
+    msg_id = message.get('MessageId')
+    if not msg_id:
+        custom_headers = message.get('CustomHeaders', [])
+        for header in custom_headers:
+            if header.get('Name') == 'Message-ID':
+                msg_id = header.get('Value')
     if not msg_id:
         log.error('No MessageId in message, aborting.')
         log.error(message)
@@ -21,6 +27,7 @@ def process_email(message, **kwargs):
     cache_key = 'process_email:%s' % msg_id
     if cache.get(cache_key):
         log.error('Already processed [%s] in the last 60s, skipping' % msg_id)
+        log.error(message)
         return
     cache.set(cache_key, 'yes', 60)
     res = add_email_to_activity_log_wrapper(message)
--- src/olympia/activity/tests/test_tasks.py
+++ src/olympia/activity/tests/test_tasks.py
@@ -1,6 +1,7 @@
 import mock
 
 from olympia.activity.tasks import process_email
+from olympia.activity.tests.test_utils import sample_message_content
 
 
 @mock.patch('olympia.activity.tasks.add_email_to_activity_log_wrapper')
@@ -8,7 +9,19 @@ def test_process_email(_mock):
     # MessageId not in the message we pass to process_email should fail too.
     process_email({})
     assert _mock.call_count == 0
-    message = {'MessageId': 'Dave'}
+    message = sample_message_content.get('Message')
+    process_email(message)
+    assert _mock.call_count == 1
+    # don't try to process the same message twice
+    process_email(message)
+    assert _mock.call_count == 1
+
+
+@mock.patch('olympia.activity.tasks.add_email_to_activity_log_wrapper')
+def test_process_email_different_messageid(_mock):
+    # Test 'Message-ID' works too.
+    message = {'CustomHeaders': [
+        {'Name': 'Message-ID', 'Value': '<gmail_tastic>'}]}
     process_email(message)
     assert _mock.call_count == 1
     # don't try to process the same message twice
