3efd6c796080f8df2893b96c69f859f150b6f63a
Andrew Williamson <awilliamson@mozilla.com>
2017-02-07 10:54:17 +0000

    webext_permissions.file_id as an unsigned int

--- src/olympia/migrations/918-add-webext-permissions.sql
+++ src/olympia/migrations/918-add-webext-permissions.sql
@@ -1,9 +1,11 @@
+DROP TABLE IF EXISTS `webext_permissions`;
+
 CREATE TABLE `webext_permissions` (
-    `id` int(11) unsigned AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `id` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `created` datetime NOT NULL,
     `modified` datetime NOT NULL,
-    `permissions` longtext default "",
-    `file_id` int(11) NOT NULL
+    `permissions` longtext,
+    `file_id` int(11) unsigned NOT NULL
 ) DEFAULT CHARSET=utf8;
 
 ALTER TABLE `webext_permissions` ADD CONSTRAINT `webext_permissions_file`
