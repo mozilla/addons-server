e5ddc8ea236a958c42215e299c2d1d864bbcefbc
Andrew Williamson <awilliamson@mozilla.com>
2017-02-08 20:00:43 +0000

    fix broken migration sql

--- src/olympia/migrations/919-log-activity-emails.sql
+++ src/olympia/migrations/919-log-activity-emails.sql
@@ -5,6 +5,6 @@ CREATE TABLE `log_activity_emails` (
     `messageid` char(255) NOT NULL UNIQUE
 ) DEFAULT CHARSET=utf8;
 
-DELETE FROM TABLE `log_activity_tokens`;
+DELETE FROM `log_activity_tokens`;
 
 ALTER TABLE `log_activity_tokens` ADD UNIQUE KEY (`version_id`, `user_id`);
