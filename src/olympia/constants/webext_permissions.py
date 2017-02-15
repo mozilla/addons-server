65764f3b5240e634e8e08c3a0b4902fb0aa3605b
Andrew Williamson <awilliamson@mozilla.com>
2017-02-15 16:25:32 +0100

    Display requested permissions on detail page. (#4670)

--- src/olympia/constants/webext_permissions.py
+++ src/olympia/constants/webext_permissions.py
@@ -5,19 +5,68 @@ from django.utils.translation import ugettext_lazy as _lazy
 Permission = namedtuple('Permission',
                         'name, description, long_description')
 
+
+ALL_URLS_PERMISSION = Permission(
+    u'all_urls',
+    _lazy(u'Access your data for all websites'),
+    '')
+
 WEBEXT_PERMISSIONS = {
-    u'activeTab': Permission(
-        u'activeTab',
-        _lazy(u'Requests that the extension be granted permissions according '
-              u'to the activeTab specification.'),
-        _lazy(u'Requests that the extension be granted permissions according '
-              u'to the activeTab specification.')),
-    u'alarms': Permission(
-        u'alarms',
-        _lazy(u'Gives the extension access to the chrome.alarms API.'),
-        _lazy(u'Gives the extension access to the chrome.alarms API.')),
+    u'<all_urls>': ALL_URLS_PERMISSION,
+    u'http://*/*': ALL_URLS_PERMISSION,
+    u'https://*/*': ALL_URLS_PERMISSION,
+    u'*://*/*': ALL_URLS_PERMISSION,
+
     u'bookmarks': Permission(
         u'bookmarks',
-        _lazy(u'Read and modify bookmarks.'),
-        _lazy(u'Read and modify bookmarks.')),
+        _lazy(u'Access bookmarks'),
+        ''),
+    u'clipboard': Permission(
+        u'clipboard',
+        _lazy(u'Access text clipboard'),
+        ''),
+    u'downloads': Permission(
+        u'downloads',
+        _lazy(u"Download files and read and modify the browser's download"
+              u"history"),
+        ''),
+    u'history': Permission(
+        u'history',
+        _lazy(u'Access browser history'),
+        ''),
+    u'nativeMessaging': Permission(
+        u'nativeMessaging',
+        _lazy(u'Exchange messages with programs other than Firefox'),
+        ''),
+    u'notifications': Permission(
+        u'notifications',
+        _lazy(u'Display notifications to you'),
+        ''),
+    u'sessions': Permission(
+        u'sessions',
+        _lazy(u'Access browser history to restore tabs'),
+        ''),
+    u'tabs': Permission(
+        u'tabs',
+        _lazy(u'Access browser tabs'),
+        ''),
+    u'topSites': Permission(
+        u'topSites',
+        _lazy(u'Access browsing history'),
+        ''),
+    u'webNavigation': Permission(
+        u'webNavigation',
+        _lazy(u'Access browser activity during navigation'),
+        ''),
+    u'unlimitedStorage': Permission(
+        u'unlimitedStorage',
+        _lazy(u'Provide unlimited storage of client-side data'),
+        ''),
+    u'webRequest': Permission(
+        u'webRequest',
+        _lazy(u'Access browser during Web activity'),
+        ''),
 }
+
+# webRequestBlocking has the same description as webRequest.
+WEBEXT_PERMISSIONS[u'webRequestBlocking'] = WEBEXT_PERMISSIONS[u'webRequest']
