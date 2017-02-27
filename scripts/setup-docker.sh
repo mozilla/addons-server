b7ac6368356fea0440fcb7dd4b75d31e5aa46ad9
jrbenny35 <bennyjr169@gmail.com>
2017-02-27 10:49:04 -0800

    Update travis and move ui test requirements

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -7,18 +7,6 @@ python manage.py loaddata initial.json
 python manage.py import_prod_versions
 schematic --fake src/olympia/migrations/
 
-#create Superuser
-RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-python manage.py createsuperuser \
-  --username ${RANDOM} \
-  --email ${RANDOM}@restmail.net \
-  --add-to-supercreate-group \
-  --save-api-credentials=tests/ui/variables.json \
-  --hostname=http://olympia.dev \
-  --noinput
-python manage.py waffle_switch --create super-create-accounts on
-#python manage.py loaddata zadmin/users
-
 # update_assets:
 make update_assets
 
