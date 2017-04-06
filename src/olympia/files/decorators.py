17c65c2ce2a2ca2c5f9d260831d577815f6e55c9
Christopher Grebs <cg@webshox.org>
2017-04-06 19:34:46 +0700

    Disallow public file-viewer for now. Fixes bug 1353788 (#5115)
    * Disallow public file-viewer for now. Fixes bug 1353788
    
    See bug 1353788 for more details.
    
    * Fix tests

--- src/olympia/files/decorators.py
+++ src/olympia/files/decorators.py
@@ -26,10 +26,12 @@ def allowed(request, file):
 
     # General case: addon is listed.
     if version.channel == amo.RELEASE_CHANNEL_LISTED:
-        if ((addon.view_source and addon.status in amo.REVIEWED_STATUSES) or
-                acl.check_addons_reviewer(request) or
-                acl.check_addon_ownership(request, addon, viewer=True,
-                                          dev=True)):
+        # We don't show the file-browser publicly because of potential DOS
+        # issues, we're working on a fix but for now, let's not do this.
+        # (cgrebs, 06042017)
+        is_owner = acl.check_addon_ownership(
+            request, addon, viewer=True, dev=True)
+        if (acl.check_addons_reviewer(request) or is_owner):
             return True  # Public and sources are visible, or reviewer.
         raise PermissionDenied  # Listed but not allowed.
     # Not listed? Needs an owner or an "unlisted" admin.
