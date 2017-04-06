967728ffc33b2975bbda0ab780b6154025216759
Mathieu Pillard <mpillard@mozilla.com>
2017-04-06 17:03:39 +0200

    Improve docstrings slightly

--- src/olympia/core/logger.py
+++ src/olympia/core/logger.py
@@ -13,7 +13,12 @@ def getLogger(name=None):
 
 
 class AmoLoggerAdapter(logging.LoggerAdapter):
-    """Adds the REMOTE_ADDR and USERNAME to every logging message's kwargs."""
+    """
+    Adapter adding the REMOTE_ADDR and USERNAME to every logging message's
+    kwargs extra dict, which in return will automatically be merged in every
+    LogRecord nstance, making those properties available for the formatter(s)
+    to use.
+    """
 
     def __init__(self, logger, extra=None):
         super(AmoLoggerAdapter, self).__init__(logger, extra or {})
@@ -27,7 +32,9 @@ class AmoLoggerAdapter(logging.LoggerAdapter):
 
 
 class Formatter(logging.Formatter):
-    """Formatter that makes sure REMOTE_ADDR and USERNAME are available."""
+    """Formatter that makes sure REMOTE_ADDR and USERNAME are available.
+
+    Relies on AmoLoggerAdapter to make sure those variables will be set."""
 
     def format(self, record):
         for name in 'REMOTE_ADDR', 'USERNAME':
@@ -40,6 +47,7 @@ class JsonFormatter(dockerflow.logging.JsonLogFormatter):
     current user and ip, following mozlog format.
 
     See Formatter above for the legacy, console version of this."""
+
     def format(self, record):
         record.__dict__['uid'] = record.__dict__.pop('USERNAME', '')
         record.__dict__['remoteAddressChain'] = record.__dict__.pop(
