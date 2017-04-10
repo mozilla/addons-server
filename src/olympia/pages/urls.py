f0e907d9db514e56be2e2cbd08d43158cf0083b2
raymak <kamyar.ardekani@gmail.com>
2017-04-10 14:08:00 -0700

    fix #5144 Shield Study 10 Page Reload consent page

--- src/olympia/pages/urls.py
+++ src/olympia/pages/urls.py
@@ -55,6 +55,9 @@ urlpatterns = patterns(
     url('^shield_study_9$',
         TemplateView.as_view(template_name='pages/shield_study_9.html'),
         name='pages.shield_study_9'),
+    url('^shield_study_10$',
+        TemplateView.as_view(template_name='pages/shield_study_10.html'),
+        name='pages.shield_study_10'),
 
     url('^pages/compatibility_firstrun$',
         lambda r: perma_redirect(reverse('pages.acr_firstrun'))),
