mozilla/addons-server

commit f6da2b8ee800112bd2a4c58c26435e7559087a92

Make 'Compatible with Firefox 57+' label a link, include it in search results

src/olympia/addons/templates/addons/impala/details.html
@@ -1,4 +1,6 @@
 {% extends "impala/base_shared.html" %}
+{% from "addons/macros.html" import addon_flags %}
+
 {% set is_author = request.check_ownership(addon, require_owner=False) %}
 {% set is_reviewer = action_allowed("Addons", "Review") %}
 {% set is_staff = action_allowed("ReviewerAdminTools", "View") %}
@@ -102,14 +104,7 @@
         <img id="addon-icon" itemprop="image" src="{{ addon.get_icon_url(64) }}" class="icon" alt="{{ _('Icon of {0}')|fe(addon.name) }}">
         <h1 class="addon"{{ addon.name|locale_html }}>
           <span itemprop="name">{{ addon.name }}</span>
-          {% if version %}
-            <span class="version-number" itemprop="version">{{ version.version }}</span>
-            {% if version.is_webextension %}
-              &nbsp;<span class="is-webextension" title="{{ _('This add-on is built with new technology that is compatible with Firefox 57 and beyond.') }}">{{ _('Compatible with Firefox 57+') }}</span>
-            {% elif version.requires_restart %}
-              &nbsp;<span class="requires-restart">{{ _('Requires Restart') }}</span>
-            {% endif %}
-          {% endif %}
+          {{ addon_flags(addon, version) }}
         </h1>
         <h4 class="author">{{ _('by') }} {{ users_list(addon.listed_authors) }}</h4>
       </hgroup>

src/olympia/addons/tests/test_views.py
@@ -981,17 +981,19 @@ class TestDetailPage(TestCase):
         assert span_restart not in r.content
 
     def test_is_webextension(self):
-        is_webextension_span = '<span class="is-webextension"'
-        f = self.addon.current_version.all_files[0]
+        file_ = self.addon.current_version.all_files[0]
 
-        assert f.is_webextension is False
-        r = self.client.get(self.url)
-        assert is_webextension_span not in r.content
+        assert file_.is_webextension is False
+        response = self.client.get(self.url)
+        doc = pq(response.content)
+        assert not doc('a.is-webextension')
 
-        f.update(is_webextension=True)
-        assert f.is_webextension is True
-        r = self.client.get(self.url)
-        assert is_webextension_span in r.content
+        file_.update(is_webextension=True, no_restart=True)
+        assert file_.is_webextension is True
+        response = self.client.get(self.url)
+        doc = pq(response.content)
+        link = doc('a.is-webextension')
+        assert link.attr['href'] == reverse('pages.webextensions_info')
 
     def test_disabled_user_message(self):
         self.addon.update(disabled_by_user=True)

src/olympia/addons/templates/addons/impala/listing/items.html
@@ -1,4 +1,4 @@
-{% from "addons/macros.html" import sort_vital %}
+{% from "addons/macros.html" import sort_vital, addon_flags %}
 {% cache addons %}
   {% for addon in addons %}
     <div class="item addon">
@@ -10,9 +10,7 @@
           {% endif %}
           <a href="{{ dl_url }}">
             <img src="{{ addon.icon_url }}" alt="">{{ addon.name }}</a>
-          {% if addon.requires_restart %}
-            <span class="requires-restart">{{ _('Requires Restart') }}</span>
-          {% endif %}
+          {{ addon_flags(addon, addon.current_version) }}
           {% if addon.is_featured(request.APP, request.LANG) %}
             <span class="featured">{{ _('Featured') }}</span>
           {% endif %}

src/olympia/addons/templates/addons/macros.html
@@ -36,6 +36,16 @@
   </div>
 {% endmacro %}
 
+{% macro addon_flags(addon, version) %}
+    {% if version %}
+      {% if version.requires_restart %}
+        <span class="requires-restart">{{ _('Requires Restart') }}</span>
+      {% elif version.is_webextension %}
+        <a href="{{ url('pages.webextensions_info') }}" class="is-webextension" title="{{ _('This add-on is built with new technology that is compatible with Firefox 57 and beyond.') }}">{{ _('Compatible with Firefox 57+') }}</a>
+      {% endif %}
+    {% endif %}
+{% endmacro %}
+
 {% macro addon_heading(addon, version) %}
   <h1{{ addon.name|locale_html }}>
     {{ addon.name }}

src/olympia/pages/tests.py
@@ -49,3 +49,18 @@ class TestRedirects(TestCase):
                             follow=False)
         self.assert3xx(r, reverse(
             'devhub.docs', args=['policies/agreement']), 301)
+
+    def test_webextensions_info_redirect(self):
+        assert (reverse('pages.webextensions_info') ==
+                '/en-US/firefox/pages/webextensions_info')
+        self._check({
+            '/en-US/firefox/pages/webextensions_info':
+                'https://support.mozilla.org/kb/webextensions-en-us',
+            '/fr/firefox/pages/webextensions_info':
+                'https://support.mozilla.org/kb/webextensions-fr',
+            '/zh-CN/firefox/pages/webextensions_info':
+                'https://support.mozilla.org/kb/webextensions-zh-cn',
+            # Unsupported locale for this page, it should use en-US instead.
+            '/sv-SE/firefox/pages/webextensions_info':
+                'https://support.mozilla.org/kb/webextensions-en-us',
+        })

src/olympia/pages/urls.py
@@ -71,6 +71,9 @@ urlpatterns = patterns(
     url('^pages/validation$',
         lambda r: perma_redirect(settings.VALIDATION_FAQ_URL)),
 
+    url('^pages/webextensions_info$', views.webextensions_info,
+        name='pages.webextensions_info'),
+
     url('^sunbird$',
         TemplateView.as_view(template_name='pages/sunbird.html'),
         name='pages.sunbird'),

src/olympia/pages/views.py
@@ -2,10 +2,11 @@ from collections import defaultdict
 
 from django.conf import settings
 from django.db.transaction import non_atomic_requests
+from django.http import HttpResponsePermanentRedirect
 
 from olympia.activity.models import ActivityLog
 from olympia.users.models import UserProfile
-from olympia.amo.utils import render
+from olympia.amo.utils import find_language, render
 
 
 @non_atomic_requests
@@ -72,3 +73,17 @@ def credits(request):
     }
 
     return render(request, 'pages/credits.html', context)
+
+
+@non_atomic_requests
+def webextensions_info(request):
+    """Redirect to the support page for webextensions in the right locale (
+    defaulting to english if there isn't one)."""
+    supported_languages = (
+        'de', 'en-US', 'es', 'fr', 'ja', 'pl', 'pt-PT', 'ru', 'zh-CN'
+    )
+    lang = find_language(request.LANG)
+    if lang not in supported_languages:
+        lang = 'en-US'
+    url = 'https://support.mozilla.org/kb/webextensions-%s' % (lang.lower())
+    return HttpResponsePermanentRedirect(url)

static/css/impala/addon_details.less
@@ -486,7 +486,7 @@ h1.addon,
 }
 
 span.requires-restart,
-span.is-webextension,
+div a.is-webextension,
 span.featured {
     background-color: #e8933a;
     border-radius: 2px;
@@ -499,12 +499,12 @@ span.featured {
     vertical-align: 3px;
 }
 span.featured,
-span.is-webextension {
+div a.is-webextension {
     background-color: #093;
 }
 
 .html-rtl span.requires-restart,
-.html-rtl span.iswebextension,
+.html-rtl div a.iswebextension,
 .html-rtl span.featured {
     margin: 4px 4px 0 0;
 }

commit 9f3b115bb3a7723646f72341a191a2e3bd6e11f4

Add back version number in detail page that was mistakenly taken out
(Broke in f6da2b8ee800112bd2a4c58c26435e7559087a92 when refactoring
 the addon flags displayed on detail and listing page)

src/olympia/addons/templates/addons/impala/details.html
@@ -104,7 +104,10 @@
         <img id="addon-icon" itemprop="image" src="{{ addon.get_icon_url(64) }}" class="icon" alt="{{ _('Icon of {0}')|fe(addon.name) }}">
         <h1 class="addon"{{ addon.name|locale_html }}>
           <span itemprop="name">{{ addon.name }}</span>
-          {{ addon_flags(addon, version) }}
+          {% if version %}
+            <span class="version-number" itemprop="version">{{ version.version }}</span>
+            {{ addon_flags(addon, version) }}
+          {% endif %}
         </h1>
         <h4 class="author">{{ _('by') }} {{ users_list(addon.listed_authors) }}</h4>
       </hgroup>

src/olympia/addons/tests/test_views.py
@@ -997,6 +997,11 @@ class TestDetailPage(TestCase):
         link = doc('a.is-webextension')
         assert link.attr['href'] == reverse('pages.webextensions_info')
 
+    def test_version_displayed(self):
+        response = self.client.get(self.url)
+        doc = pq(response.content)
+        assert doc('.version-number').text() == '2.1.072'
+
     def test_disabled_user_message(self):
         self.addon.update(disabled_by_user=True)
         res = self.client.get(self.url)