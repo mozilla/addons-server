f9b6d3997097ad7fd59086108828f7f2799a4e66
Andrew Williamson <awilliamson@mozilla.com>
2017-05-03 18:23:32 +0100

    Handle different foriegn keys on prod and local docker

--- src/olympia/migrations/939-drop-l10n-from-bio-user-profile-cleanup.sql
+++ src/olympia/migrations/939-drop-l10n-from-bio-user-profile-cleanup.sql
@@ -1,12 +1,30 @@
 /* Follow-on from migration 938 -
 Drop the (now obsolete) bio and old translations. */
 
-/*  ** Note: FOREIGN KEY is prod's - you will need to change this for your
-local DB ** */
+DROP PROCEDURE IF EXISTS drop_bio_fk;
 
-/* Drop the constraint so we can clear the translations.*/
-ALTER TABLE `users`
-    DROP FOREIGN KEY `users_ibfk_1`;
+DELIMITER ';;'
+
+CREATE PROCEDURE drop_bio_fk() BEGIN
+    /* drop the bio foreign key so we can clear the translations.
+    - we know prod and local dbs have different keys, so try to handle both */
+
+    /* prod fk name */
+    IF EXISTS (SELECT 1 FROM information_schema.table_constraints WHERE table_schema = (SELECT DATABASE()) AND table_name = 'users' AND constraint_name = 'users_ibfk_1') THEN
+        ALTER TABLE reviews DROP FOREIGN KEY users_ibfk_1;
+    END IF;
+
+    /* local docker default fk name */
+    IF EXISTS (SELECT 1 FROM information_schema.table_constraints WHERE table_schema = (SELECT DATABASE()) AND table_name = 'users' AND constraint_name = 'users_bio_21cc3d1ebf26e19_fk_translations_id') THEN
+        ALTER TABLE reviews DROP FOREIGN KEY users_bio_21cc3d1ebf26e19_fk_translations_id;
+    END IF;
+END;;
+
+DELIMITER ';'
+
+CALL drop_bio_fk();
+
+DROP PROCEDURE IF EXISTS drop_bio_fk;
 
 /* Clear out the translations. */
 DELETE FROM `translations` WHERE `id` in (SELECT `bio` FROM `users`);
