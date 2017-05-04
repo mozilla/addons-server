c95a6d00d11681147ceeb211d8fe23c2693de312
Andrew Williamson <awilliamson@mozilla.com>
2017-05-04 16:34:53 +0100

    "olympia" to (SELECT DATABASE()) in migration (#5333)

--- src/olympia/migrations/940-drop-l10n-from-bio-user-profile-cleanup.sql
+++ src/olympia/migrations/940-drop-l10n-from-bio-user-profile-cleanup.sql
@@ -1,7 +1,7 @@
 /* Follow-on from migration 938 -
 Drop the (now obsolete) bio and old translations. */
 
-SET @CONSTRAINT_NAME := (SELECT CONSTRAINT_NAME FROM information_schema.key_column_usage WHERE TABLE_SCHEMA="olympia" AND TABLE_NAME="users" and COLUMN_NAME="bio" AND REFERENCED_TABLE_NAME = "translations");
+SET @CONSTRAINT_NAME := (SELECT CONSTRAINT_NAME FROM information_schema.key_column_usage WHERE TABLE_SCHEMA=(SELECT DATABASE()) AND TABLE_NAME="users" and COLUMN_NAME="bio" AND REFERENCED_TABLE_NAME = "translations");
 
 SET @QUERY = CONCAT('ALTER TABLE users DROP FOREIGN KEY ', @constraint_name, ';');
 PREPARE stmt FROM @QUERY;
