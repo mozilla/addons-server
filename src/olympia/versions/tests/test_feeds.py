b11f9df51678846e23cdaa2bf6c7259457ea861c
Mathieu Pillard <mpillard@mozilla.com>
2017-05-10 15:04:50 +0200

    Split versions tests into multiple files

---
+++ src/olympia/versions/tests/test_feeds.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import mock
+from pyquery import PyQuery
+
+from olympia import amo
+from olympia.amo.tests import TestCase
+from olympia.amo.urlresolvers import reverse
+from olympia.addons.models import Addon
+from olympia.files.models import File
+from olympia.versions import feeds
+
+
+class TestFeeds(TestCase):
+    fixtures = ['addons/eula+contrib-addon', 'addons/default-to-compat']
+    rel_ns = {'atom': 'http://www.w3.org/2005/Atom'}
+
+    def setUp(self):
+        super(TestFeeds, self).setUp()
+        patcher = mock.patch.object(feeds, 'PER_PAGE', 1)
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def get_feed(self, slug, **kwargs):
+        beta = kwargs.pop('beta', False)
+        url = reverse('addons.beta-versions.rss' if beta
+                      else 'addons.versions.rss',
+                      args=[slug])
+        r = self.client.get(url, kwargs, follow=True)
+        return PyQuery(r.content, parser='xml')
+
+    def test_feed_elements_present(self):
+        """specific elements are present and reasonably well formed"""
+        doc = self.get_feed('a11730')
+        assert doc('rss channel title')[0].text == (
+            'IPv6 Google Search Version History')
+        assert doc('rss channel link')[0].text.endswith('/en-US/firefox/')
+        # assert <description> is present
+        assert len(doc('rss channel description')[0].text) > 0
+        # description doesn not contain the default object to string
+        desc_elem = doc('rss channel description')[0]
+        assert 'Content-Type:' not in desc_elem
+        # title present
+        assert len(doc('rss channel item title')[0].text) > 0
+        # link present and well formed
+        item_link = doc('rss channel item link')[0]
+        assert item_link.text.endswith('/addon/a11730/versions/20090521')
+        # guid present
+        assert len(doc('rss channel item guid')[0].text) > 0
+        # proper date format for item
+        item_pubdate = doc('rss channel item pubDate')[0]
+        assert item_pubdate.text == 'Thu, 21 May 2009 05:37:15 +0000'
+
+    def test_status_beta_without_beta_builds(self):
+        doc = self.get_feed('a11730', beta=True)
+        assert len(doc('rss channel item link')) == 0
+
+    def test_status_beta_with_beta_builds(self):
+        addon = Addon.objects.get(id=11730)
+        qs = File.objects.filter(version=addon.current_version)
+        qs.update(status=amo.STATUS_BETA)
+
+        doc = self.get_feed('a11730', beta=True)
+        item_link = doc('rss channel item link')[0]
+        assert item_link.text.endswith('/addon/a11730/versions/20090521')
+
+    def assert_page_relations(self, doc, page_relations):
+        rel = doc[0].xpath('//channel/atom:link', namespaces=self.rel_ns)
+        relations = dict((link.get('rel'), link.get('href')) for link in rel)
+        assert relations.pop('first').endswith('format:rss')
+
+        assert len(relations) == len(page_relations)
+        for rel, href in relations.iteritems():
+            page = page_relations[rel]
+            assert href.endswith('format:rss' if page == 1 else
+                                 'format:rss?page=%s' % page)
+
+    def test_feed_first_page(self):
+        """first page has the right elements and page relations"""
+        doc = self.get_feed('addon-337203', page=1)
+        assert doc('rss item title')[0].text == (
+            'Addon for DTC 1.3 - December  5, 2011')
+        self.assert_page_relations(doc, {'self': 1, 'next': 2, 'last': 4})
+
+    def test_feed_middle_page(self):
+        """a middle page has the right elements and page relations"""
+        doc = self.get_feed('addon-337203', page=2)
+        assert doc('rss item title')[0].text == (
+            'Addon for DTC 1.2 - December  5, 2011')
+        self.assert_page_relations(doc, {'previous': 1, 'self': 2, 'next': 3,
+                                         'last': 4})
+
+    def test_feed_last_page(self):
+        """last page has the right elements and page relations"""
+        doc = self.get_feed('addon-337203', page=4)
+        assert doc('rss item title')[0].text == (
+            'Addon for DTC 1.0 - December  5, 2011')
+        self.assert_page_relations(doc, {'previous': 3, 'self': 4, 'last': 4})
+
+    def test_feed_invalid_page(self):
+        """an invalid page falls back to page 1"""
+        doc = self.get_feed('addon-337203', page=5)
+        assert doc('rss item title')[0].text == (
+            'Addon for DTC 1.3 - December  5, 2011')
+
+    def test_feed_no_page(self):
+        """no page defaults to page 1"""
+        doc = self.get_feed('addon-337203')
+        assert doc('rss item title')[0].text == (
+            'Addon for DTC 1.3 - December  5, 2011')
