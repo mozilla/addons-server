dbd02d416554ccf3caec7d7fb5a5501e9db6947c
Andrew Williamson <awilliamson@mozilla.com>
2017-05-12 09:35:52 +0100

    Drop webext-permissions waffle now the feature has launched

--- src/olympia/addons/tests/test_views.py
+++ src/olympia/addons/tests/test_views.py
@@ -698,14 +698,6 @@ class TestDetailPage(TestCase):
         privacy_url = reverse('addons.privacy', args=[self.addon.slug])
         assert doc('.privacy-policy').attr('href').endswith(privacy_url)
 
-    @override_switch('webext-permissions', active=False)
-    def test_permissions_not_shown_without_waffle(self):
-        response = self.client.get(self.url)
-        doc = pq(response.content)
-        assert doc('a.webext-permissions').length == 0
-        assert doc('#webext-permissions').length == 0
-
-    @override_switch('webext-permissions', active=True)
     def test_permissions_webext(self):
         file_ = self.addon.current_version.all_files[0]
         file_.update(is_webextension=True)
@@ -734,7 +726,6 @@ class TestDetailPage(TestCase):
             u'Exchange messages with programs other than Firefox '
             u'Read and modify bookmarks')
 
-    @override_switch('webext-permissions', active=True)
     def test_permissions_webext_no_permissions(self):
         file_ = self.addon.current_version.all_files[0]
         file_.update(is_webextension=True)
@@ -746,7 +737,6 @@ class TestDetailPage(TestCase):
         # And no model dialog
         assert doc('#webext-permissions').length == 0
 
-    @override_switch('webext-permissions', active=True)
     def test_permissions_non_webext(self):
         file_ = self.addon.current_version.all_files[0]
         file_.update(is_webextension=False)
@@ -762,7 +752,6 @@ class TestDetailPage(TestCase):
             doc('#webext-permissions div.prose').text())
         assert doc('.webext-permissions-list').length == 0
 
-    @override_switch('webext-permissions', active=True)
     def test_permissions_non_extension(self):
         self.addon.update(type=amo.ADDON_THEME)
         file_ = self.addon.current_version.all_files[0]
@@ -774,7 +763,6 @@ class TestDetailPage(TestCase):
         # And no model dialog
         assert doc('#webext-permissions').length == 0
 
-    @override_switch('webext-permissions', active=True)
     def test_permissions_xss_single_url(self):
         file_ = self.addon.current_version.all_files[0]
         file_.update(is_webextension=True)
@@ -788,7 +776,6 @@ class TestDetailPage(TestCase):
         assert '<script>alert(' not in response.content
         assert '&lt;script&gt;alert(' in response.content
 
-    @override_switch('webext-permissions', active=True)
     def test_permissions_xss_multiple_url(self):
         file_ = self.addon.current_version.all_files[0]
         file_.update(is_webextension=True)
