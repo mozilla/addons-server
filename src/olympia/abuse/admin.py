2e0a745a285ab80a73425ca1739b60c5cbb8ce66
Mathieu Pillard <mpillard@mozilla.com>
2017-06-08 13:04:38 +0200

    Display delete action in abuse admin changelist, and make the detail fields readonly

--- src/olympia/abuse/admin.py
+++ src/olympia/abuse/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import AbuseReport
 
 
-admin.site.register(AbuseReport)
+class AbuseReportAdmin(admin.ModelAdmin):
+    raw_id_fields = ('addon', 'user', 'reporter')
+    readonly_fields = ('ip_address', 'message', 'created', 'addon', 'user',
+                       'reporter')
+    list_display = ('reporter', 'ip_address', 'type', 'target', 'message',
+                    'created')
+    actions = ('delete_selected',)
+
+
+admin.site.register(AbuseReport, AbuseReportAdmin)
