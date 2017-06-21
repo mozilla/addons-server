1eb9b5a99940564753a483445d4066a339e1d068
Mathieu Pillard <diox@users.noreply.github.com>
2017-06-21 13:11:12 +0200

    Delete old FileUpload rows and their files after some time in gc cron (#5721)
    Delete old FileUpload rows and their files after some time in gc cron

--- src/olympia/amo/tests/test_cron.py
+++ src/olympia/amo/tests/test_cron.py
@@ -1,5 +1,4 @@
 import datetime
-import unittest
 from datetime import timedelta
 
 from django.core.signals import request_finished, request_started
@@ -7,9 +6,11 @@ from django.core.signals import request_finished, request_started
 import mock
 import pytest
 
+from olympia.amo.cron import gc
+from olympia.amo.tests import TestCase
 from olympia.amo.celery import app, task
 from olympia.amo.utils import utc_millesecs_from_epoch
-
+from olympia.files.models import FileUpload
 
 fake_task_func = mock.Mock()
 
@@ -19,7 +20,7 @@ def fake_task(**kw):
     fake_task_func()
 
 
-class TestTaskTiming(unittest.TestCase):
+class TestTaskTiming(TestCase):
 
     def setUp(self):
         patch = mock.patch('olympia.amo.celery.cache')
@@ -61,7 +62,7 @@ class TestTaskTiming(unittest.TestCase):
 
 @pytest.mark.skipif('PostRequestTask' not in unicode(app.task_cls),
                     reason='requires PostRequestTask to be active')
-class TestTaskQueued(unittest.TestCase):
+class TestTaskQueued(TestCase):
     """Test that our celery tasks are queued to be triggered only when the
     request is finished, thanks to django-post-request-task."""
     def setUp(self):
@@ -90,3 +91,45 @@ class TestTaskQueued(unittest.TestCase):
         assert fake_task_func.call_count == 0
         request_finished.send_robust(sender=self)
         assert fake_task_func.call_count == 1
+
+
+@mock.patch('olympia.amo.cron.storage')
+class TestGC(TestCase):
+    def test_file_uploads_deletion(self, storage_mock):
+        fu_new = FileUpload.objects.create(path='/tmp/new', name='new')
+        fu_new.update(created=self.days_ago(178))
+        fu_old = FileUpload.objects.create(path='/tmp/old', name='old')
+        fu_old.update(created=self.days_ago(181))
+
+        gc()
+
+        assert FileUpload.objects.count() == 1
+        assert storage_mock.delete.call_count == 1
+        assert storage_mock.delete.call_args[0][0] == fu_old.path
+
+    def test_file_uploads_deletion_no_path_somehow(self, storage_mock):
+        fu_old = FileUpload.objects.create(path='', name='foo')
+        fu_old.update(created=self.days_ago(181))
+
+        gc()
+
+        assert FileUpload.objects.count() == 0  # FileUpload was deleted.
+        assert storage_mock.delete.call_count == 0  # No path to delete.
+
+    def test_file_uploads_deletion_oserror(self, storage_mock):
+        fu_older = FileUpload.objects.create(path='/tmp/older', name='older')
+        fu_older.update(created=self.days_ago(300))
+        fu_old = FileUpload.objects.create(path='/tmp/old', name='old')
+        fu_old.update(created=self.days_ago(181))
+
+        storage_mock.delete.side_effect = OSError
+
+        gc()
+
+        # Even though delete() caused a OSError, we still deleted the
+        # FileUploads rows, and tried to delete each corresponding path on
+        # the filesystem.
+        assert FileUpload.objects.count() == 0
+        assert storage_mock.delete.call_count == 2
+        assert storage_mock.delete.call_args_list[0][0][0] == fu_older.path
+        assert storage_mock.delete.call_args_list[1][0][0] == fu_old.path
