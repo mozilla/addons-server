0a2e626eeb55ac15f24eb973e5033f6a7ff896bd
Christopher Grebs <cg@webshox.org>
2017-06-22 10:47:59 +0800

    Drop the foreign key first before dropping the index.

--- src/olympia/migrations/952-correct-index-theme-user-counts.sql
+++ src/olympia/migrations/952-correct-index-theme-user-counts.sql
@@ -1,4 +1,20 @@
--- Drop old index first, if it exists.
+-- Drop foreign key first so that we can drop the index next.
+SET @FOREIGN_KEY_ADDON_ID := (
+    SELECT CONSTRAINT_NAME FROM information_schema.KEY_COLUMN_USAGE WHERE
+        TABLE_SCHEMA = (SELECT DATABASE()) AND
+        TABLE_NAME = 'theme_user_counts' AND
+        COLUMN_NAME = 'addon_id');
+
+SET @QUERY_DROP_FOREIGN_KEY_ADDON_ID = IF(
+    @FOREIGN_KEY_ADDON_ID IS NOT NULL,
+    CONCAT('ALTER TABLE theme_user_counts DROP FOREIGN KEY ', @FOREIGN_KEY_ADDON_ID, ';'),
+    'SELECT 0;');
+
+PREPARE stmt FROM @QUERY_DROP_FOREIGN_KEY_ADDON_ID;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+-- Now drop old index, if it exists.
 
 -- Set to index_name (addon_date_idx) or NULL if it doesn't exist
 SET @KEY_ADDON_DATE_IDX := (
@@ -20,6 +36,9 @@ SET @QUERY_DROP_KEY_ADDON_DATE_IDX = IF(
 PREPARE stmt FROM @QUERY_DROP_KEY_ADDON_DATE_IDX;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
+
+-- First create the foreign key that we dropped earlier
+ALTER TABLE `theme_user_counts` ADD CONSTRAINT `addon_id_refs_id_ac19f783` FOREIGN KEY (`addon_id`) REFERENCES `addons` (`id`)
 
 -- Now create our new, proper index.
 ALTER TABLE `theme_user_counts` ADD CONSTRAINT `theme_user_counts_date_cc9034dde90789f_uniq` UNIQUE (`date`, `addon_id`);
