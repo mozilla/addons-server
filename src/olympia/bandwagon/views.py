mozilla/addons-server

commit 218c0c75dffbfdd161ccdee3180bd0ec54caa4ba

Fix some django deprecation warnings (#5811)
* Replace SortedDict with OrderedDict

* Replace get_field_by_name() by get_field()

* Consider warnings as errors to catch DeprecationWarnings

* Upgrade pytest django, since it generates a lot of deprecation warnings

* Ignore DeprecationWarning from django-csp

* Replace old option parsing syntax in management commands

* Replace request.REQUEST with request.POST

* Replace custom sql to create indexes by model options

* Replace get_all_field_names() with get_fields()

* don't keep this

* Try removing the create-db, we're having errors at teardown

* Fix commands args parsing

* Re-consider DeprecationWarnings as errors

* Remove usage of Context()

* Fix DeprecationWarning caused by paypal PaypalError self.message

* Fix linter test using exception.message

* Fix mock to prevent passing a callable to a queryset filter

* Remove reference to non-existent fixture

* django.forms.util module has been renamed. Use django.forms.utils instead

* (WIP) Support zh-hant & zh-hans instead of zh-TW and zh-CN, respectively.

* Don't pass callable to queryset

* ignore jwt deprecation warnings

* Don't use TestCase.urls anymore

* test against exception message directly

* Simplify nested context managers

* Restore this usage of Context(), it's still valid

* Restore setup.cfg

* Revert "(WIP) Support zh-hant & zh-hans instead of zh-TW and zh-CN, respectively."

This reverts commit 7d82b59850e74e8202eb55943fbea9370f97100f.

* fix flake8

src/olympia/bandwagon/views.py
@@ -381,7 +381,7 @@ def ajax_new(request):
 
     if request.method == 'POST' and form.is_valid():
         collection = form.save()
-        addon_id = request.REQUEST['addon_id']
+        addon_id = request.POST['addon_id']
         collection.add_addon(Addon.objects.get(pk=addon_id))
         log.info('Created collection %s' % collection.id)
         return http.HttpResponseRedirect(reverse('collections.ajax_list') +

src/olympia/activity/views.py
@@ -33,7 +33,7 @@ class VersionReviewNotesViewSet(AddonChildMixin, ListModelMixin,
     serializer_class = ActivityLogSerializer
 
     def get_queryset(self):
-        alog = ActivityLog.objects.for_version(self.get_version_object)
+        alog = ActivityLog.objects.for_version(self.get_version_object())
         return alog.filter(action__in=amo.LOG_REVIEW_QUEUE_DEVELOPER)
 
     def get_addon_object(self):

src/olympia/compat/views.py
@@ -35,7 +35,7 @@ def incoming(request):
 
     # Build up a new report.
     report = CompatReport(client_ip=request.META.get('REMOTE_ADDR', ''))
-    fields = CompatReport._meta.get_all_field_names()
+    fields = [field.name for field in CompatReport._meta.get_fields()]
     for key, value in data:
         if key in fields:
             setattr(report, key, value)

src/olympia/devhub/views.py
@@ -13,7 +13,7 @@ from django.db import transaction
 from django.db.models import Count
 from django.forms import Form
 from django.shortcuts import get_object_or_404, redirect
-from django.template import Context, loader
+from django.template import loader
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
@@ -436,8 +436,8 @@ def ownership(request, addon_id, addon):
         t = loader.get_template(
             'users/email/{part}.ltxt'.format(part=template_part))
         send_mail(title,
-                  t.render(Context({'author': author, 'addon': addon,
-                                    'site_url': settings.SITE_URL})),
+                  t.render({'author': author, 'addon': addon,
+                            'site_url': settings.SITE_URL}),
                   None, recipients, use_deny_list=False)
 
     if request.method == 'POST' and all([form.is_valid() for form in fs]):
@@ -1782,7 +1782,7 @@ def send_key_change_email(to_email, key):
     url = absolutify(reverse('devhub.api_key'))
     send_mail(
         ugettext('New API key created'),
-        template.render(Context({'key': key, 'url': url})),
+        template.render({'key': key, 'url': url}),
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[to_email],
     )
@@ -1793,7 +1793,7 @@ def send_key_revoked_email(to_email, key):
     url = absolutify(reverse('devhub.api_key'))
     send_mail(
         ugettext('API key revoked'),
-        template.render(Context({'key': key, 'url': url})),
+        template.render({'key': key, 'url': url}),
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[to_email],
     )

src/olympia/stats/views.py
@@ -4,6 +4,7 @@ import itertools
 import json
 import os
 import time
+from collections import OrderedDict
 from datetime import date, timedelta, datetime
 
 from django import http
@@ -15,7 +16,6 @@ from django.db.models import Avg, Count, Q, Sum
 from django.db.transaction import non_atomic_requests
 from django.shortcuts import get_object_or_404
 from django.utils.cache import add_never_cache_headers, patch_cache_control
-from django.utils.datastructures import SortedDict
 
 from dateutil.parser import parse
 from product_details import product_details
@@ -510,8 +510,8 @@ def _site_query(period, start, end, field=None, request=None):
         cursor.execute(sql, [start, end] + _KEYS.keys())
 
         # Process the results into a format that is friendly for render_*.
-        default = dict([(k, 0) for k in _CACHED_KEYS])
-        result = SortedDict()
+        default = {k: 0 for k in _CACHED_KEYS}
+        result = OrderedDict()
         for name, date_, count in cursor.fetchall():
             date_ = date_.strftime('%Y-%m-%d')
             if date_ not in result:

src/olympia/activity/management/commands/repudiate_token.py
@@ -1,5 +1,3 @@
-from optparse import make_option
-
 from django.core.management.base import BaseCommand, CommandError
 
 import olympia.core.logger
@@ -9,21 +7,24 @@ log = olympia.core.logger.getLogger('z.amo.activity')
 
 
 class Command(BaseCommand):
-    args = u'<token_uuid token_uuid ...>'
     help = u'Force expire a list of Activity Email tokens.'
 
-    option_list = BaseCommand.option_list + (
-        make_option('--version_id', action='store', type='long',
-                    dest='version_id',
-                    help='Expire all tokens on this version.'),
-    )
+    def add_arguments(self, parser):
+        """Handle command arguments."""
+        parser.add_argument('token_uuid', nargs='*')
+        parser.add_argument(
+            '--version_id', action='store', type=long,
+            dest='version_id',
+            help='Expire all tokens on this version.')
 
     def handle(self, *args, **options):
         version_pk = options.get('version_id')
-        if len(args) > 0:
+        token_uuids = options.get('token_uuid')
+        if token_uuids:
             done = [t.expire() for t in ActivityLogToken.objects.filter(
-                uuid__in=args)]
-            log.info(u'%s tokens (%s) expired' % (len(done), ','.join(args)))
+                uuid__in=token_uuids)]
+            log.info(
+                u'%s tokens (%s) expired' % (len(done), ','.join(token_uuids)))
             if version_pk:
                 print 'Warning: --version_id ignored as tokens provided too'
         elif version_pk:

src/olympia/activity/utils.py
@@ -2,7 +2,7 @@ import datetime
 import re
 
 from django.conf import settings
-from django.template import Context, loader
+from django.template import loader
 from django.utils import translation
 
 from email_reply_parser import EmailReplyParser
@@ -254,18 +254,15 @@ def log_and_notify(action, comments, note_creator, version, perm_setting=None,
     from_name = unicode(note_creator.name).replace('"', '\"')
     from_email = '"%s" <%s>' % (from_name, NOTIFICATIONS_FROM_EMAIL)
     send_activity_mail(
-        subject, template.render(Context(
-            author_context_dict, autoescape=False)),
+        subject, template.render(author_context_dict),
         version, addon_authors, from_email, perm_setting)
 
     send_activity_mail(
-        subject, template.render(Context(
-            reviewer_context_dict, autoescape=False)),
+        subject, template.render(reviewer_context_dict),
         version, reviewers, from_email, perm_setting)
 
     send_activity_mail(
-        subject, template.render(Context(
-            staff_cc_context_dict, autoescape=False)),
+        subject, template.render(staff_cc_context_dict),
         version, staff_cc, from_email, perm_setting)
 
     if action == amo.LOG.DEVELOPER_REPLY_VERSION:
@@ -328,7 +325,7 @@ def bounce_mail(message, reason):
         return
 
     body = (loader.get_template('activity/emails/bounce.txt').
-            render(Context({'reason': reason, 'SITE_URL': settings.SITE_URL})))
+            render({'reason': reason, 'SITE_URL': settings.SITE_URL}))
     send_mail(
         'Re: %s' % message.get('Subject', 'your email to us'),
         body,

src/olympia/addons/management/commands/approve_addons.py
@@ -11,21 +11,19 @@ log = olympia.core.logger.getLogger('z.addons')
 
 
 class Command(BaseCommand):
-    args = u'<addon_guid addon_guid ...>'
     help = u'Approve a list of add-ons, given their GUIDs.'
 
-    def handle(self, *args, **options):
-        if len(args) == 0:  # No GUID provided?
-            raise CommandError(
-                u'Please provide at least one add-on guid to approve.')
+    def add_arguments(self, parser):
+        parser.add_argument('addon_guid', nargs='+')
 
+    def handle(self, *args, **options):
         confirm = raw_input(
             u'Are you sure you want to bulk approve and sign all those {0} '
             u'addons? (yes/no)'.format(len(args)))
         if confirm != 'yes':
             raise CommandError(u'Aborted.')
 
-        for chunk in chunked(args, 100):
+        for chunk in chunked(options['addon_guid'], 100):
             files = get_files(chunk)
             log.info(u'Bulk approving chunk of %s files', len(files))
 

commit 0f35449ab0fbe72fe0e61732b4aeaee9fb5bb45d

Send addon_id as POST for ajax new collection create (#5879)
* Re-add support for addon_id support as GET argument.

This is a regression introduced in 218c0c75dff

Fixes #5854

* Fix this the right way, make javascript behave and send the addon_id as
POST.

* Remove test

src/olympia/bandwagon/templates/bandwagon/ajax_new.html
@@ -6,7 +6,7 @@
       {{ field(form.name, _('Name:')) }}
       {% set slug_base = url('collections.user', user.username, add_prefix=False) %}
       <p id="collection-form-slug">
-        {{ form.errors['slug'] }}
+        {% if form.errors.slug %}{{ form.errors.slug }}{% endif %}
         <label>{{ form.slug.label }}</label>
         <span id="slug_edit" class="edit_with_prefix edit_initially_hidden">
           <span>{{ slug_base }}</span>{{ form.slug }}

src/olympia/bandwagon/views.py
@@ -320,7 +320,7 @@ def collection_vote(request, username, slug, direction):
 
 
 def initial_data_from_request(request):
-    return dict(author=request.user, application=request.APP.id)
+    return {'author': request.user, 'application': request.APP.id}
 
 
 def collection_message(request, collection, option):
@@ -376,12 +376,12 @@ def add(request):
 def ajax_new(request):
     form = forms.CollectionForm(
         request.POST or None,
-        initial={'author': request.user, 'application': request.APP.id},
-    )
+        initial=initial_data_from_request(request))
 
     if request.method == 'POST' and form.is_valid():
         collection = form.save()
         addon_id = request.POST['addon_id']
+
         collection.add_addon(Addon.objects.get(pk=addon_id))
         log.info('Created collection %s' % collection.id)
         return http.HttpResponseRedirect(reverse('collections.ajax_list') +

static/js/zamboni/collections.js
@@ -514,14 +514,21 @@ $(document).ready(function () {
                 var tgt = $(this);
                 if (ct.hasClass('ajax-loading')) return;
                 ct.addClass('ajax-loading');
-                form_data = $('#add-to-collection form').serialize();
-                $.post(form_url + '?addon_id=' + addon_id, form_data, renderList, 'html');
+
+                var form_data = {};
+                $.each($('#add-to-collection form').serializeArray(), function() {
+                    form_data[this.name] = this.value;
+                })
+
+                form_data['addon_id'] = addon_id;
+
+                $.post(form_url, form_data, renderList, 'html');
             };
 
             var handleNew = function(e) {
                 e.preventDefault();
                 var tgt = $(this);
-                $.get(form_url, {'addon_id': addon_id}, function(d) {
+                $.get(form_url, function(d) {
                     $widget.html(d);
                     $widget.setWidth(410);
                     $widget.setPos(ct);