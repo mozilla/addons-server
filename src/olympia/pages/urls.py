2cc40c07f4b4316b8af00c1996e086caa9058285
Kamyar Ardekani <kamyar.ardekani@gmail.com>
2017-07-11 03:43:54 -0700

    Web Health Study consent (bug 1378555) fixes #5875 (#5876)
    * Web Health Study consent (bug 1378555) fixes #5875 
    
    * better accessibility

--- src/olympia/pages/urls.py
+++ src/olympia/pages/urls.py
@@ -63,6 +63,9 @@ urlpatterns = [
     url('^shield_study_12$',
         TemplateView.as_view(template_name='pages/shield_study_12.html'),
         name='pages.shield_study_12'),
+    url('^shield_study_13$',
+        TemplateView.as_view(template_name='pages/shield_study_13.html'),
+        name='pages.shield_study_13'),
 
     url('^pages/compatibility_firstrun$',
         lambda r: perma_redirect(reverse('pages.acr_firstrun'))),
