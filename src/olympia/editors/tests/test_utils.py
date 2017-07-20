09f7774c38a6e309ad4c0a1da280ae0db97bede2
Andreas Wagner <mail@andreaswagner.org>
2017-07-20 19:02:43 +0200

    Remove action from activity email to make email threading work (#5967)
    Remove action from activity email to make email threading work

--- src/olympia/editors/tests/test_utils.py
+++ src/olympia/editors/tests/test_utils.py
@@ -438,8 +438,7 @@ class TestReviewHelper(TestCase):
         assert not self.version.has_info_request
 
         assert len(mail.outbox) == 1
-        assert mail.outbox[0].subject == '%s %s' % (
-            self.preamble, amo.LOG.REVIEWER_REPLY_VERSION.short)
+        assert mail.outbox[0].subject == self.preamble
 
         assert self.check_log_count(amo.LOG.REVIEWER_REPLY_VERSION.id) == 1
 
@@ -451,8 +450,7 @@ class TestReviewHelper(TestCase):
         assert self.version.has_info_request
 
         assert len(mail.outbox) == 1
-        assert mail.outbox[0].subject == '%s %s' % (
-            self.preamble, amo.LOG.REQUEST_INFORMATION.short)
+        assert mail.outbox[0].subject == self.preamble
 
         assert self.check_log_count(amo.LOG.REQUEST_INFORMATION.id) == 1
 
