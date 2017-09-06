e9afcc5b4542e2b50fefadee7f0a85e2ec1ddf2b
Andrew Williamson <awilliamson@mozilla.com>
2017-09-06 12:33:42 +0800

    Update 970-disco-recommendations-waffle.sql

--- src/olympia/migrations/970-disco-recommendations-waffle.sql
+++ src/olympia/migrations/970-disco-recommendations-waffle.sql
@@ -1,4 +1,4 @@
-INSERT INTO waffle_switch (name, active, note)
-    VALUES ('disco-recommendations', 0,
+INSERT INTO waffle_switch (name, active, created, modified, note)
+    VALUES ('disco-recommendations', 0, NOW(), NOW(),
             'Enable switch to include recommendations from the taar service in '
             'the discovery api responses, if telemetry-client-id provided.');
