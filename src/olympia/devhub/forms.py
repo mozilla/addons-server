c3de24b9dbd31c9a9d39d375c3a9214a481ba7b6
Mathieu Pillard <mpillard@mozilla.com>
2017-09-11 11:56:51 +0200

    Revert "Rename Distribution Agreement to Developer Agreement when post-review is on"
    This reverts commit 5ee9ceaba06e6c8ad92b1ae36a8e0357f740b367.
    
    It turns out we don't want to call this "Developer Agreement" after all.

--- src/olympia/devhub/forms.py
+++ src/olympia/devhub/forms.py
@@ -873,5 +873,5 @@ class DistributionChoiceForm(happyforms.Form):
 
 
 class AgreementForm(happyforms.Form):
-    developer_agreement = forms.BooleanField()
+    distribution_agreement = forms.BooleanField()
     review_policy = forms.BooleanField()
