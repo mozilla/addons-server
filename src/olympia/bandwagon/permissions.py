ad5f68f06ec9cd5207da7fb8d304a389820f598f
Andrew Williamson <awilliamson@mozilla.com>
2017-09-12 14:29:29 +0100

    add contributor support to API

--- src/olympia/bandwagon/permissions.py
+++ src/olympia/bandwagon/permissions.py
@@ -8,3 +8,14 @@ class AllowCollectionAuthor(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return self.has_permission(request, view)
+
+
+class AllowCollectionContributor(BasePermission):
+    """Allow a contributor of a collection to do stuff.  Be careful how this
+    is used - """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated()
+
+    def has_object_permission(self, request, view, obj):
+        return obj in request.user.collections_publishable.all()
