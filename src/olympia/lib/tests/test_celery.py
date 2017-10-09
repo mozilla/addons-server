9fdf17388f59b80bfac23c68a980d4b0b169d57f
Christopher Grebs <cg@webshox.org>
2017-10-09 15:12:24 +0200

    Fix lib/celery_tests

--- src/olympia/lib/tests/test_celery.py
+++ src/olympia/lib/tests/test_celery.py
@@ -2,12 +2,14 @@ from django.conf import settings
 
 
 def test_celery_routes_in_queues():
-    queues_in_queues = set([q.name for q in settings.CELERY_QUEUES])
+    queues_in_queues = set([q.name for q in settings.CELERY_TASK_QUEUES])
+
     # check the default queue is defined in CELERY_QUEUES
-    assert settings.CELERY_DEFAULT_QUEUE in queues_in_queues
+    assert settings.CELERY_TASK_DEFAULT_QUEUE in queues_in_queues
+
     # then remove it as it won't be in CELERY_ROUTES
-    queues_in_queues.remove(settings.CELERY_DEFAULT_QUEUE)
+    queues_in_queues.remove(settings.CELERY_TASK_DEFAULT_QUEUE)
 
     queues_in_routes = set(
-        [c['queue'] for c in settings.CELERY_ROUTES.values()])
+        [c['queue'] for c in settings.CELERY_TASK_ROUTES.values()])
     assert queues_in_queues == queues_in_routes
