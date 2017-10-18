527ca646e1bd52e757b8a2f62f353355e8bfbc67
Mathieu Pillard <mpillard@mozilla.com>
2017-10-18 17:23:49 +0200

    Readjust position of the "AMO is getting a new look" banner
    - border-bottom on header needs to go for the banner to be
      seamlessly displayed below
    - scrollback hack is replaced with a overflow-x: hidden on the
      body because it causes a slight misalignment that is difficult
      to overcome without additional markup (which differs depending
      on which platform you're using, which does not help...)
    - padding/line-height adjusted per mocks

--- static/js/zamboni/global.js
+++ static/js/zamboni/global.js
@@ -580,34 +580,6 @@ $(document).on('click', '.mobile-link', function(e) {
     window.location.reload();
 });
 
-// See: http://stackoverflow.com/questions/13382516/getting-scroll-bar-width-using-javascript
-// Used to remove the width of scrollbars from `.header-bg`
-
-function getScrollbarWidth() {
-    var outer = document.createElement('div');
-    outer.style.visibility = 'hidden';
-    outer.style.width = '100px';
-    outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
-
-    document.body.appendChild(outer);
-
-    var widthNoScroll = outer.offsetWidth;
-    // force scrollbars
-    outer.style.overflow = 'scroll';
-
-    // add innerdiv
-    var inner = document.createElement('div');
-    inner.style.width = '100%';
-    outer.appendChild(inner);
-
-    var widthWithScroll = inner.offsetWidth;
-
-    // remove divs
-    outer.parentNode.removeChild(outer);
-
-    return widthNoScroll - widthWithScroll;
-}
-
 $(function() {
     "use strict";
 
