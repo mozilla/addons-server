aed8ec18e0499f5facf9f8afd8149e8e70a4ec4f
Christopher Grebs <cg@webshox.org>
2017-11-08 16:02:50 +0100

    Try setting globals

--- scripts/start-autograph.sh
+++ scripts/start-autograph.sh
@@ -5,9 +5,7 @@ go get go.mozilla.org/autograph
 cd $GOPATH/src/go.mozilla.org/autograph
 
 # Modify the default port to something free
-sed -i '0,/^\([[:space:]]*listen: *\).*/s//\1"0.0.0.0:5500"/;' autograph.yaml
+sed -i '0,/^\([[:space:]]*listen: *\).*/s//\1"$AUTOGRAPH_SERVER_URL"/;' autograph.yaml
 
 # Start autograph in background
 $GOPATH/bin/autograph -c autograph.yaml &>/dev/null &
-
-export AUTOGRAPH_SERVER_URL="http://localhost:5500"
