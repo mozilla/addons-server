a305d3a8d46e1907db96d5ed27970291b5ff88c8
Christopher Grebs <cg@webshox.org>
2017-11-09 07:33:37 +0100

    more go/autograph startup fixes

--- scripts/start-autograph.sh
+++ scripts/start-autograph.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Install autograph
+
+echo "installing autograph + dependencies"
 go get go.mozilla.org/autograph
 cd $GOPATH/src/go.mozilla.org/autograph
 
 # Modify the default port to something free
-sed -i '0,/^\([[:space:]]*listen: *\).*/s//\1"$AUTOGRAPH_SERVER_URL"/;' autograph.yaml
+sed -i "0,/^\([[:space:]]*listen: *\).*/s//\1\"${AUTOGRAPH_SERVER_URL}\"/;" autograph.yaml
 
 # Start autograph in background
+echo "start autograph in background"
 $GOPATH/bin/autograph -c autograph.yaml 2>&1 &
