e37f26dab7104aa5bee36da24d0999fdc38cfbf3
Christopher Grebs <cg@webshox.org>
2017-11-09 09:30:54 +0100

    Fix sed escaping, less complicated regex

--- scripts/start-autograph.sh
+++ scripts/start-autograph.sh
@@ -7,7 +7,7 @@ go get go.mozilla.org/autograph
 cd $GOPATH/src/go.mozilla.org/autograph
 
 # Modify the default port to something free
-sed -i "0,/^\([[:space:]]*listen: *\).*/s//\1\"${AUTOGRAPH_SERVER_URL}\"/;" autograph.yaml
+sed -i "s@http://localhost:8000@$AUTOGRAPH_SERVER_URL@" autograph.yaml
 
 # Start autograph in background
 echo "start autograph in background"
