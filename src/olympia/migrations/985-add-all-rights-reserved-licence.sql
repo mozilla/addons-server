96da10262d4262c8a127bb2d99855435bdd83166
Hemanth Kumar Veeranki <harry-7@users.noreply.github.com>
2017-11-14 22:09:16 +0530

    Add All rights reserved License (#6844)

---
+++ src/olympia/migrations/985-add-all-rights-reserved-licence.sql
@@ -0,0 +1,8 @@
+/* See constants/licenses.py for names for these licenses. */
+INSERT INTO `licenses`
+    (`name`, `builtin`, `on_form`, `creative_commons`, `some_rights`, `icons`,
+        `url`, `created`, `modified`)
+VALUES
+    (NULL, 18, true, false, false, 'copyr',
+        NULL, NOW(), NOW())                                                 /* LICENSE_COPYRIGHT_AR */
+;
