d471c254bfbfa6e9d6cacc6a7bc58fa1c3192abc
Andrew Williamson <awilliamson@mozilla.com>
2017-11-21 17:30:56 +0000

    fix migraton 988 - charity_id

--- src/olympia/migrations/988-drop-addon-paypal-fields.sql
+++ src/olympia/migrations/988-drop-addon-paypal-fields.sql
@@ -3,7 +3,7 @@ ALTER TABLE `addons`
     DROP COLUMN `the_future`,
     DROP COLUMN `wants_contributions`,
     DROP COLUMN `paypal_id`,
-    DROP COLUMN `charity`,
+    DROP COLUMN `charity_id`,
     DROP COLUMN `suggested_amount`,
     DROP COLUMN `annoying`,
     DROP COLUMN `enable_thankyou`,
