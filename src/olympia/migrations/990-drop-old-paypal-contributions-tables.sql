4794d6748aef8f212d40007d4762b7f32724dbbc
Andrew Williamson <awilliamson@mozilla.com>
2017-11-23 14:01:20 +0000

    delete `thanks` user_notifcations from db; bring back deleted py migrations

--- src/olympia/migrations/990-drop-old-paypal-contributions-tables.sql
+++ src/olympia/migrations/990-drop-old-paypal-contributions-tables.sql
@@ -1,1 +1,4 @@
 DROP TABLE IF EXISTS `stats_contributions`, `charities`;
+
+/* notification_id = 2 was the "thanks" contribution notification" */
+DELETE FROM TABLE `users_notifications` WHERE `notification_id` = 2;
