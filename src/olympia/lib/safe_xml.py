613b185d0b77d26104b07a419d689b775edecdaf
Christopher Grebs <cg@webshox.org>
2017-12-08 17:09:09 +0100

    Ignore safe_xml file

--- src/olympia/lib/safe_xml.py
+++ src/olympia/lib/safe_xml.py
@@ -3,16 +3,6 @@ Monkey patch and defuse all stdlib xml packages and lxml.
 """
 import sys
 
-from xml.sax.handler import feature_external_ges, feature_external_pes  # noqa
-
-import lxml  # noqa
-import lxml.etree  # noqa
-
-from defusedxml import defuse_stdlib  # noqa
-from rdflib.plugins.parsers import rdfxml  # noqa
-
-from olympia.lib import safe_lxml_etree  # noqa
-
 
 patched_modules = (
     'lxml', 'ElementTree', 'minidom', 'pulldom', 'rdflib',
@@ -25,10 +15,16 @@ if any(module in sys.modules for module in patched_modules):
         'this monkey patch was not applied early enough. {0}'.format(
             existing_modules))
 
+from defusedxml import defuse_stdlib  # noqa isort:skip
 
 defuse_stdlib()
 
+import lxml  # noqa isort:skip
+import lxml.etree  # noqa isort:skip
+from rdflib.plugins.parsers import rdfxml  # noqa isort:skip
+from xml.sax.handler import feature_external_ges, feature_external_pes  # noqa isort:skip
 
+from olympia.lib import safe_lxml_etree  # noqa isort:skip
 
 
 lxml.etree = safe_lxml_etree
