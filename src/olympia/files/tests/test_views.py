ca7f9b194f4cae51706c75de6bc5b1008c6fb9ba
Christopher Grebs <cg@webshox.org>
2017-12-12 15:03:33 +0100

    Correctly quote etags according to specification.
    Django 1.11 partially expects this, or rather does this now correctly
    all over the place so we should use their official helpers too.

--- src/olympia/files/tests/test_views.py
+++ src/olympia/files/tests/test_views.py
@@ -6,7 +6,7 @@ import urlparse
 
 from django.conf import settings
 from django.core.cache import cache
-from django.utils.http import http_date
+from django.utils.http import http_date, quote_etag
 from django.test.utils import override_settings
 
 import pytest
@@ -168,7 +168,7 @@ class FilesBase(object):
         self.file_viewer.extract()
         self.file_viewer.select('install.js')
         obj = getattr(self.file_viewer, 'left', self.file_viewer)
-        etag = obj.selected.get('sha256')
+        etag = quote_etag(obj.selected.get('sha256'))
         res = self.client.get(self.file_url('install.js'),
                               HTTP_IF_NONE_MATCH=etag)
         assert res.status_code == 304
