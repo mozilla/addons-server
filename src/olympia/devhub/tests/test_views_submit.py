9db875d05f11ab8b34705e1b1f8f6593bced70f0
Mathieu Pillard <mpillard@mozilla.com>
2018-02-15 13:31:03 +0100

    Update template for "successful submission to AMO" notification

--- src/olympia/devhub/tests/test_views_submit.py
+++ src/olympia/devhub/tests/test_views_submit.py
@@ -871,6 +871,7 @@ class TestAddonSubmitFinish(TestSubmitBase):
     def test_welcome_email_for_newbies(self, send_welcome_email_mock):
         self.client.get(self.url)
         context = {
+            'addon_name': 'Delicious Bookmarks',
             'app': unicode(amo.FIREFOX.pretty),
             'detail_url': 'http://b.ro/en-US/firefox/addon/a3615/',
             'version_url': 'http://b.ro/en-US/developers/addon/a3615/versions',
@@ -887,6 +888,7 @@ class TestAddonSubmitFinish(TestSubmitBase):
         new_addon.addonuser_set.create(user=self.addon.authors.all()[0])
         self.client.get(self.url)
         context = {
+            'addon_name': 'Delicious Bookmarks',
             'app': unicode(amo.FIREFOX.pretty),
             'detail_url': 'http://b.ro/en-US/firefox/addon/a3615/',
             'version_url': 'http://b.ro/en-US/developers/addon/a3615/versions',
@@ -906,6 +908,7 @@ class TestAddonSubmitFinish(TestSubmitBase):
         new_addon.addonuser_set.create(user=self.addon.authors.all()[0])
         self.client.get(self.url)
         context = {
+            'addon_name': 'Delicious Bookmarks',
             'app': unicode(amo.FIREFOX.pretty),
             'detail_url': 'http://b.ro/en-US/firefox/addon/a3615/',
             'version_url': 'http://b.ro/en-US/developers/addon/a3615/versions',
