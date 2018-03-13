cfeadcd3d74704e5bf3a178bd9a5eafc2ad89fed
Andrew Williamson <awilliamson@mozilla.com>
2018-03-13 22:15:40 +0800

    version_previews.version_id fk to UNSIGNED

--- src/olympia/migrations/1002-version-preview.sql
+++ src/olympia/migrations/1002-version-preview.sql
@@ -2,7 +2,7 @@ CREATE TABLE `version_previews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime(6) NOT NULL,
   `modified` datetime(6) NOT NULL,
-  `version_id` int(11) NOT NULL,
+  `version_id` int(11) UNSIGNED NOT NULL,
   `sizes` longtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `version_previews_version_id_fk_versions_id` (`version_id`),
