a1940381aa3bb220d233990ef987b93875250c1d
Andrew Williamson <awilliamson@mozilla.com>
2018-03-13 22:28:09 +0800

    version_previews.version_id fk to UNSIGNED (#7785)

--- src/olympia/migrations/1002-version-preview.sql
+++ src/olympia/migrations/1002-version-preview.sql
@@ -1,8 +1,9 @@
+-- Note: if the migration fails for you locally, remove the 'unsigned' next to version_id below.
 CREATE TABLE `version_previews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime(6) NOT NULL,
   `modified` datetime(6) NOT NULL,
-  `version_id` int(11) NOT NULL,
+  `version_id` int(11) UNSIGNED NOT NULL,
   `sizes` longtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `version_previews_version_id_fk_versions_id` (`version_id`),
