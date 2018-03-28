b4f78bde81f62ce9f50f51177a1da5b36fe1852b
Mathieu Pillard <mpillard@mozilla.com>
2018-03-28 13:52:48 +0200

    Rename AdminTools:View in migration

--- src/olympia/migrations/1007-add-content-curators.sql
+++ src/olympia/migrations/1007-add-content-curators.sql
@@ -1,2 +1,2 @@
-INSERT INTO groups (name, rules, created, modified)
-VALUES ('Content Curators', 'Admin:Curation', NOW(), NOW());
+INSERT INTO groups (name, rules, created, modified) VALUES ('Content Curators', 'Admin:Curation', NOW(), NOW());
+UPDATE groups SET rules=REPLACE(rules, 'AdminTools:View', 'Admin:Tools') WHERE name='Staff';
