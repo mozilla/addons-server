09b490fee98d63d25082eb09a284f134de5caf0b
Benjamin Forehand Jr <bennyjr169@gmail.com>
2018-04-18 19:13:59 -0400

    Fix typo.

--- scripts/ui-test-hostname-setup.sh
+++ scripts/ui-test-hostname-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-UI_IP="`docker inspect addonsserver_addons-frontend_1 | grep "IPAddress" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`"
+UI_IP="`docker inspect addons-server_addons-frontend_1 | grep "IPAddress" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`"
 echo $UI_IP
 HOSTS_LINE="$UI_IP\tlocalhost"
 docker-compose exec --user root selenium-firefox sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts"
