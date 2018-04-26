823136c9ea7786a1d56b33961bf7f8caaaf6dd0b
Christopher Grebs <cg@webshox.org>
2018-04-26 10:44:01 +0200

    Fix tests

--- src/olympia/addons/tests/test_models.py
+++ src/olympia/addons/tests/test_models.py
@@ -134,7 +134,7 @@ class TestCleanSlug(TestCase):
 
         # If there's a clash, use the standard clash resolution.
         a = Addon.objects.create(slug=long_slug[:30])
-        assert a.slug == '%s1' % long_slug[:28]
+        assert a.slug == '%s1' % long_slug[:27]
 
     def test_clean_slug_long_slug(self):
         long_slug = 'this_is_a_very_long_slug_that_is_longer_than_thirty_chars'
@@ -146,7 +146,7 @@ class TestCleanSlug(TestCase):
         # Now that there is a clash, test the clash resolution.
         b = Addon(slug=long_slug)
         b.clean_slug()
-        assert b.slug == '%s1' % long_slug[:28]
+        assert b.slug == '%s1' % long_slug[:27]
 
     def test_clean_slug_always_slugify(self):
         illegal_chars = 'some spaces and !?@'
