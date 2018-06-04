57b2f7f15f6e8303163b49622cb3a64e551497a1
Andreas Wagner <mail@andreaswagner.org>
2018-06-04 20:19:28 +0200

    Add note about unsigned

--- src/olympia/migrations/1017-add-version-to-reviewerscore.sql
+++ src/olympia/migrations/1017-add-version-to-reviewerscore.sql
@@ -1,3 +1,4 @@
+-- Note: if the migration fails for you locally, remove the 'UNSIGNED' next to version_id below.
 ALTER TABLE `reviewer_scores`
 	ADD COLUMN `version_id` INTEGER UNSIGNED REFERENCES `versions` (`id`);
 CREATE INDEX `reviewer_scores_version_id`
