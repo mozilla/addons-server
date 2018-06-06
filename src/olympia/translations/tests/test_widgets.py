5275da69635e26d7e694c352700a83227987d8ee
Mathieu Pillard <mpillard@mozilla.com>
2018-06-06 19:51:00 +0200

    Unhide translations widgets in the django admin
    Also add a bit of custom css/js to make them easier to edit.

--- src/olympia/translations/tests/test_widgets.py
+++ src/olympia/translations/tests/test_widgets.py
@@ -24,3 +24,43 @@ class TestWidget(TestCase):
         w.default_locale = 'pl'
         result = w.render('name', '')
         assert pq(result)('textarea:not([lang=init])').attr('lang') == 'pl'
+
+    def test_transinput(self):
+        models.Translation.objects.create(
+            id=666, locale='en-us', localized_string='test value en')
+        models.Translation.objects.create(
+            id=666, locale='fr', localized_string='test value fr')
+        models.Translation.objects.create(
+            id=666, locale='de', localized_string=None)
+
+        widget = widgets.TransInput()
+        assert not widget.is_hidden
+        expected_output = (
+            '<div id="trans-foo" class="trans" data-name="foo">'
+            '<input lang="en-us" name="foo_en-us" type="text"'
+            ' value="test value en" />'
+            '<input lang="fr" name="foo_fr" type="text"'
+            ' value="test value fr" />'
+            '<input class="trans-init hidden" lang="init" name="foo_init" '
+            'type="text" value="" /></div>')
+        assert widget.render('foo', 666) == expected_output
+
+    def test_transtextarea(self):
+        models.Translation.objects.create(
+            id=666, locale='en-us', localized_string='test value en')
+        models.Translation.objects.create(
+            id=666, locale='fr', localized_string='test value fr')
+        models.Translation.objects.create(
+            id=666, locale='de', localized_string=None)
+
+        widget = widgets.TransTextarea()
+        assert not widget.is_hidden
+        expected_output = (
+            '<div id="trans-foo" class="trans" data-name="foo">'
+            '<textarea cols="40" lang="en-us" name="foo_en-us" rows="10">\r\n'
+            'test value en</textarea>'
+            '<textarea cols="40" lang="fr" name="foo_fr" rows="10">\r\n'
+            'test value fr</textarea>'
+            '<textarea class="trans-init hidden" cols="40" lang="init" '
+            'name="foo_init" rows="10">\r\n</textarea></div>')
+        assert widget.render('foo', 666) == expected_output
