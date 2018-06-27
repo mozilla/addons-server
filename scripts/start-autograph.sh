471c4be6088621967263720cb9b843eb690d5446
Christopher Grebs <cg@webshox.org>
2018-06-27 12:26:31 +0200

    Does 2.1.3 work?

--- scripts/start-autograph.sh
+++ scripts/start-autograph.sh
@@ -5,7 +5,7 @@ echo "installing autograph + dependencies"
 curl -sL -o ./gimme https://raw.githubusercontent.com/travis-ci/gimme/v1.2.0/gimme
 chmod +x ./gimme
 eval "$(./gimme 1.10)"
-go get go.mozilla.org/autograph@2.1.4
+go get go.mozilla.org/autograph@2.1.3
 
 # Start autograph in background
 echo "start autograph in background at $(head ./scripts/autograph_travis_test_config.yaml | grep listen)"
