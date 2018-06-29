428574daa017f389a5e4a65c5cfc67f196f038f2
Andreas Wagner <mail@andreaswagner.org>
2018-06-29 17:21:56 +0200

    Add QA group

--- src/olympia/migrations/1030-remove-obsolete-groups.sql
+++ src/olympia/migrations/1030-remove-obsolete-groups.sql
@@ -1,4 +1,4 @@
-SET @GROUPS_TO_REMOVE = "Developers,Developers Credits,Engagement,Monolith API,OAuth Partner: Flightdeck,Other Contributors Credits,Past Developers Credits,Persona Reviewer MOTD,Revenue Stats Viewers,System Administrators";
+SET @GROUPS_TO_REMOVE = "Developers,Developers Credits,Engagement,Monolith API,OAuth Partner: Flightdeck,Other Contributors Credits,Past Developers Credits,Persona Reviewer MOTD,QA,Revenue Stats Viewers,System Administrators";
 
 DELETE FROM groups_users WHERE group_id IN (SELECT id FROM groups WHERE FIND_IN_SET(name, @GROUPS_TO_REMOVE));
 DELETE FROM groups WHERE FIND_IN_SET(name, @GROUPS_TO_REMOVE);
