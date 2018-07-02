c1f35fbefb617299dad7f9aff0f93d2995deb001
Andreas Wagner <mail@andreaswagner.org>
2018-07-02 19:33:30 +0200

    Add link to admin userprofile on admin membership page

--- src/olympia/access/admin.py
+++ src/olympia/access/admin.py
@@ -1,4 +1,6 @@
+from django.core.urlresolvers import reverse
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Group, GroupUser
 
@@ -6,6 +8,16 @@ from .models import Group, GroupUser
 class GroupUserInline(admin.TabularInline):
     model = GroupUser
     raw_id_fields = ('user',)
+    readonly_fields = ('user_profile_link',)
+
+    def user_profile_link(self, obj):
+        if obj.pk:
+            return format_html(
+                '<a href="{}">Admin User Profile</a>',
+                reverse('admin:users_userprofile_change', args=(obj.pk,)))
+        else:
+            return ''
+    user_profile_link.short_description = 'User Profile'
 
 
 class GroupAdmin(admin.ModelAdmin):
