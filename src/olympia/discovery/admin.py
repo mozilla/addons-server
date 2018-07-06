9a9031869e2fb9171c0ffeecb352622180773fdb
Mathieu Pillard <mpillard@mozilla.com>
2018-07-06 15:31:59 +0200

    Show preview of the content in disco pane recommendations admin tool

--- src/olympia/discovery/admin.py
+++ src/olympia/discovery/admin.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.admin.widgets import ForeignKeyRawIdWidget
+from django.utils import translation
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from olympia.addons.models import Addon
 from olympia.discovery.models import DiscoveryItem
@@ -22,10 +25,11 @@ class SlugOrPkChoiceField(forms.ModelChoiceField):
 class DiscoveryItemAdmin(admin.ModelAdmin):
     class Media:
         css = {
-            'all': ('css/admin/larger_raw_id.css',)
+            'all': ('css/admin/discovery.css',)
         }
-    raw_id_fields = ('addon',)
     list_display = ('__unicode__', 'custom_addon_name', 'custom_heading',)
+    raw_id_fields = ('addon',)
+    readonly_fields = ('previews',)
     view_on_site = False
 
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
@@ -37,6 +41,27 @@ class DiscoveryItemAdmin(admin.ModelAdmin):
             return SlugOrPkChoiceField(**kwargs)
         return super(DiscoveryItemAdmin, self).formfield_for_foreignkey(
             db_field, request, **kwargs)
+
+    def build_preview(self, obj):
+        return format_html(
+            u'<br/><h2 class="heading">{}</h2>'
+            u'<div class="editorial-description">{}</div>',
+            mark_safe(obj.heading),
+            mark_safe(obj.description))
+
+    def previews(self, obj):
+        translations = []
+        with translation.override('en-US'):
+            translations.append(self.build_preview(obj))
+        with translation.override('de'):
+            translations.append(self.build_preview(obj))
+        with translation.override('es'):
+            translations.append(self.build_preview(obj))
+        with translation.override('fr'):
+            translations.append(self.build_preview(obj))
+        with translation.override('it'):
+            translations.append(self.build_preview(obj))
+        return format_html(u''.join(translations))
 
 
 admin.site.register(DiscoveryItem, DiscoveryItemAdmin)
