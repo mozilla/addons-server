7f37460be2c5aadceaace8dd0e11b497bc875e94
Mathieu Pillard <mpillard@mozilla.com>
2018-07-06 18:16:44 +0200

    Add command to generate a file containing disco pane editorial strings for extraction

--- src/olympia/lib/settings_base.py
+++ src/olympia/lib/settings_base.py
@@ -536,6 +536,15 @@ INSTALLED_APPS = (
     'django_statsd',
 )
 
+# This needs to point to prod, because that's where the database lives. You can
+# change it locally to test the extraction process, but be careful not to
+# accidentally nuke translations when doing that!
+DISCOVERY_EDITORIAL_CONTENT_API = (
+    'https://addons.mozilla.org/api/v4/discovery/editorial/')
+
+# Filename where the strings will be stored. Used in puente config below.
+DISCOVERY_EDITORIAL_CONTENT_FILENAME = 'src/olympia/discovery/strings.jinja2'
+
 # Tells the extract script what files to look for l10n in and what function
 # handles the extraction. The puente library expects this.
 PUENTE = {
@@ -545,6 +554,12 @@ PUENTE = {
     'DOMAIN_METHODS': {
         'django': [
             ('src/olympia/**.py', 'python'),
+
+            # Extract the generated file containing editorial content for all
+            # disco pane recommendations using jinja2 parser. It's not a real
+            # template, but it uses jinja2 syntax for convenience, hence why
+            # it's not in templates/ with a .html extension.
+            (DISCOVERY_EDITORIAL_CONTENT_FILENAME, 'jinja2'),
 
             # Make sure we're parsing django-admin templates with the django
             # template extractor
