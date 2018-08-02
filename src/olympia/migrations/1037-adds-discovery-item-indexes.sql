9e954cc466ee06615277d30e1624327a3fd85622
Mathieu Pillard <mpillard@mozilla.com>
2018-08-02 15:55:43 +0200

    Follow django naming for the database indexes

--- src/olympia/migrations/1037-adds-discovery-item-indexes.sql
+++ src/olympia/migrations/1037-adds-discovery-item-indexes.sql
@@ -1,4 +1,2 @@
-CREATE INDEX `discovery_discoveryitem_position`
-    ON `discovery_discoveryitem` (`position`);
-CREATE INDEX `discovery_discoveryitem_position_china`
-    ON `discovery_discoveryitem` (`position_china`);
+CREATE INDEX `discovery_discoveryitem_position_5898729e` ON `discovery_discoveryitem` (`position`);
+CREATE INDEX `discovery_discoveryitem_position_china_321144f9` ON `discovery_discoveryitem` (`position_china`);
