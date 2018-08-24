c77b1ea2bb27fd67c63a5b385db05b38862a70db
Mathieu Pillard <mpillard@mozilla.com>
2018-08-24 12:25:31 +0200

    Indent & comment

--- src/olympia/migrations/1044-remove-login-attempt-fields.sql
+++ src/olympia/migrations/1044-remove-login-attempt-fields.sql
@@ -1,2 +1,6 @@
--- Those fields were NULLed and removed from the model earlier, they are no longer used.
-ALTER TABLE `users` DROP COLUMN `last_login_attempt_ip`, DROP COLUMN `failed_login_attempts`, DROP COLUMN `last_login_attempt`;
+-- Those fields were NULLed and removed from the model earlier
+-- in d1a5fe81fb3a90ea2fe90e4f40312dc4b3da3c9c, they are no longer used.
+ALTER TABLE `users`
+    DROP COLUMN `last_login_attempt_ip`,
+    DROP COLUMN `failed_login_attempts`,
+    DROP COLUMN `last_login_attempt`;
