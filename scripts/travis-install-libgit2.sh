7c1817a61003d8ea222799935a39b840184c1625
Christopher Grebs <cg@webshox.org>
2018-09-24 11:49:46 +0200

    Move libgit2 installation to tox and only to reviewers-zadmin job

--- scripts/travis-install-libgit2.sh
+++ scripts/travis-install-libgit2.sh
@@ -3,6 +3,10 @@ set -o errexit -o nounset
 
 LIBGIT2_VERSION="0.27.4"
 
+if [ -d "libgit2" ]; then
+    rm -rf libgit2/
+fi
+
 mkdir libgit2
 
 wget https://github.com/libgit2/libgit2/archive/v${LIBGIT2_VERSION}.tar.gz
