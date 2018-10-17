98ed6889a4f28ec6fabea1d64d210c17569e622e
Christopher Grebs <cg@webshox.org>
2018-10-17 18:07:25 +0200

    Remove olympia.compat app and related code. (#9720)
    Fixes #9515
    Also fixes bugzilla.mozilla.org/show_bug.cgi?id=1390960
    
    None of these are used in modern WebExtension world, the urls and views
    we're deleting here have <5 requests per day according to datadog.
    
    There may be a bit more css and js/html code we're able to delete but I
    tried to be conservative and not remove too much.
    
    This doesn't remove the data from ElasticSearch or models yet, I'll file
    separate issues for these.
    
    Also removes a handful of unused json fixtures along the way:
    
    * src/olympia/addons/fixtures/addons/update-strict-compat-version.json
    * src/olympia/amo/fixtures/base/charity.json
    * src/olympia/amo/fixtures/base/garbage.json
    * src/olympia/amo/fixtures/base/licenses.json
    * src/olympia/devhub/fixtures/devhub/addon-compat-results.json
    * src/olympia/legacy_discovery/fixtures/legacy_discovery/discoverymodules.json
    * src/olympia/ratings/fixtures/ratings/three-reviews.json

--- src/olympia/devhub/tests/test_views_edit.py
+++ src/olympia/devhub/tests/test_views_edit.py
@@ -229,7 +229,7 @@ class BaseTestEditDescribe(BaseTestEdit):
                              'Ensure this value has at most 250 '
                              'characters (it has 251).')
 
-    def test_nav_links(self, show_compat_reporter=True):
+    def test_nav_links(self):
         if self.listed:
             links = [
                 self.addon.get_dev_url('edit'),  # Edit Information
@@ -247,11 +247,6 @@ class BaseTestEditDescribe(BaseTestEdit):
                 reverse('devhub.feed', args=[self.addon.slug]),  # View Recent
             ]
 
-        if show_compat_reporter:
-            # Compatibility Reporter. Only shown for legacy extensions.
-            links.append(
-                reverse('compat.reporter_detail', args=[self.addon.guid]))
-
         response = self.client.get(self.url)
         doc_links = [
             unicode(a.attrib['href'])
@@ -260,7 +255,7 @@ class BaseTestEditDescribe(BaseTestEdit):
 
     def test_nav_links_webextensions(self):
         self.addon.find_latest_version(None).files.update(is_webextension=True)
-        self.test_nav_links(show_compat_reporter=False)
+        self.test_nav_links()
 
     def _feature_addon(self, addon_id=3615):
         c_addon = CollectionAddon.objects.create(
