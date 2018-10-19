40e04de1e34d30c1152613dab8612ca3b7e696bf
William Durand <will+git@drnd.me>
2018-10-19 20:34:34 +0200

    Expose FxA URL (for editing a user email) in account response (#9758)

--- src/olympia/lib/settings_base.py
+++ src/olympia/lib/settings_base.py
@@ -1785,6 +1785,7 @@ DRF_API_GATES = {
         'collections-downloads-shim',
         'addons-locale_disambiguation-shim',
         'del-addons-created-field',
+        'del-accounts-fxa-edit-email-url',
     ),
     'v4': (
         'l10n_flat_input_output',
