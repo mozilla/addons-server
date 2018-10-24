9f159a401c10fec23029b996da1ecfb0d1267adc
William Durand <will+git@drnd.me>
2018-10-24 16:55:16 +0200

    Add extension category descriptions

--- src/olympia/constants/categories.py
+++ src/olympia/constants/categories.py
@@ -54,29 +54,145 @@ CATEGORIES = {
     FIREFOX.id: {
         ADDON_EXTENSION: {
             'alerts-updates': StaticCategory(
-                id=72, name=_(u'Alerts & Updates')),
-            'appearance': StaticCategory(id=14, name=_(u'Appearance')),
-            'bookmarks': StaticCategory(id=22, name=_(u'Bookmarks')),
+                id=72,
+                name=_(u'Alerts & Updates'),
+                description=_(
+                    u'Find Firefox extensions that help you stay up-to-date, '
+                    u'remember tasks, and improve efficiency. This category '
+                    u'includes extensions that can automatically reload tabs, '
+                    u'manage productivity, and more.'
+                )
+            ),
+            'appearance': StaticCategory(
+                id=14,
+                name=_(u'Appearance'),
+                description=_(
+                    u'Explore extensions that can modify the appearance of '
+                    u'websites and Firefox. This category includes extensions '
+                    u'for dark themes, tab arrangements, and more.'
+                )
+            ),
+            'bookmarks': StaticCategory(
+                id=22,
+                name=_(u'Bookmarks'),
+                description=_(
+                    u'Discover extensions that enhance bookmarks by '
+                    u'password-protecting them, searching for duplicates, '
+                    u'finding broken bookmarks, and more.'
+                )
+            ),
             'download-management': StaticCategory(
-                id=5, name=_(u'Download Management')),
+                id=5,
+                name=_(u'Download Management'),
+                description=_(
+                    u'Get Firefox extensions that can help download web, '
+                    u'music and video content. You can also find extensions '
+                    u'to manage downloads, share files, and more.'
+                )
+            ),
             'feeds-news-blogging': StaticCategory(
-                id=1, name=_(u'Feeds, News & Blogging')),
+                id=1,
+                name=_(u'Feeds, News & Blogging'),
+                description=_(
+                    u'Firefox extensions that remove clutter so you can stay '
+                    u'up-to-date on social media, catch up on blogs, RSS '
+                    u'feeds, reduce eye strain, and more.'
+                )
+            ),
             'games-entertainment': StaticCategory(
-                id=142, name=_(u'Games & Entertainment')),
+                id=142,
+                name=_(u'Games & Entertainment'),
+                description=_(
+                    u'Firefox extensions to help tackle slow days and boost '
+                    u'your entertainment experience. This category includes '
+                    u'extensions that can enhance gaming, control video '
+                    u'playback, and more.'
+                )
+            ),
             'language-support': StaticCategory(
-                id=37, name=_(u'Language Support')),
+                id=37,
+                name=_(u'Language Support'),
+                description=_(
+                    u'Find Firefox extensions that can grammar check, look-up '
+                    u'words, translate text, provide text-to-speech, and more.'
+                )
+            ),
             'photos-music-videos': StaticCategory(
-                id=38, name=_(u'Photos, Music & Videos')),
+                id=38,
+                name=_(u'Photos, Music & Videos'),
+                description=_(
+                    u'Find Firefox extensions that can enhance your photo, '
+                    u'music and video experiences. Extensions in this '
+                    u'category can modify audio and video playback, perform '
+                    u'reverse image searches, and more.'
+                )
+            ),
             'privacy-security': StaticCategory(
-                id=12, name=_(u'Privacy & Security')),
-            'search-tools': StaticCategory(id=13, name=_(u'Search Tools')),
-            'shopping': StaticCategory(id=141, name=_(u'Shopping')),
+                id=12,
+                name=_(u'Privacy & Security'),
+                description=_(
+                    u'Get Firefox extensions to browse privately and '
+                    u'securely. This category includes extensions to block '
+                    u'ads, prevent tracking, redirects, and more.'
+                )
+            ),
+            'search-tools': StaticCategory(
+                id=13,
+                name=_(u'Search Tools'),
+                description=_(
+                    u'Discover Firefox extensions for your search and look-up '
+                    u'needs. This category includes extensions that can '
+                    u'highlight and search text, lookup IP addresses and '
+                    u'domains, and more.'
+                )
+            ),
+            'shopping': StaticCategory(
+                id=141,
+                name=_(u'Shopping'),
+                description=_(
+                    u'Discover Firefox extensions that can enhance your '
+                    u'online shopping experience with coupon finders, deal '
+                    u'finders, review analyzers, more.'
+                )
+            ),
             'social-communication': StaticCategory(
-                id=71, name=_(u'Social & Communication')),
-            'tabs': StaticCategory(id=93, name=_(u'Tabs')),
+                id=71,
+                name=_(u'Social & Communication'),
+                description=_(
+                    u'Find Firefox extensions to elevate your experience with '
+                    u'social media and instant messaging platforms. This '
+                    u'category includes improved tab notifications, video '
+                    u'downloaders, and more.'
+                )
+            ),
+            'tabs': StaticCategory(
+                id=93,
+                name=_(u'Tabs'),
+                description=_(
+                    u'Stumble on Firefox extension to customize tabs and the '
+                    u'new tab page. Discover extensions that can control '
+                    u'tabs, change the way you interact with them and more.'
+                )
+            ),
             'web-development': StaticCategory(
-                id=4, name=_(u'Web Development')),
-            'other': StaticCategory(id=73, name=_(u'Other'), weight=333)
+                id=4,
+                name=_(u'Web Development'),
+                description=_(
+                    u'Find Firefox extensions that feature tools for web '
+                    u'development. This category includes extensions for '
+                    u'GitHub, user agent switching, cookie management, '
+                    u'and more.'
+                )
+            ),
+            'other': StaticCategory(
+                id=73,
+                name=_(u'Other'),
+                weight=333,
+                description=_(
+                    u'Firefox extensions that can be unpredictable and '
+                    u'creative, yet useful for those odd tasks.'
+                )
+            )
         },
         ADDON_THEME: {
             'animals': StaticCategory(id=30, name=_(u'Animals')),
@@ -152,21 +268,104 @@ CATEGORIES = {
     ANDROID.id: {
         ADDON_EXTENSION: {
             'device-features-location': StaticCategory(
-                id=145, name=_(u'Device Features & Location')),
-            'experimental': StaticCategory(id=151, name=_(u'Experimental')),
+                id=145,
+                name=_(u'Device Features & Location'),
+                description=_(
+                    u'Discover extensions to enhance Firefox for Android. '
+                    u'Perform quick searches, free up system resources, take '
+                    u'notes, and more.'
+                )
+            ),
+            'experimental': StaticCategory(
+                id=151,
+                name=_(u'Experimental'),
+                description=_(
+                    u'Uncover Firefox extensions that are regularly updated '
+                    u'and ready for public testing. Your feedback helps to '
+                    u'guide developers to make changes in upcoming versions.'
+                )
+            ),
             'feeds-news-blogging': StaticCategory(
-                id=147, name=_(u'Feeds, News, & Blogging')),
-            'performance': StaticCategory(id=144, name=_(u'Performance')),
-            'photos-media': StaticCategory(id=143, name=_(u'Photos & Media')),
+                id=147,
+                name=_(u'Feeds, News, & Blogging'),
+                description=_(
+                    u'Stay up-to-date with Firefox extensions for news, '
+                    u'web feeds, and blogs by removing clutter, utilizing '
+                    u'voice readers, and more.'
+                )
+            ),
+            'performance': StaticCategory(
+                id=144,
+                name=_(u'Performance'),
+                description=_(
+                    u'Discover extensions that give Firefox a performance '
+                    u'boost. Find extensions that help you be more productive '
+                    u'and efficient by blocking ads and more.'
+                )
+            ),
+            'photos-media': StaticCategory(
+                id=143,
+                name=_(u'Photos & Media'),
+                description=_(
+                    u'Firefox extensions to enhance your experience with '
+                    u'photos and media. This category includes extensions '
+                    u'to reverse search images, capture full page '
+                    u'screenshots, and more.'
+                )
+            ),
             'security-privacy': StaticCategory(
-                id=149, name=_(u'Security & Privacy')),
-            'shopping': StaticCategory(id=150, name=_(u'Shopping')),
+                id=149,
+                name=_(u'Security & Privacy'),
+                description=_(
+                    u'Get Firefox extensions to surf safely and privately. '
+                    u'Discover extensions that can stop sneaky ad trackers in '
+                    u'their tracks, easily clear browsing history, and more.'
+                )
+            ),
+            'shopping': StaticCategory(
+                id=150,
+                name=_(u'Shopping'),
+                description=_(
+                    u'Discover Firefox extensions that can enhance your '
+                    u'online shopping experience by analyzing reviews, '
+                    u'finding deals and coupons, and more.'
+                )
+            ),
             'social-networking': StaticCategory(
-                id=148, name=_(u'Social Networking')),
-            'sports-games': StaticCategory(id=146, name=_(u'Sports & Games')),
+                id=148,
+                name=_(u'Social Networking'),
+                description=_(
+                    u'Get Firefox extensions to enhance your experience on '
+                    u'popular social networking websites such as YouTube, '
+                    u'GitHub, Reddit, and more.'
+                )
+            ),
+            'sports-games': StaticCategory(
+                id=146,
+                name=_(u'Sports & Games'),
+                description=_(
+                    u'Get Firefox extensions to give your entertainment '
+                    u'experience a boost with live stream enhancers, sports '
+                    u'updates, and more.'
+                )
+            ),
             'user-interface': StaticCategory(
-                id=152, name=_(u'User Interface')),
-            'other': StaticCategory(id=153, name=_(u'Other'), weight=333)
+                id=152,
+                name=_(u'User Interface'),
+                description=_(
+                    u'Find Firefox extensions to alter web pages for easier '
+                    u'reading, searching, browsing, and more.'
+                )
+            ),
+            'other': StaticCategory(
+                id=153,
+                name=_(u'Other'),
+                weight=333,
+                description=_(
+                    u'Stumble on odd and interesting extensions that can give '
+                    u'Firefox for Android a boost.'
+                )
+            )
         }
     },
 }
