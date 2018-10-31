77278f8ed625bcc2eae3e870437608c2ae9513a8
Andrew Williamson <awilliamson@mozilla.com>
2018-10-31 10:11:18 +0000

    don't fail on a broken addon while creating AkismetReports (#9848)

--- src/olympia/devhub/utils.py
+++ src/olympia/devhub/utils.py
@@ -334,8 +334,13 @@ def get_addon_akismet_reports(user, user_agent, referrer, upload=None,
 
     if upload:
         addon = addon or upload.addon
-        data = data or Addon.resolve_webext_translations(
-            parse_addon(upload, addon, user, minimal=True), upload)
+        if not data:
+            try:
+                data = Addon.resolve_webext_translations(
+                    parse_addon(upload, addon, user, minimal=True), upload)
+            except ValidationError:
+                # The xpi is broken - it'll be rejected by the linter so abort.
+                return []
 
     reports = []
     for prop in properties:
