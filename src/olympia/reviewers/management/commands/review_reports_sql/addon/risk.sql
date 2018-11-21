2634c73835eaac64819f7d8cfbe87918117d692c
Andreas Wagner <mail@andreaswagner.org>
2018-11-21 13:16:35 +0100

    Beautify SQL

--- src/olympia/reviewers/management/commands/review_reports_sql/addon/risk.sql
+++ src/olympia/reviewers/management/commands/review_reports_sql/addon/risk.sql
@@ -1,29 +1,30 @@
 SELECT risk_category AS `Risk Category`,
        FORMAT(SUM(n), 0) AS `All Reviewers`,
        FORMAT(SUM(CASE WHEN `gruppe` = 'volunteer' THEN n ELSE 0 END), 0) AS 'Volunteers'
-FROM (
-    SELECT CASE
-               WHEN weight > @RISK_HIGHEST THEN 'highest'
-               WHEN weight > @RISK_HIGH THEN 'high'
-               WHEN weight > @RISK_MEDIUM THEN 'medium'
-               ELSE 'low'
-           END AS risk_category,
-           gruppe,
-           COUNT(*) as n
- FROM editors_autoapprovalsummary aa
+FROM
+  (SELECT CASE
+              WHEN weight > @RISK_HIGHEST THEN 'highest'
+              WHEN weight > @RISK_HIGH THEN 'high'
+              WHEN weight > @RISK_MEDIUM THEN 'medium'
+              ELSE 'low'
+          END AS risk_category,
+          gruppe,
+          COUNT(*) AS n
+   FROM editors_autoapprovalsummary aa
    JOIN
      (SELECT version_id,
              CASE WHEN user_id NOT IN
         (SELECT user_id
          FROM groups_users
-         WHERE group_id IN (
-           SELECT id FROM groups WHERE name IN ('Staff', 'No Reviewer Incentives')
-         )) THEN 'volunteer' ELSE 'all' END AS `gruppe`
+         WHERE group_id IN
+             (SELECT id
+              FROM groups
+              WHERE name IN ('Staff', 'No Reviewer Incentives'))) THEN 'volunteer' ELSE 'all' END AS `gruppe`
       FROM reviewer_scores rs
       WHERE DATE(rs.created) BETWEEN @WEEK_BEGIN AND @WEEK_END
         AND user_id <> 4757633
-        AND rs.note_key IN (10, 12, 20, 22, 30, 32, 50, 52, 102, 103, 104, 105)) iner on iner.version_id = aa.version_id
-   GROUP BY risk_category, `gruppe`
-   ) tmp
+        AND rs.note_key IN (10, 12, 20, 22, 30, 32, 50, 52, 102, 103, 104, 105)) iner ON iner.version_id = aa.version_id
+   GROUP BY risk_category,
+            `gruppe`) tmp
 GROUP BY 1
 ORDER BY FIELD(risk_category,'highest','high','medium', 'low');
--- src/olympia/reviewers/management/commands/review_reports_sql/addon/weekly.sql
+++ src/olympia/reviewers/management/commands/review_reports_sql/addon/weekly.sql
@@ -2,24 +2,27 @@ SELECT u.display_name AS `Name`,
        IF(
             (SELECT DISTINCT(user_id)
              FROM groups_users
-             WHERE group_id IN (
-               SELECT id FROM groups WHERE name IN ('Staff', 'No Reviewer Incentives')
-             )
+             WHERE group_id IN
+                 (SELECT id
+                  FROM groups
+                  WHERE name IN ('Staff', 'No Reviewer Incentives'))
                AND user_id = rs.user_id), '*', '') AS `Staff`,
        FORMAT(SUM(aa.weight), 0) AS `Total Risk`,
        FORMAT(AVG(aa.weight), 2) AS `Average Risk`,
        IFNULL(IF(
-            (SELECT DISTINCT(user_id)
-             FROM groups_users
-             WHERE group_id IN (
-               SELECT id FROM groups WHERE name IN ('Staff', 'No Reviewer Incentives')
-             )
-               AND user_id = rs.user_id), '-', SUM(rs.score)), 0) AS `Points`,
+                   (SELECT DISTINCT(user_id)
+                    FROM groups_users
+                    WHERE group_id IN
+                        (SELECT id
+                         FROM groups
+                         WHERE name IN ('Staff', 'No Reviewer Incentives'))
+                      AND user_id = rs.user_id), '-', SUM(rs.score)), 0) AS `Points`,
        FORMAT(COUNT(*), 0) AS `Add-ons Reviewed`
 FROM editors_autoapprovalsummary aa
 JOIN reviewer_scores rs ON rs.version_id = aa.version_id
 JOIN users u ON u.id = rs.user_id
-WHERE DATE(rs.created) BETWEEN @WEEK_BEGIN AND @WEEK_END AND user_id <> 4757633
-AND rs.note_key IN (10, 12, 20, 22, 30, 32, 50, 52, 102, 103, 104, 105)
+WHERE DATE(rs.created) BETWEEN @WEEK_BEGIN AND @WEEK_END
+  AND user_id <> 4757633
+  AND rs.note_key IN (10, 12, 20, 22, 30, 32, 50, 52, 102, 103, 104, 105)
 GROUP BY user_id HAVING `Add-ons Reviewed` >= 5
 ORDER BY SUM(aa.weight) DESC;
