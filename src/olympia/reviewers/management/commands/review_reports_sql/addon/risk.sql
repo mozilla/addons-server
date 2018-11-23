4d416459873986f832c787fdfffa31b49cb11c0a
Andreas Wagner <mail@andreaswagner.org>
2018-11-23 13:51:04 +0100

    Automate weekly review report (#10034)
    Fix #6474.

---
+++ src/olympia/reviewers/management/commands/review_reports_sql/addon/risk.sql
@@ -0,0 +1,32 @@
+SELECT risk_category AS `Risk Category`,
+       FORMAT(SUM(n), 0) AS `All Reviewers`,
+       FORMAT(SUM(CASE WHEN `group_category` = 'volunteer' THEN n ELSE 0 END), 0) AS 'Volunteers'
+FROM
+  (SELECT CASE
+              WHEN weight > @RISK_HIGHEST THEN 'highest'
+              WHEN weight > @RISK_HIGH THEN 'high'
+              WHEN weight > @RISK_MEDIUM THEN 'medium'
+              ELSE 'low'
+          END AS risk_category,
+          group_category,
+          COUNT(*) AS n
+   FROM editors_autoapprovalsummary aa
+   JOIN
+     (SELECT version_id,
+             CASE WHEN user_id NOT IN
+        (SELECT user_id
+         FROM groups_users
+         WHERE group_id IN
+             (SELECT id
+              FROM groups
+              WHERE name IN ('Staff', 'No Reviewer Incentives'))) THEN 'volunteer' ELSE 'all' END AS `group_category`
+      FROM reviewer_scores rs
+      WHERE DATE(rs.created) BETWEEN @WEEK_BEGIN AND @WEEK_END
+       /* Filter out internal task user */
+        AND user_id <> 4757633
+        /* The type of review, see constants/reviewers.py */
+        AND rs.note_key IN (10, 12, 20, 22, 30, 32, 50, 52, 102, 103, 104, 105)) reviews ON reviews.version_id = aa.version_id
+   GROUP BY risk_category,
+            `group_category`) risk
+GROUP BY 1
+ORDER BY FIELD(risk_category,'highest','high','medium', 'low');
