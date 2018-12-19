d96bc3caaf4788b3b52d39d1177639b1022e3de0
Mathieu Pillard <diox@users.noreply.github.com>
2018-12-19 20:56:15 +0100

    Revert "Temporarily re-enable static theme migration for LWT updates on dev"

--- src/olympia/conf/dev/settings.py
+++ src/olympia/conf/dev/settings.py
@@ -147,5 +147,3 @@ FXA_SQS_AWS_QUEUE_URL = (
     'amo-account-change-dev')
 
 VAMO_URL = 'https://versioncheck-dev.allizom.org'
-
-MIGRATED_LWT_UPDATES_ENABLED = True  # Temporarily enabled for #10246
