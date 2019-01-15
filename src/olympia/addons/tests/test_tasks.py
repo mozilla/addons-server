8a668ca3f68c8ce2c8fa464ae8486db85fb86de8
Andrew Williamson <awilliamson@mozilla.com>
2019-01-16 01:34:36 +0800

    have command to only regenerate missing theme previews (#10400)

--- src/olympia/addons/tests/test_tasks.py
+++ src/olympia/addons/tests/test_tasks.py
@@ -450,3 +450,38 @@ def test_recreate_theme_previews():
          'thumbnail': list(amo.THEME_PREVIEW_SIZES['list']['thumbnail'])},
         {'image': list(amo.THEME_PREVIEW_SIZES['single']['full']),
          'thumbnail': list(amo.THEME_PREVIEW_SIZES['single']['thumbnail'])}]
+
+
+@pytest.mark.django_db
+@mock.patch('olympia.addons.tasks.parse_addon')
+def test_create_missing_theme_previews(parse_addon_mock):
+    parse_addon_mock.return_value = {}
+    theme = addon_factory(type=amo.ADDON_STATICTHEME)
+    preview = VersionPreview.objects.create(
+        version=theme.current_version,
+        sizes={'image': [123, 456], 'thumbnail': [34, 45]})
+    VersionPreview.objects.create(
+        version=theme.current_version,
+        sizes={'image': [123, 456], 'thumbnail': [34, 45]})
+    VersionPreview.objects.create(
+        version=theme.current_version,
+        sizes={'image': [123, 456], 'thumbnail': [34, 45]})
+
+    # addon has 3 complete previews already so skip when only_missing=True
+    with mock.patch('olympia.addons.tasks.generate_static_theme_preview') as p:
+        recreate_theme_previews([theme.id], only_missing=True)
+        assert p.call_count == 0
+        recreate_theme_previews([theme.id], only_missing=False)
+        assert p.call_count == 1
+
+    # break one of the previews
+    preview.update(sizes={})
+    with mock.patch('olympia.addons.tasks.generate_static_theme_preview') as p:
+        recreate_theme_previews([theme.id], only_missing=True)
+        assert p.call_count == 1
+
+    # And delete it so the addon only has 2 previews
+    preview.delete()
+    with mock.patch('olympia.addons.tasks.generate_static_theme_preview') as p:
+        recreate_theme_previews([theme.id], only_missing=True)
+        assert p.call_count == 1
