3114c2f23c88500540f3645c89368175e3d601bd
Andrew Williamson <awilliamson@mozilla.com>
2019-01-25 22:28:29 +0800

    Update 1070-remove-name-column to drop fk first (#10505)

--- src/olympia/migrations/1070-remove-name-column.sql
+++ src/olympia/migrations/1070-remove-name-column.sql
@@ -1,3 +1,6 @@
+/* Uncomment DROP FOREIGN KEY statement below if your local dev database complains */
+/* ALTER TABLE `categories` DROP FOREIGN KEY `categories_name_09afee77_fk_translations_id`; */
+
 ALTER TABLE `categories`
  DROP COLUMN `name`;
  
