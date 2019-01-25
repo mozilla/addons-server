c482c785022e71f46ff016c77555f8e5580fdf27
Andrew Williamson <awilliamson@mozilla.com>
2019-01-25 22:28:16 +0800

    Update 1070-remove-name-column.sql

--- src/olympia/migrations/1070-remove-name-column.sql
+++ src/olympia/migrations/1070-remove-name-column.sql
@@ -1,4 +1,5 @@
-ALTER TABLE `categories` DROP foreign key `categories_name_09afee77_fk_translations_id`;
+/* Uncomment DROP FOREIGN KEY statement below if your local dev database complains */
+/* ALTER TABLE `categories` DROP FOREIGN KEY `categories_name_09afee77_fk_translations_id`; */
 
 ALTER TABLE `categories`
  DROP COLUMN `name`;
