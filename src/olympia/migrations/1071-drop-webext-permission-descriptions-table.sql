0808843f5bafe2949fb23b12d47faea23394a9b5
Shivam Singhal <shivams2799@gmail.com>
2019-01-31 12:15:23 +0530

    drop webext_permission_descriptions table (#10526)
    Fixes #10523 
    
    Remove webext_permission_descriptions table from DB.

---
+++ src/olympia/migrations/1071-drop-webext-permission-descriptions-table.sql
@@ -0,0 +1,4 @@
+/* Uncomment DROP FOREIGN KEY statement below if your local dev database complains */
+/* ALTER TABLE `webext_permission_descriptions_translation_id` DROP FOREIGN KEY `categories_name_09afee77_fk_translations_id`; */
+
+DROP TABLE IF EXISTS `webext_permission_descriptions_translation_id`;
