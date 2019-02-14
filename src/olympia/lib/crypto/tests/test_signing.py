b331b98922726c34fd661094192318a7e2775ae4
Mathieu Pillard <mpillard@mozilla.com>
2019-02-14 18:08:05 +0100

    Don't try to sign search plugins - they are just a single xml file

--- src/olympia/lib/crypto/tests/test_signing.py
+++ src/olympia/lib/crypto/tests/test_signing.py
@@ -199,7 +199,13 @@ class TestSigning(TestCase):
         assert not self.file_.hash
         assert not signing.is_signed(self.file_.file_path)
 
-    def test_no_sign_again_mozilla_signed_extensions(self):
+    def test_dont_sign_search_plugins(self):
+        self.addon.update(type=amo.ADDON_SEARCH)
+        self.file_.update(is_webextension=False)
+        signing.sign_file(self.file_)
+        self.assert_not_signed()
+
+    def test_dont_sign_again_mozilla_signed_extensions(self):
         """Don't try to resign mozilla signed extensions."""
         self.file_.update(is_mozilla_signed_extension=True)
         signing.sign_file(self.file_)
