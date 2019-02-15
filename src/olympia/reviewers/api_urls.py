9f1cf7795ee310c2d99b539553f7af0855747f22
Christopher Grebs <cg@webshox.org>
2019-02-15 15:53:09 +0100

    Expose version list for diffing, rework reviewers api to be more concise (#10684)
    * Expose version list for diffing, rework reviewers api to be more concise
    
    We're now mounting any listing/browsing/future diffing APIs under the
    already existing addon/<addon_id>/ endpoint which makes the whole
    reviewer APIs behave more consistently.
    
    Also implements the listing of versions that can be used for browsing
    and diffing.
    
    Fixes #10432
    
    * Simplify view, don't rely on @action decorator but just make it the detail view
    
    * Smaller cleanups, fix permissions
    
    * More smaller cleanups, split list/retreive better
    
    * Fix link in docs
    
    * Improve tests, remove 'should_show_channel' and 'url'
    
    * Smaller performance optimization
    
    * Fix flake8
    
    * Improve docs

--- src/olympia/reviewers/api_urls.py
+++ src/olympia/reviewers/api_urls.py
@@ -1,14 +1,21 @@
 from django.conf.urls import include, url
 
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
-from .views import AddonReviewerViewSet, BrowseVersionViewSet
+from .views import (
+    AddonReviewerViewSet, ReviewAddonVersionViewSet)
 
 
 addons = SimpleRouter()
 addons.register(r'addon', AddonReviewerViewSet, basename='reviewers-addon')
-addons.register(r'browse', BrowseVersionViewSet, basename='reviewers-browse')
+
+versions = NestedSimpleRouter(addons, r'addon', lookup='addon')
+versions.register(
+    r'versions', ReviewAddonVersionViewSet, basename='reviewers-versions')
+
 
 urlpatterns = [
     url(r'', include(addons.urls)),
+    url(r'', include(versions.urls)),
 ]
