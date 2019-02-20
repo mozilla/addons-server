d9a471541bc1c07ff63fa76d972dac7b59ba01f0
Christopher Grebs <cg@webshox.org>
2019-02-20 11:59:00 +0100

    Fix tests

--- src/olympia/core/tests/test_db.py
+++ src/olympia/core/tests/test_db.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pytest
 
 from olympia.core.tests.db_tests_testapp.models import TestRegularCharField
@@ -5,9 +6,9 @@ from olympia.core.tests.db_tests_testapp.models import TestRegularCharField
 
 @pytest.mark.django_db
 @pytest.mark.parametrize('value', [
-    'a',
-    '🔍',  # Magnifying Glass Tilted Left (U+1F50D)
-    '❤',  # Heavy Black Heart (U+2764, U+FE0F)
+    u'a',
+    u'🔍',  # Magnifying Glass Tilted Left (U+1F50D)
+    u'❤',  # Heavy Black Heart (U+2764, U+FE0F)
 ])
 def test_max_length_utf8mb4(value):
     TestRegularCharField.objects.create(name=value * 255)
