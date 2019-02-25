5643a595f6403a86d1c7a331ca9bb6db1ea5ef2d
Andreas Wagner <mail@andreaswagner.org>
2019-02-25 10:35:38 +0100

    Add django admin for versions

--- src/olympia/versions/admin.py
+++ src/olympia/versions/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from .models import License, Version
@@ -9,5 +10,30 @@ class LicenseAdmin(admin.ModelAdmin):
     ordering = ('builtin',)
 
 
+class VersionAdmin(admin.ModelAdmin):
+    view_on_site = False
+    readonly_fields = ('version', 'channel')
+
+    raw_id_fields = ('addon', 'license', 'release_notes')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'addon', 'version', 'channel', 'release_notes',
+                'approval_notes', 'license', 'source')
+        }),
+        ('Flags', {
+            'fields': ('deleted',)
+        }),
+    )
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super(VersionAdmin, self).formfield_for_dbfield(
+            db_field, **kwargs)
+        if db_field.name == 'release_notes':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        return formfield
+
+
 admin.site.register(License, LicenseAdmin)
-admin.site.register(Version)
+admin.site.register(Version, VersionAdmin)
