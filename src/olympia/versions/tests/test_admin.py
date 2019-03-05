f6b3e29559c6f443c67f4b2252baf2616a747510
Andreas Wagner <mail@andreaswagner.org>
2019-03-05 11:11:56 +0100

    Add test for unauthorized user

--- src/olympia/versions/tests/test_admin.py
+++ src/olympia/versions/tests/test_admin.py
@@ -6,7 +6,7 @@ from olympia.amo.urlresolvers import reverse
 
 class TestVersionAdmin(TestCase):
 
-    def test_detail_view_has_download_link(self):
+    def test_authorized_user_has_access(self):
         addon = addon_factory()
         version = addon.current_version
         detail_url = reverse(
@@ -21,3 +21,15 @@ class TestVersionAdmin(TestCase):
 
         doc = pq(response.content)
         assert doc('textarea#id_release_notes').length == 1
+
+    def test_unauthorized_user_has_no_access(self):
+        addon = addon_factory()
+        version = addon.current_version
+        detail_url = reverse(
+            'admin:versions_version_change', args=(version.pk,)
+        )
+        user = user_factory()
+        self.grant_permission(user, 'Admin:Tools')
+        self.client.login(email=user.email)
+        response = self.client.get(detail_url, follow=True)
+        assert response.status_code == 403
