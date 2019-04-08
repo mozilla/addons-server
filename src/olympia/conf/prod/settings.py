74ebeb9dd3d41e737aed15877678a00dec3fe1c6
Andrew Williamson <awilliamson@mozilla.com>
2019-04-08 17:12:14 +0100

    add conf defaults for EXTERNAL_SITE_URL

--- src/olympia/conf/prod/settings.py
+++ src/olympia/conf/prod/settings.py
@@ -22,7 +22,8 @@ CDN_HOST = 'https://addons.cdn.mozilla.net'
 DOMAIN = env('DOMAIN', default='addons.mozilla.org')
 SERVER_EMAIL = 'zprod@addons.mozilla.org'
 SITE_URL = 'https://' + DOMAIN
-EXTERNAL_SITE_URL = env('EXTERNAL_SITE_URL', default=SITE_URL)
+EXTERNAL_SITE_URL = env('EXTERNAL_SITE_URL',
+                        default='https://addons.mozilla.org')
 SERVICES_URL = env('SERVICES_URL',
                    default='https://services.addons.mozilla.org')
 CODE_MANAGER_URL = env('CODE_MANAGER_URL',
