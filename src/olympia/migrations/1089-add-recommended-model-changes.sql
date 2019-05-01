ebf9fce5232941053209db1c847d1717ef554483
Andrew Williamson <awilliamson@mozilla.com>
2019-05-01 11:37:47 +0100

    sql make more correctish

--- src/olympia/migrations/1089-add-recommended-model-changes.sql
+++ src/olympia/migrations/1089-add-recommended-model-changes.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `discovery_discoveryitem` ADD COLUMN `recommendable` boolean DEFAULT False;
-ALTER TABLE `versions` ADD COLUMN `recommendation_approved` boolean DEFAULT False;
+ALTER TABLE `discovery_discoveryitem` ADD COLUMN `recommendable` boolean NOT null DEFAULT 0;
+ALTER TABLE `versions` ADD COLUMN `recommendation_approved` boolean NOT null DEFAULT 0;
