2f2216a4bde9aa27c9d784cf356d6ff35d5d735e
Christopher Grebs <cg@webshox.org>
2019-05-13 12:43:45 +0200

    Make sure category is serialized as a string

--- src/olympia/reviewers/serializers.py
+++ src/olympia/reviewers/serializers.py
@@ -14,6 +14,7 @@ from django.utils.functional import cached_property
 from django.utils.encoding import force_text
 from django.utils.timezone import FixedOffset
 
+from olympia import amo
 from olympia.activity.models import DraftComment
 from olympia.amo.urlresolvers import reverse
 from olympia.amo.templatetags.jinja_helpers import absolutify
@@ -381,7 +382,11 @@ class DraftCommentSerializer(serializers.ModelSerializer):
 class CannedResponseSerializer(serializers.ModelSerializer):
     # Title is actually more fitting than the internal "name"
     title = serializers.CharField(source='name')
+    category = serializers.SerializerMethodField()
 
     class Meta:
         model = CannedResponse
         fields = ('id', 'title', 'response', 'category')
+
+    def get_category(self, obj):
+        return amo.CANNED_RESPONSE_CATEGORY_CHOICES[obj.category]
