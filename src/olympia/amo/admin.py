6ac30c699b8bf0ec1631af1cbb24368df6cd9384
Mathieu Pillard <diox@users.noreply.github.com>
2019-05-20 10:38:32 +0200

    Make Abuse Report Admin search depend on report type being searched (#11438)
    Make Abuse Report Admin search depend on report type being searched

---
+++ src/olympia/amo/admin.py
@@ -0,0 +1,103 @@
+import functools
+
+from django.contrib import admin
+from django.contrib.admin.options import operator
+from django.core.exceptions import FieldDoesNotExist
+from django.db import models
+
+
+class CommaSearchInAdminMixin:
+    def get_search_id_field(self, request):
+        """
+        Return the field to use when all search terms are numeric.
+
+        Default is to return pk, but in some cases it'll make more sense to
+        return a foreign key.
+        """
+        return 'pk'
+
+    def get_search_results(self, request, queryset, search_term):
+        """
+        Return a tuple containing a queryset to implement the search,
+        and a boolean indicating if the results may contain duplicates.
+
+        Originally copied from Django's, but with the following differences:
+        - The operator joining the query parts is dynamic: if the search term
+          contain a comma and no space, then the comma is used as the separator
+          instead, and the query parts are joined by OR, not AND, allowing
+          admins to search by a list of ids, emails or usernames and find all
+          objects in that list.
+        - If the search terms are all numeric and there is more than one, then
+          we also restrict the fields we search to the one returned by
+          get_search_id_field(request) using a __in ORM lookup directly.
+
+        """
+        # Apply keyword searches.
+        def construct_search(field_name):
+            if field_name.startswith('^'):
+                return "%s__istartswith" % field_name[1:]
+            elif field_name.startswith('='):
+                return "%s__iexact" % field_name[1:]
+            elif field_name.startswith('@'):
+                return "%s__icontains" % field_name[1:]
+            # Use field_name if it includes a lookup.
+            opts = queryset.model._meta
+            lookup_fields = field_name.split(models.constants.LOOKUP_SEP)
+            # Go through the fields, following all relations.
+            prev_field = None
+            for path_part in lookup_fields:
+                if path_part == 'pk':
+                    path_part = opts.pk.name
+                try:
+                    field = opts.get_field(path_part)
+                except FieldDoesNotExist:
+                    # Use valid query lookups.
+                    if prev_field and prev_field.get_lookup(path_part):
+                        return field_name
+                else:
+                    prev_field = field
+                    if hasattr(field, 'get_path_info'):
+                        # Update opts to follow the relation.
+                        opts = field.get_path_info()[-1].to_opts
+            # Otherwise, use the field with icontains.
+            return "%s__icontains" % field_name
+
+        use_distinct = False
+        search_fields = self.get_search_fields(request)
+        filters = []
+        joining_operator = operator.and_
+        if not (search_fields and search_term):
+            # return early if we have nothing special to do
+            return queryset, use_distinct
+
+        if ' ' not in search_term and ',' in search_term:
+            separator = ','
+            joining_operator = operator.or_
+        else:
+            separator = None
+        search_terms = search_term.split(separator)
+        all_numeric = all(term.isnumeric() for term in search_terms)
+        if all_numeric and len(search_terms) > 1:
+            # if we have multiple numbers assume we're doing a bulk id search
+            orm_lookup = '%s__in' % self.get_search_id_field(request)
+            queryset = queryset.filter(**{orm_lookup: search_terms})
+        else:
+            orm_lookups = [
+                construct_search(str(search_field))
+                for search_field in search_fields]
+            for bit in search_terms:
+                or_queries = [models.Q(**{orm_lookup: bit})
+                              for orm_lookup in orm_lookups]
+
+                q_for_this_term = models.Q(
+                    functools.reduce(operator.or_, or_queries))
+                filters.append(q_for_this_term)
+
+            use_distinct |= any(
+                admin.utils.lookup_needs_distinct(self.opts, search_spec)
+                for search_spec in orm_lookups)
+
+            if filters:
+                queryset = queryset.filter(
+                    functools.reduce(joining_operator, filters))
+        return queryset, use_distinct
