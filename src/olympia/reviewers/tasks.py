2994659f823f8ec79166e248b04f28f673aaf0cc
Abhi-khandelwal <abhikhandelwal1234@gmail.com>
2019-07-23 16:52:40 +0530

    remove task functions

--- src/olympia/reviewers/tasks.py
+++ src/olympia/reviewers/tasks.py
@@ -12,38 +12,6 @@ log = olympia.core.logger.getLogger('z.task')
 
 
 @task
-def add_commentlog(items, **kw):
-    log.info('[%s@%s] Adding CommentLog starting with ActivityLog: %s' %
-             (len(items), add_commentlog.rate_limit, items[0]))
-
-    for al in ActivityLog.objects.filter(pk__in=items):
-        # Delete existing entries:
-        CommentLog.objects.filter(activity_log=al).delete()
-
-        # Create a new entry:
-        if 'comments' in al.details:
-            CommentLog(comments=al.details['comments'], activity_log=al).save()
-
-
-@task
-def add_versionlog(items, **kw):
-    log.info('[%s@%s] Adding VersionLog starting with ActivityLog: %s' %
-             (len(items), add_versionlog.rate_limit, items[0]))
-
-    for al in ActivityLog.objects.filter(pk__in=items):
-        # Delete existing entries:
-        VersionLog.objects.filter(activity_log=al).delete()
-
-        for a in al.arguments:
-            if isinstance(a, Version):
-                vl = VersionLog(version=a, activity_log=al)
-                vl.save()
-                # We need to save it twice to backdate the created date.
-                vl.created = al.created
-                vl.save()
-
-
-@task
 @use_primary_db
 def recalculate_post_review_weight(ids):
     """Recalculate the post-review weight that should be assigned to
