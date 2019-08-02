a1df7f8dda5817046c9f1ccad441b739ddaba357
Andrew Williamson <awilliamson@mozilla.com>
2019-08-02 17:18:23 +0100

    disable view on site link from Primary hero inline admin module (#11996)

--- src/olympia/hero/admin.py
+++ src/olympia/hero/admin.py
@@ -6,3 +6,4 @@ from .models import PrimaryHero
 class PrimaryHeroInline(admin.StackedInline):
     model = PrimaryHero
     fields = ('image', 'gradient_color', 'is_external', 'enabled')
+    view_on_site = False
