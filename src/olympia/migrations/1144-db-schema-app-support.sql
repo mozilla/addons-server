5bbfeb5efd88ee3dbce4ac93a40e463233eb1363
Andrew Williamson <awilliamson@mozilla.com>
2019-09-04 16:46:51 +0100

    missing comma

--- src/olympia/migrations/1144-db-schema-app-support.sql
+++ src/olympia/migrations/1144-db-schema-app-support.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `appsupport`
-    MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT
+    MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     MODIFY `created` datetime(6) NOT NULL,
     MODIFY `modified` datetime(6) NOT NULL,
     MODIFY `addon_id` int(10) unsigned NOT NULL,
