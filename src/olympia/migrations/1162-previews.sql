ddca8dadb2c534de8c0b245c4ac8d3c6d492f362
Mathieu Pillard <mpillard@mozilla.com>
2019-09-06 14:35:57 +0200

    Migrations to bring dev/stage/prod columns and constraints closer to django
    Tables m to z, except for the following which are not included on purpose:
    - translations, update_counts, users, versions: waiting for next push window so that the migration step of the push doesn't take too much time
    - waffle_flag, waffle_flag_groups, waffle_flag_users, waffle_sample, waffle_switch: since those are coming from the external `waffle` app, need additional investigation into whether they should be done via schematic or via django-migrations

---
+++ src/olympia/migrations/1162-previews.sql
@@ -0,0 +1,15 @@
+ALTER TABLE `previews`
+    DROP FOREIGN KEY `previews_ibfk_1`,
+    DROP FOREIGN KEY `previews_ibfk_2`,
+    DROP INDEX `previews_ibfk_2`,
+    CHANGE COLUMN `modified` `modified` DATETIME (6) NOT NULL,
+    CHANGE COLUMN `id` `id` INT (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    CHANGE COLUMN `addon_id` `addon_id` INT (10) UNSIGNED NOT NULL,
+    CHANGE COLUMN `caption` `caption` INT (10) UNSIGNED DEFAULT NULL,
+    CHANGE COLUMN `position` `position` INT (11) NOT NULL,
+    CHANGE COLUMN `created` `created` DATETIME (6) NOT NULL,
+    ADD INDEX `previews_caption_f5d9791a_fk_translations_id` (`caption`),
+    ADD INDEX `previews_addon_id_320f2325_fk_addons_id` (`addon_id`),
+    ADD UNIQUE INDEX `caption` (`caption`),
+    ADD CONSTRAINT `previews_addon_id_320f2325_fk_addons_id` FOREIGN KEY (`addon_id`) REFERENCES `addons` (`id`),
+    ADD CONSTRAINT `previews_caption_f5d9791a_fk_translations_id` FOREIGN KEY (`caption`) REFERENCES `translations` (`id`);
