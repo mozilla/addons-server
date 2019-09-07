c6526bcf3e2e79543523a2367b275a2c787fefbe
Andrew Williamson <awilliamson@mozilla.com>
2019-09-07 07:49:06 +0100

    temporarily comment out part of migration 1151

--- src/olympia/migrations/1151-db-schema-collections.sql
+++ src/olympia/migrations/1151-db-schema-collections.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `collections`
+/* ALTER TABLE `collections`
     MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     MODIFY `created` datetime(6) NOT NULL,
     MODIFY `modified` datetime(6) NOT NULL,
@@ -10,12 +10,12 @@ ALTER TABLE `collections`
     MODIFY `addonCount` int(10) unsigned NOT NULL,
     DROP KEY `name`,
     DROP KEY `description`,
-    DROP FOREIGN KEY `collections_ibfk_4`,  /* `name` */
-    DROP FOREIGN KEY `collections_ibfk_5`,  /* `description` */
-    DROP FOREIGN KEY `collections_ibfk_7`,  /* `author_id` */
+    DROP FOREIGN KEY `collections_ibfk_4`,  /* `name` *
+    DROP FOREIGN KEY `collections_ibfk_5`,  /* `description` *
+    DROP FOREIGN KEY `collections_ibfk_7`,  /* `author_id` *
     ADD CONSTRAINT `collections_author_id_c9323760_fk_users_id` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
     ADD CONSTRAINT `collections_description_64108f9e_fk_translations_id` FOREIGN KEY (`description`) REFERENCES `translations` (`id`),
-    ADD CONSTRAINT `collections_name_c898e2c4_fk_translations_id` FOREIGN KEY (`name`) REFERENCES `translations` (`id`);
+    ADD CONSTRAINT `collections_name_c898e2c4_fk_translations_id` FOREIGN KEY (`name`) REFERENCES `translations` (`id`);*/
 
 ALTER TABLE `collections`
     ADD UNIQUE KEY `name` (`name`),
