4cd7fe9fcbc97ba273ae47e7a416cda507ebafb7
Andrew Williamson <awilliamson@mozilla.com>
2019-09-09 16:37:30 +0100

    Update 1144-db-schema-app-support.sql

--- src/olympia/migrations/1144-db-schema-app-support.sql
+++ src/olympia/migrations/1144-db-schema-app-support.sql
@@ -1,3 +1,9 @@
+/* There are some old entries in appsupport where the addon has already been hard-deleted so there is no matching id in addons.
+As we're adding a foreign key constraint to the table we need to first delete the invalid data or the ALTER TABLE fails.*/
+DELETE FROM `appsupport` USING `appsupport`
+    LEFT JOIN `addons` ON `appsupport`.`addon_id`=`addons`.id`
+    WHERE `addons`.`id` IS NULL AND `appsupport`.`addon_id` IS NOT NULL;
+
 ALTER TABLE `appsupport`
     MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     MODIFY `created` datetime(6) NOT NULL,
