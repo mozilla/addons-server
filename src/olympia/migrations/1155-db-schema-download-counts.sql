4e489ae893b4fa8b43b2ef69fb1f0e217be4d39e
Andrew Williamson <awilliamson@mozilla.com>
2019-09-09 16:12:37 +0100

    Update 1155-db-schema-download-counts.sql (#12299)

--- src/olympia/migrations/1155-db-schema-download-counts.sql
+++ src/olympia/migrations/1155-db-schema-download-counts.sql
@@ -1,3 +1,9 @@
+/* There are some old entries in download_counts where the addon has already been hard-deleted so there is no matching id in addons.
+As we're adding a foreign key constraint to the table we need to first delete the invalid data or the ALTER TABLE fails.*/
+DELETE FROM `download_counts` USING `download_counts`
+    LEFT JOIN `addons` ON `addon_id` = `addons`.`id`
+    WHERE `addons`.`id` IS NULL AND `addon_id` IS NOT NULL;
+
 ALTER TABLE `download_counts`
     MODIFY `addon_id` int(10) unsigned NOT NULL,
     MODIFY `count` int(10) unsigned NOT NULL,
