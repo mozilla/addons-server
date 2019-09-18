mozilla/addons-server

commit cfee9addcc87a8784f1ee100f775c759cddfd68b

Eliminate a few duplicate queries in reviewer tools
- Stop fetching all CommentLog and Version.
  This was done to display comments from hard-deleted versions, but versions
  have been soft-deleted for more than 3 years now.
- Stop re-fetching the add-on and re-calling its default transformer
  once per version being displayed (7 duplicate queries per version)
- Stop calling the Version transformer twice (2 duplicate queries per version)

src/olympia/reviewers/templates/reviewers/review.html
@@ -75,7 +75,7 @@
   <div class="results">
     <div class="results-inner">
       <table id="review-files" class="item-history">
-        {% for version in pager.object_list|reverse %}
+        {% for version in pager.object_list %}
         <tr class="listing-header">
           <th colspan="2">
             {% trans version = version.version, created = version.created|date, version_status = version_status(addon, version) %}

src/olympia/reviewers/views.py
@@ -1,7 +1,7 @@
 import json
 import time
 
-from collections import OrderedDict, defaultdict
+from collections import OrderedDict
 from datetime import date, datetime, timedelta
 
 from django import http
@@ -629,49 +629,6 @@ def queue_expired_info_requests(request):
                   qs=qs, SearchForm=None)
 
 
-def _get_comments_for_hard_deleted_versions(addon):
-    """Versions are soft-deleted now but we need to grab review history for
-    older deleted versions that were hard-deleted so the only record we have
-    of them is in the review log.  Hard deletion was pre Feb 2016.
-
-    We don't know if they were unlisted or listed but given the time overlap
-    they're most likely listed so we assume that."""
-    class PseudoVersion(object):
-        def __init__(self):
-            self.all_activity = []
-
-        all_files = ()
-        approval_notes = None
-        compatible_apps_ordered = ()
-        release_notes = None
-        status = 'Deleted'
-        deleted = True
-        channel = amo.RELEASE_CHANNEL_LISTED
-        is_ready_for_auto_approval = False
-
-        @property
-        def created(self):
-            return self.all_activity[0].created
-
-        @property
-        def version(self):
-            return (self.all_activity[0].activity_log
-                        .details.get('version', '[deleted]'))
-
-    comments = (CommentLog.objects
-                .filter(activity_log__action__in=amo.LOG_REVIEW_QUEUE,
-                        activity_log__versionlog=None,
-                        activity_log__addonlog__addon=addon)
-                .order_by('created')
-                .select_related('activity_log'))
-
-    comment_versions = defaultdict(PseudoVersion)
-    for c in comments:
-        c.version = c.activity_log.details.get('version', c.created)
-        comment_versions[c.version].all_activity.append(c)
-    return list(comment_versions.values())
-
-
 def perform_review_permission_checks(
         request, addon, channel, content_review_only=False):
     """Perform the permission checks needed by the review() view or anything
@@ -876,25 +833,24 @@ def review(request, addon, channel=None):
     # the comments form).
     actions_comments = [k for (k, a) in actions if a.get('comments', True)]
 
-    versions = (Version.unfiltered.filter(addon=addon, channel=channel)
-                                  .select_related('autoapprovalsummary')
-                                  .order_by('-created')
-                                  .transform(Version.transformer_activity)
-                                  .transform(Version.transformer))
-
-    # We assume comments on old deleted versions are for listed versions.
-    # See _get_comments_for_hard_deleted_versions above for more detail.
-    all_versions = (_get_comments_for_hard_deleted_versions(addon)
-                    if channel == amo.RELEASE_CHANNEL_LISTED else [])
-    all_versions.extend(versions)
-    all_versions.sort(key=lambda v: v.created,
-                      reverse=True)
+    versions_qs = (
+        # We want to load all Versions, even deleted ones, while using the
+        # addon.versions related manager to get `addon` property pre-cached on
+        # each version.
+        addon.versions(manager='unfiltered_for_relations')
+             .filter(channel=channel)
+             .select_related('autoapprovalsummary')
+             .order_by('created')
+        # Add activity transformer to prefetch all related activity logs on
+        # top of the regular transformers.
+             .transform(Version.transformer_activity)
+    )
 
     deleted_addon_ids = (
         ReusedGUID.objects.filter(guid=addon.guid).values_list(
             'addon_id', flat=True) if addon.guid else [])
 
-    pager = paginate(request, all_versions, 10)
+    pager = paginate(request, versions_qs, 10)
     num_pages = pager.paginator.num_pages
     count = pager.paginator.count
 

src/olympia/reviewers/tests/test_views.py
@@ -3037,59 +3037,6 @@ class TestReview(ReviewBase):
         self.grant_permission(self.reviewer, 'Addons:ReviewUnlisted')
         self.test_item_history(channel=amo.RELEASE_CHANNEL_UNLISTED)
 
-    def generate_deleted_versions(self):
-        self.addon = addon_factory(version_kw={
-            'version': '1.0', 'created': self.days_ago(1)})
-        self.url = reverse('reviewers.review', args=[self.addon.slug])
-
-        versions = ({'version': '0.1', 'action': 'comment',
-                     'comments': 'millenium hand and shrimp'},
-                    {'version': '0.1', 'action': 'public',
-                     'comments': 'buggrit'},
-                    {'version': '0.2', 'action': 'comment',
-                     'comments': 'I told em'},
-                    {'version': '0.3'})
-
-        for i, version_data in enumerate(versions):
-            version = version_factory(
-                addon=self.addon, version=version_data['version'],
-                created=self.days_ago(-i),
-                file_kw={'status': amo.STATUS_AWAITING_REVIEW})
-
-            if 'action' in version_data:
-                data = {'action': version_data['action'],
-                        'operating_systems': 'win',
-                        'applications': 'something',
-                        'comments': version_data['comments']}
-                self.client.post(self.url, data)
-                version.delete(hard=True)
-
-        self.addon.current_version.delete(hard=True)
-
-    @patch('olympia.reviewers.utils.sign_file')
-    def test_item_history_deleted(self, mock_sign):
-        self.generate_deleted_versions()
-
-        response = self.client.get(self.url)
-        assert response.status_code == 200
-        table = pq(response.content)('#review-files')
-
-        # Check the history for all versions.
-        ths = table.children('tr > th')
-        assert ths.length == 3  # The 2 with the same number will be coalesced.
-        assert '0.1' in ths.eq(0).text()
-        assert '0.2' in ths.eq(1).text()
-        assert '0.3' in ths.eq(2).text()
-        for idx in range(2):
-            assert 'Deleted' in ths.eq(idx).text()
-
-        bodies = table.children('.listing-body')
-        assert 'millenium hand and shrimp' in bodies.eq(0).text()
-        assert 'buggrit' in bodies.eq(0).text()
-        assert 'I told em' in bodies.eq(1).text()
-
-        assert mock_sign.called
-
     def test_item_history_compat_ordered(self):
         """ Make sure that apps in compatibility are ordered. """
         av = AppVersion.objects.all()[0]

commit c47ca83196955933f7d53edf918ac17121f6f666

Fix reviewer tools pagination regression from cfee9add
We're paginating versions by newest first, but want to display them
on each page in chronological order (oldest first).

src/olympia/reviewers/templates/reviewers/review.html
@@ -75,7 +75,8 @@
   <div class="results">
     <div class="results-inner">
       <table id="review-files" class="item-history">
-        {% for version in pager.object_list %}
+        {# We're paginating by newest first, but want to display the current page by oldest first #}
+        {% for version in pager.object_list|reverse %}
         <tr class="listing-header">
           <th colspan="2">
             {% trans version = version.version, created = version.created|date, version_status = version_status(addon, version) %}

src/olympia/reviewers/views.py
@@ -833,6 +833,9 @@ def review(request, addon, channel=None):
     # the comments form).
     actions_comments = [k for (k, a) in actions if a.get('comments', True)]
 
+    # Queryset to be paginated for versions. We use the default ordering to get
+    # most recently created first (Note that the template displays each page
+    # in reverse order, older first).
     versions_qs = (
         # We want to load all Versions, even deleted ones, while using the
         # addon.versions related manager to get `addon` property pre-cached on
@@ -840,7 +843,6 @@ def review(request, addon, channel=None):
         addon.versions(manager='unfiltered_for_relations')
              .filter(channel=channel)
              .select_related('autoapprovalsummary')
-             .order_by('created')
         # Add activity transformer to prefetch all related activity logs on
         # top of the regular transformers.
              .transform(Version.transformer_activity)

src/olympia/reviewers/tests/test_views.py
@@ -3016,6 +3016,33 @@ class TestReview(ReviewBase):
             assert ((reviewer_name == self.reviewer.name) or
                     (reviewer_name == self.other_reviewer.name))
 
+    def test_item_history_pagination(self):
+        addon = self.addons['Public']
+        addon.current_version.update(created=self.days_ago(366))
+        for i in range(0, 10):
+            # Add versions 1.0 to 1.9
+            version_factory(
+                addon=addon, version=f'1.{i}', created=self.days_ago(365 - i))
+        response = self.client.get(self.url)
+        assert response.status_code == 200
+        doc = pq(response.content)
+        table = doc('#review-files')
+        ths = table.children('tr > th')
+        assert ths.length == 10
+        # Original version should not be there any more, it's on the second
+        # page. Versions on the page should be displayed in chronological order
+        assert '1.0' in ths.eq(0).text()
+        assert '1.1' in ths.eq(1).text()
+        assert '1.9' in ths.eq(9).text()
+
+        response = self.client.get(self.url, {'page': 2})
+        assert response.status_code == 200
+        doc = pq(response.content)
+        table = doc('#review-files')
+        ths = table.children('tr > th')
+        assert ths.length == 1
+        assert '0.1' in ths.eq(0).text()
+
     def test_item_history_with_unlisted_versions_too(self):
         # Throw in an unlisted version to be ignored.
         version_factory(