d1bae8681dfb2831c34f1bf90c2707c15bcfdca8
Mathieu Pillard <diox@users.noreply.github.com>
2019-09-18 16:56:06 +0200

    Anonymize duplicate usernames and fix collation to be case-insensitive (#12375)
    Anonymize duplicate usernames and fix collation to be case-insensitive

---
+++ src/olympia/migrations/1212-unique-lowercase-usernames.sql
@@ -0,0 +1,11 @@
+/* Create a table with all usernames that have duplicates in lowercase. */
+CREATE TEMPORARY TABLE `duplicate_usernames`
+    SELECT LOWER(`username`) AS `username` FROM `users`
+        GROUP BY LOWER(`username`) HAVING COUNT(*) > 1;
+/* Update the username for all users in that table that have a username and lowercase username not being equal. */
+UPDATE `users`
+    SET `username`=CONCAT('anonymous-', MD5(RAND()), 32)
+    WHERE LOWER(`username`) != `username`
+    AND LOWER(`username`) IN (SELECT `username` FROM `duplicate_usernames`);
+/* Alter users, removing the utf8mb4_bin collation now that duplicates should be gone. */
+ALTER TABLE `users` CHANGE COLUMN `username` `username` VARCHAR (255) NOT NULL;
