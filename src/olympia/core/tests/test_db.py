6ba36ad19ce06260d340812187e66ad1466322f8
Andrew Williamson <awilliamson@mozilla.com>
2019-09-19 19:00:53 +0100

    remove the duplicate migrations test too

--- src/olympia/core/tests/test_db.py
+++ src/olympia/core/tests/test_db.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 import pytest
 
 from olympia.core.tests.db_tests_testapp.models import TestRegularCharField
@@ -15,15 +14,3 @@ def test_max_length_utf8mb4(value):
     TestRegularCharField.objects.create(name=value * 255)
 
     assert TestRegularCharField.objects.get().name == value * 255
-
-
-def test_no_duplicate_migration_ids():
-    seen = set()
-
-    migration_ids = [
-        fname.split('-')[0] for fname in os.listdir('src/olympia/migrations/')
-        if fname.endswith('.sql')]
-
-    duplicates = {x for x in migration_ids if x in seen or seen.add(x)}
-
-    assert not duplicates
