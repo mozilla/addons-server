ab41941da11bfdcaf2685b1f0a0adc774666468e
William Durand <will+git@drnd.me>
2019-09-19 16:12:16 +0200

    Allow valid dot dot files (#12382)

--- src/olympia/files/utils.py
+++ src/olympia/files/utils.py
@@ -712,7 +712,10 @@ def archive_member_validator(archive, member):
     """Validate a member of an archive member (TarInfo or ZipInfo)."""
     filename = getattr(member, 'filename', getattr(member, 'name', None))
     filesize = getattr(member, 'file_size', getattr(member, 'size', None))
+    _validate_archive_member_name_and_size(filename, filesize)
 
+
+def _validate_archive_member_name_and_size(filename, filesize):
     if filename is None or filesize is None:
         raise forms.ValidationError(ugettext('Unsupported archive type.'))
 
@@ -721,29 +724,23 @@ def archive_member_validator(archive, member):
     except UnicodeDecodeError:
         # We can't log the filename unfortunately since it's encoding
         # is obviously broken :-/
-        log.error('Extraction error, invalid file name encoding in '
-                  'archive: %s' % archive)
-        # L10n: {0} is the name of the invalid file.
-        msg = ugettext(
-            'Invalid file name in archive. Please make sure '
-            'all filenames are utf-8 or latin1 encoded.')
-        raise forms.ValidationError(msg.format(filename))
+        log.error('Extraction error, invalid file name encoding')
+        msg = ugettext('Invalid file name in archive. Please make sure '
+                       'all filenames are utf-8 or latin1 encoded.')
+        raise forms.ValidationError(msg)
 
-    if '..' in filename or filename.startswith('/'):
-        log.error('Extraction error, invalid file name (%s) in '
-                  'archive: %s' % (filename, archive))
+    if '../' in filename or '..' == filename or filename.startswith('/'):
+        log.error('Extraction error, invalid file name: %s' % (filename))
         # L10n: {0} is the name of the invalid file.
         msg = ugettext('Invalid file name in archive: {0}')
         raise forms.ValidationError(msg.format(filename))
 
     if filesize > settings.FILE_UNZIP_SIZE_LIMIT:
-        log.error('Extraction error, file too big (%s) for file (%s): '
-                  '%s' % (archive, filename, filesize))
+        log.error('Extraction error, file too big for file (%s): '
+                  '%s' % (filename, filesize))
         # L10n: {0} is the name of the invalid file.
-        raise forms.ValidationError(
-            ugettext(
-                'File exceeding size limit in archive: {0}'
-            ).format(filename))
+        msg = ugettext('File exceeding size limit in archive: {0}')
+        raise forms.ValidationError(msg.format(filename))
 
 
 class SafeZip(object):
