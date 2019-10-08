6a48d10793b312f803eee439e7cab5b642d69fc5
William Durand <will+git@drnd.me>
2019-10-08 18:09:00 +0200

    Add filter on yara admin (#12560)

--- src/olympia/yara/admin.py
+++ src/olympia/yara/admin.py
@@ -1,12 +1,41 @@
 import json
 
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from django.db.models import Q
 from django.utils.html import format_html
+from django.utils.translation import ugettext
 
 from olympia import amo
 from olympia.amo.urlresolvers import reverse
 
 from .models import YaraResult
+
+
+class MatchesFilter(SimpleListFilter):
+    title = ugettext('matches')
+    parameter_name = 'matches'
+
+    def lookups(self, request, model_admin):
+        return (
+            (None, 'With matched rules only'),
+            ('all', 'With/without matched rules'),
+        )
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        if self.value() == 'all':
+            return queryset
+        return queryset.filter(~Q(matches='[]'))
 
 
 @admin.register(YaraResult)
@@ -15,6 +44,7 @@ class YaraResultAdmin(admin.ModelAdmin):
     view_on_site = False
 
     list_display = ('id', 'formatted_addon', 'channel', 'matched_rules')
+    list_filter = (MatchesFilter,)
     list_select_related = ('version',)
 
     fields = ('id', 'upload', 'formatted_addon', 'channel',
