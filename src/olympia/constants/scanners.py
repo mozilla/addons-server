3184c035a555db9438f128c89d5e4434f72b3964
William Durand <will+git@drnd.me>
2019-10-21 17:38:28 +0200

    Add a Rule model (#12656)

--- src/olympia/constants/scanners.py
+++ src/olympia/constants/scanners.py
@@ -1,3 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+
+
 CUSTOMS = 1
 WAT = 2
 YARA = 3
@@ -6,4 +9,13 @@ SCANNERS = {
     CUSTOMS: 'customs',
     WAT: 'wat',
     YARA: 'yara',
+}
+
+# Action IDs are also used for severity (the higher, the more severe).
+NO_ACTION = 1
+FLAG_FOR_HUMAN_REVIEW = 20
+
+ACTIONS = {
+    NO_ACTION: _('no action'),
+    FLAG_FOR_HUMAN_REVIEW: _('flag for human review'),
 }
--- src/olympia/scanners/models.py
+++ src/olympia/scanners/models.py
@@ -1,44 +1,47 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from django_extensions.db.fields.json import JSONField
 
 from olympia.amo.models import ModelBase
-from olympia.constants.scanners import SCANNERS
+from olympia.constants.scanners import SCANNERS, ACTIONS, NO_ACTION
 from olympia.files.models import FileUpload
 
 
 class ScannerResult(ModelBase):
-    upload = models.ForeignKey(FileUpload,
-                               related_name='scanners_results',
-                               on_delete=models.SET_NULL,
-                               null=True)
+    upload = models.ForeignKey(
+        FileUpload,
+        related_name='scanners_results',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     # Store the "raw" results of a scanner (optionally).
     results = JSONField(default=None)
     scanner = models.PositiveSmallIntegerField(choices=SCANNERS.items())
-    version = models.ForeignKey('versions.Version',
-                                related_name='scanners_results',
-                                on_delete=models.CASCADE,
-                                null=True)
+    version = models.ForeignKey(
+        'versions.Version',
+        related_name='scanners_results',
+        on_delete=models.CASCADE,
+        null=True,
+    )
     matches = JSONField(default=[])
     has_matches = models.NullBooleanField()
 
     class Meta:
         db_table = 'scanners_results'
         constraints = [
-            models.UniqueConstraint(fields=('upload', 'scanner', 'version'),
-                                    name='scanners_results_upload_id_scanner_'
-                                         'version_id_ad9eb8a6_uniq'),
+            models.UniqueConstraint(
+                fields=('upload', 'scanner', 'version'),
+                name='scanners_results_upload_id_scanner_'
+                'version_id_ad9eb8a6_uniq',
+            )
         ]
-        indexes = [
-            models.Index(fields=('has_matches',)),
-        ]
+        indexes = [models.Index(fields=('has_matches',))]
 
     def add_match(self, rule, tags=None, meta=None):
         """This method is used to store a matched rule."""
-        self.matches.append({
-            'rule': rule,
-            'tags': tags or [],
-            'meta': meta or {},
-        })
+        self.matches.append(
+            {'rule': rule, 'tags': tags or [], 'meta': meta or {}}
+        )
         self.has_matches = True
 
     def save(self, *args, **kwargs):
@@ -48,4 +51,24 @@ class ScannerResult(ModelBase):
 
     @property
     def matched_rules(self):
+        """This method returns a sorted list of matched rule names."""
         return sorted({match['rule'] for match in self.matches})
+
+
+class ScannerRule(ModelBase):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text=_('This is the exact name of the rule used by a scanner.'),
+    )
+    scanner = models.PositiveSmallIntegerField(choices=SCANNERS.items())
+    action = models.PositiveSmallIntegerField(
+        choices=ACTIONS.items(), default=NO_ACTION
+    )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'scanners_rules'
+
+    def __str__(self):
+        return self.name
