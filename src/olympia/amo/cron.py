3ad428538520b9fe42e49ba9012b56aef2835b5d
William Durand <will+git@drnd.me>
2019-10-25 12:11:51 +0200

    Delete scanner results when both upload and version are None

--- src/olympia/amo/cron.py
+++ src/olympia/amo/cron.py
@@ -15,6 +15,7 @@ from olympia.bandwagon.models import Collection
 from olympia.constants.base import VALID_ADDON_STATUSES, VALID_FILE_STATUSES
 from olympia.files.models import FileUpload
 from olympia.lib.es.utils import raise_if_reindex_in_progress
+from olympia.scanners.models import ScannerResult
 
 from . import tasks
 
@@ -56,6 +57,12 @@ def gc(test_result=True):
             except OSError:
                 pass
         file_upload.delete()
+
+    stale_scanner_results = ScannerResult.objects.filter(
+        upload=None, version=None
+    ).order_by('id')
+    for scanner_result in stale_scanner_results:
+        scanner_result.delete()
 
 
 def category_totals():
