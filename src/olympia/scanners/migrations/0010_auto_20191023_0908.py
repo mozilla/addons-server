44de357acedc197e5bd761384fb53ed6857df9f0
William Durand <will+git@drnd.me>
2019-10-25 15:47:42 +0200

    Fix scanners migration (#12714)

--- src/olympia/scanners/migrations/0010_auto_20191023_0908.py
+++ src/olympia/scanners/migrations/0010_auto_20191023_0908.py
@@ -3,14 +3,9 @@
 from django.db import migrations
 
 
-def add_matched_rules(apps, schema_editor):
-    ScannerResult = apps.get_model('scanners', 'ScannerResult')
-    for result in ScannerResult.objects.filter(has_matches=True):
-        result.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [('scanners', '0009_auto_20191023_0906')]
 
-    operations = [migrations.RunPython(add_matched_rules)]
+    # Nothing to do...
+    operations = []
