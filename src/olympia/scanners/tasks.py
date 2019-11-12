217bf05040c2d4b27d8eac394473ba5fb131d078
Mathieu Pillard <mpillard@mozilla.com>
2019-11-12 16:18:24 +0100

    Always execute "flag for human review" action in all scanner actions

--- src/olympia/scanners/tasks.py
+++ src/olympia/scanners/tasks.py
@@ -212,6 +212,8 @@ def _flag_for_human_review(version):
 
 def _delay_auto_approval(version):
     """Delay auto-approval for the whole add-on for 24 hours."""
+    # Always flag for human review.
+    _flag_for_human_review(version)
     in_twenty_four_hours = datetime.now() + timedelta(hours=24)
     AddonReviewerFlags.objects.update_or_create(
         addon=version.addon,
@@ -220,6 +222,8 @@ def _delay_auto_approval(version):
 
 def _delay_auto_approval_indefinitely(version):
     """Delay auto-approval for the whole add-on indefinitely."""
+    # Always flag for human review.
+    _flag_for_human_review(version)
     AddonReviewerFlags.objects.update_or_create(
         addon=version.addon,
         defaults={'auto_approval_delayed_until': datetime.max})
