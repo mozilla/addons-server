1d9b451b70e2e0c2962bcbad99f40ff17bf1bf7e
Andrew Williamson <awilliamson@mozilla.com>
2019-12-10 14:30:58 +0000

    partially obfuscate blocklist data

--- src/olympia/blocklist/tests/test_commands.py
+++ src/olympia/blocklist/tests/test_commands.py
@@ -41,17 +41,17 @@ class TestImportBlocklist(TestCase):
 
     def test_regex(self):
         """ Test regex style "guids" are parsed and expanded to blocks."""
-        addon_factory(guid='_qdMembers_@extmys.mysearch.com')
-        addon_factory(guid='_r4Members_@install.salah-time.com')
-        addon_factory(guid='{90ac1d06-caf8-46b9-9325-59c82190b687}')
+        addon_factory(guid='_qdNembers_@exmys.myysarch.com')
+        addon_factory(guid='_dqMNemberstst_@www.dowespedtgttest.com')
+        addon_factory(guid='{90ac2d06-caf8-46b9-5325-59c82190b687}')
         addon_factory()
         call_command('import_blocklist')
         assert Block.objects.count() == 3
         blocks = list(Block.objects.all())
         this_block = blocklist_json['data'][0]
-        assert blocks[0].guid == '_qdMembers_@extmys.mysearch.com'
-        assert blocks[1].guid == '_r4Members_@install.salah-time.com'
-        assert blocks[2].guid == '{90ac1d06-caf8-46b9-9325-59c82190b687}'
+        assert blocks[0].guid == '_qdNembers_@exmys.myysarch.com'
+        assert blocks[1].guid == '_dqMNemberstst_@www.dowespedtgttest.com'
+        assert blocks[2].guid == '{90ac2d06-caf8-46b9-5325-59c82190b687}'
         # the rest of the metadata should be the same
         for block in blocks:
             assert block.url == this_block['details']['bug']
@@ -65,14 +65,14 @@ class TestImportBlocklist(TestCase):
             assert block.modified == datetime(2019, 11, 29, 22, 22, 46, 785000)
 
     def test_single_guid(self):
-        addon_factory(guid='{99454877-875a-473e-a0c7-03ab910a8461}')
-        addon_factory(guid='Stark-vpn.5.14@firefox.com')
+        addon_factory(guid='{99454877-975a-443e-a0c7-03ab910a8461}')
+        addon_factory(guid='Ytarkovpn.5.14@firefox.com')
         addon_factory()
         call_command('import_blocklist')
         assert Block.objects.count() == 2
         blocks = list(Block.objects.all())
 
-        assert blocks[0].guid == '{99454877-875a-473e-a0c7-03ab910a8461}'
+        assert blocks[0].guid == '{99454877-975a-443e-a0c7-03ab910a8461}'
         assert blocks[0].url == blocklist_json['data'][1]['details']['bug']
         assert blocks[0].reason == blocklist_json['data'][1]['details']['why']
         assert blocks[0].min_version == (
@@ -83,7 +83,7 @@ class TestImportBlocklist(TestCase):
         assert blocks[0].include_in_legacy
         assert blocks[0].modified == datetime(2019, 11, 29, 15, 32, 56, 477000)
 
-        assert blocks[1].guid == 'Stark-vpn.5.14@firefox.com'
+        assert blocks[1].guid == 'Ytarkovpn.5.14@firefox.com'
         assert blocks[1].url == blocklist_json['data'][2]['details']['bug']
         assert blocks[1].reason == blocklist_json['data'][2]['details']['why']
         assert blocks[1].min_version == (
@@ -96,9 +96,9 @@ class TestImportBlocklist(TestCase):
 
     def test_target_application(self):
         fx_addon = addon_factory(
-            guid='mozilla_cc2.2@internetdownloadmanager.com')
+            guid='mozilla_ccc2.2@inrneg4gdownlomanager.com')
         # Block only for Thunderbird
-        addon_factory(guid='{0D2172E4-C5AE-465A-B80D-53A840275B5E}')
+        addon_factory(guid='{0D2172E4-C3AE-465A-B80D-53F840275B5E}')
 
         addon_factory()
         call_command('import_blocklist')
@@ -112,8 +112,8 @@ class TestImportBlocklist(TestCase):
     def test_bracket_escaping(self):
         """Some regexs don't escape the {} which is invalid in mysql regex.
         Check we escape it correctly."""
-        addon1 = addon_factory(guid='{f0af464e-5167-45cf-9cf0-66b396d1918c}')
-        addon2 = addon_factory(guid='{01e86e69-a2f8-48a0-b068-83869bdba3d0}')
+        addon1 = addon_factory(guid='{f0af364e-5167-45ca-9cf0-66b396d1918c}')
+        addon2 = addon_factory(guid='{01e26e69-a2d8-48a0-b068-87869bdba3d0}')
 
         addon_factory()
         call_command('import_blocklist')
