49d86540f258d93ec573a6c4490c8cfe20f89cb1
William Durand <will+git@drnd.me>
2020-01-13 13:15:57 +0100

    Add get_files_by_matched_rules() to ScannerResult
    This is a patch for https://github.com/mozilla/addons-server/issues/13220.
    I am not sure whether we can expose the JSON format used by customs, though..

--- src/olympia/scanners/models.py
+++ src/olympia/scanners/models.py
@@ -94,6 +94,24 @@ class AbstractScannerResult(ModelBase):
     def get_pretty_results(self):
         return json.dumps(self.results, indent=2)
 
+    def get_files_by_matched_rules(self):
+        res = dict()
+        if self.scanner is YARA:
+            for item in self.results:
+                if item['rule'] not in res:
+                    res[item['rule']] = []
+                res[item['rule']].append(item['meta']['filename'])
+        elif self.scanner is CUSTOMS:
+            scanMap = (self.results['scanMap'] if 'scanMap' in self.results
+                       else {})
+            for filename, rules in scanMap.items():
+                for ruleId, data in rules.items():
+                    if data.get('RULE_HAS_MATCHED', False):
+                        if ruleId not in res:
+                            res[ruleId] = []
+                        res[ruleId].append(filename)
+        return res
+
     def can_report_feedback(self):
         return (
             self.has_matches and self.state == UNKNOWN and self.scanner != WAT
