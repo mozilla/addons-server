aa4b371b41eeebae1a108507ce85f01201808be1
Mathieu Pillard <mpillard@mozilla.com>
2020-01-16 13:49:28 +0100

    Really fix migration conflict

--- src/olympia/scanners/migrations/0019_auto_20200113_1258.py
+++ src/olympia/scanners/migrations/0019_auto_20200113_1258.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scanners', '0017_auto_20200107_1650'),
+        ('scanners', '0018_auto_20200114_1359'),
     ]
 
     operations = [
