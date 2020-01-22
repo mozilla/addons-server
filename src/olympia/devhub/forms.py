4111e90688dee4e851335571758225fccea18cf9
Andreas Wagner <mail@andreaswagner.org>
2020-01-22 16:03:03 +0100

    Update save

--- src/olympia/devhub/forms.py
+++ src/olympia/devhub/forms.py
@@ -165,18 +165,18 @@ class AddonFormBase(TranslationFormMixin, forms.ModelForm):
                         .values_list('tag_text', flat=True))
 
     def save(self, *args, **kwargs):
-        metadata_content_review = waffle.switch_is_active(
-            'metadata-content-review')
+        metadata_content_review = (waffle.switch_is_active(
+            'metadata-content-review') and
+            self.instance and
+            self.instance.has_listed_versions())
         existing_data = (
             fetch_existing_translations_from_addon(
                 self.instance, self.fields_to_trigger_content_review)
-            if self.instance and
-            self.instance.has_listed_versions() and
-            metadata_content_review else {})
+            if metadata_content_review else {})
         obj = super().save(*args, **kwargs)
         if not metadata_content_review:
             return obj
-        if obj.has_listed_versions():
+        else:
             new_data = (
                 fetch_existing_translations_from_addon(
                     obj, self.fields_to_trigger_content_review)
