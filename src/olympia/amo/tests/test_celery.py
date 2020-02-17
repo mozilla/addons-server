2e3dd6b2d65ffd2d8e6b9923a897f660975b9a1d
Mathieu Pillard <mpillard@mozilla.com>
2020-02-17 13:48:58 +0100

    Let `default` be selected as a celery queue in explicit routing

--- src/olympia/amo/tests/test_celery.py
+++ src/olympia/amo/tests/test_celery.py
@@ -25,9 +25,6 @@ def test_celery_routes_in_queues():
     # check the default queue is defined in CELERY_QUEUES
     assert settings.CELERY_TASK_DEFAULT_QUEUE in queues_in_queues
 
-    # then remove it as it won't be in CELERY_ROUTES
-    queues_in_queues.remove(settings.CELERY_TASK_DEFAULT_QUEUE)
-
     queues_in_routes = set(
         [c['queue'] for c in settings.CELERY_TASK_ROUTES.values()])
     assert queues_in_queues == queues_in_routes
