5efe856b8108f473b6a12515aa357ba8043e49e3
Andrew Williamson <awilliamson@mozilla.com>
2020-03-16 21:42:15 +0000

    refactor test_serializer.py

--- src/olympia/blocklist/tests/test_serializers.py
+++ src/olympia/blocklist/tests/test_serializers.py
@@ -7,34 +7,30 @@ from olympia.blocklist.serializers import BlockSerializer
 
 
 class TestBlockSerializer(TestCase):
-    def test_basic(self):
-        block = Block.objects.create(
+    def setUp(self):
+        self.block = Block.objects.create(
             guid='foo@baa',
             min_version='45',
             reason='something happened',
             url='https://goo.gol',
             updated_by=user_factory())
-        serializer = BlockSerializer(instance=block)
+
+    def test_basic(self):
+        serializer = BlockSerializer(instance=self.block)
         assert serializer.data == {
-            'id': block.id,
+            'id': self.block.id,
             'guid': 'foo@baa',
             'min_version': '45',
             'max_version': '*',
             'reason': 'something happened',
             'url': 'https://goo.gol',
-            'created': block.created.isoformat()[:-7] + 'Z',
-            'modified': block.modified.isoformat()[:-7] + 'Z',
+            'created': self.block.created.isoformat()[:-7] + 'Z',
+            'modified': self.block.modified.isoformat()[:-7] + 'Z',
         }
 
     def test_wrap_outgoing_links(self):
-        block = Block.objects.create(
-            guid='foo@baa',
-            min_version='45',
-            reason='something happened',
-            url='https://goo.gol',
-            updated_by=user_factory())
         request = APIRequestFactory().get('/', {'wrap_outgoing_links': 1})
         serializer = BlockSerializer(
-            instance=block, context={'request': request})
+            instance=self.block, context={'request': request})
 
-        assert serializer.data['url'] == get_outgoing_url(block.url)
+        assert serializer.data['url'] == get_outgoing_url(self.block.url)
