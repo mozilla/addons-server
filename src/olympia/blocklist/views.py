d54dc781dce3f6398bf7d004065cc05de651302b
Andrew Williamson <awilliamson@mozilla.com>
2020-03-17 09:23:21 +0000

    add blocklist/block api (#13737)
    * add blocklist/block api
    
    * refactor test_serializer.py

---
+++ src/olympia/blocklist/views.py
@@ -0,0 +1,16 @@
+from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+
+from .models import Block
+from .serializers import BlockSerializer
+
+
+class BlockViewSet(RetrieveModelMixin, GenericViewSet):
+    queryset = Block.objects
+    serializer_class = BlockSerializer
+
+    def get_object(self):
+        identifier = self.kwargs.pop('pk')
+        self.lookup_field = 'pk' if identifier.isdigit() else 'guid'
+        self.kwargs[self.lookup_field] = identifier
+        return super().get_object()
