1152b0784de0839813c9451012121f7c9ed84fe7
Andrew Williamson <awilliamson@mozilla.com>
2020-03-20 11:17:24 +0000

    Expose add-on name in blocklist/block api (#13786)
    * Expose add-on name in blocklist/block api
    
    * add a generic test in api/tests/test_fields.py

--- src/olympia/blocklist/tests/test_views.py
+++ src/olympia/blocklist/tests/test_views.py
@@ -1,4 +1,6 @@
-from olympia.amo.tests import reverse_ns, TestCase, user_factory
+from django.test.utils import override_settings
+
+from olympia.amo.tests import addon_factory, reverse_ns, TestCase, user_factory
 from olympia.amo.urlresolvers import get_outgoing_url
 from olympia.blocklist.models import Block
 from olympia.blocklist.serializers import BlockSerializer
@@ -30,3 +32,30 @@ class TestBlockViewSet(TestCase):
         response = self.client.get(url + '?wrap_outgoing_links')
         assert response.status_code == 200
         assert response.json()['url'] == get_outgoing_url(self.block.url)
+
+    def test_addon_name(self):
+        url = reverse_ns(
+            'blocklist-block-detail',
+            args=(self.block.guid,),
+            api_version='v5')
+        addon = addon_factory(
+            guid=self.block.guid,
+            name='English name',
+            default_locale='en-CA')
+        addon.name = {'fr': 'Lé name Francois'}
+        addon.save()
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert response.json()['addon_name'] == {
+            'en-CA': 'English name',
+            'fr': 'Lé name Francois'}
+
+        url += '?lang=de-DE'
+        response = self.client.get(url)
+        assert response.json()['addon_name'] == {'en-CA': 'English name'}
+
+        overridden_api_gates = {
+            'v5': ('l10n_flat_input_output',)}
+        with override_settings(DRF_API_GATES=overridden_api_gates):
+            response = self.client.get(url)
+        assert response.json()['addon_name'] == 'English name'
