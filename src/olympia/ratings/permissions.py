2bf5255c615885e6ac00bc4c9797c3ec69b05366
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2020-03-27 17:31:19 -0400

    added unit test for permissions.py, modified styles of permissions.py to satisfy pep8

--- src/olympia/ratings/permissions.py
+++ src/olympia/ratings/permissions.py
@@ -5,7 +5,7 @@ from olympia.access import acl
 
 
 def user_can_delete_rating(request, rating):
-    """Return whether or not the request.user can delete a rating.
+  """Return whether or not the request.user can delete a rating.
 
     People who can delete ratings:
       * The original rating author.
@@ -14,56 +14,56 @@ def user_can_delete_rating(request, rating):
       * Users in a group with "Users:Edit" or "Addons:Edit" privileges and
         they are not an author of this add-on.
     """
-    is_rating_author = (
-        request.user.is_authenticated and rating.user_id == request.user.id)
-    is_addon_author = rating.addon.has_author(request.user)
-    is_moderator = (
-        acl.action_allowed(request, amo.permissions.RATINGS_MODERATE) and
-        rating.editorreview
-    )
-    can_edit_users_or_addons = (
-        acl.action_allowed(request, amo.permissions.USERS_EDIT) or
-        acl.action_allowed(request, amo.permissions.ADDONS_EDIT)
-    )
+  is_rating_author = (
+      request.user.is_authenticated and rating.user_id == request.user.id)
+  is_addon_author = rating.addon.has_author(request.user)
+  is_moderator = (
+      acl.action_allowed(request, amo.permissions.RATINGS_MODERATE) and
+      rating.editorreview
+  )
+  can_edit_users_or_addons = (
+      acl.action_allowed(request, amo.permissions.USERS_EDIT) or
+      acl.action_allowed(request, amo.permissions.ADDONS_EDIT)
+  )
 
-    return (
-        is_rating_author or
-        (not is_addon_author and (is_moderator or can_edit_users_or_addons))
-    )
+  return (
+      is_rating_author or
+      (not is_addon_author and (is_moderator or can_edit_users_or_addons))
+  )
 
 
 def user_can_vote_rating(request, rating):
-    """Return whether or not the request.user can vote for a rating.
+  """Return whether or not the request.user can vote for a rating.
 
     People who can vote for ratings:
       * Not the original rating author.
       * Not the original add-on author.
     """
-    is_rating_author = (
-        request.user.is_authenticated and rating.user_id == request.user.id)
-    is_addon_author = rating.addon.has_author(request.user)
+  is_rating_author = (
+      request.user.is_authenticated and rating.user_id == request.user.id)
+  is_addon_author = rating.addon.has_author(request.user)
 
-    return (
-        (not is_rating_author) and
-        (not is_addon_author)
-    )
-
-
+  return (
+      (not is_rating_author) and
+      (not is_addon_author)
+  )
 
 
 class CanDeleteRatingPermission(BasePermission):
-    """A DRF permission class wrapping user_can_delete_rating()."""
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
+  """A DRF permission class wrapping user_can_delete_rating()."""
 
-    def has_object_permission(self, request, view, obj):
-        return user_can_delete_rating(request, obj)
+  def has_permission(self, request, view):
+    return request.user.is_authenticated
+
+  def has_object_permission(self, request, view, obj):
+    return user_can_delete_rating(request, obj)
 
 
-class CanVotePermission(BasePermission):
-    """A DRF permission class wrapping user_can_vote_rating()."""
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
+class CanVoteRatingPermission(BasePermission):
+  """A DRF permission class wrapping user_can_vote_rating()."""
 
-    def has_object_permission(self, request, view, obj):
-        return user_can_vote_rating(request, obj)
+  def has_permission(self, request, view):
+    return request.user.is_authenticated
+
+  def has_object_permission(self, request, view, obj):
+    return user_can_vote_rating(request, obj)
--- src/olympia/ratings/tests/test_permissions.py
+++ src/olympia/ratings/tests/test_permissions.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AnonymousUser
 from django.test import RequestFactory
 
 from olympia.amo.tests import TestCase
-from olympia.ratings.permissions import CanDeleteRatingPermission
+from olympia.ratings.permissions import CanDeleteRatingPermission, CanVoteRatingPermission
 from olympia.users.models import UserProfile
 
 
@@ -29,5 +29,30 @@ class TestCanDeleteRatingPermission(TestCase):
     @mock.patch('olympia.ratings.permissions.user_can_delete_rating')
     def test_has_object_permission_false(self, user_can_delete_rating_mock):
         user_can_delete_rating_mock.return_value = False
+        assert not self.perm.has_object_permission(
+            self.request, None, object())
+
+
+class TestCanVoteRatingPermission(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        self.request.user = AnonymousUser()
+        self.perm = CanVoteRatingPermission()
+
+    def test_has_permission_anonymous(self):
+        assert not self.perm.has_permission(self.request, None)
+
+    def test_has_permission_authenticated(self):
+        self.request.user = UserProfile()
+        assert self.perm.has_permission(self.request, None)
+
+    @mock.patch('olympia.ratings.permissions.user_can_vote_rating')
+    def test_has_object_permission(self, user_can_vote_rating_mock):
+        user_can_vote_rating_mock.return_value = True
+        assert self.perm.has_object_permission(self.request, None, object())
+
+    @mock.patch('olympia.ratings.permissions.user_can_delete_rating')
+    def test_has_object_permission_false(self, user_can_vote_rating_mock):
+        user_can_vote_rating_mock.return_value = False
         assert not self.perm.has_object_permission(
             self.request, None, object())
