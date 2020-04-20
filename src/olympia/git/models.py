ea71b1e477a77b0f5e8f912e9339768c9d1e22e8
William Durand <will+git@drnd.me>
2020-04-20 15:08:25 +0200

    Create GitExtractionQueue model

--- src/olympia/git/models.py
+++ src/olympia/git/models.py
@@ -9,7 +9,17 @@ class GitExtraction(ModelBase):
     This is an add-on related model that stores information related to the git
     extraction (for code-manager).
     """
+
     addon = models.OneToOneField(
         Addon, on_delete=models.CASCADE, primary_key=True
     )
     in_progress = models.BooleanField(default=False)
+
+
+class GitExtractionQueue(ModelBase):
+    """
+    This is a model that represents a queue of add-ons for git extraction. When
+    an add-on is in this queue, its versions should be extracted to a git repo.
+    """
+
+    addon = models.ForeignKey(Addon, on_delete=models.CASCADE, null=True)
