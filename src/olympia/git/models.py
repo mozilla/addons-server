9d5a5706e7e4739d00ee4f6ed1e0e9334a75651a
William Durand <will+git@drnd.me>
2020-04-22 17:59:17 +0200

    Add new `GitExtractionEntry` model (in new 'git' app) (#14017)

---
+++ src/olympia/git/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from olympia.addons.models import Addon
+from olympia.amo.models import ModelBase
+
+
+class GitExtractionEntry(ModelBase):
+    """
+    This is a model that represents an entry in a "queue" of add-ons scheduled
+    for git extraction. When an add-on is in this queue, its versions should be
+    extracted to a git repository.
+    """
+
+    addon = models.ForeignKey(Addon, on_delete=models.CASCADE)
+    in_progress = models.NullBooleanField(default=None)
+
+    class Meta(ModelBase.Meta):
+        unique_together = ('addon', 'in_progress')
