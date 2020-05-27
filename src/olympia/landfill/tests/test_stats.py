45d02a90a79e11dd059e3c52aeae6ede76b680a9
William Durand <will+git@drnd.me>
2020-05-27 12:01:39 +0200

    Generate stats data when generating fake add-ons

---
+++ src/olympia/landfill/tests/test_stats.py
@@ -0,0 +1,39 @@
+from olympia import amo
+from olympia.addons.models import Addon
+from olympia.amo.tests import TestCase
+from olympia.stats.models import DownloadCount, UpdateCount
+from olympia.landfill.stats import (
+    generate_download_counts,
+    generate_update_counts,
+)
+
+
+class TestGenerateDownloadCounts(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.addon = Addon.objects.create(type=amo.ADDON_EXTENSION)
+
+    def test_generate_download_counts(self):
+        x_days = 10
+
+        generate_download_counts(self.addon, x_days)
+
+        assert DownloadCount.objects.all().count() == x_days
+
+
+class TestGenerateUpdateCounts(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.addon = Addon.objects.create(type=amo.ADDON_EXTENSION)
+
+    def test_generate_update_counts(self):
+        x_days = 10
+
+        generate_update_counts(self.addon, x_days)
+
+        assert UpdateCount.objects.all().count() == x_days
+        update_count = UpdateCount.objects.all()[0]
+        assert 'Darwin' in update_count.oses
+        assert 'en-US' in update_count.locales
