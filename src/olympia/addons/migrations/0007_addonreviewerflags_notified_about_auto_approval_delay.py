dca7d8c024555c2710dd31636a4f8564cb2feaa7
Mathieu Pillard <mpillard@mozilla.com>
2020-06-01 11:50:19 +0200

    Move migration

--- src/olympia/addons/migrations/0007_addonreviewerflags_notified_about_auto_approval_delay.py
+++ src/olympia/addons/migrations/0007_addonreviewerflags_notified_about_auto_approval_delay.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('addons', '0004_auto_20191126_1712'),
+        ('addons', '0006_auto_20200527_0843'),
     ]
 
     operations = [
