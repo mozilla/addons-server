0aaeafcafac26fa4a65c8fe5d70071839ea6bda2
Andrew Williamson <awilliamson@mozilla.com>
2020-06-25 17:11:04 +0100

    seperate log.exception and raise with --log arg

--- src/olympia/zadmin/tasks.py
+++ src/olympia/zadmin/tasks.py
@@ -7,14 +7,17 @@ log = olympia.core.logger.getLogger('z.task')
 
 
 @task
-def celery_error(**kw):
+def celery_error(*, capture_and_log=False, **kw):
     """
     This task raises an exception from celery to test error logging and
     Sentry hookup.
     """
-    log.info('about to raise an exception from celery')
+    log.info('About to raise an exception from celery')
     try:
-        raise RuntimeError('this is an exception from celery')
+        raise RuntimeError('This is an exception from celery')
     except Exception as exception:
-        log.exception('Capturing exception as a log', exc_info=exception)
-        raise exception
+        if capture_and_log:
+            log.exception(
+                'Capturing celery exception as a log', exc_info=exception)
+        else:
+            raise exception
