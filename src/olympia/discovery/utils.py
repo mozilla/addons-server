64ca0226f0c8a36db1ce4b09fd4357dd485f829e
Lucky Sivagurunathan <25218389+luckyvs1@users.noreply.github.com>
2020-07-06 01:42:23 -0700

    Update logging of exception to not use custom formatting
    Co-authored-by: Andreas Wagner <mail@andreaswagner.org>

--- src/olympia/discovery/utils.py
+++ src/olympia/discovery/utils.py
@@ -52,7 +52,7 @@ def call_recommendation_server(server, client_id_or_guid, data, verb='get'):
         if response.status_code != 200:
             raise requests.exceptions.RequestException()
     except requests.exceptions.RequestException as e:
-        log.exception('Calling recommendation engine failed: %s' % e)
+        log.exception('Calling recommendation engine failed: %s', e)
         statsd.incr('services.recommendations.fail')
         return None
     else:
