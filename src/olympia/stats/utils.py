502ac913b0575df2d453cbd34f71638e67d719c0
William Durand <will+git@drnd.me>
2020-07-06 18:56:40 +0200

    Exclude null GUIDs in get_addons_and_average_daily_users_from_bigquery() (#14855)

--- src/olympia/stats/utils.py
+++ src/olympia/stats/utils.py
@@ -107,4 +107,7 @@ GROUP BY addon_id"""
 
     rows = client.query(query).result()
 
-    return [(row['addon_id'], row['count']) for row in rows if row['count']]
+    return [
+        (row['addon_id'], row['count'])
+        for row in rows if row['addon_id'] and row['count']
+    ]
