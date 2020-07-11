cd359261569e72641e8a72ac15cf5b32a8016b36
lvs1 <lvs1@sfu.ca>
2020-07-11 02:40:46 -0700

    Update assertion message to include additional system GUIDs

--- src/olympia/signing/tests/test_views.py
+++ src/olympia/signing/tests/test_views.py
@@ -329,7 +329,7 @@ class TestUploadVersion(BaseUploadVersionTestMixin, TestCase):
         assert response.data['error'] == (
             'You cannot submit an add-on using an ID ending with '
             '"@mozilla.com" or "@mozilla.org" or "@pioneer.mozilla.org" or '
-            '"@search.mozilla.org" or "@shield.mozilla.org"')
+            '"@search.mozilla.org" or "@shield.mozilla.org" or "@mozillaonline.com"')
 
     def test_system_addon_update_allowed(self):
         """Updates to system addons are allowed from anyone."""
