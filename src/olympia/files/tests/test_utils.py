9d00aee12d3a4e2cbd4b6a7b54d607136c8199e3
William Durand <will+git@drnd.me>
2020-07-17 11:47:40 +0200

    Revert "Do not allow trademarks in add-on name whatsoever (#14660)"
    This reverts commit 7ae1cadc3296a8b278314a05294e7b8d68fc8bc5.

--- src/olympia/files/tests/test_utils.py
+++ src/olympia/files/tests/test_utils.py
@@ -406,7 +406,7 @@ class TestManifestJSONExtractor(AppVersionsMixin, TestCase):
 
     @mock.patch('olympia.addons.models.resolve_i18n_message')
     @override_switch('content-optimization', active=False)
-    def test_mozilla_trademark_for_prefix_not_allowed(self, resolve_message):
+    def test_mozilla_trademark_for_prefix_allowed(self, resolve_message):
         resolve_message.return_value = 'Notify for Mozilla'
 
         addon = amo.tests.addon_factory()
@@ -415,8 +415,7 @@ class TestManifestJSONExtractor(AppVersionsMixin, TestCase):
             'src/olympia/files/fixtures/files/notify-link-clicks-i18n.xpi')
 
         with amo.tests.copy_file(fixture, file_obj.file_path):
-            with self.assertRaises(forms.ValidationError):
-                utils.parse_xpi(file_obj.file_path)
+            utils.parse_xpi(file_obj.file_path)
 
     def test_apps_use_default_versions_if_applications_is_omitted(self):
         """
