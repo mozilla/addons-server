891ca542507318ebb1e46d002265db3bc8cf75ff
Andrew Williamson <awilliamson@mozilla.com>
2020-07-23 19:30:17 +0100

    Update 0014_auto_20200717_1120.py

--- src/olympia/hero/migrations/0014_auto_20200717_1120.py
+++ src/olympia/hero/migrations/0014_auto_20200717_1120.py
@@ -13,7 +13,7 @@ def add_promoted_for_each_recommended(apps, schema_editor):
     for disco in DiscoveryItem.objects.all():
         group = RECOMMENDED if disco.recommendable else NOT_PROMOTED
         promoted = PromotedAddon.objects.create(
-            addon=disco_addon.addon, group_id=group.id)
+            addon=disco.addon, group_id=group.id)
         if hasattr(disco, 'primaryhero'):
             disco.primaryhero.promoted_addon = promoted
 
