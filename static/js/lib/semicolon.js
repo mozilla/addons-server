4db87672f5f76506e0284a8bfaba8806c3fc0702
William Durand <will+git@drnd.me>
2020-08-04 19:23:22 +0200

    Add semi-colon back (#15178)

--- static/js/lib/semicolon.js
+++ static/js/lib/semicolon.js
@@ -1,3 +1,4 @@
-/* This is just to fix jquery.pjax.js breaking our hokey concatenation and
- * minification process.
- */
+// prettier-ignore
+; /* This is just to fix jquery.pjax.js breaking our hokey concatenation and
+   * minification process.
+   */
