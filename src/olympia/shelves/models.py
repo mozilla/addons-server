d74bc529b3c4e027f30a0d05e4f3e007b83f3acd
xlisachan <xlisachan@gmail.com>
2020-08-07 13:57:20 -0400

    Updated migration file

--- src/olympia/shelves/models.py
+++ src/olympia/shelves/models.py
@@ -10,16 +10,16 @@ ENDPOINT_CHOICES = tuple((ty, ty) for ty in ENDPOINTS)
 class Shelf(ModelBase):
     title = models.CharField(max_length=200)
     endpoint = models.CharField(
-        max_length=200, choices=ENDPOINT_CHOICES)
+        max_length=200, choices=ENDPOINT_CHOICES, db_column='shelf_type')
     criteria = models.CharField(
         max_length=200,
-        help_text='<i>?recommended=true&sort=random&type=extension</i>')
+        help_text='e.g., ?recommended=true&sort=random&type=extension')
     footer_text = models.CharField(
         max_length=200, blank=True,
-        help_text='<i>See more recommended extensions</i>')
+        help_text='e.g., See more recommended extensions')
     footer_pathname = models.CharField(
         max_length=255, blank=True,
-        help_text='<i>collections/4757633/privacy-matters</i>')
+        help_text='e.g., collections/4757633/privacy-matters')
 
     class Meta:
         verbose_name_plural = 'shelves'
