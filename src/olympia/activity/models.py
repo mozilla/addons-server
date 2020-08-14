100faa13abbdfe85f245b05d129107c015c5d629
Andreas Wagner <mail@andreaswagner.org>
2020-08-14 15:53:29 +0200

    Update argument

--- src/olympia/activity/models.py
+++ src/olympia/activity/models.py
@@ -538,7 +538,7 @@ class ActivityLog(ModelBase):
                     validation = 'ignored'
 
                 file_ = self.f(u'<a href="{0}">{1}</a> (validation {2})',
-                               arg.get_url_path(arg.pk),
+                               arg.get_url_path(src=None),
                                arg.filename,
                                validation)
                 arguments.remove(arg)
