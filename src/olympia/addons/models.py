13ffc5431c0d75a9ac0a0f491b918813b1dc5a8f
William Durand <will+git@drnd.me>
2020-09-09 15:04:10 +0200

    Remove Addon.total_downloads field in model

--- src/olympia/addons/models.py
+++ src/olympia/addons/models.py
@@ -397,8 +397,6 @@ class Addon(OnChangeMixin, ModelBase):
         default=0, db_column='textreviewscount')
     weekly_downloads = models.PositiveIntegerField(
         default=0, db_column='weeklydownloads')
-    total_downloads = models.PositiveIntegerField(
-        default=0, null=True, db_column='totaldownloads')
     hotness = models.FloatField(default=0)
 
     average_daily_users = models.PositiveIntegerField(default=0)
