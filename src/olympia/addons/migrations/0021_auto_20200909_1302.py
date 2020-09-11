61d87fa193bb576a7e02e9363d38a3a3d912a02a
William Durand <will+git@drnd.me>
2020-09-11 08:54:41 +0200

    Fix conflict in addons migrations

--- src/olympia/addons/migrations/0021_auto_20200909_1302.py
+++ src/olympia/addons/migrations/0021_auto_20200909_1302.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("addons", "0019_auto_20200901_1459"),
+        ("addons", "0020_auto_20200805_1350"),
     ]
 
     operations = [
