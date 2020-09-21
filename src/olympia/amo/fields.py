bbaf05fe1b016c54ffdb20871ad8c55472a16da9
Rochisha Agarwal <45593458+rochisha0@users.noreply.github.com>
2020-09-21 21:49:40 +0530

    Replaced URLValidatorBackport with URLValidator (#15527)
    * changed
    
    * Revert "changed"
    Yes
    
    This reverts commit 932bdaa5a7987a3b3141c1a93ca88fbb811eff69.
    
    * rm urlvalidatorbackport
    
    * added line

--- src/olympia/amo/fields.py
+++ src/olympia/amo/fields.py
@@ -28,22 +28,13 @@ class PositiveAutoField(models.AutoField):
         return models.PositiveIntegerField().db_type(connection=connection)
 
 
-class URLValidatorBackport(URLValidator):
-    def __call__(self, value):
-        # stupid backport of https://github.com/django/django/commit/a9e188
-        try:
-            return super(URLValidatorBackport, self).__call__(value)
-        except ValueError:
-            raise exceptions.ValidationError(self.message, code=self.code)
-
-
 class HttpHttpsOnlyURLField(fields.URLField):
 
     def __init__(self, *args, **kwargs):
         super(HttpHttpsOnlyURLField, self).__init__(*args, **kwargs)
 
         self.validators = [
-            URLValidatorBackport(schemes=('http', 'https')),
+            URLValidator(schemes=('http', 'https')),
             # Reject AMO URLs, see:
             # https://github.com/mozilla/addons-server/issues/9012
             RegexValidator(
