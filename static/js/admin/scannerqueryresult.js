f6ad686a352a33ec918850c0059be98715a6588f
Mathieu Pillard <mpillard@mozilla.com>
2020-09-23 14:18:47 +0200

    Group scanner query results by add-on and channel
    The way django generates the changelist makes it a bit tricky to
    customize, so we run some javascript once the dom is loaded to tweak
    the rowspans, remove duplicate cells and add relevant css classnames.

---
+++ static/js/admin/scannerqueryresult.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+  if (!document.body.matches('.change-list.model-scannerqueryresult')) {
+    // This is only for the scanners query result change list page.
+    return;
+  }
+
+  let current_addon = {
+    cell: null,
+    link: null,
+  };
+
+  for (row of result_list.tBodies[0].rows) {
+    let new_addon = {
+      cell: row.querySelector('.field-formatted_addon'),
+      link: row.querySelector('.field-formatted_addon a'),
+    };
+
+    // FIXME: for performance, it might be better to store the elements we
+    // want to change in a list and change them in one go outside of this
+    // loop ?
+    if (new_addon.link) {
+      let classNames = [];
+
+      if (
+        current_addon.cell &&
+        current_addon.link &&
+        new_addon.link.text == current_addon.link.text
+      ) {
+        // That new row contains the same add-on as the previous one.
+        classNames.push('same-addon');
+        if (new_addon.link.href == current_addon.link.href) {
+          // If it's the same link as before, we remove the cell and
+          // increment the rowspwan of the first cell of the row we
+          // found for that add-on.
+          current_addon.cell.rowSpan++;
+          new_addon.cell.parentElement.removeChild(new_addon.cell);
+        } else {
+          // If it's not the same link, then we have just changed
+          // channels for the same add-on. We don't do anything to
+          // the cell, but add a classname to differentiate.
+          classNames.push('different-channel');
+          // The "new_addon" need to be updated so that we
+          // "start" the row from there.
+          current_addon = {
+            cell: new_addon.cell,
+            link: new_addon.link,
+          };
+        }
+      } else {
+        // New add-on, we add a different class to the row and store
+        // this add-on as the current one.
+        classNames.push('different-addon');
+        current_addon = {
+          cell: new_addon.cell,
+          link: new_addon.link,
+        };
+      }
+      // Add the className(s) now that we're done determining what we
+      // need.
+      if (classNames) {
+        row.classList.add(...classNames);
+      }
+    }
+  }
+});
