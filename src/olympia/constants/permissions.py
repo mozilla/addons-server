46243ea51bb8db6e205f5b50fa9ae80eede8604b
Andreas Wagner <mail@andreaswagner.org>
2020-09-24 15:17:34 +0200

    Re-add permission

--- src/olympia/constants/permissions.py
+++ src/olympia/constants/permissions.py
@@ -15,6 +15,9 @@ ANY_ADMIN = AclPermission('Admin', '%')
 # access to anything.
 SUPERPOWERS = AclPermission('*', '*')
 
+# Can access admin-specific tools.
+ADMIN_TOOLS = AclPermission('Admin', 'Tools')
+
 # Can modify editorial content on the site.
 ADMIN_CURATION = AclPermission('Admin', 'Curation')
 # Can edit the properties of any add-on (pseduo-admin).
