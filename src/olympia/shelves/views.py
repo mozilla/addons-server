8ea3e2f62ea64809a777672401e3193eaa2970d0
xlisachan <xlisachan@gmail.com>
2020-09-29 18:53:04 -0400

    Added permission_classes back in

--- src/olympia/shelves/views.py
+++ src/olympia/shelves/views.py
@@ -7,4 +7,5 @@ from olympia.shelves.serializers import ShelfSerializer
 class ShelfViewSet(viewsets.ModelViewSet):
     queryset = Shelf.objects.filter(
         shelfmanagement__enabled=True).order_by('shelfmanagement__position')
+    permission_classes = []
     serializer_class = ShelfSerializer
