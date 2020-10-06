6bdae0385e0a4c353229cd4798a3e53a4b02dbe4
Andrew Williamson <awilliamson@mozilla.com>
2020-10-06 15:30:39 +0100

    handle promoted addons in AddonAutocompleteSearchView

--- src/olympia/addons/tests/test_views.py
+++ src/olympia/addons/tests/test_views.py
@@ -1950,7 +1950,7 @@ class TestAddonAutoCompleteSearchView(ESTestCase):
         qset = view.get_queryset()
 
         includes = set((
-            'default_locale', 'icon_type', 'id', 'modified',
+            'current_version', 'default_locale', 'icon_type', 'id', 'modified',
             'name_translations', 'promoted', 'slug', 'type'))
 
         assert set(qset.to_dict()['_source']['includes']) == includes
@@ -2007,6 +2007,23 @@ class TestAddonAutoCompleteSearchView(ESTestCase):
             data = self.perform_search(self.url, {'q': 'my', 'sort': 'users'})
             assert {itm['id'] for itm in data['results']} == {
                 addon.pk, addon2.pk}
+
+    def test_promoted(self):
+        not_promoted = addon_factory(name='not promoted')
+        sponsored = addon_factory(name='is promoted')
+        self.make_addon_promoted(sponsored, VERIFIED_ONE, approve_version=True)
+        addon_factory(name='something')
+
+        self.refresh()
+
+        data = self.perform_search(self.url, {'q': 'promoted'})  # No db query.
+        assert 'count' not in data
+        assert 'next' not in data
+        assert 'prev' not in data
+        assert len(data['results']) == 2
+
+        assert {itm['id'] for itm in data['results']} == {
+            not_promoted.pk, sponsored.pk}
 
 
 class TestAddonFeaturedView(ESTestCase):
