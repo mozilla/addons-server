024628c23721c88b36c36f476cd60109abea74f5
Mathieu Pillard <diox@users.noreply.github.com>
2020-10-07 17:06:43 +0200

    Use regular download URL in file admin (#15668)
    * Use regular download URL in file admin
    
    Since 7bef1c187e85a8d2cfbf8798c0314b69131f1da3 last year, the regular
    download views supports deleted/disabled/unlisted files/addons (provided
    the user has relevant permissions) so we don't need the extra download
    view in files/admin.py
    
    Bonus: pass attachment=True to get Content-Disposition: attachment
    added to the download response to avoid triggering add-on install
    from Firefox.
    
    * Remove extra imports

--- src/olympia/files/admin.py
+++ src/olympia/files/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from django.conf.urls import url
-from django.shortcuts import get_object_or_404
-
-from olympia.versions.views import download_file
-from olympia.addons.models import Addon
-from olympia.amo.urlresolvers import reverse
 
 from .models import File
 
@@ -54,24 +48,7 @@ class FileAdmin(admin.ModelAdmin):
     def file_download_url(self, instance):
         return format_html(
             '<a href="{}">Download file</a>',
-            reverse('admin:files_file_download', args=[instance.pk]))
+            instance.get_absolute_url(attachment=True))
 
     file_download_url.short_description = u'Download this file'
     file_download_url.allow_tags = True
-
-    def get_urls(self):
-        urls = super(FileAdmin, self).get_urls()
-        custom_urls = [
-            url(
-                r'^([^\/]+?)/download/$',
-                self.admin_site.admin_view(self.download_view),
-                name='files_file_download'),
-        ]
-
-        return custom_urls + urls
-
-    def download_view(self, request, file_id):
-        file_ = get_object_or_404(File.objects, pk=file_id)
-        addon = get_object_or_404(Addon.unfiltered,
-                                  pk=file_.version.addon_id)
-        return download_file(request, file_id, file_=file_, addon=addon)
--- src/olympia/files/tests/test_admin.py
+++ src/olympia/files/tests/test_admin.py
@@ -1,12 +1,7 @@
-from django.conf import settings
 from django.utils.encoding import force_text
-from django.utils.http import urlquote
 
-from olympia import amo
 from olympia.amo.tests import TestCase, addon_factory, user_factory
 from olympia.amo.urlresolvers import reverse
-from olympia.amo.templatetags.jinja_helpers import user_media_url
-from olympia.amo.utils import urlparams
 
 
 class TestFileAdmin(TestCase):
@@ -82,76 +77,5 @@ class TestFileAdmin(TestCase):
         response = self.client.get(detail_url, follow=True)
         assert response.status_code == 200
 
-        expected_url = reverse('admin:files_file_download', args=(file_.pk,))
+        expected_url = file_.get_absolute_url(attachment=True)
         assert expected_url in force_text(response.content)
-
-    def test_download_view(self):
-        """Regular listed files are served through the CDN"""
-        addon = addon_factory()
-        file_ = addon.current_version.all_files[0]
-        user = user_factory()
-        self.grant_permission(user, 'Admin:Tools')
-        self.grant_permission(user, 'Admin:Advanced')
-        self.client.login(email=user.email)
-
-        download_url = reverse('admin:files_file_download', args=(file_.pk,))
-        response = self.client.get(download_url, follow=False)
-        assert response.status_code == 302
-
-        path = user_media_url('addons')
-
-        assert response.url == (
-            urlparams('%s%s/%s' % (
-                path, addon.id, urlquote(file_.filename)
-            ), filehash=file_.hash))
-        assert response['X-Target-Digest'] == file_.hash
-
-    def test_download_view_disabled_file(self):
-        """Disabled files are not served through the CDN"""
-        addon = addon_factory()
-        file_ = addon.current_version.all_files[0]
-        file_.update(status=amo.STATUS_DISABLED)
-        user = user_factory()
-        self.grant_permission(user, 'Admin:Tools')
-        self.grant_permission(user, 'Admin:Advanced')
-
-        # The default add-on  created by `addon_factory` is listed
-        # and we still require proper reviewers permission
-        self.grant_permission(user, 'Addons:ReviewListed')
-        self.grant_permission(user, 'Addons:Review')
-
-        self.client.login(email=user.email)
-
-        download_url = reverse('admin:files_file_download', args=(file_.pk,))
-        response = self.client.get(download_url, follow=True)
-        assert response.status_code == 200
-        assert response[settings.XSENDFILE_HEADER]
-        assert response['Content-Type'] == 'application/x-xpinstall'
-
-    def test_download_view_disabled_file_permission_denied(self):
-        """Disabled files are not served through the CDN"""
-        addon = addon_factory()
-        file_ = addon.current_version.all_files[0]
-        file_.update(status=amo.STATUS_DISABLED)
-        user = user_factory()
-        self.grant_permission(user, 'Admin:Tools')
-
-        self.client.login(email=user.email)
-
-        download_url = reverse('admin:files_file_download', args=(file_.pk,))
-        response = self.client.get(download_url, follow=True)
-        assert response.status_code == 404
-
-    def test_download_view_listed_public_file_permission_denied(self):
-        """Disabled files are not served through the CDN"""
-        addon = addon_factory()
-        file_ = addon.current_version.all_files[0]
-        file_.update()
-        user = user_factory()
-        self.grant_permission(user, 'Admin:Tools')
-
-        self.client.login(email=user.email)
-
-        download_url = reverse('admin:files_file_download', args=(file_.pk,))
-        response = self.client.get(download_url, follow=True)
-        assert response.status_code == 404
