80f7e7c54e26d36b41ea239102650e2b7e274ae0
Mathieu Pillard <mpillard@mozilla.com>
2020-11-03 12:17:29 +0100

    Quote table & column names in reviewer reports SQL

--- src/olympia/reviewers/management/commands/review_reports_sql/addon/risk.sql
+++ src/olympia/reviewers/management/commands/review_reports_sql/addon/risk.sql
@@ -1,34 +1,34 @@
-SELECT risk_category AS `Risk Category`,
+SELECT `risk_category` AS `Risk Category`,
        IFNULL(FORMAT(SUM(n), 0), 0) AS `All Reviewers`,
        IFNULL(FORMAT(SUM(CASE WHEN `group_category` = 'volunteer' THEN n ELSE 0 END), 0), 0) AS 'Volunteers'
 FROM
   (SELECT CASE
-              WHEN weight > @RISK_HIGHEST THEN 'highest'
-              WHEN weight > @RISK_HIGH THEN 'high'
-              WHEN weight > @RISK_MEDIUM THEN 'medium'
+              WHEN `weight` > @RISK_HIGHEST THEN 'highest'
+              WHEN `weight` > @RISK_HIGH THEN 'high'
+              WHEN `weight` > @RISK_MEDIUM THEN 'medium'
               ELSE 'low'
-          END AS risk_category,
-          group_category,
-          COUNT(*) AS n
-   FROM editors_autoapprovalsummary aa
+          END AS `risk_category`,
+          `group_category`,
+          COUNT(*) AS `n`
+   FROM `editors_autoapprovalsummary` `aa`
    JOIN
-     (SELECT version_id,
-             CASE WHEN user_id NOT IN
-        (SELECT user_id
-         FROM groups_users
-         WHERE group_id IN
-             (SELECT id
-              FROM groups
-              WHERE name = 'No Reviewer Incentives')) THEN 'volunteer' ELSE 'all' END AS `group_category`
-      FROM reviewer_scores rs
-      JOIN users u ON u.id = rs.user_id
-      WHERE DATE(rs.created) BETWEEN @WEEK_BEGIN AND @WEEK_END
-        AND u.deleted = 0
+     (SELECT `version_id`,
+             CASE WHEN `user_id` NOT IN
+        (SELECT `user_id`
+         FROM `groups_users`
+         WHERE `group_id` IN
+             (SELECT `id`
+              FROM `groups`
+              WHERE `name` = 'No Reviewer Incentives')) THEN 'volunteer' ELSE 'all' END AS `group_category`
+      FROM `reviewer_scores` `rs`
+      JOIN `users` `u` ON `u`.`id` = `rs`.`user_id`
+      WHERE DATE(`rs`.`created`) BETWEEN @WEEK_BEGIN AND @WEEK_END
+        AND `u`.`deleted` = 0
         /* Filter out internal task user */
-        AND user_id <> 4757633
+        AND `user_id` <> 4757633
         /* The type of review, see constants/reviewers.py */
-        AND rs.note_key IN (10, 12, 20, 22, 30, 32, 50, 52, 102, 103, 104, 105)) reviews ON reviews.version_id = aa.version_id
-   GROUP BY risk_category,
-            `group_category`) risk
+        AND `rs`.`note_key` IN (10, 12, 20, 22, 30, 32, 50, 52, 102, 103, 104, 105)) `reviews` ON `reviews`.`version_id` = `aa`.`version_id`
+   GROUP BY `risk_category`,
+            `group_category`) `risk`
 GROUP BY 1
-ORDER BY FIELD(risk_category,'highest','high','medium', 'low');
+ORDER BY FIELD(`risk_category`,'highest','high','medium', 'low');
--- src/olympia/reviewers/management/commands/review_reports_sql/content/weekly.sql
+++ src/olympia/reviewers/management/commands/review_reports_sql/content/weekly.sql
@@ -1,29 +1,29 @@
-SELECT IFNULL(u.display_name, CONCAT('Firefox user ', u.id)) AS `Name`,
+SELECT IFNULL(`u`.`display_name`, CONCAT('Firefox user ', `u`.`id`)) AS `Name`,
        IF(
-            (SELECT DISTINCT(user_id)
-             FROM groups_users
-             WHERE group_id IN
-                 (SELECT id
-                  FROM groups
-                  WHERE name = 'No Reviewer Incentives')
-               AND user_id = rs.user_id), '*', '') AS `Staff`,
+            (SELECT DISTINCT(`user_id`)
+             FROM `groups_users`
+             WHERE `group_id` IN
+                 (SELECT `id`
+                  FROM `groups`
+                  WHERE `name` = 'No Reviewer Incentives')
+               AND `user_id` = `rs`.`user_id`), '*', '') AS `Staff`,
        IFNULL(IF(
-                   (SELECT DISTINCT(user_id)
-                    FROM groups_users
-                    WHERE group_id IN
-                        (SELECT id
-                         FROM groups
-                         WHERE name = 'No Reviewer Incentives')
-                      AND user_id = rs.user_id), '-', SUM(rs.score)), 0) AS `Points`,
+                   (SELECT DISTINCT(`user_id`)
+                    FROM `groups_users`
+                    WHERE `group_id` IN
+                        (SELECT `id`
+                         FROM `groups`
+                         WHERE `name` = 'No Reviewer Incentives')
+                      AND `user_id` = `rs`.`user_id`), '-', SUM(`rs`.`score`)), 0) AS `Points`,
        FORMAT(COUNT(*), 0) AS `Add-ons Reviewed`
-FROM reviewer_scores rs 
-LEFT JOIN editors_autoapprovalsummary aa ON aa.version_id = rs.version_id
-JOIN users u ON u.id = rs.user_id
-WHERE DATE(rs.created) BETWEEN @WEEK_BEGIN AND @WEEK_END
-  AND u.deleted = 0
+FROM `reviewer_scores` `rs`
+LEFT JOIN `editors_autoapprovalsummary` `aa` ON `aa`.`version_id` = `rs`.`version_id`
+JOIN `users` `u` ON `u`.`id` = `rs`.`user_id`
+WHERE DATE(`rs`.`created`) BETWEEN @WEEK_BEGIN AND @WEEK_END
+  AND `u`.`deleted` = 0
   /* Filter out internal task user */
-  AND user_id <> 4757633
+  AND `user_id` <> 4757633
   /* The type of review, see constants/reviewers.py */
-  AND rs.note_key IN (101)
-GROUP BY user_id HAVING `Add-ons Reviewed` >= 10
+  AND `rs`.`note_key` IN (101)
+GROUP BY `user_id` HAVING `Add-ons Reviewed` >= 10
 ORDER BY `Add-ons Reviewed` DESC;
