8cc88443f9795a7435887d8272e2695749fe4726
Andrew Williamson <awilliamson@mozilla.com>
2020-11-30 14:43:08 +0000

    switch adzerk decision endpoint to use multi-winner (#16040)

--- src/olympia/shelves/tests/test_utils.py
+++ src/olympia/shelves/tests/test_utils.py
@@ -62,27 +62,18 @@ def test_call_adzerk_server(statsd_mock):
     data = {
         'placements': [
             {
-                "divName": 'div0',
+                "divName": 'multi',
                 "networkId": network_id,
                 "siteId": site_id,
-                "adTypes": [5]},
-            {
-                "divName": 'div1',
-                "networkId": network_id,
-                "siteId": site_id,
-                "adTypes": [5]},
-            {
-                "divName": 'div2',
-                "networkId": network_id,
-                "siteId": site_id,
-                "adTypes": [5]},
+                "adTypes": [5],
+                "count": 3,
+            },
         ]}
     results = call_adzerk_server(settings.ADZERK_URL, data)
     assert responses.calls[0].request.body == force_bytes(json.dumps(
         data))
-    assert 'div0' in results['decisions']
-    assert 'div1' in results['decisions']
     # we're using a real response that only contains two divs
+    assert len(results['decisions']['multi']) == 2
     statsd_mock.assert_called_with('services.adzerk.success')
 
 
@@ -107,12 +98,11 @@ def test_call_adzerk_server_empty_response(statsd_mock):
 
 
 def test_process_adzerk_results():
-    placeholders = ['foo1', 'foo2', 'foo3', 'foo4', 'foo5']
-    assert process_adzerk_results(response={}, placeholders=placeholders) == {}
+    assert process_adzerk_results(response={}) == {}
 
     response = {
-        'decisions': {
-            'foo1': {
+        'decisions': {'multi': [
+            {
                 "clickUrl": "https://e-9999.adzerk.net/r?e=eyJ2IjoiMS42I&iwiA",
                 "impressionUrl": "https://e-9999.adzerk.net/i.gif?e=eyJ2I&jLg",
                 "contents": [{
@@ -127,7 +117,7 @@ def test_process_adzerk_results():
                     "url": "https://e-9999.adzerk.net/e.gif?e=454545"
                 }]
             },
-            'foo2': {
+            {
                 "clickUrl": "https://e-9999.adzerk.net/r?e=different",
                 "impressionUrl": "https://e-9999.adzerk.net/i.gif?e=values",
                 "contents": [{
@@ -142,7 +132,7 @@ def test_process_adzerk_results():
                     "url": "https://e-9999.adzerk.net/e.gif?e=787878"
                 }]
             },
-            'foo3': {
+            {
                 "clickUrl": "https://e-9999.adzerk.net/r?e=eyJ2IjoiMS42IiwiA",
                 "impressionUrl": "https://e-9999.adzerk.net/i.gif?e=eyJ2IjLg",
                 "contents": [{
@@ -154,19 +144,7 @@ def test_process_adzerk_results():
                 }],
                 "events": []
             },
-            # no foo4
-            'extrafoo': {
-                "clickUrl": "https://e-9999.adzerk.net/r?e=eyJ2IjoiMS42IiwiA",
-                "impressionUrl": "https://e-9999.adzerk.net/i.gif?e=eyJ2IjLg",
-                "contents": [{
-                    "data": {
-                        "customData": {
-                            "id": "5565"
-                        }
-                    },
-                }],
-            },
-            'foo5': {
+            {
                 "clickUrl": "https://e-9999.adzerk.net/r?e=ey44545",
                 "impressionUrl": "https://e-9999.adzerk.net/i.gif?e=thesfsg",
                 "contents": [{
@@ -181,10 +159,9 @@ def test_process_adzerk_results():
                     "url": ""
                 }]
             },
-            'foo6': None,
-        }
+        ]}
     }
-    results = process_adzerk_results(response, placeholders)
+    results = process_adzerk_results(response)
     assert results == {
         '1234': {
             'impression': r'i.gif%3Fe%3DeyJ2I%26jLg',
@@ -207,17 +184,13 @@ def test_get_addons_from_adzerk(call_server_mock, process_mock):
     data = {
         'placements': [
             {
-                "divName": 'div0',
+                "divName": 'multi',
                 "networkId": network_id,
                 "siteId": site_id,
                 "adTypes": [5],
-                "eventIds": [2]},
-            {
-                "divName": 'div1',
-                "networkId": network_id,
-                "siteId": site_id,
-                "adTypes": [5],
-                "eventIds": [2]},
+                "eventIds": [2],
+                "count": 2,
+            },
         ]}
 
     call_server_mock.return_value = None
@@ -229,18 +202,11 @@ def test_get_addons_from_adzerk(call_server_mock, process_mock):
 
     call_server_mock.return_value = {'something': 'something'}
     assert get_addons_from_adzerk(3) == {'thing': {}}
-    data['placements'].append(
-        {
-            "divName": 'div2',
-            "networkId": network_id,
-            "siteId": site_id,
-            "adTypes": [5],
-            "eventIds": [2]},
-    )
+    data['placements'][0]['count'] = 3
     call_server_mock.assert_called_with(
         settings.ADZERK_URL, data)
     process_mock.assert_called_with(
-        {'something': 'something'}, ['div0', 'div1', 'div2'])
+        {'something': 'something'})
 
 
 @mock.patch('olympia.shelves.utils.statsd.incr')
