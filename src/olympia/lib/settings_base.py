849cf7fecf5c6cee09106508e67726a0cd35e453
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2021-01-18 16:40:35 +0530

    cleaning up remaining templates from #10918

--- src/olympia/lib/settings_base.py
+++ src/olympia/lib/settings_base.py
@@ -1286,10 +1286,6 @@ CSP_CONNECT_SRC = (
     ANALYTICS_HOST,
     PROD_CDN_HOST,
 )
-CSP_FORM_ACTION = (
-    "'self'",
-    'https://developer.mozilla.org',
-)
 CSP_FONT_SRC = (
     "'self'",
     PROD_CDN_HOST,
