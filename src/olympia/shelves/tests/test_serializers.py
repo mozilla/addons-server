8af5b29db41241fb661efceda34b7a040876474f
Andrew Williamson <awilliamson@mozilla.com>
2021-01-26 14:54:28 +0000

    drop sponsored shelf and adzerk integration (#16396)

--- src/olympia/shelves/tests/test_serializers.py
+++ src/olympia/shelves/tests/test_serializers.py
@@ -3,22 +3,17 @@ from urllib import parse
 from rest_framework.settings import api_settings
 from rest_framework.test import APIRequestFactory
 
-from freezegun import freeze_time
-
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 
 from olympia import amo
-from olympia.addons.models import Addon
-from olympia.addons.tests.test_serializers import AddonSerializerOutputTestMixin
 from olympia.amo.tests import addon_factory, collection_factory, ESTestCase, reverse_ns
 from olympia.bandwagon.models import CollectionAddon
 from olympia.constants.promoted import RECOMMENDED
 from olympia.users.models import UserProfile
 
 from ..models import Shelf
-from ..serializers import ESSponsoredAddonSerializer, ShelfSerializer
-from ..views import SponsoredShelfViewSet
+from ..serializers import ShelfSerializer
 
 
 class TestShelvesSerializer(ESTestCase):
@@ -157,83 +152,3 @@ class TestShelvesSerializer(ESTestCase):
         data = self.serialize(self.collections_shelf)
         assert data['url'] == self._get_result_url(self.collections_shelf)
         assert data['addons'][0]['name']['en-US'] == ('test addon privacy01')
-
-
-class TestESSponsoredAddonSerializer(AddonSerializerOutputTestMixin, ESTestCase):
-    serializer_class = ESSponsoredAddonSerializer
-    view_class = SponsoredShelfViewSet
-
-    def tearDown(self):
-        super().tearDown()
-        self.empty_index('default')
-        self.refresh()
-
-    def search(self):
-        self.reindex(Addon)
-
-        view = self.view_class()
-        view.request = self.request
-        qs = view.get_queryset()
-
-        # We don't even filter - there should only be one addon in the index
-        # at this point
-        return qs.execute()[0]
-
-    def serialize(self, adzerk_results=None):
-        view = self.view_class(action='list')
-        view.request = self.request
-        view.adzerk_results = adzerk_results or {}
-        self.serializer = self.serializer_class(
-            context={
-                'request': self.request,
-                'view': view,
-            }
-        )
-
-        obj = self.search()
-
-        with self.assertNumQueries(0):
-            result = self.serializer.to_representation(obj)
-        return result
-
-    def _test_author(self, author, data):
-        """Override because the ES serializer doesn't include picture_url."""
-        assert data == {
-            'id': author.pk,
-            'name': author.name,
-            'url': author.get_absolute_url(),
-            'username': author.username,
-        }
-
-    def get_request(self, path, data=None, **extra):
-        api_version = 'v5'  # choose v5 to ignore 'l10n_flat_input_output' gate
-        request = APIRequestFactory().get(f'/api/{api_version}{path}', data, **extra)
-        request.versioning_scheme = api_settings.DEFAULT_VERSIONING_CLASS()
-        request.version = api_version
-        return request
-
-    @freeze_time('2020-01-01')
-    def test_click_url_and_data(self):
-        self.addon = addon_factory()
-        adzerk_results = {str(self.addon.id): {'click': 'foobar'}}
-        result = self.serialize(adzerk_results)
-        assert result['click_url'] == (
-            'http://testserver/api/v5/shelves/sponsored/click/'
-        )
-        assert result['click_data'] == ('foobar:1imRQe:mJEcjX6cM3cvkSbb2qMMPPHWC8o')
-
-    @freeze_time('2020-01-01')
-    def test_events(self):
-        self.addon = addon_factory()
-        adzerk_results = {
-            str(self.addon.id): {
-                'click': 'foobar',
-                'impression': 'impressive',
-                'conversion': 'hyhyhy',
-            }
-        }
-        result = self.serialize(adzerk_results)
-        assert result['event_data'] == {
-            'click': 'foobar:1imRQe:mJEcjX6cM3cvkSbb2qMMPPHWC8o',
-            'conversion': 'hyhyhy:1imRQe:NQyj05lumKmHaj5Zj4yF69Q9bS4',
-        }
