d6103094706b3cc9ebf24cb636bc80ca54b42e91
Andrew Williamson <awilliamson@mozilla.com>
2021-02-23 14:38:19 +0000

    mark urlparams filter output as safe for jinja templates

--- src/olympia/amo/tests/test_helpers.py
+++ src/olympia/amo/tests/test_helpers.py
@@ -198,7 +198,7 @@ def test_urlparams():
 
     # Adding query with existing params.
     s = render('{{ base_query|urlparams(frag, sort=sort) }}', c)
-    amo.tests.assert_url_equal(s, '%s?sort=name&amp;x=y#frag' % url)
+    amo.tests.assert_url_equal(s, '%s?sort=name&x=y#frag' % url)
 
     # Replacing a query param.
     s = render('{{ base_query|urlparams(frag, x="z") }}', c)
@@ -239,6 +239,18 @@ def test_urlparams_returns_safe_string():
 
     s = render('{{ "https://foo.com/"|urlparams(param="%AAA") }}', {})
     assert s == 'https://foo.com/?param=%AAA'
+
+    string = render(
+        '{{ unsafe_url|urlparams }}',
+        {
+            'unsafe_url': "http://url.with?foo=<script>alert('awesome')</script>"
+            "&baa=that"
+        },
+    )
+    assert string == (
+        'http://url.with?foo=%3Cscript%3Ealert%28%27awesome%27%29%3C%2Fscript%3E'
+        '&baa=that'
+    )
 
 
 def test_isotime():
