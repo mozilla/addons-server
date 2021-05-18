9d38e110a883c3bd09d116b59ca91ac2404ea338
Andrew Williamson <awilliamson@mozilla.com>
2021-05-18 11:00:58 +0100

    save all resized jpegs with a white background (#17167)
    * save all resized jpegs with a white background
    
    * rename white_background

--- src/olympia/amo/utils.py
+++ src/olympia/amo/utils.py
@@ -726,13 +726,12 @@ def resize_image(source, destination, size=None, *, format='png', quality=80):
             im.save(dest_file, 'png', icc_profile=None)
             pngcrush_image(destination)
         else:
-            if source_fileext == '.png':
-                # Create a white rgba background for transparency
-                white_background = Image.new('RGBA', im.size, 'WHITE')
-                white_background.paste(im, (0, 0), im)
-                im = white_background
-            im = im.convert('RGB')
-            im.save(dest_file, 'JPEG', quality=quality)
+            # Create a white rgba background for transparency
+            final_im = Image.new('RGBA', im.size, 'WHITE')
+            final_im.paste(im, (0, 0), im)
+            final_im = final_im.convert('RGB')
+            final_im.save(dest_file, 'JPEG', quality=quality)
+            final_im.close()
     new_size = im.size
     im.close()
     return (new_size, original_size)
