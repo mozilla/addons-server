d41363f04f5348343a3fe8d982ddd88e496ab491
Andrew Williamson <awilliamson@mozilla.com>
2021-06-15 14:43:50 +0100

    :roll_eyes:

--- src/olympia/amo/management/commands/cron.py
+++ src/olympia/amo/management/commands/cron.py
@@ -55,7 +55,7 @@ class Command(BaseCommand):
         )
 
         getattr(module, name)(*args, **kwargs)
-        
+
         end_millis = datetime.now().timestamp() * 1000
         log.info(
             f'Ending job: {name} {args} {kwargs} '
