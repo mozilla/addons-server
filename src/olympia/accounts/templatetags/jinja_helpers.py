98e9b1f320a9044869bb7f9abebf1fc69453d160
Akash Negi <55234838+NegiAkash890@users.noreply.github.com>
2021-07-10 18:27:18 +0530

    Replaced all the instances

--- src/olympia/accounts/templatetags/jinja_helpers.py
+++ src/olympia/accounts/templatetags/jinja_helpers.py
@@ -6,13 +6,13 @@ from olympia.accounts import utils
 
 
 @library.global_function
-@jinja2.contextfunction
+@jinja2.pass_context
 def login_link(context):
     return utils.default_fxa_login_url(context['request'])
 
 
 @library.global_function
-@jinja2.contextfunction
+@jinja2.pass_context
 def register_link(context):
     return utils.default_fxa_register_url(context['request'])
 
