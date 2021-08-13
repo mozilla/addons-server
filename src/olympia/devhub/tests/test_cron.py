200f7071530c57baa501a630347db8b53701dbb5
Andrew Williamson <awilliamson@mozilla.com>
2021-08-13 16:24:15 +0100

    import developer blog posts as json (replace rdf) (#17707)

--- src/olympia/devhub/tests/test_cron.py
+++ src/olympia/devhub/tests/test_cron.py
@@ -1,30 +1,36 @@
 import datetime
+import json
 import os
 
 from django.conf import settings
 
-from olympia.amo.tests import TestCase
+import responses
+
 from olympia.devhub.cron import update_blog_posts
 from olympia.devhub.models import BlogPost
 
 
-class TestRSS(TestCase):
-    def test_rss_cron(self):
-        url = os.path.join(
-            settings.ROOT, 'src', 'olympia', 'devhub', 'tests', 'rss_feeds', 'blog.xml'
-        )
+def test_update_blog_posts():
+    blog_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'json_feeds', 'blog.json'
+    )
+    with open(blog_path) as blog_file:
+        content = json.load(blog_file)
 
-        settings.DEVELOPER_BLOG_URL = url
+    responses.add(
+        responses.GET,
+        settings.DEVELOPER_BLOG_URL,
+        json=content,
+    )
 
-        update_blog_posts()
+    update_blog_posts()
 
-        assert BlogPost.objects.count() == 5
+    assert BlogPost.objects.count() == 5
 
-        bp = BlogPost.objects.all()[0]
-        url = (
-            'http://blog.mozilla.com/addons/2011/06/10/'
-            'update-in-time-for-thunderbird-5/'
-        )
-        assert bp.title == 'Test!'
-        assert bp.date_posted == datetime.date(2011, 6, 10)
-        assert bp.permalink == url
+    post = BlogPost.objects.all()[0]
+    assert post.title == 'Thank you, Recommended Extensions Community Board!'
+    assert post.date_posted == datetime.date(2021, 8, 5)
+    assert post.permalink == (
+        'https://blog.mozilla.org/addons/2021/08/05/'
+        'thank-you-recommended-extensions-community-board/'
+    )
