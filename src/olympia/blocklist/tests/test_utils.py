5833d85b71957ded00ccf1a376c5505b5c6c5865
Andrew Williamson <awilliamson@mozilla.com>
2021-09-06 18:18:35 +0100

    rm all legacy blocklist update integration

--- src/olympia/blocklist/tests/test_utils.py
+++ src/olympia/blocklist/tests/test_utils.py
@@ -1,67 +1,6 @@
 from datetime import datetime
-from unittest import mock
 
-import pytest
-
-from olympia.amo.tests import user_factory
-from olympia.blocklist.models import Block
-from olympia.blocklist.utils import (
-    datetime_to_ts,
-    legacy_delete_blocks,
-    legacy_publish_blocks,
-)
-from olympia.lib.remote_settings import RemoteSettings
-
-
-@pytest.mark.django_db
-@mock.patch.object(RemoteSettings, 'publish_record')
-def test_legacy_publish_blocks(publish_mock):
-    publish_mock.return_value = {'id': 'a-legacy-id'}
-    block_new = Block.objects.create(guid='new@guid', updated_by=user_factory())
-    block_regex = Block.objects.create(
-        guid='regex@guid', updated_by=user_factory(), legacy_id='*regex'
-    )
-    block_update = Block.objects.create(
-        guid='update@guid', updated_by=user_factory(), legacy_id='update'
-    )
-
-    data = {
-        'details': {'bug': '', 'why': '', 'name': ''},
-        'enabled': True,
-        'versionRange': [
-            {
-                'severity': 3,
-                'minVersion': '0',
-                'maxVersion': '*',
-            }
-        ],
-    }
-
-    legacy_publish_blocks([block_new, block_regex, block_update])
-    assert publish_mock.call_args_list == [
-        mock.call(dict(guid='new@guid', **data)),
-        mock.call(dict(guid='update@guid', **data), 'update'),
-    ]
-    assert block_new.legacy_id == 'a-legacy-id'
-    assert block_regex.legacy_id == '*regex'  # not changed
-    assert block_update.legacy_id == 'update'  # not changed
-
-
-@pytest.mark.django_db
-@mock.patch.object(RemoteSettings, 'delete_record')
-def test_legacy_delete_blocks(delete_record_mock):
-    block = Block.objects.create(
-        guid='legacy@guid', updated_by=user_factory(), legacy_id='legacy'
-    )
-    block_regex = Block.objects.create(
-        guid='regex@guid', updated_by=user_factory(), legacy_id='*regex'
-    )
-    block_not_legacy = Block.objects.create(guid='not@guid', updated_by=user_factory())
-
-    legacy_delete_blocks([block, block_regex, block_not_legacy])
-    assert delete_record_mock.call_args_list == [mock.call('legacy')]
-    assert block.legacy_id == ''
-    assert block_regex.legacy_id == ''  # cleared anyway
+from olympia.blocklist.utils import datetime_to_ts
 
 
 def test_datetime_to_ts():
--- src/olympia/blocklist/utils.py
+++ src/olympia/blocklist/utils.py
@@ -1,12 +1,8 @@
 from datetime import datetime
 
-from django.conf import settings
-
 import olympia.core.logger
 from olympia import amo
 from olympia.activity import log_create
-from olympia.constants.blocklist import REMOTE_SETTINGS_COLLECTION_LEGACY
-from olympia.lib.remote_settings import RemoteSettings
 from olympia.users.utils import get_task_user
 
 
@@ -28,16 +24,12 @@ def add_version_log_for_blocked_versions(obj, old_obj, al):
 
 def block_activity_log_save(obj, change, submission_obj=None, old_obj=None):
     action = amo.LOG.BLOCKLIST_BLOCK_EDITED if change else amo.LOG.BLOCKLIST_BLOCK_ADDED
-    legacy_inclusion = getattr(
-        submission_obj if submission_obj else obj, 'in_legacy_blocklist'
-    )
     details = {
         'guid': obj.guid,
         'min_version': obj.min_version,
         'max_version': obj.max_version,
         'url': obj.url,
         'reason': obj.reason,
-        'include_in_legacy': legacy_inclusion,
         'comments': f'Versions {obj.min_version} - {obj.max_version} blocked.',
     }
     if submission_obj:
@@ -69,7 +61,6 @@ def block_activity_log_delete(obj, *, submission_obj=None, delete_user=None):
         'max_version': obj.max_version,
         'url': obj.url,
         'reason': obj.reason,
-        'include_in_legacy': obj.in_legacy_blocklist,
         'comments': f'Versions {obj.min_version} - {obj.max_version} unblocked.',
     }
     if submission_obj:
@@ -100,59 +91,6 @@ def block_activity_log_delete(obj, *, submission_obj=None, delete_user=None):
 
 def splitlines(text):
     return [line.strip() for line in str(text or '').splitlines()]
-
-
-def legacy_publish_blocks(blocks):
-    bucket = settings.REMOTE_SETTINGS_WRITER_BUCKET
-    server = RemoteSettings(bucket, REMOTE_SETTINGS_COLLECTION_LEGACY)
-    for block in blocks:
-        needs_creating = not block.legacy_id
-
-        if block.is_imported_from_legacy_regex:
-            log.info(
-                f'Block [{block.guid}] was imported from a regex guid so '
-                "can't be safely updated.  Skipping."
-            )
-            continue
-        data = {
-            'guid': block.guid,
-            'details': {
-                'bug': block.url,
-                'why': block.reason,
-                'name': str(block.reason).partition('.')[0],  # required
-            },
-            'enabled': True,
-            'versionRange': [
-                {
-                    'severity': 3,  # Always high severity now.
-                    'minVersion': block.min_version,
-                    'maxVersion': block.max_version,
-                }
-            ],
-        }
-        if needs_creating:
-            record = server.publish_record(data)
-            block.update(legacy_id=record.get('id', ''))
-        else:
-            server.publish_record(data, block.legacy_id)
-
-    server.complete_session()
-
-
-def legacy_delete_blocks(blocks):
-    bucket = settings.REMOTE_SETTINGS_WRITER_BUCKET
-    server = RemoteSettings(bucket, REMOTE_SETTINGS_COLLECTION_LEGACY)
-    for block in blocks:
-        if block.legacy_id:
-            if block.is_imported_from_legacy_regex:
-                log.info(
-                    f'Block [{block.guid}] was imported from a regex guid so '
-                    "can't be safely deleted.  Skipping."
-                )
-            else:
-                server.delete_record(block.legacy_id)
-            block.update(legacy_id='')
-    server.complete_session()
 
 
 def datetime_to_ts(dt=None):
