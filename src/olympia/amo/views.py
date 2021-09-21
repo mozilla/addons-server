c819571f2c70a56e80417ddf89589a71c6bf1172
William Durand <will+git@drnd.me>
2021-09-21 21:26:49 +0200

    Expose addons-linter version at /__version__ (#17940)

--- src/olympia/amo/views.py
+++ src/olympia/amo/views.py
@@ -141,15 +141,22 @@ def loaded(request):
 @non_atomic_requests
 def version(request):
     path = os.path.join(settings.ROOT, 'version.json')
-    py_info = sys.version_info
     with open(path) as f:
         contents = json.loads(f.read())
+
+    py_info = sys.version_info
     contents['python'] = '{major}.{minor}'.format(
         major=py_info.major, minor=py_info.minor
     )
     contents['django'] = '{major}.{minor}'.format(
         major=django.VERSION[0], minor=django.VERSION[1]
     )
+
+    path = os.path.join(settings.ROOT, 'package.json')
+    with open(path) as f:
+        data = json.loads(f.read())
+        contents['addons-linter'] = data['dependencies']['addons-linter']
+
     res = HttpResponse(json.dumps(contents), content_type='application/json')
     res.headers['Access-Control-Allow-Origin'] = '*'
     return res
