dcaca7d3be453c9b0896bcf4e36bebcd419d6ddb
Mathieu Pillard <mpillard@mozilla.com>
2021-12-03 12:12:13 +0100

    Remove useless constant for now

--- src/olympia/constants/site_permissions.py
+++ src/olympia/constants/site_permissions.py
@@ -1,4 +1,2 @@
-# List of site_permissions names we know about.
-SITE_PERMISSIONS = {'midi-sysex'}  # WebMIDI
 # Minimum Firefox version that supports site permission add-ons.
 SITE_PERMISSION_MIN_VERSION = '97.0'
