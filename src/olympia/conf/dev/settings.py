a55620c9369f6e46e36089b5775cd313b5bc9e80
Mathieu Pillard <diox@users.noreply.github.com>
2021-12-07 16:44:58 +0100

    Revert "Re-enable API throttling on dev (#18463)"
    This reverts commit d312be4dcd41ec10bc2ccadb5b91dd518d3080e4.

--- src/olympia/conf/dev/settings.py
+++ src/olympia/conf/dev/settings.py
@@ -13,7 +13,7 @@ EMAIL_HOST_PASSWORD = EMAIL_URL['EMAIL_HOST_PASSWORD']
 ENV = env('ENV')
 RAISE_ON_SIGNAL_ERROR = True
 
-API_THROTTLING = True
+API_THROTTLING = False
 
 DOMAIN = env('DOMAIN', default='addons-dev.allizom.org')
 SERVER_EMAIL = 'zdev@addons.mozilla.org'
