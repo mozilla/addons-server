eviljeff/olympia

use str for paths everywhere; make our Storage subclass safer

settings_test.py
@@ -42,7 +42,7 @@ MEDIA_URL = '%s/user-media/' % CDN_HOST
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Overrides whatever storage you might have put in local settings.
-DEFAULT_FILE_STORAGE = 'olympia.amo.utils.LocalFileStorage'
+DEFAULT_FILE_STORAGE = 'olympia.amo.utils.RootStorage'
 
 TASK_USER_ID = 1337
 

src/olympia/addons/tasks.py
@@ -18,7 +18,7 @@ from olympia.addons.models import (
 from olympia.addons.utils import compute_last_updated
 from olympia.amo.celery import task
 from olympia.amo.decorators import use_primary_db
-from olympia.amo.utils import LocalFileStorage, extract_colors_from_image
+from olympia.amo.utils import RootStorage, extract_colors_from_image
 from olympia.devhub.tasks import resize_image
 from olympia.files.utils import get_filepath, parse_addon
 from olympia.lib.es.utils import index_objects
@@ -89,7 +89,7 @@ def unindex_addons(ids, **kw):
 
 
 def make_checksum(header_path):
-    ls = LocalFileStorage()
+    ls = RootStorage()
     raw_checksum = ls._open(header_path).read()
     return hashlib.sha224(raw_checksum).hexdigest()
 

src/olympia/lib/settings_base.py
@@ -1442,7 +1442,7 @@ CSRF_FAILURE_VIEW = 'olympia.amo.views.csrf_failure'
 CSRF_USE_SESSIONS = True
 
 # Default file storage mechanism that holds media.
-DEFAULT_FILE_STORAGE = 'olympia.amo.utils.LocalFileStorage'
+DEFAULT_FILE_STORAGE = 'olympia.amo.utils.RootStorage'
 
 # And how long we'll give the server to respond for monitoring.
 # We currently do not have any actual timeouts during the signing-process.

mozilla/addons-server

settings_test.py
@@ -42,7 +42,7 @@ MEDIA_URL = '%s/user-media/' % CDN_HOST
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Overrides whatever storage you might have put in local settings.
-DEFAULT_FILE_STORAGE = 'olympia.amo.utils.LocalFileStorage'
+DEFAULT_FILE_STORAGE = 'olympia.amo.utils.SafeStorage'
 
 TASK_USER_ID = 1337
 

src/olympia/addons/tasks.py
@@ -18,7 +18,7 @@ from olympia.addons.models import (
 from olympia.addons.utils import compute_last_updated
 from olympia.amo.celery import task
 from olympia.amo.decorators import use_primary_db
-from olympia.amo.utils import LocalFileStorage, extract_colors_from_image
+from olympia.amo.utils import SafeStorage, extract_colors_from_image
 from olympia.devhub.tasks import resize_image
 from olympia.files.utils import get_filepath, parse_addon
 from olympia.lib.es.utils import index_objects
@@ -89,7 +89,7 @@ def unindex_addons(ids, **kw):
 
 
 def make_checksum(header_path):
-    ls = LocalFileStorage()
+    ls = SafeStorage()
     raw_checksum = ls._open(header_path).read()
     return hashlib.sha224(raw_checksum).hexdigest()
 

src/olympia/lib/settings_base.py
@@ -1442,7 +1442,7 @@ CSRF_FAILURE_VIEW = 'olympia.amo.views.csrf_failure'
 CSRF_USE_SESSIONS = True
 
 # Default file storage mechanism that holds media.
-DEFAULT_FILE_STORAGE = 'olympia.amo.utils.LocalFileStorage'
+DEFAULT_FILE_STORAGE = 'olympia.amo.utils.SafeStorage'
 
 # And how long we'll give the server to respond for monitoring.
 # We currently do not have any actual timeouts during the signing-process.