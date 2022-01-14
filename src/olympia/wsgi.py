eabb4aaff5bf4037d73f91a6cefa06ab747b738a
Mathieu Pillard <mpillard@mozilla.com>
2022-01-14 15:41:58 +0100

    Clean up wsgi.py

--- src/olympia/wsgi.py
+++ src/olympia/wsgi.py
@@ -1,7 +1,3 @@
-import logging
-
-from datetime import datetime
-
 import django
 import django.conf
 import django.core.management
@@ -10,27 +6,11 @@ import django.utils
 from django.core.wsgi import get_wsgi_application
 
 
-log = logging.getLogger('z.startup')
-
-# Remember when mod_wsgi loaded this file so we can track it in nagios.
-wsgi_loaded = datetime.now()
-
-
 # Do validate and activate translations before running the app.
 django.setup()
 django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
 
-# This is what mod_wsgi runs.
-django_app = get_wsgi_application()
 
-
-# Normally we could let WSGIHandler run directly, but while we're dark
-# launching, we want to force the script name to be empty so we don't create
-# any /z links through reverse.  This fixes bug 554576.
+# This is referenced in docker/uwsgi.ini: module = olympia.wsgi:application
 def application(env, start_response):
-    if 'HTTP_X_ZEUS_DL_PT' in env:
-        env['SCRIPT_URL'] = env['SCRIPT_NAME'] = ''
-    env['wsgi.loaded'] = wsgi_loaded
-    env['hostname'] = django.conf.settings.HOSTNAME
-    env['datetime'] = str(datetime.now())
-    return django_app(env, start_response)
+    return get_wsgi_application()(env, start_response)
