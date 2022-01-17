869a4c4052ea975bd25081cbad62ab703cf10979
Mathieu Pillard <mpillard@mozilla.com>
2022-01-17 16:54:23 +0100

    black

--- src/olympia/devhub/views.py
+++ src/olympia/devhub/views.py
@@ -171,8 +171,7 @@ def site_permission_generator(request):
             % (reverse('devhub.developer_agreement'), '?to=', quote(request.path))
         )
     form = forms.SitePermissionGeneratorForm(
-        request.POST if request.method == 'POST' else None,
-        request=request
+        request.POST if request.method == 'POST' else None, request=request
     )
     success = None
     if request.method == 'POST' and form.is_valid():
