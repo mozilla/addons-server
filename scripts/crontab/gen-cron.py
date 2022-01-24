ef84f771bf081cdaf804223a39681ad12323556a
Andrew Williamson <awilliamson@mozilla.com>
2022-01-24 13:04:55 +0000

    use modern syntax in gen-cron.py; format consistently (#18667)
    * use modern syntax in gen-cron.py; format consistently
    
    * change to format from f string; add test

--- scripts/crontab/gen-cron.py
+++ scripts/crontab/gen-cron.py
@@ -8,37 +8,38 @@ TEMPLATE = open(os.path.join(os.path.dirname(__file__), 'crontab.tpl')).read()
 
 def main():
     parser = OptionParser()
-    parser.add_option("-z", "--zamboni",
-                      help="Location of zamboni (required)")
-    parser.add_option("-u", "--user",
-                      help=("Prefix cron with this user. "
-                            "Only define for cron.d style crontabs"))
-    parser.add_option("-p", "--python", default="/usr/bin/python2.7",
-                      help="Python interpreter to use")
-    parser.add_option("-d", "--deprecations", default=False,
-                      help="Show deprecation warnings")
+    parser.add_option('-z', '--zamboni', help='Location of addons-server (required)')
+    parser.add_option(
+        '-u',
+        '--user',
+        help='Prefix cron with this user. Only define for cron.d style crontabs',
+    )
+    parser.add_option(
+        '-p', '--python', default='/usr/bin/python', help='Python interpreter to use'
+    )
+    parser.add_option(
+        '-d', '--deprecations', default=False, help='Show deprecation warnings'
+    )
 
     (opts, args) = parser.parse_args()
 
     if not opts.zamboni:
-        parser.error("-z must be defined")
+        parser.error('-z must be defined')
 
     if not opts.deprecations:
         opts.python += ' -W ignore::DeprecationWarning'
 
-    ctx = { "django": "cd %s; %s manage.py" % (opts.zamboni, opts.python) }
-
-    ctx['z_cron'] = '%s cron' % ctx['django']
-
-    if opts.user:
-        for k, v in ctx.iteritems():
-            ctx[k] = '%s %s' % (opts.user, v)
-
-    # Needs to stay below the opts.user injection.
-    ctx['python'] = opts.python
+    django_cmd = '{u} cd {z}; {p} manage.py'.format(
+        u=(opts.user or ''), z=opts.zamboni, p=opts.python
+    ).strip()
+    ctx = {
+        'django': django_cmd,
+        'z_cron': '{django_cmd} cron'.format(django_cmd=django_cmd),
+        'python': opts.python,
+    }
 
     print(TEMPLATE % ctx)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
