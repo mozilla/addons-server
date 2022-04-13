20b133baa7402d8eec8bd3bf1e877d5cf856e1fb
Mathieu Pillard <mpillard@mozilla.com>
2022-04-13 15:21:35 +0200

    Compute ETA with full elapsed time

--- src/olympia/files/management/commands/migrate_guarded_addons.py
+++ src/olympia/files/management/commands/migrate_guarded_addons.py
@@ -13,42 +13,47 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.migrate()
 
-    def print_eta(self, *, elapsed, remaining_entries):
+    def print_eta(self, *, elapsed, entries_migrated, entries_remaining):
         # total_seconds() keeps microseconds - it should often be 0.xxxxxx in
         # our case. We use the full precision for computing the ETA, but round
         # to the second when displaying.
         total_seconds = elapsed.total_seconds()
-        eta = timedelta(seconds=int(total_seconds * remaining_entries))
-        self.stdout.write(f'ETA {eta} ; Remaining entries {remaining_entries}\n')
+        eta = (
+            timedelta(seconds=int(total_seconds / entries_migrated * entries_remaining))
+            if entries_migrated
+            else 'Unknown'
+        )
+        self.stdout.write(f'ETA {eta} ; Remaining entries {entries_remaining}\n')
 
     def migrate(self):
         guarded_addons_path = os.path.join(settings.STORAGE_ROOT, 'guarded-addons')
         # Note: we handle empty directories, but for ETA to be reliable, it's
         # best to remove them first by running find . -type d -empty -delete.
-        total_entries = os.stat(guarded_addons_path).st_nlink - 1
+        entries_total = os.stat(guarded_addons_path).st_nlink - 1
         entries_migrated = 0
         entries = os.scandir(guarded_addons_path)
+        start_time = datetime.now()
         for addon in entries:
             if not addon.name.startswith('.'):
-                elapsed = self.migrate_addon(Entry(addon.name, addon.path))
+                self.migrate_addon(Entry(addon.name, addon.path))
                 # Since we use the add-ons and not the files to compute the ETA
                 # it's never going to be 100% accurate, but it should be good
                 # enough.
                 entries_migrated += 1
                 if entries_migrated == 1 or entries_migrated % 1000 == 0:
+                    elapsed = datetime.now() - start_time
                     self.print_eta(
                         elapsed=elapsed,
-                        remaining_entries=total_entries - entries_migrated,
+                        entries_migrated=entries_migrated,
+                        entries_remaining=entries_total - entries_migrated,
                     )
 
     def migrate_addon(self, addon):
-        start_time = datetime.now()
         new_addon_path = os.path.join(settings.ADDONS_PATH, addon.name)
         os.makedirs(new_addon_path, exist_ok=True)
         files = os.scandir(addon.path)
         for file_ in files:
             self.migrate_file(addon, Entry(file_.name, file_.path))
-        return datetime.now() - start_time
 
     def migrate_file(self, addon, file_):
         old_path = file_.path
--- src/olympia/files/tests/test_commands.py
+++ src/olympia/files/tests/test_commands.py
@@ -3,8 +3,6 @@ import os
 from datetime import timedelta
 from unittest import mock
 
-from freezegun import freeze_time
-
 from django.conf import settings
 
 from olympia.amo.tests import TestCase
@@ -53,22 +51,16 @@ class TestMigrateGuardedAddons(TestCase):
         self.command.guarded_addons_path = os.path.join(
             settings.STORAGE_ROOT, 'guarded-addons'
         )
-        expected_elapsed = timedelta(seconds=42)
         addon = Entry('foo', os.path.join(self.command.guarded_addons_path, 'foo'))
         os.makedirs(addon.path)
         file_path = os.path.join(addon.path, 'bar.xpi')
         with open(file_path, 'w') as f:
             f.write('a')
         expected_file = Entry('bar.xpi', file_path)
-        with freeze_time('2022-04-13 12:00') as frozen_time:
-            with mock.patch.object(self.command, 'migrate_file') as migrate_file_mock:
-                migrate_file_mock.side_effect = lambda *args: frozen_time.tick(
-                    delta=expected_elapsed
-                )
-                rval = self.command.migrate_addon(addon)
+        with mock.patch.object(self.command, 'migrate_file') as migrate_file_mock:
+            self.command.migrate_addon(addon)
         assert migrate_file_mock.call_count == 1
         assert migrate_file_mock.call_args[0] == (addon, expected_file)
-        assert rval == expected_elapsed
 
     def test_migrate_addon_empty_dir(self):
         self.command.guarded_addons_path = os.path.join(
@@ -76,38 +68,46 @@ class TestMigrateGuardedAddons(TestCase):
         )
         addon = Entry('foo', os.path.join(self.command.guarded_addons_path, 'foo'))
         os.makedirs(addon.path)
-        with freeze_time('2022-04-13 12:00') as frozen_time:
-            with mock.patch.object(self.command, 'migrate_file') as migrate_file_mock:
-                migrate_file_mock.side_effect = lambda *args: frozen_time.tick(
-                    delta=timedelta(seconds=42)
-                )
-                rval = self.command.migrate_addon(addon)
+        with mock.patch.object(self.command, 'migrate_file') as migrate_file_mock:
+            self.command.migrate_addon(addon)
         assert migrate_file_mock.call_count == 0
-        # migrate_file() takes 42 seconds but was never called so elapsed time
-        # should be 0.
-        assert rval == timedelta(seconds=0)
 
     def test_print_eta(self):
-        elapsed = timedelta(seconds=0, microseconds=191332)
-        remaining_entries = 481516
-        self.command.print_eta(elapsed=elapsed, remaining_entries=remaining_entries)
+        self.command.print_eta(
+            elapsed=timedelta(seconds=0, microseconds=191332),
+            entries_migrated=4,
+            entries_remaining=815162342,
+        )
         self.command.stdout.seek(0)
         assert (
             self.command.stdout.read()
-            == 'ETA 1 day, 1:35:29 ; Remaining entries 481516\n'
+            == 'ETA 451 days, 7:01:00 ; Remaining entries 815162342\n'
         )
 
     def test_print_eta_shorter(self):
-        elapsed = timedelta(seconds=0, microseconds=566784)
-        remaining_entries = 2342
-        self.command.print_eta(elapsed=elapsed, remaining_entries=remaining_entries)
+        self.command.print_eta(
+            elapsed=timedelta(minutes=20, seconds=30, microseconds=424567),
+            entries_migrated=12345,
+            entries_remaining=678,
+        )
         self.command.stdout.seek(0)
-        assert self.command.stdout.read() == 'ETA 0:22:07 ; Remaining entries 2342\n'
+        assert self.command.stdout.read() == 'ETA 0:01:07 ; Remaining entries 678\n'
+
+    def test_print_eta_0_migrated(self):
+        self.command.print_eta(
+            elapsed=timedelta(seconds=1),
+            entries_migrated=0,
+            entries_remaining=9999,
+        )
+        self.command.stdout.seek(0)
+        assert self.command.stdout.read() == 'ETA Unknown ; Remaining entries 9999\n'
 
     def test_print_eta_0_remaining(self):
-        elapsed = timedelta(seconds=0, microseconds=123456)
-        remaining_entries = 0
-        self.command.print_eta(elapsed=elapsed, remaining_entries=remaining_entries)
+        self.command.print_eta(
+            elapsed=timedelta(hours=8, minutes=7, seconds=6, microseconds=540530),
+            entries_migrated=1000000000,
+            entries_remaining=0,
+        )
         self.command.stdout.seek(0)
         assert self.command.stdout.read() == 'ETA 0:00:00 ; Remaining entries 0\n'
 
