74c6012be607735916348b01cfe82e16ee98b664
Mathieu Pillard <mpillard@mozilla.com>
2022-04-14 15:02:11 +0200

    Fix double import

--- src/olympia/ratings/tests/test_models.py
+++ src/olympia/ratings/tests/test_models.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 from django.core import mail
 
-from elasticsearch_dsl import Search
+from elasticsearch_dsl import Q, Search
 
 from olympia import amo
 from olympia.activity.models import ActivityLog
@@ -13,8 +13,6 @@ from olympia.amo.tests import ESTestCase, TestCase, addon_factory, user_factory
 from olympia.ratings.models import Rating, RatingFlag
 from olympia.amo.search import get_es
 from olympia.users.models import UserProfile
-
-from elasticsearch_dsl import Q, Search
 
 
 class TestRatingModel(TestCase):
