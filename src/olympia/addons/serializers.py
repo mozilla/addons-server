f834b61fa04bed071726f0d7b3dfc33a70028480
Andrew Williamson <awilliamson@mozilla.com>
2022-04-19 10:22:06 +0100

    prevent extension previews from being created for themes via the addons api (#19121)

--- src/olympia/addons/serializers.py
+++ src/olympia/addons/serializers.py
@@ -3,6 +3,7 @@ import re
 
 from django.utils.translation import gettext
 from django.urls import reverse
+from django.utils.translation import gettext
 
 from django_statsd.clients import statsd
 from rest_framework import exceptions, serializers
@@ -172,6 +173,13 @@ class PreviewSerializer(serializers.ModelSerializer):
         request = self.context.get('request', None)
         if request and is_gate_active(request, 'del-preview-position'):
             data.pop('position', None)
+        return data
+
+    def validate(self, data):
+        if self.context['view'].get_addon_object().type == amo.ADDON_STATICTHEME:
+            raise exceptions.ValidationError(
+                gettext('Previews cannot be created for themes.')
+            )
         return data
 
     def create(self, validated_data):
