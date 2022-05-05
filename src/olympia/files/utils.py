17c201eeb8d0b1288060989a859d193a630f11f8
Mathieu Pillard <mpillard@mozilla.com>
2022-05-05 13:25:42 +0200

    Outdated debugging comment

--- src/olympia/files/utils.py
+++ src/olympia/files/utils.py
@@ -815,7 +815,7 @@ def parse_xpi(xpi, addon=None, minimal=False, user=None):
     add-on: guid and version.
     """
     try:
-        xpi = get_file(xpi)  # Suspicious operation, hidden by our try/except
+        xpi = get_file(xpi)
         zip_file = SafeZip(xpi)
 
         certificate = os.path.join('META-INF', 'mozilla.rsa')
