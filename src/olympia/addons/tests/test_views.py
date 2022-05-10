a7027202650c654ffcd0e59023b796a6a3d4f200
Andrew Williamson <awilliamson@mozilla.com>
2022-05-10 15:44:27 +0100

    restrict creating author invitations to owners

--- src/olympia/addons/tests/test_views.py
+++ src/olympia/addons/tests/test_views.py
@@ -6094,3 +6094,30 @@ class TestAddonPendingAuthorViewSet(TestCase):
             id=self.pending_author.id
         ).exists()
         assert pending_user not in self.addon.reload().authors.all()
+
+    def test_developer_role(self):
+        AddonUser.objects.get(user=self.user).update(role=amo.AUTHOR_ROLE_DEV)
+        # edge-case: user is an owner of a *different* add-on too
+        addon_factory(users=(self.user,))
+        self.client.login_api(self.user)
+
+        # developer role authors should be able to view all details of authors
+        response = self.client.get(self.detail_url)
+        assert response.status_code == 200, response.content
+        assert response.data == AddonPendingAuthorSerializer().to_representation(
+            instance=self.pending_author
+        )
+        # but not update
+        response = self.client.patch(self.detail_url, {'role': 'owner'})
+        assert response.status_code == 403, response.content
+
+        # or create
+        response = self.client.post(
+            self.list_url,
+            data={'user_id': user_factory(display_name='me!').id, 'role': 'owner'},
+        )
+        assert response.status_code == 403, response.content
+
+        # and not delete either
+        response = self.client.delete(self.detail_url)
+        assert response.status_code == 403, response.content
