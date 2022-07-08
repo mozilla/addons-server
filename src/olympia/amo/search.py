02128fb6e1217f8aa1cda4f2f1db7e4208c66aa1
Mathieu Pillard <mpillard@mozilla.com>
2022-07-08 20:02:11 +0200

    Remove deprecated, no longer necessary send_get_body_as parameter

--- src/olympia/amo/search.py
+++ src/olympia/amo/search.py
@@ -6,5 +6,5 @@ from elasticsearch import Elasticsearch
 def get_es():
     """Create an ES object and return it."""
     return Elasticsearch(
-        settings.ES_HOSTS, timeout=settings.ES_TIMEOUT, send_get_body_as='POST'
+        settings.ES_HOSTS, timeout=settings.ES_TIMEOUT
     )
