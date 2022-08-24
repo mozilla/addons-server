516c141fc6d4f418c0807694653bac89b48dc06b
Andrew Williamson <awilliamson@mozilla.com>
2022-08-24 16:51:25 +0100

    deal with some non-django deprecation warnings

--- src/olympia/addons/tests/test_indexers.py
+++ src/olympia/addons/tests/test_indexers.py
@@ -577,9 +577,7 @@ class TestAddonIndexerWithES(ESTestCase):
         indexer = AddonIndexer()
         real_index_name = self.get_index_name('default')
         alias = indexer.get_index_alias()
-        mappings = self.es.indices.get_mapping(alias, include_type_name=False)[
-            real_index_name
-        ]['mappings']
+        mappings = self.es.indices.get_mapping(index=alias)[real_index_name]['mappings']
 
         actual_properties = mappings['properties']
         indexer_properties = indexer.get_mapping()['properties']
