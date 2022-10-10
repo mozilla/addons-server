554379c95a7e8b920ca086843651196c13ab584c
Andrew Williamson <awilliamson@mozilla.com>
2022-10-10 11:52:53 +0100

    add comments to explain ManifestStaticFilesStorageNotMaps.patterns

--- src/olympia/lib/storage.py
+++ src/olympia/lib/storage.py
@@ -6,11 +6,14 @@ class ManifestStaticFilesStorageNotMaps(ManifestStaticFilesStorage):
         (
             '*.css',
             (
+                # These regexs are copied from HashedFilesMixin in django4.1+
                 r"""(?P<matched>url\(['"]{0,1}\s*(?P<url>.*?)["']{0,1}\))""",
                 (
                     r"""(?P<matched>@import\s*["']\s*(?P<url>.*?)["'])""",
                     """@import url("%(url)s")""",
                 ),
+                # We are ommiting the sourceMappingURL regexs for .css and .js as they
+                # don't work how we copy over the souces in Makefile-docker.copy_node_js
             ),
         ),
     )
