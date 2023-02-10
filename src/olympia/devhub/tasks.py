b15f9131d0f4834ba5603078dd73f579fb44a8ea
Mathieu Pillard <mpillard@mozilla.com>
2023-02-10 14:31:54 +0100

    Update tests for the task itself

--- src/olympia/devhub/tasks.py
+++ src/olympia/devhub/tasks.py
@@ -19,7 +19,7 @@ from django.db import transaction
 from django.template import loader
 from django.urls import reverse
 from django.utils.encoding import force_str
-from django.utils.translation import activate, gettext
+from django.utils.translation import gettext
 
 from celery.result import AsyncResult
 from django_statsd.clients import statsd
@@ -30,13 +30,12 @@ from olympia import amo
 from olympia.addons.models import Addon
 from olympia.amo.celery import task
 from olympia.amo.decorators import use_primary_db
-from olympia.amo.templatetags.jinja_helpers import absolutify
+from olympia.amo.reverse import override_url_prefix
 from olympia.amo.utils import (
     image_size,
     resize_image,
     send_html_mail_jinja,
     send_mail,
-    to_language,
     utc_millesecs_from_epoch,
 )
 from olympia.api.models import SYMMETRIC_JWT_TYPE, APIKey
@@ -621,28 +620,28 @@ def send_initial_submission_acknowledgement_email(addon_pk, channel, email, **kw
     except Addon.DoesNotExist:
         # Add-on already deleted ? Ignore.
         return
-    # The email itself is not translated, but we should get the right locale
-    # prefix for the URLs by activating a language that makes sense.
-    activate(to_language(addon.default_locale))
-    context = {
-        'addon_name': str(addon.name),
-        'app': str(amo.FIREFOX.pretty),
-        'listed': channel == amo.CHANNEL_LISTED,
-        'detail_url': absolutify(addon.get_url_path()),
-    }
-    subject = f'Mozilla Add-ons: {addon.name} has been submitted to addons.mozilla.org!'
-    html_template = 'devhub/emails/submission.html'
-    text_template = 'devhub/emails/submission.txt'
-    return send_html_mail_jinja(
-        subject,
-        html_template,
-        text_template,
-        context,
-        recipient_list=[email],
-        from_email=settings.ADDONS_EMAIL,
-        use_deny_list=False,
-        perm_setting='individual_contact',
-    )
+    with override_url_prefix(locale=addon.default_locale):
+        context = {
+            'addon_name': str(addon.name),
+            'app': str(amo.FIREFOX.pretty),
+            'listed': channel == amo.CHANNEL_LISTED,
+            'detail_url': addon.get_absolute_url(),
+        }
+        subject = (
+            f'Mozilla Add-ons: {addon.name} has been submitted to addons.mozilla.org!'
+        )
+        html_template = 'devhub/emails/submission.html'
+        text_template = 'devhub/emails/submission.txt'
+        return send_html_mail_jinja(
+            subject,
+            html_template,
+            text_template,
+            context,
+            recipient_list=[email],
+            from_email=settings.ADDONS_EMAIL,
+            use_deny_list=False,
+            perm_setting='individual_contact',
+        )
 
 
 def send_api_key_revocation_email(emails):
