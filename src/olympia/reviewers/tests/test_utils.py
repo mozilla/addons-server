39d3c2d7952876d434c114b287ef0f5a121fa376
Mathieu Pillard <diox@users.noreply.github.com>
2023-03-21 20:03:37 +0100

    Allow confirmation of auto-approval of a disabled/deleted add-on (#20489)
    * Allow confirmation of auto-approval of a disabled/deleted add-on

--- src/olympia/reviewers/tests/test_utils.py
+++ src/olympia/reviewers/tests/test_utils.py
@@ -1413,13 +1413,12 @@ class TestReviewHelper(TestReviewHelperBase):
 
     def test_public_addon_confirm_auto_approval(self):
         self.grant_permission(self.user, 'Addons:Review')
-        self.setup_data(
-            amo.STATUS_APPROVED, file_status=amo.STATUS_APPROVED, human_review=True
-        )
         summary = AutoApprovalSummary.objects.create(
             version=self.review_version, verdict=amo.AUTO_APPROVED, weight=151
         )
-        assert summary.confirmed is None
+        self.setup_data(
+            amo.STATUS_APPROVED, file_status=amo.STATUS_APPROVED, human_review=True
+        )
         self.create_paths()
 
         # Safeguards.
@@ -1427,6 +1426,7 @@ class TestReviewHelper(TestReviewHelperBase):
         assert self.file.status == amo.STATUS_APPROVED
         assert self.addon.current_version.file.status == (amo.STATUS_APPROVED)
         assert not self.review_version.human_review_date
+        assert summary.confirmed is None
 
         self.helper.handler.confirm_auto_approved()
 
@@ -1624,6 +1624,70 @@ class TestReviewHelper(TestReviewHelperBase):
 
         flags.refresh_from_db()
         assert not flags.needs_human_review_by_mad
+
+    def test_deleted_addon_confirm_auto_approval(self):
+        self.grant_permission(self.user, 'Addons:Review')
+        self.grant_permission(self.user, 'Reviews:Admin')
+        self.setup_data(amo.STATUS_APPROVED, file_status=amo.STATUS_APPROVED)
+        self.review_version = self.addon.current_version
+        self.addon.delete()
+        self.review_version.reload()
+        self.file = self.review_version.file
+        summary = AutoApprovalSummary.objects.create(
+            version=self.review_version, verdict=amo.AUTO_APPROVED, weight=42
+        )
+        self.helper = self.get_helper()
+        self.helper.set_data(self.get_data())
+
+        assert 'confirm_auto_approved' in self.helper.actions
+        self.helper.handler.confirm_auto_approved()
+
+        summary.reload()
+        assert summary.confirmed
+        approvals_counter = AddonApprovalsCounter.objects.get(addon=self.addon)
+        self.assertCloseToNow(approvals_counter.last_human_review)
+        assert self.check_log_count(amo.LOG.CONFIRM_AUTO_APPROVED.id) == 1
+
+    def test_disabled_by_user_addon_confirm_auto_approval(self):
+        self.grant_permission(self.user, 'Addons:Review')
+        self.grant_permission(self.user, 'Reviews:Admin')
+        self.setup_data(amo.STATUS_APPROVED, file_status=amo.STATUS_APPROVED)
+        self.review_version = self.addon.current_version
+        self.addon.update(disabled_by_user=True)
+        self.review_version.reload()
+        self.file = self.review_version.file
+        summary = AutoApprovalSummary.objects.create(
+            version=self.review_version, verdict=amo.AUTO_APPROVED, weight=42
+        )
+        self.helper = self.get_helper()
+        self.helper.set_data(self.get_data())
+
+        assert 'confirm_auto_approved' in self.helper.actions
+        self.helper.handler.confirm_auto_approved()
+
+        summary.reload()
+        assert summary.confirmed
+        approvals_counter = AddonApprovalsCounter.objects.get(addon=self.addon)
+        self.assertCloseToNow(approvals_counter.last_human_review)
+        assert self.check_log_count(amo.LOG.CONFIRM_AUTO_APPROVED.id) == 1
+
+    def test_current_version_not_auto_approved_confirm_auto_approval_not_present(self):
+        self.grant_permission(self.user, 'Addons:Review')
+        self.grant_permission(self.user, 'Reviews:Admin')
+        self.setup_data(amo.STATUS_APPROVED, file_status=amo.STATUS_APPROVED)
+        self.review_version = version_factory(
+            # Prevent new version from becoming the current_version...
+            addon=self.addon,
+            file_kw={'status': amo.STATUS_DISABLED},
+        )
+        # ... But pretend it was auto-approved initially.
+        AutoApprovalSummary.objects.create(
+            version=self.review_version, verdict=amo.AUTO_APPROVED, weight=666
+        )
+        self.helper = self.get_helper()
+        self.helper.set_data(self.get_data())
+
+        assert 'confirm_auto_approved' not in self.helper.actions
 
     def test_confirm_multiple_versions_with_version_scanner_flags(self):
         self.grant_permission(self.user, 'Addons:ReviewUnlisted')
