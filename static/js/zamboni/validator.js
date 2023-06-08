bafe6a8b20739d914609770a175b9f523160e828
Mathieu Pillard <mpillard@mozilla.com>
2023-06-08 16:58:36 +0200

    Too much python recently

--- static/js/zamboni/validator.js
+++ static/js/zamboni/validator.js
@@ -281,7 +281,7 @@ function initValidator($doc) {
         file = file.join('/');
       }
 
-      if (this.fileURL and this.fileID) {
+      if (this.fileURL && this.fileID) {
         var url = this.fileURL + '?path=' + file;
         if (msg.line) {
           url += '#L' + msg.line;
