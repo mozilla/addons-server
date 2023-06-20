19a6106c8926c4408c60b84d544adb363dbad694
Andrew Williamson <awilliamson@mozilla.com>
2023-06-20 16:26:00 +0100

    return is_all_versions property in Block serializer

--- src/olympia/blocklist/tests/test_serializers.py
+++ src/olympia/blocklist/tests/test_serializers.py
@@ -1,4 +1,5 @@
-from olympia.amo.tests import TestCase, addon_factory, user_factory
+from olympia import amo
+from olympia.amo.tests import TestCase, addon_factory, user_factory, version_factory
 from olympia.amo.urlresolvers import get_outgoing_url
 
 from ..models import Block, BlockVersion
@@ -29,6 +30,7 @@ class TestBlockSerializer(TestCase):
                 'outgoing': get_outgoing_url('https://goo.gol'),
             },
             'versions': [],
+            'is_all_versions': True,
             'created': self.block.created.isoformat()[:-7] + 'Z',
             'modified': self.block.modified.isoformat()[:-7] + 'Z',
         }
@@ -41,3 +43,30 @@ class TestBlockSerializer(TestCase):
         serializer = BlockSerializer(instance=self.block)
         assert serializer.data['addon_name'] == {'en-US': 'Addón náme'}
         assert serializer.data['versions'] == [self.block.addon.current_version.version]
+
+    def test_is_all_versions(self):
+        # no add-on so True
+        assert BlockSerializer(instance=self.block).data['is_all_versions'] is True
+
+        addon = addon_factory(
+            guid=self.block.guid, name='Addón náme', file_kw={'is_signed': True}
+        )
+        BlockVersion.objects.create(block=self.block, version=addon.current_version)
+        version_factory(addon=addon, file_kw={'is_signed': False})  # not signed
+        self.block.refresh_from_db()
+        del self.block.addon
+        # add-on with only one signed version - so all blocked - but not disabled
+        assert BlockSerializer(instance=self.block).data['is_all_versions'] is False
+
+        addon.update(status=amo.STATUS_DISABLED)
+        del self.block.addon
+        # now the add-on is disabled with all signed versions blocked
+        assert BlockSerializer(instance=self.block).data['is_all_versions'] is True
+
+        # but if there was another signed version, even deleted on a deleted addon...
+        old_addon = addon_factory(file_kw={'is_signed': True})
+        old_addon.current_version.delete()
+        old_addon.delete()
+        old_addon.addonguid.update(guid=addon.guid)
+        # ... the guid isn't completely blocked
+        assert BlockSerializer(instance=self.block).data['is_all_versions'] is False
