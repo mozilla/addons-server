7bd1883f6f9ea90f9b82e3e4d49b0066565dd8b5
Andrew Williamson <awilliamson@mozilla.com>
2023-06-30 16:09:36 +0100

    add select all|none links for [un]block version checkboxes (#20912)

--- static/js/admin/blocklist_blocklistsubmission.js
+++ static/js/admin/blocklist_blocklistsubmission.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  const checkAllCheckboxes = (event) =>
+    event.target.parentElement
+      .querySelectorAll(`input[name="changed_version_ids"]`)
+      .forEach((checkbox) => checkbox.setAttribute('checked', true));
+  const clearAllCheckboxes = (event) =>
+    event.target.parentElement
+      .querySelectorAll(`input[name="changed_version_ids"]`)
+      .forEach((checkbox) => checkbox.removeAttribute('checked'));
+  document
+    .querySelectorAll('a.select-all-versions')
+    .forEach((a) => a.addEventListener('click', checkAllCheckboxes, true));
+  document
+    .querySelectorAll('a.select-none-versions')
+    .forEach((a) => a.addEventListener('click', clearAllCheckboxes, true));
+
   const enableIfChecked = (isChecked, fieldId) => {
     const field = document.querySelector(`#${fieldId}`);
     if (isChecked) {
