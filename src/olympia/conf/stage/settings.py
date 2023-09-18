9ba40386ec91f799a74d44b0178816402eb453dd
Mathieu Pillard <diox@users.noreply.github.com>
2023-09-18 15:32:56 +0200

    Switch remote settings URL to new GCP-specific endpoint on stage & prod (#21204)
    * Revert "Revert "Update Remote Settings URLs after GCP migration (#20642)" (#20729)"
    
    This reverts commit 257b6283c0bd7e2178b5048425a94cb693a2c10e.
    
    * Add support for REMOTE_SETTINGS_WRITER_URL env variable (defaulting to GCP URL if absent)

--- src/olympia/conf/stage/settings.py
+++ src/olympia/conf/stage/settings.py
@@ -74,5 +74,7 @@ EXTENSION_WORKSHOP_URL = env(
 )
 
 REMOTE_SETTINGS_API_URL = 'https://firefox.settings.services.allizom.org/v1/'
-REMOTE_SETTINGS_WRITER_URL = 'https://settings-writer.stage.mozaws.net/v1/'
+REMOTE_SETTINGS_WRITER_URL = env(
+    'REMOTE_SETTINGS_WRITER_URL', default='https://remote-settings.allizom.org/v1/'
+)
 REMOTE_SETTINGS_WRITER_BUCKET = 'staging'
