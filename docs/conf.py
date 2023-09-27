542089081bf958d0f69693241195b8c0ed92ab1c
Mathieu Pillard <diox@users.noreply.github.com>
2023-09-28 00:26:06 +0200

    Include sphinxcontrib-jquery to fix local install search

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.coverage',
     'extensions.src_role',
     'sphinxcontrib.httpdomain',
+    'sphinxcontrib.jquery',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
