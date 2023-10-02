8d00231f29890e4363f0b9e079823f4ff55a3429
Mathieu Pillard <diox@users.noreply.github.com>
2023-10-02 16:12:51 +0200

    Harmonize footers across devhub (and frontend pages a bit more) (#21244)
    * Harmonize footers across devhub (and frontend pages a bit more)
    
    Changes:
    - Copy over fonts from addons-frontend, use Inter everywhere
    - Use photon-footer everywhere, adding a light blue margin before
      to make the transition less aggressive and match header
    - Fix error pages block to align content correctly
    - Translate strings in footer

--- src/olympia/lib/settings_base.py
+++ src/olympia/lib/settings_base.py
@@ -564,12 +564,12 @@ EDITORIAL_CONTENT_FILENAME = 'src/olympia/discovery/strings.jinja2'
 # and js files that can be bundled together by the minify app.
 MINIFY_BUNDLES = {
     'css': {
+        'common/fonts': ('css/common/fonts.less',),
+        'common/footer': ('css/common/footer.less',),
         'restyle/css': ('css/restyle/restyle.less',),
         # CSS files our DevHub (currently only required for the
         # new landing page)
         'devhub/new-landing/css': ('css/devhub/new-landing/base.less',),
-        # Responsive error page styling.
-        'errors/css': ('css/errors/base.less',),
         # CSS files common to the entire site.
         'zamboni/css': (
             'css/legacy/main.css',
@@ -582,7 +582,6 @@ MINIFY_BUNDLES = {
             'css/impala/suggestions.less',
             'css/impala/header.less',
             'css/impala/moz-tab.css',
-            'css/impala/footer.less',
             'css/impala/faux-zamboni.less',
         ),
         'zamboni/stats': ('css/zamboni/stats.less',),
