b2bfa6698b3b29b3ede16091426c3fbae995e94b
Shridhar Shatrughan <shridharshatrughan2001@gmail.com>
2023-10-03 18:06:16 -0400

    Update utils.py

--- src/olympia/files/utils.py
+++ src/olympia/files/utils.py
@@ -1222,10 +1222,14 @@ def resolve_i18n_message(message, messages, locale, default_locale=None):
     if 'placeholders' in message:
         for placeholder, content in message['placeholders'].items():
             # Replace the placeholder with its content
-            resolved_message = re.sub(f'\\${placeholder}\\$', content['content'], resolved_message, flags=re.IGNORECASE)
+            resolved_message = re.sub(
+                f'\\${placeholder}\\$',
+                content['content'],
+                resolved_message,
+                flags=re.IGNORECASE,
+            )
 
     return resolved_message
-
 
 
 def get_background_images(file_obj, theme_data, header_only=False):
