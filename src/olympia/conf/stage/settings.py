43eef6f31d2d268896ebee427a6ff1aa1f7e1da2
Mathieu Pillard <diox@users.noreply.github.com>
2024-01-19 16:29:29 +0100

    Fix SERVICES_DOMAIN to be correct on dev/stage/prod (#21714)

--- src/olympia/conf/stage/settings.py
+++ src/olympia/conf/stage/settings.py
@@ -16,11 +16,12 @@ ENV = env('ENV')
 API_THROTTLING = True
 
 DOMAIN = env('DOMAIN', default='addons.allizom.org')
+SERVICES_DOMAIN = env('SERVICES_DOMAIN', default='services.addons.allizom.org')
 SERVER_EMAIL = 'zstage@addons.mozilla.org'
 SITE_URL = 'https://' + DOMAIN
 INTERNAL_SITE_URL = env('INTERNAL_SITE_URL', default='https://addons.allizom.org')
 EXTERNAL_SITE_URL = env('EXTERNAL_SITE_URL', default='https://addons.allizom.org')
-SERVICES_URL = env('SERVICES_URL', default='https://services.addons.allizom.org')
+SERVICES_URL = 'https://' + SERVICES_DOMAIN
 CODE_MANAGER_URL = env('CODE_MANAGER_URL', default='https://code.addons.allizom.org')
 STATIC_URL = '%s/static-server/' % EXTERNAL_SITE_URL
 MEDIA_URL = '%s/user-media/' % EXTERNAL_SITE_URL
