fb8a38ed45c9e4c9cc80c5cc1ec8e12c925ce88d
Kevin Meinhardt <kmeinhardt7@gmail.com>
2024-02-01 13:26:29 +0100

    Add CinderPolicy model and sync_cinder_policies management command (#21773)
    * CinderPolicy model: add parent and make uuid unique
    
    * Add sync_cinder_policies management command + task

---
+++ src/olympia/abuse/management/commands/sync_cinder_policies.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+import olympia.core.logger
+from olympia.abuse.tasks import sync_cinder_policies
+
+
+class Command(BaseCommand):
+    log = olympia.core.logger.getLogger('z.abuse')
+
+    def handle(self, *args, **options):
+        sync_cinder_policies.apply_async()
+
+        self.log.info('Triggered policy sync task.')
