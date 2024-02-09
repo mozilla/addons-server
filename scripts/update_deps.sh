b22f84644b686589073be9fb1adfb1d5d2d861eb
Kevin Meinhardt <kmeinhardt@mozilla.com>
2024-02-09 18:06:17 +0100

    tmp: splitting pip from the rest to ensure we have correct pip before any other packages are installed.

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -7,14 +7,16 @@ error() { echo -e "\033[31mERROR: $1\033[0m" && exit 1; }
 # Cleanup python build directory
 rm -rf /deps/build/*
 
+$PIP_COMMAND install --progress-bar=off --no-deps --exists-action=w -r "requirements/pip.txt"
+
+info "Installing prod dependencies"
+$PIP_COMMAND install --progress-bar=off --no-deps --exists-action=w -r "requirements/prod.txt"
+
 # Install dev dependencies unless --prod is passed
 if [[ ! "$@" == *"--prod"* ]]; then
   info "Installing dev dependencies"
   $PIP_COMMAND install --progress-bar=off --no-deps --exists-action=w -r "requirements/dev.txt"
 fi
-
-info "Installing prod dependencies"
-$PIP_COMMAND install --progress-bar=off --no-deps --exists-action=w -r "requirements/prod.txt"
 
 # Install python olympia module
 $PIP_COMMAND install --no-use-pep517 -e .
