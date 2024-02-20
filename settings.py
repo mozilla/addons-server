020742f26373283f694304900b10c01963d2a77a
Kevin Meinhardt <kmeinhardt@mozilla.com>
2024-02-20 12:27:20 +0100

    Create empty local_settings.py if none exsits

--- settings.py
+++ settings.py
@@ -132,17 +132,16 @@ CUSTOMS_API_KEY = 'customssecret'
 
 REMOTE_SETTINGS_IS_TEST_SERVER = True
 
-# If you have settings you want to overload, put them in a local_settings.py.
-try:
-    from local_settings import *  # noqa
-except ImportError:
-    import traceback
-    import warnings
+local_settings_path = path('local_settings.py')
 
-    warnings.warn(
-        'Could not import local_settings module. {}'.format(traceback.format_exc()),
-        stacklevel=1,
-    )
+if not os.path.exists(local_settings_path):
+    with open(local_settings_path, 'w') as file:
+        file.write(
+            '# If you have settings you want to overload,'
+            'put them in a local_settings.py.\n'
+        )
+
+from local_settings import *  # noqa
 
 SITEMAP_DEBUG_AVAILABLE = True
 
