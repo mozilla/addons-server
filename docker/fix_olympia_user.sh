7f1ff511381c02ad6baaca1881297ca5b39b7e68
Kevin Meinhardt <kmeinhardt7@gmail.com>
2024-03-18 11:42:04 +0100

    Automate L10n extract/merge with reusable docker run github action (#21966)
    * Reusable Docker run action (Bunch of other stuff here to make it work)
    
    * extract locales
    
    * remove extraneous log
    
    ---------
    
    Co-authored-by: Mozilla Add-ons Robot <addons-dev-automation+github@mozilla.com>

--- docker/fix_olympia_user.sh
+++ docker/fix_olympia_user.sh
@@ -1,3 +1,12 @@
+
+#!/bin/bash
+
+set -e
+
+echo "Updating olympia user to host defined UID/GID..."
+echo "UID: $UID"
+echo "GID: $GID"
+
 # Alter the uid/gid of the olympia user/group to match the host
 usermod -u ${UID} olympia
 groupmod -g ${GID} olympia
--- scripts/extract-l10n.sh
+++ scripts/extract-l10n.sh
@@ -25,8 +25,6 @@ GIT_REMOTE="https://github.com/mozilla/addons-server.git"  # Upstream.
 REV=""
 MESSAGE=""
 BRANCH_NAME=""
-ROBOT_EMAIL="addons-dev-automation+github@mozilla.com"
-ROBOT_NAME="Mozilla Add-ons Robot"
 
 DRY_RUN=false
 
@@ -79,13 +77,6 @@ function init_environment {
     make -f Makefile-docker update_deps
 }
 
-function commit {
-    info "Committing..."
-    git -c user.name="$ROBOT_NAME" -c user.email="$ROBOT_EMAIL" commit -m "$MESSAGE" --author "$ROBOT_NAME <$ROBOT_EMAIL>" --no-gpg-sign locale/*/LC_MESSAGES/*.po locale/templates/
-    info "Committed locales extraction to local branch."
-}
-
-
 function generate_post_data()
 {
   cat <<EOF
@@ -99,8 +90,6 @@ EOF
 
 
 function create_pull_request {
-    info "Pushing the branch..."
-    git push -q "https://addons-robot:$GITHUB_TOKEN@github.com/mozilla/addons-server/"
     info "Creating the auto merge pull request for $BRANCH_NAME ..."
     curl --verbose -H "Authorization: token $GITHUB_TOKEN" --data "$(generate_post_data)" "https://api.github.com/repos/mozilla/addons-server/pulls"
     info "Pull request created."
@@ -117,7 +106,7 @@ fi
 ./scripts/run_l10n_extraction.sh
 
 if [[ $DRY_RUN == false ]]; then
-  commit
+  ./scripts/push_l10n_extraction.sh
 
   # This script is meant to be run inside a virtualenv or inside our docker
   # container. If it's the latter, it doesn't necessarily have access to the ssh
