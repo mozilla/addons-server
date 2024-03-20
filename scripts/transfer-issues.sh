302a57609c4f0f1e384c53cc55f86732fef316bf
Kevin Meinhardt <kmeinhardt@mozilla.com>
2024-03-20 10:45:47 +0100

    tmp wip

--- scripts/transfer-issues.sh
+++ scripts/transfer-issues.sh
@@ -2,11 +2,31 @@
 
 COUNT=1
 
-gh issue list \
+FROM_REPO="https://github.com/mozilla/addons-server"
+TO_REPO="https://github.com/mozilla/addons"
+
+gh label clone $FROM_REPO -R $TO_REPO --force
+
+ISSUE_NUMBERS=$(gh issue list \
+  -R $FROM_REPO \
   -s all \
   -L $COUNT \
   --json number \
   --search sort:created-asc \
-  --jq '.[] | .number' | \
-  xargs -I% gh issue transfer % https://github.com/mozilla/addons
+  --jq '.[] | .number'
+)
 
+transfer_issue() {
+  issue_number=$1
+  to_repo=$2
+  result=$(gh issue transfer "$issue_number" "$to_repo")
+
+  echo $result
+}
+
+export -f transfer_issue
+
+echo "Transferring issues from \"$FROM_REPO\" to \"$TO_REPO\""
+echo "Issues: $ISSUE_NUMBERS"
+
+echo "$ISSUE_NUMBERS" | xargs -P 4 -I % sh -c -e 'transfer_issue % '"$TO_REPO"
