2f06bf324db248c1cb6ce7e7c0c59f75d3b57e6a
Kevin Meinhardt <kmeinhardt@mozilla.com>
2024-03-20 14:29:05 +0100

    Make better the stuffs

--- scripts/transfer-issues.sh
+++ scripts/transfer-issues.sh
@@ -1,32 +1,43 @@
 #! /bin/bash
 
+set -x
+
 COUNT=1
 
-FROM_REPO="https://github.com/mozilla/addons-server"
-TO_REPO="https://github.com/mozilla/addons"
+GITHUB_URL="https://github.com"
+GITHUB_ORG="mozilla"
+
+FROM_NAME="addons-server"
+TO_NAME="addons"
+
+FROM_REPO="$GITHUB_URL/$GITHUB_ORG/$FROM_NAME"
+TO_REPO="$GITHUB_URL/$GITHUB_ORG/$TO_NAME"
+
+REPO_LABEL="repository:$FROM_NAME"
 
 gh label clone $FROM_REPO -R $TO_REPO --force
 
-ISSUE_NUMBERS=$(gh issue list \
-  -R $FROM_REPO \
-  -s all \
-  -L $COUNT \
-  --json number \
-  --search sort:created-asc \
-  --jq '.[] | .number'
-)
+gh label create $REPO_LABEL
 
 transfer_issue() {
   issue_number=$1
-  to_repo=$2
+  from_repo=$2
+  to_repo=$3
+  repo_label=$4
+
+  echo "$from_repo/issues/$issue_number" >> old-issues.txt
+
   result=$(gh issue transfer "$issue_number" "$to_repo")
 
-  echo $result
+  echo "$result" >> new-issues.txt
+
+  echo "Transferred issue $issue_number to $result"
+
+  gh issue -R $to_repo edit $result --add-label $repo_label
 }
 
 export -f transfer_issue
 
 echo "Transferring issues from \"$FROM_REPO\" to \"$TO_REPO\""
-echo "Issues: $ISSUE_NUMBERS"
 
-echo "$ISSUE_NUMBERS" | xargs -P 4 -I % sh -c -e 'transfer_issue % '"$TO_REPO"
+gh issue list -R "$FROM_REPO" -s all -L $COUNT --json number --search sort:created-asc --jq '.[] | .number' | xargs -P 4 -I % bash -c -e 'transfer_issue % '"$FROM_REPO $TO_REPO $REPO_LABEL" 2>&1
