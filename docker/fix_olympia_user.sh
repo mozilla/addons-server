5338c393be1c1cdea92c7c9d37789deda817d36a
Kevin Meinhardt <kmeinhardt@mozilla.com>
2024-04-22 16:14:58 +0200

    Use docker compose to run CI actions
    TMP: remove olympia mount from ci.yml
    
    TMP: olympia mount compose_files
    
    TMP: remove localization

--- docker/fix_olympia_user.sh
+++ docker/fix_olympia_user.sh
@@ -1,12 +1,21 @@
-
 #!/bin/bash
 
-set -e
+set -ue
+whoami
 
 echo "Updating olympia user to host defined UID/GID..."
-echo "UID: $UID"
-echo "GID: $GID"
+echo "UID: $(id -u olympia) -> $UID"
+echo "GID: $(id -g olympia) -> $GID"
 
 # Alter the uid/gid of the olympia user/group to match the host
 usermod -u ${UID} olympia
 groupmod -g ${GID} olympia
+
+# Ensure the `new` olympia user has access to the deps directory
+chown -R olympia:olympia /deps
+
+su -s /bin/bash olympia <<EOF
+whoami
+set -xue
+$@
+EOF
