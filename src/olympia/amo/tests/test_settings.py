0d0c846ff3473bb0f7694bad07b2dca2380fb226
Kevin Meinhardt <kmeinhardt@mozilla.com>
2024-04-22 10:12:18 +0200

    Generate version.json as part of docker build: - remove git dependency from version.json generation - use only version.json file for sentry release with soft/hard fail

--- src/olympia/amo/tests/test_settings.py
+++ src/olympia/amo/tests/test_settings.py
@@ -1,63 +1,39 @@
 import json
 import os
 from copy import deepcopy
+from unittest import mock
 
 from django.conf import settings
 
+import pytest
 from sentry_sdk.hub import Hub
 
 from olympia.core.sentry import get_sentry_release
 
 
-def test_sentry_release_config():
-    version_json = os.path.join(settings.ROOT, 'version.json')
-    original = None
-    sentry_client = Hub.current.client
-    current_release = sentry_client.options.get('release')
-    assert get_sentry_release() == current_release
+@mock.patch.dict('os.environ', {'REQUIRE_SENTRY_VERSION': 'True'})
+def test_sentry_release_nothing_provided_strict():
+    version = {}
+    with mock.patch('builtins.open', mock.mock_open(read_data=json.dumps(version))):
+        pytest.raises(ValueError, get_sentry_release)
 
-    # There is a version.json that contains `version: origin/master`
-    # by default in the repository. We should ignore that and fetch
-    # the git commit.
-    assert len(get_sentry_release()) == 40
 
-    # Cleanup for tests
-    if os.path.exists(version_json):
-        with open(version_json) as fobj:
-            original = fobj.read()
+def test_sentry_release_nothing_provided_loose():
+    version = {}
+    with mock.patch('builtins.open', mock.mock_open(read_data=json.dumps(version))):
+        assert get_sentry_release() is None
 
-        os.remove(version_json)
 
-    # by default, if no version.json exists it simply fetches a git-sha
-    assert len(get_sentry_release()) == 40
+def test_sentry_release_version_provided():
+    version = {'version': '2024.01.01'}
+    with mock.patch('builtins.open', mock.mock_open(read_data=json.dumps(version))):
+        assert get_sentry_release() == version['version']
 
-    # It fetches `version` from the version.json
-    with open(version_json, 'w') as fobj:
-        fobj.write(json.dumps({'version': '2018.07.19'}))
 
-    assert get_sentry_release() == '2018.07.19'
-
-    os.remove(version_json)
-
-    # Or tries to get the commit from version.json alternatively
-    with open(version_json, 'w') as fobj:
-        fobj.write(json.dumps({'commit': '1111111'}))
-
-    assert get_sentry_release() == '1111111'
-
-    if original:
-        with open(version_json, 'w') as fobj:
-            fobj.write(original)
-
-    # Usual state of things, version is empty but commit is set
-    with open(version_json, 'w') as fobj:
-        fobj.write(json.dumps({'version': '', 'commit': '1111111'}))
-
-    assert get_sentry_release() == '1111111'
-
-    if original:
-        with open(version_json, 'w') as fobj:
-            fobj.write(original)
+def test_sentry_release_commit_provided():
+    version = {'commit': 'abc'}
+    with mock.patch('builtins.open', mock.mock_open(read_data=json.dumps(version))):
+        assert get_sentry_release() == version['commit']
 
 
 def test_sentry_data_scrubbing():
