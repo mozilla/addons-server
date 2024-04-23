b1add2b705c29223b0fbcf41806c32d184986dd6
Mathieu Pillard <mpillard@mozilla.com>
2024-04-23 12:36:09 +0200

    Refactor tests to use addon_factory()

--- src/olympia/versions/tests/test_models.py
+++ src/olympia/versions/tests/test_models.py
@@ -970,44 +970,40 @@ class TestVersion(AMOPaths, TestCase):
         assert version.should_have_due_date
 
     def test_should_have_due_date_listed_theme(self):
-        addon = Addon.objects.get(id=3615)
-        addon.update(type=amo.ADDON_STATICTHEME)
-        version = addon.current_version
+        addon = addon_factory(
+            status=amo.STATUS_NULL,
+            type=amo.ADDON_STATICTHEME,
+            file_kw={'status': amo.STATUS_AWAITING_REVIEW},
+        )
+        version = addon.versions.get()
 
+        # Listed version of an incomplete add-on should not have a due date.
         assert not version.should_have_due_date
-        # having the needs_human_review flag means a due dute is needed
+
+        # Unless they have the explicit a NeedsHumanReview flag active.
         needs_human_review = NeedsHumanReview.objects.create(version=version)
         assert version.should_have_due_date
 
-        # A theme version awaiting review will need a due date since they are
-        # not auto-approved.
         needs_human_review.update(is_active=False)
-        version.file.update(status=amo.STATUS_AWAITING_REVIEW)
-        assert version.should_have_due_date
-
-        # Not if the add-on is incomplete though.
-        addon.update(status=amo.STATUS_NULL)
         assert not version.should_have_due_date
 
     def test_should_have_due_date_unlisted_theme(self):
-        addon = Addon.objects.get(id=3615)
-        addon.update(type=amo.ADDON_STATICTHEME)
-        version = addon.current_version
-        version.update(channel=amo.CHANNEL_UNLISTED)
+        addon = addon_factory(
+            status=amo.STATUS_NULL,
+            type=amo.ADDON_STATICTHEME,
+            version_kw={'channel': amo.CHANNEL_UNLISTED},
+            file_kw={'status': amo.STATUS_AWAITING_REVIEW},
+        )
+        version = addon.versions.get()
 
-        assert not version.should_have_due_date
-        # having the needs_human_review flag means a due dute is needed
+        # Unlisted version of an incomplete add-on should have a due date.
+        assert version.should_have_due_date
+
+        # Whether they have the explicit a NeedsHumanReview flag active or not.
         needs_human_review = NeedsHumanReview.objects.create(version=version)
         assert version.should_have_due_date
 
-        # A theme version awaiting review will need a due date since they are
-        # not auto-approved.
         needs_human_review.update(is_active=False)
-        version.file.update(status=amo.STATUS_AWAITING_REVIEW)
-        assert version.should_have_due_date
-
-        # Even if incomplete, since that's an unlisted version.
-        addon.update(status=amo.STATUS_NULL)
         assert version.should_have_due_date
 
     def _test_should_have_due_date_disabled(self, channel):
