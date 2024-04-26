b394e1ddfe2c786c45357bd49b2a1552e88fb7b6
Mathieu Pillard <mpillard@mozilla.com>
2024-04-26 20:29:38 +0200

    Use cls since we're in a class method

--- src/olympia/versions/models.py
+++ src/olympia/versions/models.py
@@ -1234,10 +1234,8 @@ class VersionProvenance(models.Model):
                 statsd.incr(f'{formatted_source}.webext_version.{webext_version}')
         # Create the instance no matter what (client_info may be empty, that's
         # fine).
-        return VersionProvenance.objects.create(
-            version=version,
-            source=source,
-            client_info=client_info,
+        return cls.objects.create(
+            version=version, source=source, client_info=client_info
         )
 
 
