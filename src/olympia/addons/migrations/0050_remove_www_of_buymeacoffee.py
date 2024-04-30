3638d2abfdd7d1a9a13de88f16aba97aa41959be
Sébastien Règne <regseb@gmail.com>
2024-04-30 11:57:52 +0200

    Add import

--- src/olympia/addons/migrations/0050_remove_www_of_buymeacoffee.py
+++ src/olympia/addons/migrations/0050_remove_www_of_buymeacoffee.py
@@ -1,8 +1,16 @@
 from django.db import migrations
+from django.db.models import Value
+from django.db.models.functions import Replace
 
 def contributions_url_remove_www(apps, schema_editor):
     Addon = apps.get_model('addons', 'Addon')
-    Addon.unfiltered.filter(contributions__startswith='https://www.buymeacoffee.com').update(contributions=Replace('contributions', Value('www.buymeacoffee.com'), Value('buymeacoffee.com')))
+    Addon.unfiltered.filter(contributions__startswith='https://www.buymeacoffee.com').update(
+        contributions=Replace(
+            'contributions',
+            Value('www.buymeacoffee.com'),
+            Value('buymeacoffee.com'),
+        ),
+    )
 
 class Migration(migrations.Migration):
     dependencies = [
