21027e7e0d3823942e1b4b97572bcdf03af4cac8
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2024-04-30 10:02:40 -0700

    Add web extension permission description to the permissions tab. (#272)
    * Add web extension permission description to the permissions tab.
    
    * Re-add the sentence about granting permissions, and if we're an experiment only show the "Full access" permission line.

--- src/olympia/addons/models.py
+++ src/olympia/addons/models.py
@@ -1477,7 +1477,8 @@ class Addon(OnChangeMixin, ModelBase):
         return (self.type == amo.ADDON_EXTENSION and
                 version and version.all_files[0] and
                 (not version.all_files[0].is_webextension or
-                 version.all_files[0].webext_permissions))
+                 version.all_files[0].webext_permissions or
+                 version.all_files[0].is_experiment))
 
     @property
     def needs_admin_code_review(self):
