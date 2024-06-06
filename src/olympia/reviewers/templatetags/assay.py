5849f63b5404aa43095331b91d8122ecb57c118d
Christina Lin <chrlin@mozilla.com>
2024-06-06 11:41:48 -0400

    template tag name

--- src/olympia/reviewers/templatetags/assay.py
+++ src/olympia/reviewers/templatetags/assay.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def assay_url(addon_guid, version, file=None):
-    url = f'vscode://mozilla.assay/review/{addon_guid}/{version}'
-    if file:
-        url = f'{url}?path={file}'
+def assay_url(addon_guid, version_string, filepath=None):
+    url = f'vscode://mozilla.assay/review/{addon_guid}/{version_string}'
+    if filepath:
+        url = f'{url}?path={filepath}'
     return url
