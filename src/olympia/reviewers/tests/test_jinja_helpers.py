5a81b1510b3b896f4bb97c0bec187e66673eb7c9
Christina Lin <chrlin@mozilla.com>
2024-06-06 12:27:54 -0400

    ruff format

--- src/olympia/reviewers/tests/test_jinja_helpers.py
+++ src/olympia/reviewers/tests/test_jinja_helpers.py
@@ -9,7 +9,9 @@ pytestmark = pytest.mark.django_db
 def test_create_an_assay_url():
     assert jinja_helpers.assay_url(
         addon_guid='{guid}', version_string='version', filepath='file.js'
-    ) == assay.assay_url(addon_guid='{guid}', version_string='version', filepath='file.js')
+    ) == assay.assay_url(
+        addon_guid='{guid}', version_string='version', filepath='file.js'
+    )
 
 
 def test_create_a_code_manager_url():
