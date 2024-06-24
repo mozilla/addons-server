3de4b38e9044f294d9580552f8f177ba3f8b0a33
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2024-06-24 14:42:26 -0700

    Remove personas from profile (#296)
    * Remove the persona section from user profile
    
    * Prevent addons without their persona from crashing the frontend.

--- src/olympia/addons/templatetags/jinja_helpers.py
+++ src/olympia/addons/templatetags/jinja_helpers.py
@@ -236,6 +236,12 @@ def new_context(context, **kw):
 @jinja2.contextfunction
 def persona_preview(context, persona, size='large', linked=True, extra=None,
                     details=False, title=False, caption=False, url=None):
+    # Signal to the html that we don't want to render anything
+    if persona is None:
+        c = dict(context.items())
+        c.update({'persona': None})
+        return context.items()
+
     preview_map = {'large': persona.preview_url,
                    'small': persona.thumb_url}
     addon = persona.addon
