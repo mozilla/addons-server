63eedd31cf3ab3eb8253f39ec4a87bd90879931d
Mathieu Pillard <diox@users.noreply.github.com>
2024-07-23 14:45:09 +0200

    Avoid extra queries around banned user content in user admin (#22500)
    * Avoid extra queries around banned user content in user admin
    
    Before this change these queries would happen all the time: we display counts
    of disabled content on ban for each type, that's 4 queries, and for every user,
    even not banned, Django would execute them once to display the hidden instance
    used to create additional instances when the user adds one, and another time
    for the existing default instance displayed because of the `extra` bit.
    
    Because those queries do JOINs _and_ there isn't even an actual
    banned_user_content row to find, there were extra slow on top of being useless.
    
    Preventing adds (it's an internal model anyway that admins should look at, but
    not manually add) and removing the extra default instance fixes the problem.

--- src/olympia/users/tests/test_admin.py
+++ src/olympia/users/tests/test_admin.py
@@ -2,6 +2,7 @@ from unittest import mock
 
 from django.contrib import admin
 from django.contrib.auth.models import AnonymousUser
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.messages.storage import default_storage as default_messages_storage
 from django.db import connection
 from django.test import RequestFactory
@@ -46,6 +47,9 @@ class TestUserAdmin(TestCase):
         self.delete_url = reverse(
             'admin:users_userprofile_delete', args=(self.user.pk,)
         )
+        # Preload content type for Add-on so that it's done before we check
+        # SQL queries
+        ContentType.objects.get_for_model(UserProfile)
 
     def test_search_by_email_simple(self):
         user = user_factory(email='someone@mozilla.com')
@@ -476,6 +480,27 @@ class TestUserAdmin(TestCase):
         assert alog.action == amo.LOG.ADMIN_USER_EDITED.id
         assert alog.arguments == [self.user]
         assert alog.details == {'username': [old_username, 'foo']}
+
+    def test_queries_detail(self):
+        user = user_factory(email='someone@mozilla.com')
+        # We want to see absolutely everything, so make our user a superadmin.
+        self.grant_permission(user, '*:*')
+        self.client.force_login(user)
+        with self.assertNumQueries(21):
+            # - 4 savepoint/release
+            # - 2 current logged in user & groups
+            # - 2 target user & groups
+            # - 1 banned user content instance
+            # - 8 related content counts
+            #     (collections, addons, ratings, activity, abuse reports by
+            #      this user, abuse reports against this user,
+            #      restriction history, user history for email changes)
+            # - 1 last activity date
+            # - 1 known activity ips
+            # - 1 api key
+            # - 1 all group names (for dropdown where we can add groups)
+            response = self.client.get(self.detail_url)
+        assert response.status_code == 200
 
     @mock.patch.object(UserProfile, '_delete_related_content')
     def test_can_not_delete_with_users_edit_permission(
