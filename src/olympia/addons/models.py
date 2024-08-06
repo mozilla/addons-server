1eb9fdd5346d9e63250692f1c632ce391aa63cc3
Andrew Williamson <awilliamson@mozilla.com>
2024-08-06 13:56:39 +0100

    fix linelength that ruff format didn't autofix

--- src/olympia/addons/models.py
+++ src/olympia/addons/models.py
@@ -367,7 +367,9 @@ class AddonManager(ManagerBase):
                 ),
                 **{
                     name: Exists(versions_due_qs.filter(q))
-                    for name, q in Version.unfiltered.get_due_date_reason_q_objects().items()
+                    for name, q in (
+                        Version.unfiltered.get_due_date_reason_q_objects().items()
+                    )
                 },
             )
             .filter(first_version_id__isnull=False)
