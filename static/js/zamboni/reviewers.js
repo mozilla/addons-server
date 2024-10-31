076e02fb9efabc93819a467bcb71830914a4fd84
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2024-10-31 15:17:12 -0400

    Show proper pastebin attachment input if it already contains data

--- static/js/zamboni/reviewers.js
+++ static/js/zamboni/reviewers.js
@@ -303,19 +303,28 @@ function initExtraReviewActions() {
     }),
   );
 
-  $('#toggle_attachment_file').on('click', function (e) {
-    e.preventDefault();
+  const showFileWrapper = (e) => {
+    e?.preventDefault();
     $('#attachment-type-toggle').addClass('hidden');
     $('#attachment_input_wrapper').addClass('hidden');
     $('#attachment_file_wrapper').removeClass('hidden');
-  });
-
-  $('#toggle_attachment_input').on('click', function (e) {
-    e.preventDefault();
+  }
+  const showInputWrapper = (e) => {
+    e?.preventDefault();
     $('#attachment-type-toggle').addClass('hidden');
     $('#attachment_file_wrapper').addClass('hidden');
     $('#attachment_input_wrapper').removeClass('hidden');
-  });
+  }
+
+  if($('#id_attachment_file').prop('files').length){
+    showFileWrapper();
+  }
+  if($('#id_attachment_input').val()){
+    showInputWrapper();
+  }
+
+  $('#toggle_attachment_file').on('click', showFileWrapper);
+  $('#toggle_attachment_input').on('click', showInputWrapper);
 
   // One-off-style buttons.
   $('.more-actions button.oneoff[data-api-url]').click(
