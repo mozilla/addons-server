92955f3d60e45921c6347f49af059cfdb388e980
Kevin Meinhardt <kmeinhardt7@gmail.com>
2024-12-16 13:55:16 +0100

    Skip reindex on normal make up where index alias exists (#22819)
    * Skip reindex on normal make up where index alias exists
    
    * Update src/olympia/search/management/commands/reindex.py
    
    Co-authored-by: Mathieu Pillard <diox@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Mathieu Pillard <diox@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mathieu Pillard <diox@users.noreply.github.com>

--- src/olympia/amo/management/commands/initialize.py
+++ src/olympia/amo/management/commands/initialize.py
@@ -62,4 +62,6 @@ class Command(BaseDataCommand):
         # We should reindex even if no data is loaded/modified
         # because we might have a fresh instance of elasticsearch
         else:
-            call_command('reindex', '--wipe', '--force', '--noinput')
+            call_command(
+                'reindex', '--wipe', '--force', '--noinput', '--skip-if-exists'
+            )
