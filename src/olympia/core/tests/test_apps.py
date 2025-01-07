e3242606a836cf65ce2597c42abbe8e9863fbf9f
Kevin Meinhardt <kmeinhardt@mozilla.com>
2025-01-07 10:21:55 +0100

    Better checks that static file routing works as expected - Removed obsolete file checks and user validation from Makefile-docker. - Updated Nginx configuration to improve static file handling and added headers for better traceability. - Refactored storage management commands in the Python codebase:   - Renamed `clean_storage` to `make_storage` for clarity and added a `clean` parameter.   - Updated command implementations to use the new `make_storage` method. - Introduced a new system check for Nginx configurations to ensure proper file accessibility and response validation.

--- src/olympia/core/tests/test_apps.py
+++ src/olympia/core/tests/test_apps.py
@@ -1,9 +1,13 @@
+import os
+import tempfile
 from unittest import mock
 
-from django.core.management import call_command
+from django.core.management import CommandError, call_command
 from django.core.management.base import SystemCheckError
 from django.test import TestCase
 from django.test.utils import override_settings
+
+import responses
 
 from olympia.core.utils import REQUIRED_VERSION_KEYS
 
@@ -20,10 +24,24 @@ class SystemCheckIntegrationTest(TestCase):
         }
         patch = mock.patch(
             'olympia.core.apps.get_version_json',
-            return_value=self.default_version_json,
         )
         self.mock_get_version_json = patch.start()
+        self.mock_get_version_json.return_value = self.default_version_json
         self.addCleanup(patch.stop)
+
+        mock_dir = tempfile.mkdtemp(prefix='static-root')
+        self.fake_css_file = os.path.join(mock_dir, 'foo.css')
+        with open(self.fake_css_file, 'w') as f:
+            f.write('body { background: red; }')
+
+        patch_command = mock.patch('olympia.core.apps.call_command')
+        self.mock_call_command = patch_command.start()
+        self.mock_call_command.side_effect = (
+            lambda command, dry_run, stdout: stdout.write(f'{self.fake_css_file}\n')
+        )
+        self.addCleanup(patch_command.stop)
+
+        self.media_root = tempfile.mkdtemp(prefix='media-root')
 
     @mock.patch('olympia.core.apps.connection.cursor')
     def test_db_charset_check(self, mock_cursor):
@@ -85,3 +103,115 @@ class SystemCheckIntegrationTest(TestCase):
 
         with override_settings(HOST_UID=1000):
             call_command('check')
+
+    def test_static_check_no_assets_found(self):
+        """
+        Test static_check fails if compress_assets reports no files.
+        """
+        self.mock_get_version_json.return_value['target'] = 'production'
+        # Simulate "compress_assets" returning no file paths.
+        self.mock_call_command.side_effect = (
+            lambda command, dry_run, stdout: stdout.write('')
+        )
+        with self.assertRaisesMessage(
+            SystemCheckError, 'No compressed asset files were found.'
+        ):
+            call_command('check')
+
+    @mock.patch('os.path.exists')
+    def test_static_check_missing_assets(self, mock_exists):
+        """
+        Test static_check fails if at least one specified compressed
+        asset file does not exist.
+        """
+        self.mock_get_version_json.return_value['target'] = 'production'
+        # Simulate "compress_assets" returning a couple of files.
+        self.mock_call_command.side_effect = (
+            lambda command, dry_run, stdout: stdout.write(
+                f'{self.fake_css_file}\nfoo.js\n'
+            )
+        )
+        # Pretend neither file exists on disk.
+        mock_exists.return_value = False
+
+        with self.assertRaisesMessage(
+            SystemCheckError,
+            # Only the first missing file triggers the AssertionError message check
+            'Compressed asset file does not exist: foo.js',
+        ):
+            call_command('check')
+
+    def test_static_check_command_error(self):
+        """
+        Test static_check fails if there's an error during compress_assets.
+        """
+        self.mock_get_version_json.return_value['target'] = 'production'
+        self.mock_call_command.side_effect = CommandError('Oops')
+        with self.assertRaisesMessage(
+            SystemCheckError, 'Error running compress_assets command: Oops'
+        ):
+            call_command('check')
+
+    def test_static_check_command_success(self):
+        """
+        Test static_check succeeds if compress_assets runs without errors.
+        """
+        self.mock_get_version_json.return_value['target'] = 'production'
+        self.mock_call_command.side_effect = (
+            lambda command, dry_run, stdout: stdout.write(f'{self.fake_css_file}\n')
+        )
+        call_command('check')
+
+    def test_nginx_skips_check_on_production_target(self):
+        fake_media_root = '/fake/not/real'
+        with override_settings(MEDIA_ROOT=fake_media_root):
+            call_command('check')
+
+    def test_nginx_raises_missing_directory(self):
+        self.mock_get_version_json.return_value['target'] = 'development'
+        fake_media_root = '/fake/not/real'
+        with override_settings(MEDIA_ROOT=fake_media_root):
+            with self.assertRaisesMessage(
+                SystemCheckError,
+                f'{fake_media_root} does not exist',
+            ):
+                call_command('check')
+
+    def _test_nginx_response(
+        self, base_url, status_code=200, response_text='', served_by='nginx'
+    ):
+        self.mock_get_version_json.return_value['target'] = 'development'
+        url = f'{base_url}/test.txt'
+
+        responses.add(
+            responses.GET,
+            url,
+            status=status_code,
+            body=response_text,
+            headers={'X-Served-By': served_by},
+        )
+
+        expected_config = (
+            (status_code, 200),
+            (response_text, self.media_root),
+            (served_by, 'nginx'),
+        )
+
+        with override_settings(MEDIA_ROOT=self.media_root):
+            with self.assertRaisesMessage(
+                SystemCheckError,
+                f'Failed to access {url}. {expected_config}',
+            ):
+                call_command('check')
+
+    def test_nginx_raises_non_200_status_code(self):
+        """Test that files return a 200 status code."""
+        self._test_nginx_response('http://nginx/user-media', status_code=404)
+
+    def test_nginx_raises_unexpected_content(self):
+        """Test that files return the expected content."""
+        self._test_nginx_response('http://nginx/user-media', response_text='foo')
+
+    def test_nginx_raises_unexpected_served_by(self):
+        """Test that files are served by nginx and not redirected elsewhere."""
+        self._test_nginx_response('http://nginx/user-media', served_by='wow')
