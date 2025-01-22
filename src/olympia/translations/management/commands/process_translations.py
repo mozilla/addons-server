9e6427ecc98db698363b3d2f43adf0295ee43ac9
Mathieu Pillard <mpillard@mozilla.com>
2025-01-22 22:01:10 +0100

    Put this back the way it was

--- src/olympia/translations/management/commands/process_translations.py
+++ src/olympia/translations/management/commands/process_translations.py
@@ -1,7 +1,10 @@
+import os
+
 from django.db.models import Q
 
 from olympia.amo.management import ProcessObjectsCommand
 from olympia.translations.models import Translation
+from olympia.translations.tasks import update_outgoing_url
 
 
 class Command(ProcessObjectsCommand):
@@ -9,9 +12,17 @@ class Command(ProcessObjectsCommand):
         return Translation
 
     def get_tasks(self):
+        # Change this on dev/stage using an environement variable. The trailing
+        # slash is important (matches how settings.REDIRECT_URL is set).
+        old_outgoing_url = os.environ.get(
+            'OLD_OUTGOING_URL', 'https://outgoing.prod.mozaws.net/v1/'
+        )
         return {
-            'copy_spanish_translations': {
-                #                'task': ...,
-                'queryset_filters': [Q(locale='es')],
+            'update_outgoing_url': {
+                'task': update_outgoing_url,
+                'queryset_filters': [
+                    Q(localized_string_clean__icontains=old_outgoing_url),
+                ],
+                'kwargs': {'old_outgoing_url': old_outgoing_url},
             },
         }
