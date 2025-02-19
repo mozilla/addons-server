d87095927831a43dbdac9a931aece432a8c68eb2
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2025-02-19 14:37:24 -0500

    use promoted_group() in indexer

--- src/olympia/addons/indexers.py
+++ src/olympia/addons/indexers.py
@@ -663,7 +663,7 @@ class AddonIndexer:
         data['has_privacy_policy'] = bool(obj.privacy_policy)
 
         data['is_recommended'] = bool(
-            obj.promoted and obj.promoted.group_id == PROMOTED_GROUP_CHOICES.RECOMMENDED
+            PROMOTED_GROUP_CHOICES.RECOMMENDED in obj.promoted_group().group_id
         )
 
         data['previews'] = [
@@ -678,7 +678,7 @@ class AddonIndexer:
 
         data['promoted'] = (
             {
-                'group_id': obj.promoted.group_id,
+                'group_id': obj.promoted_group().group_id,
                 # store the app approvals because .approved_applications needs it.
                 'approved_for_apps': [
                     app.id for app in obj.promoted_version().approved_applications
