1bc57286682867b86edb5b635f0b42636b791dda
Andrew Williamson <awilliamson@mozilla.com>
2025-02-27 14:03:00 +0000

    add retry_unreported_abuse_reports command to retry AbuseReports (#23108)
    * add retry_unreported_abuse_reports command to retry AbuseReports
    
    * optimize qs to only return ids

--- src/olympia/abuse/tests/test_commands.py
+++ src/olympia/abuse/tests/test_commands.py
@@ -78,3 +78,34 @@ def test_backfill_cinder_escalations():
     assert new_appeal_job
     assert new_appeal_job.resolvable_in_reviewer_tools is True
     assert appealled_decision.reload().appeal_job == new_appeal_job
+
+
+@pytest.mark.django_db
+def test_retry_unreported_abuse_reports():
+    addon = addon_factory()
+    reported_job = CinderJob.objects.create()
+    reported = AbuseReport.objects.create(
+        guid=addon.guid,
+        reason=AbuseReport.REASONS.HATEFUL_VIOLENT_DECEPTIVE,
+        cinder_job=reported_job,
+    )
+    unreported = AbuseReport.objects.create(
+        guid=addon.guid, reason=AbuseReport.REASONS.HATEFUL_VIOLENT_DECEPTIVE
+    )
+    notactionable = AbuseReport.objects.create(
+        guid=addon.guid, reason=AbuseReport.REASONS.FEEDBACK_SPAM
+    )
+    responses.add(
+        responses.POST,
+        f'{settings.CINDER_SERVER_URL}create_report',
+        json={'job_id': '1234-xyz'},
+        status=201,
+    )
+
+    call_command('retry_unreported_abuse_reports')
+
+    assert reported.reload().cinder_job == reported_job  # not changed
+    assert unreported.reload().cinder_job is not None
+    assert unreported.cinder_job.job_id == '1234-xyz'
+    assert notactionable.reload().cinder_job is None
+    assert len(responses.calls) == 1
