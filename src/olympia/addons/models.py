e19820059c45857cc6bb67757710c146a282d19b
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2025-03-03 10:38:38 -0500

    correct all_apps, approved_apps

--- src/olympia/addons/models.py
+++ src/olympia/addons/models.py
@@ -1590,12 +1590,17 @@ class Addon(OnChangeMixin, ModelBase):
     @property
     def all_applications(self):
         from olympia.addons.serializers import APP_IDS
+        from olympia.constants.applications import APP_USAGE
 
-        apps = self.promotedaddonpromotion.exclude(application_id=None).values_list(
+        apps = self.promotedaddonpromotion.values_list(
             'application_id', flat=True
         )
 
-        return [APP_IDS.get(app_id) for app_id in apps]
+        return (
+            [APP_IDS.get(app_id) for app_id in apps]
+            if apps
+            else [app for app in APP_USAGE]
+        )
 
     @property
     def approved_applications(self):
@@ -1610,9 +1615,9 @@ class Addon(OnChangeMixin, ModelBase):
         if self._is_recommended_theme() and self.promoted:
             return self.all_applications if self.current_version else []
 
-        return self.promoted_version(
+        return [version for version in self.promoted_version(
             promoted_group=promoted_group
-        ).approved_applications
+        ).approved_applications if version in self.all_applications]
 
     @cached_property
     def promoted(self):
