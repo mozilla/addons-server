c1d69de93f1c6fa5fd30251c6571061d8c54836e
Kevin Meinhardt <kmeinhardt@mozilla.com>
2025-03-26 16:31:50 +0100

    Fix: 'alpha-value-notation'

--- stylelint.config.js
+++ stylelint.config.js
@@ -18,7 +18,6 @@ export default {
     'selector-pseudo-class-no-unknown': null,
     'declaration-block-no-shorthand-property-overrides': null,
     'selector-pseudo-element-no-unknown': null,
-    'alpha-value-notation': null,
     'color-hex-length': null,
     'length-zero-no-unit': null,
     'function-url-quotes': null,
