587c2989a74fbe8b6e1e72290ecde958df4286a0
Kevin Meinhardt <kmeinhardt@mozilla.com>
2025-03-26 16:32:17 +0100

    Fix: 'color-hex-length'

--- stylelint.config.js
+++ stylelint.config.js
@@ -18,7 +18,6 @@ export default {
     'selector-pseudo-class-no-unknown': null,
     'declaration-block-no-shorthand-property-overrides': null,
     'selector-pseudo-element-no-unknown': null,
-    'color-hex-length': null,
     'length-zero-no-unit': null,
     'function-url-quotes': null,
     'selector-attribute-quotes': null,
