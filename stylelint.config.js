ace9831ec6e9968e773ec3068e429184e6cfe2a0
Kevin Meinhardt <kmeinhardt@mozilla.com>
2025-03-26 16:29:17 +0100

    FIx: 'property-no-vendor-prefix'

--- stylelint.config.js
+++ stylelint.config.js
@@ -18,7 +18,6 @@ export default {
     'selector-pseudo-class-no-unknown': null,
     'declaration-block-no-shorthand-property-overrides': null,
     'selector-pseudo-element-no-unknown': null,
-    'property-no-vendor-prefix': null,
     'shorthand-property-no-redundant-values': null,
     'color-function-notation': null,
     'alpha-value-notation': null,
