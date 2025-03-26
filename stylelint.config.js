8e3efff238c422d24680fb56500aa4036427089d
Kevin Meinhardt <kmeinhardt@mozilla.com>
2025-03-26 16:37:20 +0100

    Fix: 'value-no-vendor-prefix'

--- stylelint.config.js
+++ stylelint.config.js
@@ -19,7 +19,6 @@ export default {
     'declaration-block-no-shorthand-property-overrides': null,
     'selector-pseudo-element-no-unknown': null,
     'selector-attribute-quotes': null,
-    'value-no-vendor-prefix': null,
     'comment-whitespace-inside': null,
     'selector-no-vendor-prefix': null,
     'media-feature-range-notation': null,
