93126c4ff9b451b6ecb2b27e6b28736af4f81f8c
Mathieu Pillard <diox@users.noreply.github.com>
2025-03-27 13:16:34 +0100

    Add command to give "By Firefox" badge to themes with "Firefox" as author (#23224)
    * Add command to give "By Firefox" badge to themes with "Firefox" as author

---
+++ src/olympia/promoted/management/commands/promote_by_firefox_themes.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from olympia import amo
+from olympia.addons.models import Addon
+from olympia.constants.promoted import PROMOTED_GROUP_CHOICES
+from olympia.promoted.models import (
+    PromotedAddon,
+)
+from olympia.users.models import UserProfile
+
+
+class Command(BaseCommand):
+    help = 'Give themes with Firefox as author the "By Firefox" badge'
+
+    def handle(self, *args, **options):
+        firefox_user = UserProfile.objects.get(pk=settings.TASK_USER_ID)
+        addons = (
+            Addon.objects.public()
+            .filter(type=amo.ADDON_STATICTHEME, authors=firefox_user)
+            .no_transforms()
+        )
+        for addon in addons:
+            self.stdout.write(f'Promoting {addon.slug}')
+            promotion, created = PromotedAddon.objects.get_or_create(
+                addon=addon,
+                defaults={
+                    'application_id': amo.FIREFOX.id,
+                    'group_id': PROMOTED_GROUP_CHOICES.LINE,
+                },
+            )
+            promotion.approve_for_addon()
