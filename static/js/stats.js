6893bb15a808c862e014174055d717ecca62a274
Kevin Meinhardt <kmeinhardt7@gmail.com>
2025-04-09 14:51:44 +0200

    Revert ./stats/js/stats files closer to original state before vite migration (#23290)

--- static/js/stats.js
+++ static/js/stats.js
@@ -3,8 +3,13 @@ import './stats/helpers';
 import './stats/dateutils';
 import './stats/manager';
 import './stats/controls';
-import './stats/overview';
+import { stats_overview } from './stats/overview';
 import './stats/topchart';
 import './stats/chart';
 import './stats/table';
-import './stats/stats';
+import { stats_stats } from './stats/stats';
+import { capabilities } from './zamboni/capabilities';
+
+// Initialize the stats module.
+stats_stats(window.sessionStorage, capabilities);
+stats_overview();
