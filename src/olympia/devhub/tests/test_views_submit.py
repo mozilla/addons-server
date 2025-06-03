d8eaadd9738be90c34fc18732b16a09cc831522e
Jason Gerard DeRose <jasonderose@gmail.com>
2025-06-03 09:48:28 -0600

    Ok, SingleCategoryForm again: fix test_submit_categories_set(), no doubt breaks other stuff

--- src/olympia/devhub/tests/test_views_submit.py
+++ src/olympia/devhub/tests/test_views_submit.py
@@ -1473,10 +1473,10 @@ class TestStaticThemeSubmitDetails(DetailsPageMixin, TestSubmitBase):
 
     def test_submit_categories_set(self):
         assert [cat.id for cat in self.get_addon().all_categories] == []
-        self.is_success(self.get_dict(category='causes')) # 320 is 'causes'
+        self.is_success(self.get_dict(category=320)) # 320 is 'causes'
 
         addon_cats = self.get_addon().categories.values_list('id', flat=True)
-        assert sorted(addon_cats) == [320, 420]
+        assert sorted(addon_cats) == [320]
 
     def test_submit_categories_change(self):
         Category.populate_static_categories()
