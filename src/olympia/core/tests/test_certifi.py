a18910f2904497d682c17d54d0dfa819a35f4172
Mathieu Pillard <mpillard@mozilla.com>
2025-06-17 11:00:46 +0200

    Fix intermittent test errors with test_certify.py

--- src/olympia/core/tests/test_certifi.py
+++ src/olympia/core/tests/test_certifi.py
@@ -7,9 +7,9 @@ from olympia.amo.tests import TestCase
 @pytest.mark.allow_external_http_requests
 class TestCertifi(TestCase):
     def test_no_certificate_errors_aws(self):
-        resp = requests.get('https://s3.amazonaws.com/', verify=True)
-        assert resp.status_code == 200
+        response = requests.get('https://s3.amazonaws.com/', verify=True)
+        response.raise_for_status()
 
     def test_no_certificate_errors_google(self):
-        resp = requests.get('https://www.google.com', verify=True)
-        assert resp.status_code == 200
+        response = requests.get('https://www.google.com', verify=True)
+        response.raise_for_status()
