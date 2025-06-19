1b61f66c2a3d8b04dd8abe2e4260e4606127fecc
Andrew Williamson <awilliamson@mozilla.com>
2025-06-19 11:03:18 +0100

    exclude 2nd level approval requeued jobs from being auto resolved

--- src/olympia/abuse/models.py
+++ src/olympia/abuse/models.py
@@ -216,6 +216,7 @@ class CinderJob(ModelBase):
             action_date=datetime.now(),
             reviewer_user_id=settings.TASK_USER_ID,
             cinder_job=self,
+            override_of=self.final_decision,
         )
         decision.policies.set(
             CinderPolicy.objects.filter(
