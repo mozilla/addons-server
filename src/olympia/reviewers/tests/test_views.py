9352e1075dd5d54d0c04c820ab71e21ab49f15df
Andreas Wagner <mail@andreaswagner.org>
2025-08-12 17:37:07 +0000

    Fix tests

--- src/olympia/reviewers/tests/test_views.py
+++ src/olympia/reviewers/tests/test_views.py
@@ -1380,12 +1380,15 @@ class TestQueueBasics(QueueTest):
         assert response.status_code == 200
         doc = pq(response.content)
         links = doc('.tabnav li a').map(lambda i, e: e.attrib['href'])
-        expected.extend(
-            [
-                reverse('reviewers.queue_pending_rejection'),
-                reverse('reviewers.queue_decisions'),
-            ]
-        )
+        expected.append(reverse('reviewers.queue_pending_rejection'))
+        assert links == expected
+
+        self.grant_permission(self.user, 'Addons:HighImpactApprove')
+        response = self.client.get(self.url)
+        assert response.status_code == 200
+        doc = pq(response.content)
+        links = doc('.tabnav li a').map(lambda i, e: e.attrib['href'])
+        expected.append(reverse('reviewers.queue_decisions'))
         assert links == expected
 
     @override_settings(DEBUG=True)
@@ -7784,7 +7787,9 @@ class TestHeldDecisionQueue(ReviewerTest):
             action=DECISION_ACTIONS.AMO_DELETE_RATING,
             rating=Rating.objects.create(addon=addon_factory(), user=user_factory()),
         )
-        self.login_as_admin()
+        user = user_factory()
+        self.grant_permission(user, 'Addons:HighImpactApprove')
+        self.client.force_login(user)
 
     def test_results(self):
         response = self.client.get(self.url)
