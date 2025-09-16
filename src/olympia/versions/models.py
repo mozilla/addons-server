48fb1f21231563269885271cae9a5c33589d34fe
Mathieu Pillard <mpillard@mozilla.com>
2025-09-16 12:08:15 +0200

    Add ActivityLog when disabling auto-approval because of a restriction

--- src/olympia/versions/models.py
+++ src/olympia/versions/models.py
@@ -570,9 +570,18 @@ class Version(OnChangeMixin, ModelBase):
                 else 'auto_approval_disabled_unlisted'
             )
             reviewer_flags_defaults[flag] = True
-
-            # FIXME: add activity log here indicating we disabled auto-approval
-            # because we hit a restriction.
+            activity.log_create(
+                amo.LOG.DISABLE_AUTO_APPROVAL,
+                addon,
+                details={
+                    'channel': version.channel,
+                    'comments': (
+                        f'{version.get_channel_display()} auto-approval automatically '
+                        'disabled because of a restriction'
+                    ),
+                },
+                user=get_task_user(),
+            )
 
         if reviewer_flags_defaults:
             AddonReviewerFlags.objects.update_or_create(
