366d13e4fa900e0e0610f84268c13c43876c3c69
Mathieu Pillard <mpillard@mozilla.com>
2025-09-24 12:05:59 +0200

    Fix versions not being flagged for review after 2nd level approval requeue
    This refactors how versions are passed when flagging for review in
    CinderAddonHandledByReviewers - we were expecting version strings,
    which is inefficient and confusing, but the requeue method was
    passing a Version queryset directly.

--- src/olympia/abuse/tests/test_models.py
+++ src/olympia/abuse/tests/test_models.py
@@ -3894,6 +3894,26 @@ class TestContentDecision(TestCase):
         assert job.decision == decision
         self._check_requeue_decision(job.final_decision, job, decision, user)
 
+    def test_requeue_held_action_existing_job_unlisted(self):
+        addon = addon_factory()
+        self.make_addon_unlisted(addon)
+        user = user_factory()
+        job = CinderJob.objects.create(target_addon=addon)
+        decision = ContentDecision.objects.create(
+            addon=addon,
+            action=DECISION_ACTIONS.AMO_DISABLE_ADDON,
+            reviewer_user=self.reviewer_user,
+            cinder_job=job,
+            cinder_id='1234',
+        )
+        decision.target_versions.add(addon.versions.get())
+
+        decision.requeue_held_action(user=user, notes='go!')
+
+        assert job.reload().resolvable_in_reviewer_tools is True
+        assert job.decision == decision
+        self._check_requeue_decision(job.final_decision, job, decision, user)        
+
     def test_get_target_review_url(self):
         addon = addon_factory()
         decision = ContentDecision.objects.create(
