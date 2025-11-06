23476804963e9460f171fb51968f7c06fe5fe181
Andrew Williamson <awilliamson@mozilla.com>
2025-11-06 13:12:38 +0000

    set 2FA for account shortcuts on fake fxa login (#24105)

--- static/js/zamboni/users.js
+++ static/js/zamboni/users.js
@@ -7,6 +7,7 @@ $('#admin-login').click(function () {
 
 $('#fake_fxa_authorization .accounts a').click(function (e) {
   $('#id_email').prop('value', $(this).prop('href').replace('mailto:', ''));
+  $('input[name="fake_two_factor_authentication"]').prop('checked', 'true');
   e.preventDefault();
   $('#fake_fxa_authorization').submit();
 });
