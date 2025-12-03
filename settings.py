b910d58eb0993e6323fecc8e46b74821028583fb
William Durand <will+git@drnd.me>
2025-12-03 18:15:00 +0100

    Add a way to use the source-builder project locally (#24197)

--- settings.py
+++ settings.py
@@ -180,3 +180,5 @@ GOOGLE_APPLICATION_CREDENTIALS_BIGQUERY = env(
 )
 
 CINDER_SERVER_URL = 'https://mozilla-staging.cinderapp.com/api/v1/'
+
+SOURCE_BUILDER_API_URL = 'http://source-builder:10200/submit'
