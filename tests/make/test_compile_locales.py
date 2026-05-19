38bce34e1b70bc1e1316468fa19c2bd33ce659be
Mathieu Pillard <mpillard@mozilla.com>
2026-05-19 22:41:22 +0200

    Remove obsolete marker

--- tests/make/test_compile_locales.py
+++ tests/make/test_compile_locales.py
@@ -11,7 +11,6 @@ from scripts.compile_locales import compile_locales, process_po_file
 from tests import override_env
 
 
-@pytest.mark.needs_locales_compilation
 class TestCompileLocales(TestCase):
     def setUp(self):
         self.home_dir = Path(tempfile.mkdtemp())
