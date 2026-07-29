eae3096543d86c72444d6d98227b3dc08660df2e
eviljeff <awilliamson@mozilla.com>
2026-07-29 18:29:25 +0100

    create activity and send email when overriding to same enforcement action

--- src/olympia/abuse/models.py
+++ src/olympia/abuse/models.py
@@ -1392,6 +1392,8 @@ class ContentDecision(ModelBase):
             log_entry = OverriddenActionClass.reverse_action(
                 reversed_decision=overridden, new_decision=self
             )
+        else:
+            log_entry = None
 
         # Reverse any follow-up actions (e.g. delayed blocks) of the overridden
         # decision too.
@@ -1421,6 +1423,13 @@ class ContentDecision(ModelBase):
                 # action first, then apply the new action below.
                 self.reverse_overridden_action()
                 log_entry = action_helper.process_action(release_hold=release_hold)
+                if not log_entry and not self.activities.exists():
+                    # If there was no action taken (e.g. the content was already
+                    # removed, so the action was a no-op) we still need to log that the
+                    # decision was created so there is a record in the reviewer tools
+                    # history, and we can send out an accurate email with policy text,
+                    # etc.
+                    action_helper.log_action(amo.LOG.DECISION_CREATED)
                 # But only save it afterwards in case process_action failed
                 self.save(update_fields=('action_date',))
             else:
@@ -1430,8 +1439,6 @@ class ContentDecision(ModelBase):
         if self.action_date:
             for followup in self.followup_actions.filter(action_date__isnull=True):
                 followup.execute_action()
-
-        log_entry = log_entry or self.activities.first()
 
         if self.cinder_job and self.addon_id:
             if self.action_date:
