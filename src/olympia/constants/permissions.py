a41fa975f9f6e1823c5666bbe5fcd98ccb318f27
eviljeff <awilliamson@mozilla.com>
2026-08-03 12:30:48 +0100

    Add group with new Addons:ApiView permissions

--- src/olympia/constants/permissions.py
+++ src/olympia/constants/permissions.py
@@ -1,7 +1,12 @@
 from collections import defaultdict, namedtuple
 
 
-AclPermission = namedtuple('AclPermission', 'app, action')
+class AclPermission(namedtuple('AclPermission', 'app, action')):
+    __slots__ = ()
+
+    def __str__(self):
+        return f'{self.app}:{self.action}'
+
 
 # Null rule.  Only useful in tests really as no access group should have this.
 NONE = AclPermission('None', 'None')
